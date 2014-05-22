d34aa097cad83bb5da438fed4eac67ef751a8892
Vincent LAURENT <vlaurent@actualys.com>
2014-05-22 18:37:55 +0200

    Ajout d'un script d'installation Désactivation des deprecated en mode dev

---
+++ project/bin/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+. bin/config.inc
+
+echo "Création et configuration du fichier de configuration config/databases.yml"
+cat config/databases.yml.example | sed "s|dsn: http://localhost:5984/|dsn: http://$COUCHDBDOMAIN:$COUCHDBPORT/|" | sed "s|dbname: ava|dbname: $COUCHDBBASE|" > config/databases.yml
+
+
+echo "Création du fichier de configuration apps/declaration/config/factories.yml"
+cp apps/declaration/config/factories.yml{.example,}
+
+echo "Création de la base de données"
+curl -X PUT "http://$COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE"
