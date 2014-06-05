334954dec4a50b6e3c0b2090a3dfd987df6f8c19
Vincent LAURENT <vlaurent@actualys.com>
2014-06-05 11:31:28 +0200

    Fixe saut de ligne

--- project/bin/install.sh
+++ project/bin/install.sh
@@ -5,7 +5,6 @@
 echo "Création et configuration du fichier de configuration config/databases.yml"
 cat config/databases.yml.example | sed "s|dsn: http://localhost:5984/|dsn: http://$COUCHDBDOMAIN:$COUCHDBPORT/|" | sed "s|dbname: ava|dbname: $COUCHDBBASE|" > config/databases.yml
 
-
 echo "Création du fichier de configuration apps/declaration/config/factories.yml"
 cp apps/declaration/config/factories.yml{.example,}
 
