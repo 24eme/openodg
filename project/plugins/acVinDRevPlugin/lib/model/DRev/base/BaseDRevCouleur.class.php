5291d7601a826648767c8c09bd4cff5297097f64
Vincent LAURENT <vlaurent@actualys.com>
2014-06-10 14:32:47 +0200

    Ajout des prelevements dans le schema

--- project/plugins/acVinDRevPlugin/lib/model/DRev/base/BaseDRevCouleur.class.php
+++ project/plugins/acVinDRevPlugin/lib/model/DRev/base/BaseDRevCouleur.class.php
@@ -1,8 +1,30 @@
 <?php
+/**
+ * BaseDRevCouleur
+ * 
+ * Base model for DRevCouleur
+
+ * @property string $libelle
+ * @property integer $actif
+ * @property float $total_superficie
+ * @property float $volume_revendique
+
+ * @method string getLibelle()
+ * @method string setLibelle()
+ * @method integer getActif()
+ * @method integer setActif()
+ * @method float getTotalSuperficie()
+ * @method float setTotalSuperficie()
+ * @method float getVolumeRevendique()
+ * @method float setVolumeRevendique()
+ 
+ */
 
 abstract class BaseDRevCouleur extends _DRevDeclarationNoeud {
+                
     public function configureTree() {
        $this->_root_class_name = 'DRev';
        $this->_tree_class_name = 'DRevCouleur';
     }
+                
 }
