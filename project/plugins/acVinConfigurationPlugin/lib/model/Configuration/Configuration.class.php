27a1020cdfd2efbe5c4d60c4d7a91ae7b67dc132
Vincent LAURENT <vlaurent@actualys.com>
2014-06-11 17:28:56 +0200

    Import CSV dans la DREV de la DR pour les données de revendication

--- project/plugins/acVinConfigurationPlugin/lib/model/Configuration/Configuration.class.php
+++ project/plugins/acVinConfigurationPlugin/lib/model/Configuration/Configuration.class.php
@@ -83,6 +83,11 @@ class Configuration extends BaseConfiguration {
       return $libelle;
     }
 
+    public function getRecolte() {
+
+      return $this->getDeclaration();
+    }
+
     public function identifyProduct($appellation, $lieu, $cepage) {
       $appid = null;
       $lieuid = 'lieu';
