7af87a7c8fb9a64e4ec3649a48a17617fcab1fca
Vincent LAURENT <vlaurent@actualys.com>
2014-07-18 15:28:32 +0200

    * Ajout des prélévements dans l'export des données de revendication * Mapping des cépages

--- project/bin/import/drev.sh
+++ project/bin/import/drev.sh
@@ -9,6 +9,10 @@ mkdir $WORKDIR 2> /dev/null
 
 bash bin/import/operateur.sh
 
+#===PRODUITS===
+
+cat $DATADIR/AVA_GROUPE_VIN.csv | iconv -f iso88591 -t utf8 | tr -d "\r" | cut -d ";" -f 1,3 | sort -t ";" -k 1,1 > $WORKDIR/cepage.csv
+cat $DATADIR/AVA_GRANDCRU.csv | iconv -f iso88591 -t utf8 | tr -d "\r" | sort -t ";" -k 1,1 > $WORKDIR/grdcru.csv
 
 #===REVENDICATION===
 
@@ -36,14 +40,22 @@ join -t ";" -1 4 -2 1 $WORKDIR/valeur_cvi.csv $WORKDIR/valeur_param.csv | awk -F
 
 #===LOTS===
 
-cat $DATADIR/AVA_NOMBRE_LOTS.csv | iconv -f iso88591 -t utf8 | tr -d "\r" | cut -d ";" -f 2,3,4,5,6,7 | sort -t ";" -k 1,1 > $WORKDIR/lots.csv
+cat $DATADIR/AVA_NOMBRE_LOTS.csv | iconv -f iso88591 -t utf8 | tr -d "\r" | cut -d ";" -f 2,3,4,5,6,7 | sort -t ";" -k 4,4 > $WORKDIR/lot.csv
 
-join -t ";" -1 2 -2 1 $WORKDIROPERATEUR/id_evv_cvi.csv $WORKDIR/lots.csv | cut -d ";" -f 3,4,5,6,7,8 | awk -F ";" '{ print $1 ";" $2 ";2.LOTS;;;;;" $3 ";" $4 ";" $5 ";" $6 }' > $WORKDIR/lots_cvi.csv
+join -t ";" -1 4 -2 1 $WORKDIR/lot.csv $WORKDIR/cepage.csv | sort -t ";" -k 1,1 | cut -d ";" -f 2,3,4,5,6,7 | sort -t ";" -k 1,1 > $WORKDIR/lot_cepage.csv
+
+join -t ";" -1 2 -2 1 $WORKDIROPERATEUR/id_evv_cvi.csv $WORKDIR/lot_cepage.csv | cut -d ";" -f 3,4,5,6,7,8 | awk -F ";" '{ print $1 ";" $2 ";2.LOT;;;;;" $5 ";" $3 ";" $6 ";" $4 }' > $WORKDIR/lot_cvi.csv
+
+#===PRELEVEMENTS===
+
+cat $DATADIR/AVA_DREV_PRELEVEMENT.csv | iconv -f iso88591 -t utf8 | tr -d "\r" | sort -t ";" -k 1,1 > $WORKDIR/prelevement.csv
+
+join -t ";" -1 2 -2 1 $WORKDIR/dossier_cvi.csv $WORKDIR/prelevement.csv | cut -d ";" -f 2,3,4,6,7,8 | awk -F ";" '{ print $1 ";" $2 ";3.PREL;;;;;" $3 ";;;" $6 ";" $4 ";" $5 }' > $WORKDIR/prelevement_cvi.csv
 
 #===FINAL===
 
-echo "cvi;annee;type ligne;rev num ligne;rev type id;rev type libelle;rev valeur;lot id grdcru;lot id groupe vin;lot nb;lot type aoc" > $WORKDIR/drev.csv
+echo "cvi;annee;type ligne;rev num ligne;rev type id;rev type libelle;rev valeur;aoc;grdcru;cepage;nb lot;annee prelevement;semaine prelevement" > $WORKDIR/drev.csv
 
-cat $WORKDIR/valeur_cvi_param.csv $WORKDIR/lots_cvi.csv | sort >> $WORKDIR/drev.csv
+cat $WORKDIR/valeur_cvi_param.csv $WORKDIR/lot_cvi.csv $WORKDIR/prelevement_cvi.csv | sort >> $WORKDIR/drev.csv
 
 
