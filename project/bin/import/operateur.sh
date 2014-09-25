4cb653718a028dbe49ca7e577db469182ffe9a82
Vincent LAURENT <vince.laurent@gmail.com>
2014-09-25 19:23:16 +0200

    Import des etablissements : * Familles * Chais * Numéro Siret * Meilleur gestion des numéros de téléphone

--- project/bin/import/operateur.sh
+++ project/bin/import/operateur.sh
@@ -20,7 +20,7 @@ cat $WORKDIR/evv.csv | cut -d ";" -f 2,3,4,5,6,7,8,9,10 | sed -r 's/^([0-9]+);/\
 
 cat $DATADIR/PPM.csv | iconv -f iso88591 -t utf8 | tr -d "\r" | cut -d ";" -f 2,25,26 | grep -Ev "[a-zA-Z]+"  | grep -E "^[0-9]+;" | grep -v ";;$" | sort -t ";" -k 1,1 > $WORKDIR/ppm.csv
 
-join -t ";" -1 2 -2 1 $WORKDIR/id_evv_cvi.csv $WORKDIR/ppm.csv | cut -d ";" -f 3,4,5 | sort -t ";" -k 1,1 | sed -r 's/^([0-9]+);/\1;3.SIRE;;;;;;;;;;;;;;;/' > $WORKDIR/siret_cvi.csv
+join -t ";" -1 2 -2 1 $WORKDIR/id_evv_cvi.csv $WORKDIR/ppm.csv | cut -d ";" -f 3,4,5 | sort -t ";" -k 1,1 | sed -r 's/^([0-9]+);/\1;3.SIRE;;;;;;;;;;;;;;;;/' > $WORKDIR/siret_cvi.csv
 
 #---COMMUNICATION---
 cat $DATADIR/COMMUNICATION.csv | iconv -f iso88591 -t utf8 | tr -d "\r" | cut -d ";" -f 1,3,4,8,9,10,11,12,13,14 | sort -t ";" -k 2,2 > $WORKDIR/communication.csv
@@ -50,7 +50,7 @@ join -t ";" -1 1 -2 1 $WORKDIR/id_evv_cvi.sort_evv.csv $WORKDIR/evv_chai.csv | c
 join -t ";" -1 2 -2 1 $WORKDIR/id_chai_cvi.csv $WORKDIR/chai.csv | cut -d ";" -f 2,3,4,5,6,7,8,9,10,11,12,13 | sed -r 's/^([0-9]+);/\1;2.CHAI;/' | sed -r 's/(;[0-9-]+;+[0-9-]+;[0-9-]+)$/;\1/' | sed -r 's/0;([0-9-]+;[0-9-]+)$/,CHAI_DE_VINIFICATION-1;\1/' | sed -r 's/-1;0;([0-9-]+)$/,CENTRE_DE_CONDITIONNEMENT-1;-1;\1/' | sed -r 's/-1;-1;0$/,LIEU_DE_STOCKAGE/' | sed 's/-1;-1;-1$//' | sed 's/;,/;/' | sed -r 's/^([0-9]+;2.CHAI);([a-Z0-9]*);(.+)$/\1;\3;\2/' > $WORKDIR/chai_cvi.csv
 
 #---FINAL---
-cat $WORKDIR/cvi.csv $WORKDIR/chai_cvi.csv $WORKDIR/communication_cvi.csv $WORKDIR/coordonnees_cvi.csv $WORKDIR/attributs_cvi.csv | sort | sed -r 's/[ ]+/ /g' | sed -r 's/\t/ /g' | sort -t ";" -k 7,7 > $WORKDIR/operateurs.sorted_by_commune.csv
+cat $WORKDIR/cvi.csv $WORKDIR/chai_cvi.csv $WORKDIR/siret_cvi.csv $WORKDIR/communication_cvi.csv $WORKDIR/coordonnees_cvi.csv $WORKDIR/attributs_cvi.csv | sort | sed -r 's/[ ]+/ /g' | sed -r 's/\t/ /g' | sort -t ";" -k 7,7 > $WORKDIR/operateurs.sorted_by_commune.csv
 
 
 cat $DATADIR/LOCALITE_FRANCAISE.csv | iconv -f iso88591 -t utf8 | tr -d "\r" | cut -d ";" -f 1,3 | sort | uniq | sort -t ";" -k 1,1 > $WORKDIR/communes.csv
--- project/lib/task/import/importEtablissementsTask.class.php
+++ project/lib/task/import/importEtablissementsTask.class.php
@@ -21,7 +21,8 @@ class importEtablissementsTask extends sfBaseTask
     const CSV_PORTABLE              = 15;
     const CSV_EMAIL                 = 16;
     const CSV_WEB                   = 17;
-    const CSV_DATE_CHANGEMENT_ACTIF = 18;
+    const CSV_SIREN                 = 18;
+    const CSV_SIRET                 = 19;
 
     protected function configure()
     {
@@ -89,6 +90,12 @@ EOF;
     }
 
     protected function saveEtablissement($etablissement) {
+
+        if(count($etablissement->familles) == 0) {
+            echo sprintf("ERROR;%s;#LINE;%s\n", "Aucune famille", $etablissement->cvi);
+            return;
+        }
+
         if($etablissement->isNew()) {
             echo sprintf("SUCCESS;%s;%s\n", "Création", $etablissement->cvi);
         } else {
@@ -115,9 +122,19 @@ EOF;
             return $this->importLineChai($data, $etablissement);
         }
 
-        if($data[self::CSV_TYPE_LIGNE] == "3.COMM") {
+        if($data[self::CSV_TYPE_LIGNE] == "3.SIRE") {
+            
+            return $this->importLineSiret($data, $etablissement);
+        }
+
+        if($data[self::CSV_TYPE_LIGNE] == "4.COMM") {
             
             return $this->importLineCommunication($data, $etablissement);
+        }
+
+        if($data[self::CSV_TYPE_LIGNE] == "6.ATTR") {
+
+            return $this->importLineAttribut($data, $etablissement);
         }
     }
 
@@ -135,23 +152,127 @@ EOF;
         $etablissement->cvi = $data[self::CSV_CVI];
         $etablissement->raison_sociale = $data[self::CSV_RAISON_SOCIALE];
         $etablissement->nom = $etablissement->raison_sociale;
-        $etablissement->adresse = trim(preg_replace("/[ ]+/", " ", sprintf("%s %s %s", $data[self::CSV_ADRESSE_1], $data[self::CSV_ADRESSE_2], $data[self::CSV_ADRESSE_3])));
+        $adresse = $this->formatAdresse($data);
+        if(!$adresse) {
+            throw new sfException("Adresse vide");
+        }
+        $etablissement->adresse = $adresse;
         $etablissement->commune = $data[self::CSV_COMMUNE];
-        $etablissement->code_postal = $data[self::CSV_CODE_INSEE];
-        $etablissement->code_insee = $data[self::CSV_CODE_POSTAL];
-
+        $etablissement->code_postal = $data[self::CSV_CODE_POSTAL];
+        $etablissement->code_insee = $data[self::CSV_CODE_INSEE];
+        $etablissement->chais = array();
+        $etablissement->familles = array();
     }
 
     protected function importLineChai($data, $etablissement) {
+        $chai = $etablissement->chais->add();
+        if($data[self::CSV_RAISON_SOCIALE] && !$data[self::CSV_ADRESSE_1]) {
+            $data[self::CSV_ADRESSE_1] = $data[self::CSV_RAISON_SOCIALE];
+        }
 
+        $adresse = $this->formatAdresse($data);
+        
+        if(!$adresse) {
+            throw new sfException("Adresse vide");
+        }
+        $chai->adresse = $adresse;
+        $chai->commune = $data[self::CSV_COMMUNE];
+        $chai->code_postal = $data[self::CSV_CODE_POSTAL];
+    }
+
+    protected function importLineSiret($data, $etablissement) {
+        $siret = $data[self::CSV_SIRET];
+
+        if(!$data[self::CSV_SIRET] && $data[self::CSV_SIREN]) {
+            $siret = $data[self::CSV_SIREN];
+        }
+
+        $siret = str_replace(" ", "", $siret);
+
+        if($siret && !preg_match("/^[0-9]+$/", $siret)) {
+
+            throw new Exception("Le SIRET n'est pas au bon format");
+        }
+
+        $etablissement->siret = ($siret) ? $siret : null;
     }
 
     protected function importLineCommunication($data, $etablissement) {
 
-        $etablissement->telephone = $data[self::CSV_TEL];
-        $etablissement->portable = $data[self::CSV_PORTABLE];
-        $etablissement->fax = $data[self::CSV_FAX];
-        $etablissement->email = $data[self::CSV_EMAIL];
+        $telephone = $this->formatPhone($data[self::CSV_TEL]);
+        $mobile = $this->formatPhone($data[self::CSV_PORTABLE]);
+        $fax = $this->formatPhone($data[self::CSV_FAX]);  
+
+        if($data[self::CSV_TYPE] == "bur") {
+            $etablissement->telephone_bureau = $telephone;
+            $etablissement->telephone_mobile = $mobile;
+            $etablissement->fax = $fax;  
+        }
+
+        if($data[self::CSV_TYPE] != "bur") {
+            if($telephone) {
+                $etablissement->telephone_prive = $telephone;
+            }
+
+            if($telephone && !$etablissement->telephone_bureau) {
+                $etablissement->telephone_bureau = $telephone;
+            }
+
+            if($fax && !$etablissement->fax) {
+                $etablissement->fax = $fax;
+            }
+
+            if($mobile) {
+                $etablissement->telephone_prive = $mobile;
+            }
+
+            if($mobile && !$etablissement->telephone_mobile) {
+                $etablissement->telephone_mobile = $mobile;
+            }
+        }
+
+        $email = trim($data[self::CSV_EMAIL]);
+        if($email && !preg_match("/^[a-zA-Z0-9\._-]+@[a-zA-Z0-9\._-]+$/", $email)) {
+            throw new Exception("L'email n'est pas au bon format"); 
+        }
+
+        $email = ($email) ? $email : null;
+
+        if($data[self::CSV_TYPE] == "bur") {
+            $etablissement->email = $email;  
+        }
+
+        if($email && !$etablissement->email && $data[self::CSV_TYPE] == "pri") {
+            $etablissement->email = $email;  
+        }
+    }
+
+    protected function importLineAttribut($data, $etablissement) {
+        if(preg_match("/Vinificateur/", $data[self::CSV_ATTRIBUTS])) {
+            $etablissement->familles->add(EtablissementClient::FAMILLE_VINIFICATEUR);
+        }
+
+        if(preg_match("/Producteur/", $data[self::CSV_ATTRIBUTS])) {
+            $etablissement->familles->add(EtablissementClient::FAMILLE_PRODUCTEUR);
+        }
+
+        if(preg_match("/Distillation/", $data[self::CSV_ATTRIBUTS])) {
+            $etablissement->familles->add(EtablissementClient::FAMILLE_DISTILLATEUR);
+        }
+    }
+
+    protected function formatAdresse($data) {
+
+        return trim(preg_replace("/[ ]+/", " ", sprintf("%s %s %s", $data[self::CSV_ADRESSE_1], $data[self::CSV_ADRESSE_2], $data[self::CSV_ADRESSE_3])));
+    }
+
+    protected function formatPhone($numero) {
+        $numero = trim(preg_replace("/[_\.]+/", "", $numero));
+        if($numero && !preg_match("/^[0-9]{10}$/", $numero)) {
+            throw new Exception("Téléphone invalide"); 
+        }
+
+        return ($numero) ? $numero : null;
     }
 
 }
