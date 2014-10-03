6f7d570a5e653ce4a7058d13c519b850cc4bd65d
Vincent LAURENT <vlaurent@actualys.com>
2014-10-03 14:14:16 +0200

    Désactivation du csrf protection

--- project/apps/declaration/modules/auth/templates/loginSuccess.php
+++ project/apps/declaration/modules/auth/templates/loginSuccess.php
@@ -1,14 +1,16 @@
 <div class="row row-margin">
     <form method="post" action="" role="form">
 
-    <?php echo $form->renderHiddenFields(); ?>
-    <?php echo $form->renderGlobalErrors(); ?>
-    <div class="col-xs-offset-3">
-        <?php echo $form["login"]->renderError(); ?>
-        <?php echo $form["login"]->renderLabel(null, array("class" => "control-label")); ?>
-        <?php echo $form["login"]->render(array("class" => "")); ?>
+        <?php echo $form->renderHiddenFields(); ?>
+        <?php echo $form->renderGlobalErrors(); ?>
+        <div class="col-xs-offset-4 col-xs-4">
+            <div class="form-group">
+            <?php echo $form["login"]->renderError(); ?>
+            <?php echo $form["login"]->renderLabel(null, array("class" => "control-label")); ?>
+            <?php echo $form["login"]->render(array("class" => "")); ?>
+            </div>
 
-        <button type="submit" class="btn btn-default">Se connecter</button>
-    </div>
+            <button type="submit" class="btn btn-default">Se connecter</button>
+        </div>
     </form>
 </div>
