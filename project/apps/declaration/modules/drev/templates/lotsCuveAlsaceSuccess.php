0a0daeb2c92c87ce02d9f9c64c2918ac3d78b850
JB <jblemetayer@actualys.com>
2014-10-10 11:09:26 +0200

    Gestion couleur des boutons de la navigation par onglet

--- project/apps/declaration/modules/drev/templates/lotsCuveAlsaceSuccess.php
+++ project/apps/declaration/modules/drev/templates/lotsCuveAlsaceSuccess.php
@@ -15,14 +15,13 @@
     
     <div class="row row-margin row-button">
         <div class="col-xs-6">
-            <a href="<?php echo url_for("drev_degustation_conseil", $drev) ?>" class="btn btn-primary btn-lg btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retourner <small>à l'organisation</small></a>
+            <a href="<?php echo url_for("drev_degustation_conseil", $drev) ?>" class="btn btn-primary btn-lg btn-upper btn-primary-step"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retourner <small>à l'organisation</small></a>
         </div>
         <div class="col-xs-6 text-right">
         	<?php if ($drev->exist('etape') && $drev->etape == DrevEtapes::ETAPE_VALIDATION): ?>
-	        <button id="btn-validation" type="submit" class="btn btn-warning btn-lg btn-upper">Enregistrer <small>et revalider</small>&nbsp;&nbsp;<span class="eleganticon arrow_carrot-right"></span></button>
-	        <button type="submit" class="btn btn-default btn-sm btn-upper btn-spacing">Continuer <small>en répartissant les lots suivant</small>&nbsp;&nbsp;<span class="eleganticon arrow_carrot-right"></span></button>
+	        <button id="btn-validation" type="submit" class="btn btn-default btn-lg btn-upper"><span class="glyphicon glyphicon-check"></span> Retourner <small>à la validation</small>&nbsp;&nbsp;</button>
 	        <?php else: ?>
-	        <button type="submit" class="btn btn-default btn-lg btn-upper">Continuer <small>en répartissant les lots suivant</small>&nbsp;&nbsp;<span class="eleganticon arrow_carrot-right"></span></button>
+	        <button type="submit" class="btn btn-default btn-lg btn-upper btn-default-step">Continuer <small>en répartissant les lots suivant</small>&nbsp;&nbsp;<span class="eleganticon arrow_carrot-right"></span></button>
 	        <?php endif; ?>
         </div>
     </div>
