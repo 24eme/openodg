ab65ef9f83eb13d44fc29200855c628eabd716f0
mathurin <mpetit@actualys.com>
2014-10-14 10:47:44 +0200

    ajout d'un contrôle dans le post validator des drevMarcs pour les date de distillation ( la date de fin de distillation doit être nécessairement supérieure à la date de début)

--- project/plugins/acVinDRevMarcPlugin/lib/validator/ValidatorDRevMarcRevendication.class.php
+++ project/plugins/acVinDRevMarcPlugin/lib/validator/ValidatorDRevMarcRevendication.class.php
@@ -15,6 +15,9 @@ class ValidatorDRevMarcRevendication extends sfValidatorBase {
 
     public function configure($options = array(), $messages = array()) {
         $this->addMessage('invalid_volume_marc', 'Le volume d\'alcool pur semble être exprimé en litre');
+        $this->addMessage('invalid_periode_distillation', 'La date de début de distillation doit être antérieure à la date de fin de distillation');
+    
+        
     }
 
     protected function doClean($values) {
@@ -23,7 +26,17 @@ class ValidatorDRevMarcRevendication extends sfValidatorBase {
         
         if ($values['volume_obtenu'] > $values['qte_marc'] * 20 / 10000) {
             $errorSchema->addError(new sfValidatorError($this, 'invalid_volume_marc'), 'volume_obtenu');
+        }        
+        
+        $date_debut_distillation = Date::getIsoDateFromFrenchDate($values['debut_distillation']);
+        $date_fin_distillation = Date::getIsoDateFromFrenchDate($values['fin_distillation']);      
+        
+        
+        if ($date_debut_distillation >= $date_fin_distillation) {
+            $errorSchema->addError(new sfValidatorError($this, 'invalid_periode_distillation'), 'debut_distillation');
         }
+        
+        
         if (count($errorSchema)) {
             throw new sfValidatorErrorSchema($this, $errorSchema);
         }
