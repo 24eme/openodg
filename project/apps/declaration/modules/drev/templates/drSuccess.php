82e48816f466ddb57d3affcda45cc1c10e3e9cb3
Vincent LAURENT <vlaurent@actualys.com>
2014-10-16 14:17:14 +0200

    Lien de la page d'erreur d'import de la DR

--- project/apps/declaration/modules/drev/templates/drSuccess.php
+++ project/apps/declaration/modules/drev/templates/drSuccess.php
@@ -5,7 +5,7 @@
 
 <p>Vous allez être redirigé sur la plateforme du CIVA afin de récupérer les données de votre déclaration de Récolte.</p>
 <div class="row row-margin row-button">
-    <div class="col-xs-6"><a href="<?php echo url_for("home") ?>#drev" class="btn btn-primary btn-lg btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retour</a></div>
+    <div class="col-xs-6"><a href="<?php echo url_for("drev_exploitation", $drev) ?>#drev" class="btn btn-primary btn-lg btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retour</a></div>
     <div class="col-xs-6 text-right">
         <a class="btn btn-link" href="<?php echo url_for("drev_revendication", $drev) ?>">Passer</a>
         <a class="btn btn-default btn-lg btn-upper" href="<?php echo url_for("drev_dr_recuperation", $drev) ?>">Récupérer les données&nbsp;&nbsp;<span class="eleganticon arrow_carrot-right"></span></a>
