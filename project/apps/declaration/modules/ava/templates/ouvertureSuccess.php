f40eb2d3a3cf29bd9a4af64b46e9eee8be038706
mathurin <mpetit@actualys.com>
2014-10-23 19:30:26 +0200

    ajout de l'historique dans le panel de l'ouvertrure

--- project/apps/declaration/modules/ava/templates/ouvertureSuccess.php
+++ project/apps/declaration/modules/ava/templates/ouvertureSuccess.php
@@ -1,13 +1,13 @@
 <?php use_helper("Date"); ?>
 <?php
-$annee = substr($date_ouverture_drev, 0,4);
+$annee = substr($date_ouverture_drev, 0, 4);
 $dateFr = format_date($date_ouverture_drev, "dd MMMM", "fr_FR");
 ?>
 <div class="page-header">
-	<h2>Déclaration de Revendication</h2>
+    <h2>Déclaration de Revendication</h2>
 </div>
 <div class="row">
-	    <div class="col-xs-4">
+    <div class="col-xs-4">
         <div class="panel panel-info">
             <div class="panel-heading">
                 <h3>Déclaration de Revendication</h3>
@@ -17,4 +17,5 @@ $dateFr = format_date($date_ouverture_drev, "dd MMMM", "fr_FR");
             </div>
         </div>
     </div>
+    	<?php include_component('ava', 'history'); ?>
 </div>
