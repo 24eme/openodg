93a3aabf87691e384f3860b5bb46b289b9397e4b
mathurin <mpetit@actualys.com>
2014-10-24 16:16:55 +0200

    changement de template pour l'ouverture DREV et DREVMARC

--- project/apps/declaration/modules/drevmarc/templates/_monEspace.php
+++ project/apps/declaration/modules/drevmarc/templates/_monEspace.php
@@ -1,34 +1,38 @@
-<?php if(!count($drevmarcsHistory) && !$etablissement->hasFamille(EtablissementClient::FAMILLE_DISTILLATEUR)): ?>
+<?php if (!count($drevmarcsHistory) && !$etablissement->hasFamille(EtablissementClient::FAMILLE_DISTILLATEUR)): ?>
     <?php return; ?>
 <?php endif; ?>
 
-    <div class="col-xs-4">
-        <?php if($etablissement->hasFamille(EtablissementClient::FAMILLE_DISTILLATEUR)): ?>
-        <div class="panel <?php if ($drevmarc && $drevmarc->validation): ?>panel-success<?php else: ?>panel-primary<?php endif; ?>">     
-            <div class="panel-heading">
-                <h3>Marc&nbsp;d'Alsace&nbsp;Gewurzt.&nbsp;<?php echo ConfigurationClient::getInstance()->getCampagneManager()->getCurrent(); ?></h3>
+<div class="col-xs-4">
+    <?php if ($etablissement->hasFamille(EtablissementClient::FAMILLE_DISTILLATEUR)): ?>
+        <?php if (!$drevmarc_non_ouverte): ?>
+            <div class="panel <?php if ($drevmarc && $drevmarc->validation): ?>panel-success<?php else: ?>panel-primary<?php endif; ?>">     
+                <div class="panel-heading">
+                    <h3>Marc&nbsp;d'Alsace&nbsp;Gewurzt.&nbsp;<?php echo ConfigurationClient::getInstance()->getCampagneManager()->getCurrent(); ?></h3>
+                </div>
+                <div class="panel-body">
+                    <?php if ($drevmarc && $drevmarc->validation): ?>
+                        <p>
+                            <a class="btn btn-lg btn-block btn-primary" href="<?php echo url_for('drevmarc_visualisation', $drevmarc) ?>">Visualiser</a>
+                        </p>
+                        <p>
+                            <a class="btn btn-xs btn-danger pull-right" href="<?php echo url_for('drevmarc_delete', $drevmarc) ?>"><span class="glyphicon glyphicon-trash"></span>&nbsp;&nbsp;Supprimer</a>
+                        </p>
+                    <?php elseif ($drevmarc): ?>
+                        <p>
+                            <a class="btn btn-lg btn-block btn-default" href="<?php echo url_for('drevmarc_edit', $drevmarc) ?>">Continuer</a>
+                        </p>
+                        <p>
+                            <a class="btn btn-xs btn-danger pull-right" href="<?php echo url_for('drevmarc_delete', $drevmarc) ?>"><span class="glyphicon glyphicon-trash"></span>&nbsp;&nbsp;Supprimer le brouillon</a>
+                        </p>
+                    <?php else: ?>
+                        <p>
+                            <a class="btn btn-lg btn-block btn-default" href="<?php echo url_for('drevmarc_create', $etablissement) ?>">Démarrer</a>
+                        </p>
+                    <?php endif; ?>
+                </div>
             </div>
-            <div class="panel-body">
-                <?php if ($drevmarc && $drevmarc->validation): ?>
-                    <p>
-                        <a class="btn btn-lg btn-block btn-primary" href="<?php echo url_for('drevmarc_visualisation', $drevmarc) ?>">Visualiser</a>
-                    </p>
-                    <p>
-                        <a class="btn btn-xs btn-danger pull-right" href="<?php echo url_for('drevmarc_delete', $drevmarc) ?>"><span class="glyphicon glyphicon-trash"></span>&nbsp;&nbsp;Supprimer</a>
-                    </p>
-                <?php elseif ($drevmarc): ?>
-                    <p>
-                        <a class="btn btn-lg btn-block btn-default" href="<?php echo url_for('drevmarc_edit', $drevmarc) ?>">Continuer</a>
-                    </p>
-                    <p>
-                        <a class="btn btn-xs btn-danger pull-right" href="<?php echo url_for('drevmarc_delete', $drevmarc) ?>"><span class="glyphicon glyphicon-trash"></span>&nbsp;&nbsp;Supprimer le brouillon</a>
-                    </p>
-                <?php else: ?>
-                    <p>
-                        <a class="btn btn-lg btn-block btn-default" href="<?php echo url_for('drevmarc_create', $etablissement) ?>">Démarrer</a>
-                    </p>
-                <?php endif; ?>
-            </div>
-        </div>
+        <?php else: ?>
+            <?php include_partial('drevmarcNonOuvert', array('date_ouverture_drevmarc' => $date_ouverture_drevmarc)); ?>
         <?php endif; ?>
-    </div>
+    <?php endif; ?>
+</div>
