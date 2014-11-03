d1eb6833177d46883c59679c51ecb59cb80178b0
Vincent LAURENT <vlaurent@actualys.com>
2014-11-03 17:54:33 +0100

    Save ajax sur la récap

--- project/apps/declaration/modules/drev/templates/_stepRevendication.php
+++ project/apps/declaration/modules/drev/templates/_stepRevendication.php
@@ -3,9 +3,9 @@
     <li class="<?php if(isset($noeud) && $appellation->getHash() == $noeud->getHash()): ?>active<?php endif; ?>"><a role="tab" class="ajax" href="<?php echo url_for("drev_revendication_cepage", $appellation) ?>"><small>AOC Alsace</small><br /><?php echo ucfirst(str_replace("AOC", "", str_replace("d&#039;Alsace", "", str_replace("AOC Alsace ", "", $appellation->getLibelle())))) ?></a></li>
     <?php endforeach; ?>
     <?php if($ajoutForm->hasProduits()): ?>
-        <li class="text-center"><a role="tab" data-toggle="modal" data-target="#popupAjoutAppellationForm" href="" id="btn-popup"><span class="glyphicon glyphicon-plus-sign"></span></a></li>
+        <li class="text-center"><a class="ajax" role="tab" data-toggle="modal" data-target="#popupAjoutAppellationForm" href="" id="btn-popup"><span class="glyphicon glyphicon-plus-sign"></span></a></li>
     <?php endif; ?>
-    <li class="text-center pull-right <?php if(isset($step) && $step == 'recapitulatif'): ?>active<?php endif; ?>"><a role="tab" href="<?php echo url_for("drev_revendication_recapitulatif", $drev) ?>"><span class="glyphicon glyphicon-th-list"></span><br />Récap.</a></li>
+    <li  class="text-center pull-right <?php if(isset($step) && $step == 'recapitulatif'): ?>active<?php endif; ?>"><a class="ajax" role="tab" href="<?php echo url_for("drev_revendication_recapitulatif", $drev) ?>"><span class="glyphicon glyphicon-th-list"></span><br />Récap.</a></li>
 </ul>
 
 <?php include_partial('drev/popupAjoutForm', array('url' => url_for('drev_revendication_appellation_ajout', $drev), 'form' => $ajoutForm, 'html_id' => 'popupAjoutAppellationForm')); ?>
