dfe9788a365fa748ee59064c99f9699a35ee2ab6
Vincent LAURENT <vlaurent@actualys.com>
2014-11-04 17:41:12 +0100

    On affiche le détail pour tous les produits

--- project/apps/declaration/modules/drev/templates/_revendication.php
+++ project/apps/declaration/modules/drev/templates/_revendication.php
@@ -22,15 +22,13 @@
             $totalSuperficie += $produit->superficie_revendique;
             ?>
             <tr data-toggle="collapse" data-target="#<?php echo $key_for_tr_id ?>" class="accordion-toggle 
-                <?php echo ($cpt % 2) ? "" : "table_td_zebra"; ?>
-                <?php echo (count($produit->getProduitsCepage()) > 1)? 'trAccordion' : ''; ?>" >
+                <?php echo ($cpt % 2) ? "" : "table_td_zebra"; ?>">
                 <td>
                     <div class="float-left col-xs-10">
                         <?php echo $produit->getLibelleComplet() ?> 
                     </div>
                     <div class="float-right text-right col-xs-2">
-                        <?php if (count($produit->getProduitsCepage()) > 1): ?>
-                            <small style="cursor: pointer;"><span class="glyphicon glyphicon-chevron-down">&nbsp;</span></small><?php endif; ?>
+                        <small style="cursor: pointer;"><span class="glyphicon glyphicon-chevron-down">&nbsp;</span></small>
                     </div>
                 </td>
                 <?php if(!$drev->isNonRecoltant()): ?>
@@ -38,7 +36,6 @@
             	<?php endif; ?>
                 <td class="text-center"><?php echoFloat($produit->volume_revendique) ?><?php if (!is_null($produit->volume_revendique)): ?> <small class="text-muted">hl</small><?php endif; ?></td>
             </tr>
-            <?php if (count($produit->getProduitsCepage()) > 1): ?>
                 <tr>
                     <td class="hiddenRow" colspan="3"  >
                         <div id="<?php echo $key_for_tr_id; ?>" class="accordian-body collapse" >
@@ -94,7 +91,6 @@
                         </div>
                     </td>
                 </tr>
-            <?php endif; ?>
             <?php $cpt++; ?> 
         <?php endforeach; ?>
         
