f3ae38a0ea309b30281a1dcc9b90df20df4a3b62
Vincent LAURENT <vlaurent@actualys.com>
2014-11-18 11:58:05 +0100

    Confirmation email en mode admin

--- project/apps/declaration/modules/ava/templates/homeSuccess.php
+++ project/apps/declaration/modules/ava/templates/homeSuccess.php
@@ -1,9 +1,9 @@
 <?php use_javascript("main.js", "last") ?>
 
 <div class="page-header">
-    <h2><?php if ($etablissement->needEmailConfirmation()): ?>Confirmation de votre e-mail<?php else: ?>Déclaration de Revendication<?php endif; ?></h2>
+    <h2><?php if ($etablissement->needEmailConfirmation() && !$sf_user->isAdmin()): ?>Confirmation de votre e-mail<?php else: ?>Déclaration de Revendication<?php endif; ?></h2>
 </div>
-<?php if ($etablissement->needEmailConfirmation()): ?>
+<?php if ($etablissement->needEmailConfirmation() && !$sf_user->isAdmin()): ?>
     <form action="<?php echo url_for("home") ?>" method="post" class="form-horizontal">
         <p>Pour votre première connexion sur le portail de l'Association des Viticulteurs d'Alsace, vous devez confirmer votre adresse e-mail.</p>
         <?php include_partial('etablissement/formConfirmationEmail', array('form' => $form, 'etablissement' => $etablissement)); ?>
