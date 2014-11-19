dfda6ce6cd607766479681aacf09c5eb496583c4
mathurin <mpetit@actualys.com>
2014-11-19 10:51:23 +0100

    ajout des liens dans les mails

--- project/plugins/EmailPlugin/modules/Email/templates/_send_drevmarc_confirmee.php
+++ project/plugins/EmailPlugin/modules/Email/templates/_send_drevmarc_confirmee.php
@@ -2,10 +2,7 @@ Bonjour,
 
 Après vérification, votre déclaration de Revendication Marc d'Alsace de Gewurztraminer <?php echo $drevmarc->campagne ?> a été validé par notre service.
 
-Vous trouverez en pièce jointe le document PDF de votre déclaration.
-
-
-Vous pouvez à tout moment revenir sur votre compte pour consulter votre document : <?php echo ""; ?>
+Vous pouvez à tout moment revenir sur votre compte pour consulter votre document : <?php echo sfContext::getInstance()->getRouting()->generate('drevmarc_visualisation', $drevmarc, true); ?>
 
 Bien cordialement,
 
