d8b9a160c84e3f167be5d1a6c9907a54ff2ffc0f
Vincent LAURENT <vlaurent@actualys.com>
2014-12-05 18:16:39 +0100

    Tache envoi de mail

--- project/lib/task/drev/DRevSendMailPieceTask.class.php
+++ project/lib/task/drev/DRevSendMailPieceTask.class.php
@@ -10,7 +10,7 @@ class DRevSendMailRappelDocumentTask extends sfBaseTask
 
         $this->addOptions(array(
             new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'declaration'),
-            new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+            new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'prod'),
             new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
         ));
 
