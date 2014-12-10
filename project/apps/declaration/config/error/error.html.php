ceb6d62c49f5c472cbe91b2fb2dac0750f2b87af
Vincent LAURENT <vlaurent@actualys.com>
2014-12-10 20:36:19 +0100

    Fixe previous commit

--- project/apps/declaration/config/error/error.html.php
+++ project/apps/declaration/config/error/error.html.php
@@ -85,8 +85,8 @@
 
             <div class="row row-margin row-button">
                 <div class="col-xs-4">
-                    <a style="margin-top: 10px;" href="/" class="btn btn-primary btn-sm btn-upper btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retourner à mes déclarations</a>
-                    <a href="javascript:history.back()" class="btn btn-default btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retourner à la page précédente</a>
+                    <a href="/" class="btn btn-primary btn-sm btn-upper btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retourner à mes déclarations</a>
+                    <a style="margin-top: 10px;" href="javascript:history.back()" class="btn btn-default btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retourner à la page précédente</a>
                 </div>
                 <div class="col-xs-4">
                     
--- project/apps/declaration/modules/global/templates/error404Success.php
+++ project/apps/declaration/modules/global/templates/error404Success.php
@@ -12,8 +12,8 @@
 
 <div class="row row-margin row-button">
     <div class="col-xs-4">
-        <a style="margin-top: 10px;" href="<?php echo url_for('home') ?>" class="btn btn-sm btn-primary btn-upper btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retourner à mes déclarations</a>
-        <a href="javascript:history.back()" class="btn btn-default btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retourner à la page précédente</a>
+        <a href="<?php echo url_for('home') ?>" class="btn btn-sm btn-primary btn-upper btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retourner à mes déclarations</a>
+        <a style="margin-top: 10px;" href="javascript:history.back()" class="btn btn-default btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retourner à la page précédente</a>
     </div>
     <div class="col-xs-4">
         
