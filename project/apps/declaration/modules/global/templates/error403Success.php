83cca5b5610609763ad22db89bf0ce5f8c78bab6
Vincent LAURENT <vlaurent@actualys.com>
2014-12-10 20:32:46 +0100

    Page d'erreur 403,404,500

---
+++ project/apps/declaration/modules/global/templates/error403Success.php
@@ -0,0 +1,24 @@
+<div class="page-header no-border">
+    <h2><span class="glyphicon glyphicon-minus-sign"></span>&nbsp;&nbsp;Vous n'avez pas l'autorisation pour accéder à cette page</h2>
+</div>
+
+<div class="row">
+    <div class="col-xs-12">
+        <p>
+        Vous n'êtes pas autorisé à accéder à cette page.
+        </p>
+        <p>
+        Si vous provenez de la plateforme vinsalsace.pro il est possible que votre compte n'est pas encore été créé. Dans ce cas n'hésitez pas à nous contacter.
+        </p>
+    </div>
+</div>
+
+<div class="row row-margin row-button">
+    <div class="col-xs-4">
+        <a href="javascript:history.back()" class="btn btn-default btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retourner à la page précédente</a>
+        <a style="margin-top: 10px;" href="<?php echo url_for('home') ?>" class="btn btn-primary btn-upper btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retourner à mes déclarations</a>
+    </div>
+    <div class="col-xs-4">
+        
+    </div>
+</div>
