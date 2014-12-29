64933604894c7e7c78b249bb00306752d565e1d9
Mathurin Petit <mathurin.petit@gmail.com>
2014-12-29 17:09:16 +0100

    ajout des informations complementaire dans le schema des comptes

--- project/plugins/ComptePlugin/lib/Compte/base/BaseCompte.class.php
+++ project/plugins/ComptePlugin/lib/Compte/base/BaseCompte.class.php
@@ -8,8 +8,23 @@
  * @property string $_rev
  * @property string $type
  * @property string $identifiant
+ * @property string $type_compte
+ * @property string $civilite
  * @property string $nom
- * @property acCouchdbJson $droits
+ * @property string $prenom
+ * @property string $nom_a_afficher
+ * @property string $adresse
+ * @property string $code_postal
+ * @property string $ville
+ * @property string $telephone_bureau
+ * @property string $telephone_prive
+ * @property string $telephone_mobile
+ * @property string $fax
+ * @property string $email
+ * @property string $siret
+ * @property string $cvi
+ * @property string $etablissement
+ * @property acCouchdbJson $tags
 
  * @method string get_id()
  * @method string set_id()
@@ -19,10 +34,40 @@
  * @method string setType()
  * @method string getIdentifiant()
  * @method string setIdentifiant()
+ * @method string getTypeCompte()
+ * @method string setTypeCompte()
+ * @method string getCivilite()
+ * @method string setCivilite()
  * @method string getNom()
  * @method string setNom()
- * @method acCouchdbJson getDroits()
- * @method acCouchdbJson setDroits()
+ * @method string getPrenom()
+ * @method string setPrenom()
+ * @method string getNomAAfficher()
+ * @method string setNomAAfficher()
+ * @method string getAdresse()
+ * @method string setAdresse()
+ * @method string getCodePostal()
+ * @method string setCodePostal()
+ * @method string getVille()
+ * @method string setVille()
+ * @method string getTelephoneBureau()
+ * @method string setTelephoneBureau()
+ * @method string getTelephonePrive()
+ * @method string setTelephonePrive()
+ * @method string getTelephoneMobile()
+ * @method string setTelephoneMobile()
+ * @method string getFax()
+ * @method string setFax()
+ * @method string getEmail()
+ * @method string setEmail()
+ * @method string getSiret()
+ * @method string setSiret()
+ * @method string getCvi()
+ * @method string setCvi()
+ * @method string getEtablissement()
+ * @method string setEtablissement()
+ * @method acCouchdbJson getTags()
+ * @method acCouchdbJson setTags()
  
  */
  
