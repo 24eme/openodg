716cfef25983d1316b3d2d3858a47bb1abf7e5bf
Vincent LAURENT <vince.laurent@gmail.com>
2015-01-07 15:47:37 +0100

    Gestion du pays et suppression du cedex

--- project/bin/import/compte.sh
+++ project/bin/import/compte.sh
@@ -1,4 +1,4 @@
-echo "000000#id;type_ligne;cvi;siren;siret;num_tva_intracom;civilite;raison_sociale;nom;prenom;famille;adresse_1;adresse_2;adresse_3;code_postal;commune;code_insee;cedex;date_archivage;date_creation"
+echo "000000#id;type_ligne;cvi;siren;siret;num_tva_intracom;civilite;raison_sociale;nom;prenom;famille;adresse_1;adresse_2;adresse_3;code_postal;commune;code_insee;cedex;attributs;date_archivage;date_creation;liaison"
 
 mysql -u root -pboris97 AVA_base -N -e "
 SELECT 
@@ -13,13 +13,14 @@ SELECT
        IF(p.TYPE = 'P',p.RS,'') as NOM,
        IF(p.TYPE = 'P',p.PRENOM,'') as PRENOM,
        IF(p.TYPE = 'M',p.PRENOM,'') as FAMILLE,
-       c.ADRESSE1 as ADRESSE_1,
-       c.ADRESSE2 as ADRESSE_2,
-       c.ADRESSE3 as ADRESSE_3,
+       REPLACE(c.ADRESSE1,';','') as ADRESSE_1,
+       REPLACE(c.ADRESSE2,';','') as ADRESSE_2,
+       REPLACE(c.ADRESSE3,';','') as ADRESSE_3,
        c.CODE_POSTAL as CODE_POSTAL,
-       co.NCCCT as COMMUNE,
+       co.NCCENR as COMMUNE,
        c.COMMUNE as CODE_INSEE,
        c.CEDEX as CEDEX,
+       pa.LIBCOG as PAYS,
        '' as TEL,
        '' as FAX,
        '' as PORTABLE,
@@ -27,14 +28,16 @@ SELECT
        '' as WEB,
        '' as ATTRIBUTS,
        p.DATE_ARCHIVAGE as DATE_ARCHIVAGE, 
-       p.DATE_CREATION as DATE_CREATION
+       p.DATE_CREATION as DATE_CREATION,
+       '' as LIAISON
 FROM PPM p
 LEFT JOIN COORDONNEES c ON c.CODE_IDENT_SITE = p.CODE_IDENT_SITE AND c.NR_ORDRE = 0
-LEFT JOIN COMMUNE_FRANCAISE as co ON co.INSEE = c.COMMUNE
+LEFT JOIN COMMUNE_FRANCAISE co ON co.INSEE = c.COMMUNE
+LEFT JOIN PAYS pa ON c.PAYS = pa.COG   
 " | sed 's/\t/;/g'
 
 mysql -u root -pboris97 AVA_base -N -e "
-SELECT        LPAD(pe.CODE_IDENT_SITE_EXPLT, 6, '0') as ID,         '2.   CVI' as TYPE_LIGNE,         e.NR_EVV as CVI,         '' as SIREN,         '' as SIRET,        '' as TVA_INTRA,         '' as CIVILITE,         e.INTITULE as RAISON_SOCIALE,        '' as NOM,        '' as PRENOM,        '' as FAMILLE,        e.ADRESSE1 as ADRESSE_1,        e.ADRESSE2 as ADRESSE_2,        e.ADRESSE3 as ADRESSE_3,        e.CODE_POSTAL as CODE_POSTAL,        c.NCCCT as COMMUNE,        e.COMMUNE as CODE_INSEE,        e.CEDEX as CEDEX,        '' as TEL,        '' as FAX,        '' as PORTABLE,        '' as EMAIL,        '' as WEB,        '' as ATTRIBUTS,        '' as DATE_ARCHIVAGE,         '' as DATE_CREATION FROM EVV e INNER JOIN PPM_EVV_MFV as pe ON e.CLE_EVV = pe.CLE_EVV LEFT JOIN COMMUNE_FRANCAISE as c ON c.INSEE = e.COMMUNE;" | sed 's/\t/;/g'
+SELECT        LPAD(pe.CODE_IDENT_SITE_EXPLT, 6, '0') as ID,         '2.   CVI' as TYPE_LIGNE,         e.NR_EVV as CVI,         '' as SIREN,         '' as SIRET,        '' as TVA_INTRA,         '' as CIVILITE,         e.INTITULE as RAISON_SOCIALE,        '' as NOM,        '' as PRENOM,        '' as FAMILLE,        REPLACE(e.ADRESSE1,';','') as ADRESSE_1,        REPLACE(e.ADRESSE2,';','') as ADRESSE_2,        REPLACE(e.ADRESSE3,';','') as ADRESSE_3,        e.CODE_POSTAL as CODE_POSTAL,        c.NCCCT as COMMUNE,        e.COMMUNE as CODE_INSEE,        e.CEDEX as CEDEX,       '' as PAYS,        '' as TEL,        '' as FAX,        '' as PORTABLE,        '' as EMAIL,        '' as WEB,        '' as ATTRIBUTS,        '' as DATE_ARCHIVAGE,         '' as DATE_CREATION, '' as LIAISON FROM EVV e INNER JOIN PPM_EVV_MFV as pe ON e.CLE_EVV = pe.CLE_EVV LEFT JOIN COMMUNE_FRANCAISE as c ON c.INSEE = e.COMMUNE;" | sed 's/\t/;/g'
 
 mysql -u root -pboris97 AVA_base -N -e "
 SELECT
@@ -49,13 +52,14 @@ SELECT
        '' as NOM,
        '' as PRENOM,
        c.TYPE_CHAI as FAMILLE,
-       IF(c.ADRESSE1 = '', c.INTITULE, c.ADRESSE1) as ADRESSE_1,
-       c.ADRESSE2 as ADRESSE_2,
-       c.ADRESSE3 as ADRESSE_3,
+       IF(c.ADRESSE1 = '', c.INTITULE, REPLACE(c.ADRESSE1,';','')) as ADRESSE_1,
+       REPLACE(c.ADRESSE2,';','') as ADRESSE_2,
+       REPLACE(c.ADRESSE3,';','') as ADRESSE_3,
        c.CODE_POSTAL as CODE_POSTAL,
        co.NCCCT as COMMUNE,
        c.COMMUNE as CODE_INSEE,
        c.CEDEX as CEDEX,
+       '' as PAYS,
        '' as TEL,
        '' as FAX,
        '' as PORTABLE,
@@ -63,7 +67,8 @@ SELECT
        '' as WEB,
        CONCAT(IF(c.CHAI_DE_VINIFICATION > 0, 'CHAI_DE_VINIFICATION,', ','),IF(c.CENTRE_DE_CONDITIONNEMENT > 0, 'CENTRE_DE_CONDITIONNEMENT,', ','),IF(c.LIEU_DE_STOCKAGE > 0, 'LIEU_DE_STOCKAGE,', ','),IF(c.centre_de_pressurage > 0, 'CENTRE_DE_PRESSURAGE,', ',')) as ATTRIBUTS,
        '' as DATE_ARCHIVAGE, 
-       '' as DATE_CREATION
+       '' as DATE_CREATION,
+       '' as LIAISON
 FROM CHAI c
 LEFT JOIN COMMUNE_FRANCAISE as co ON co.INSEE = c.COMMUNE
 INNER JOIN PPM_EVV_CHAI as pe ON pe.CLE_CHAI = c.CLE_CHAI
@@ -90,6 +95,7 @@ SELECT
        '' as COMMUNE,
        '' as CODE_INSEE,
        '' as CEDEX,
+       '' as PAYS,
        '' as TEL,
        '' as FAX,
        '' as PORTABLE,
@@ -97,9 +103,43 @@ SELECT
        '' as WEB,
        r.LIBELLE_ATTRIBUT as ATTRIBUTS,
        '' as DATE_ARCHIVAGE, 
-       '' as DATE_CREATION
+       '' as DATE_CREATION,
+       '' as LIAISON
 FROM PPM_ATTRIBUTS a
 LEFT JOIN PPM_ATTRIBUT_REF r ON r.CLE_ATTRIBUT = a.ATTRIBUT
+" | sed 's/\t/;/g'
+
+mysql -u root -pboris97 AVA_extra2 -N -e "
+SELECT
+       LPAD(s.CIS, 6, '0') as ID, 
+       '4.ATTRIB' as TYPE_LIGNE, 
+       '' as CVI, 
+       '' as SIREN, 
+       '' as SIRET,
+       '' as TVA_INTRA, 
+       '' as CIVILITE, 
+       '' as RAISON_SOCIALE,
+       '' as NOM,
+       '' as PRENOM,
+       '' as FAMILLE,
+       '' as ADRESSE_1,
+       '' as ADRESSE_2,
+       '' as ADRESSE_3,
+       '' as CODE_POSTAL,
+       '' as COMMUNE,
+       '' as CODE_INSEE,
+       '' as CEDEX,
+       '' as PAYS,
+       '' as TEL,
+       '' as FAX,
+       '' as PORTABLE,
+       '' as EMAIL,
+       '' as WEB,
+       'SYNDICAT' as ATTRIBUTS,
+       '' as DATE_ARCHIVAGE, 
+       '' as DATE_CREATION,
+       '' as LIAISON
+FROM Syndicats_Locaux s
 " | sed 's/\t/;/g'
 
 mysql -u root -pboris97 AVA_base -N -e "
@@ -122,6 +162,7 @@ SELECT
        '' as COMMUNE,
        '' as CODE_INSEE,
        '' as CEDEX,
+       '' as PAYS,
        c.TEL as TEL,
        c.FAX as FAX,
        c.PORTABLE as PORTABLE,
@@ -129,8 +170,44 @@ SELECT
        c.WEB as WEB,
        '' as ATTRIBUTS,
        '' as DATE_ARCHIVAGE, 
-       '' as DATE_CREATION
+       '' as DATE_CREATION,
+       '' as LIAISON
 FROM COMMUNICATION c
 WHERE c.CLE_COORDONNEES > 0
+" | sed 's/\t/;/g'
+
+mysql -u root -pboris97 AVA_extra2 -N -e "
+SELECT
+       LPAD(sm.CIS_Membre, 6, '0') as ID, 
+       '6.LIAISON' as TYPE_LIGNE, 
+       '' as CVI, 
+       '' as SIREN, 
+       '' as SIRET,
+       '' as TVA_INTRA, 
+       '' as CIVILITE, 
+       '' as RAISON_SOCIALE,
+       '' as NOM,
+       '' as PRENOM,
+       'SYNDICAT' as FAMILLE,
+       '' as ADRESSE_1,
+       '' as ADRESSE_2,
+       '' as ADRESSE_3,
+       '' as CODE_POSTAL,
+       '' as COMMUNE,
+       '' as CODE_INSEE,
+       '' as CEDEX,
+       '' as PAYS,
+       '' as TEL,
+       '' as FAX,
+       '' as PORTABLE,
+       '' as EMAIL,
+       '' as WEB,
+       '' as ATTRIBUTS,
+       '' as DATE_ARCHIVAGE, 
+       '' as DATE_CREATION,
+       LPAD(s.CIS, 6, '0') as LIAISON
+FROM Syndicats_Locaux_Membres sm
+INNER JOIN Syndicats_Locaux s ON sm.ID_SL = s.Id_Syndicats_Locaux
+WHERE sm.Cotisant = 1
 " | sed 's/\t/;/g'
 
--- project/lib/task/import/importComptesTask.class.php
+++ project/lib/task/import/importComptesTask.class.php
@@ -21,14 +21,16 @@ class importComptesTask extends sfBaseTask
     const CSV_COMMUNE               = 15;
     const CSV_CODE_INSEE            = 16;
     const CSV_CEDEX                 = 17;
-    const CSV_TEL                   = 18;
-    const CSV_FAX                   = 19;
-    const CSV_PORTABLE              = 20;
-    const CSV_EMAIL                 = 21;
-    const CSV_WEB                   = 22;
-    const CSV_ATTRIBUTS             = 23;
-    const CSV_DATE_ARCHIVAGE        = 24;
-    const CSV_DATE_CREATION         = 25;
+    const CSV_PAYS                  = 18;
+    const CSV_TEL                   = 19;
+    const CSV_FAX                   = 20;
+    const CSV_PORTABLE              = 21;
+    const CSV_EMAIL                 = 22;
+    const CSV_WEB                   = 23;
+    const CSV_ATTRIBUTS             = 24;
+    const CSV_DATE_ARCHIVAGE        = 25;
+    const CSV_DATE_CREATION         = 26;
+    const CSV_LIASON                = 27;
 
     protected function configure()
     {
@@ -96,20 +98,21 @@ EOF;
         } else {
             echo sprintf("SUCCESS;%s;%s\n", "Mise à jour", $compte->_id);
         }
-
-        $compte_existant = CompteClient::getInstance()->find($compte->_id);
+        $compte_existant = acCouchdbManager::getClient()->find($compte->_id, acCouchdbClient::HYDRATE_JSON);
         if($compte_existant) {
-            $compte_existant->delete();
+            acCouchdbManager::getClient()->deleteDoc($compte_existant);
         }
-        $compte->save();
+        $compte->save(false);
 
         if($etablissement) {
             $etablissement->constructId();
+            $etablissement->synchroFromCompte($compte);
             if($etablissement->isNew()) {
                 echo sprintf("SUCCESS;%s;%s\n", "Création", $etablissement->_id);
             } else {
                 echo sprintf("SUCCESS;%s;%s\n", "Mise à jour", $etablissement->_id);
             }
+            $etablissement->save();
         }   
     }
 
@@ -142,7 +145,7 @@ EOF;
             } else {
                 foreach($types_compte as $tc => $null) {
                     $this->importLines($datas, $tc);
-                    echo sprintf("WARNING;%s;#LINE;%s;%s\n", "Compte demultiplié", $id, $tc);
+                    $this->echoWarning("Compte demultiplié", array($id, $tc));
                 }
             }
         }
@@ -152,6 +155,14 @@ EOF;
         }
 
         $compte->type_compte = $type_compte;
+
+        if($compte->type_compte == CompteClient::TYPE_COMPTE_ETABLISSEMENT && !$compte->raison_sociale) {
+            $compte->raison_sociale = trim(sprintf("%s %s %s", $compte->civilite, $compte->nom,  $compte->prenom));
+            $compte->prenom = null;
+            $compte->nom = null;
+            $compte->civilite = null;
+        }
+
         $compte->identifiant = $this->getIdentifiantCompte($compte, $id);
 
         $this->save($compte, $etablissement);
@@ -240,22 +251,46 @@ EOF;
         } else {
             throw new sfException("Aucun nom ou raison sociale");
         }
-        $siret = str_replace(" ", "", $data[self::CSV_SIRET]);
-        if($siret && !preg_match("/^[0-9]+$/", $siret)) {
-            throw new Exception(sprintf("Le SIRET n'est pas au bon format : %s", $siret));
-        }
-        $compte->siret = $siret;
 
-        $no_accises = str_replace(" ", "", $data[self::CSV_TVA_INTRA]);
-        if($no_accises && !preg_match("/^FR[0-9]+$/", $no_accises)) {
-            throw new Exception(sprintf("Le numéro d'accises n'est pas au bon format : %s", $no_accises));
+        $compte->siret = trim(str_replace(" ", "", $data[self::CSV_SIRET]));
+        if($compte->siret && !preg_match("/^[0-9]+$/", $compte->siret)) {
+            $this->echoWarning(sprintf("Le SIRET n'est pas au bon format : %s", $compte->siret), $data);
         }
-        $compte->no_accises = $no_accises;
+
+        $compte->no_accises = trim(str_replace(" ", "", $data[self::CSV_TVA_INTRA]));
+        if($compte->no_accises && !preg_match("/^FR[0-9]+$/", $compte->no_accises)) {
+            $this->echoWarning(sprintf("Le numéro d'accises n'est pas au bon format : %s", $compte->no_accises), $data); 
+        }
 
         $compte->adresse = $this->formatAdresse($data);
-        $compte->code_postal = $data[self::CSV_CODE_POSTAL];
-        $compte->commune = $data[self::CSV_COMMUNE];
-        $compte->cedex = $data[self::CSV_CEDEX];
+        if(!$compte->adresse) {
+           $this->echoWarning("Adresse vide", $data); 
+        }
+        
+        $compte->code_postal = trim($data[self::CSV_CODE_POSTAL]);
+        if($data[self::CSV_PAYS] == "FRANCE" || !$data[self::CSV_PAYS]) {
+            if(!preg_match("/^[0-9]+$/", $compte->code_postal)) {
+                $this->echoWarning("Code postal au mauvais format", $data);
+            }
+        }
+
+        $compte->commune = trim($data[self::CSV_COMMUNE]);
+        if($data[self::CSV_PAYS] == "FRANCE" || !$data[self::CSV_PAYS]) {
+            if(!$compte->commune || $compte->commune == "Canton non précisé") {
+                $this->echoWarning("Ville non trouvé", $data);
+            }
+        }
+
+        $compte->pays = trim($data[self::CSV_PAYS]);
+
+        if(!$compte->pays) {
+            $compte->pays = "FRANCE";
+        }
+
+        if($compte->pays != "FRANCE") {
+            $compte->commune = null;
+            $compte->code_postal = null;
+        }
 
         $compte->date_creation = null;
         $compte->date_archivage = null;
@@ -300,6 +335,11 @@ EOF;
         $etablissement->commune = $compte->commune;
 
         return $etablissement;
+    }
+
+    protected function echoWarning($message, $data) {
+
+        echo sprintf("WARNING;%s;#LINE;%s\n", $message, implode(";", $data));
     }
 
     protected function importLineChai($data, $compte, $etablissement, $type_compte = null) {
@@ -386,75 +426,76 @@ EOF;
 
     protected function importLineAttribut($data, $compte, $type_compte = null) {
         
-        if(preg_match("/Porteurs de mémoires/", $data[self::CSV_ATTRIBUTS])) {
-            $compte->tags->attributs->add(CompteClient::ATTRIBUT_DEGUSTATEUR_PORTEUR_MEMOIRES, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_DEGUSTATEUR_PORTEUR_MEMOIRES));
+        if(!$type_compte || $type_compte == CompteClient::TYPE_COMPTE_DEGUSTATEUR) {
+            if(preg_match("/Porteurs de mémoires/", $data[self::CSV_ATTRIBUTS])) {
+                $compte->tags->attributs->add(CompteClient::ATTRIBUT_DEGUSTATEUR_PORTEUR_MEMOIRES, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_DEGUSTATEUR_PORTEUR_MEMOIRES));
+            }
+
+            if(preg_match("/Techniciens du produit/", $data[self::CSV_ATTRIBUTS])) {
+                $compte->tags->attributs->add(CompteClient::ATTRIBUT_DEGUSTATEUR_TECHNICIEN_PRODUIT, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_DEGUSTATEUR_TECHNICIEN_PRODUIT));
+            }
+        
+            if(preg_match("/Usagers du produit/", $data[self::CSV_ATTRIBUTS])) {
+                $compte->tags->attributs->add(CompteClient::ATTRIBUT_DEGUSTATEUR_USAGER_PRODUIT, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_DEGUSTATEUR_USAGER_PRODUIT));
+            }
         }
 
-        if(preg_match("/Techniciens du produit/", $data[self::CSV_ATTRIBUTS])) {
-            $compte->tags->attributs->add(CompteClient::ATTRIBUT_DEGUSTATEUR_TECHNICIEN_PRODUIT, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_DEGUSTATEUR_TECHNICIEN_PRODUIT));
+        if(!$type_compte || $type_compte == CompteClient::TYPE_COMPTE_AGENT_PRELEVEMENT) {
+            if(preg_match("/Préleveur/", $data[self::CSV_ATTRIBUTS])) {
+                $compte->tags->attributs->add(CompteClient::ATTRIBUT_AGENT_PRELEVEMENT_PRELEVEUR, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_AGENT_PRELEVEMENT_PRELEVEUR));
+            }
+
+            if(preg_match("/Agent de contrôle/", $data[self::CSV_ATTRIBUTS])) {
+                $compte->tags->attributs->add(CompteClient::ATTRIBUT_AGENT_PRELEVEMENT_AGENT_CONTROLE, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_AGENT_PRELEVEMENT_AGENT_CONTROLE));
+            }
         }
 
-        if(preg_match("/Usagers du produit/", $data[self::CSV_ATTRIBUTS])) {
-            $compte->tags->attributs->add(CompteClient::ATTRIBUT_DEGUSTATEUR_USAGER_PRODUIT, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_DEGUSTATEUR_USAGER_PRODUIT));
-        }
+        if(!$type_compte || $type_compte != CompteClient::TYPE_COMPTE_ETABLISSEMENT) {
+            if(preg_match("/Vinificateur/", $data[self::CSV_ATTRIBUTS])) {
+                $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_VINIFICATEUR, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_VINIFICATEUR));
+            }
 
-        if(preg_match("/Préleveur/", $data[self::CSV_ATTRIBUTS])) {
-            $compte->tags->attributs->add(CompteClient::ATTRIBUT_AGENT_PRELEVEMENT_PRELEVEUR, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_AGENT_PRELEVEMENT_PRELEVEUR));
-        }
+            if(preg_match("/Producteur de raisins en structure collective/", $data[self::CSV_ATTRIBUTS])) {
+                $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_COOPERATEUR, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_COOPERATEUR));
+            }
 
-        if(preg_match("/Agent de contrôle/", $data[self::CSV_ATTRIBUTS])) {
-            $compte->tags->attributs->add(CompteClient::ATTRIBUT_AGENT_PRELEVEMENT_AGENT_CONTROLE, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_AGENT_PRELEVEMENT_AGENT_CONTROLE));
-        }
+            if(preg_match("/Producteur/", $data[self::CSV_ATTRIBUTS])) {
+                $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_PRODUCTEUR_RAISINS, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_PRODUCTEUR_RAISINS));
+            }
 
-        if($type_compte && $type_compte != CompteClient::TYPE_COMPTE_ETABLISSEMENT) {
+            if(preg_match("/Distillation/", $data[self::CSV_ATTRIBUTS])) {
+                $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_DISTILLATEUR, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_DISTILLATEUR));
+            }
 
-            return null;
-        }
+            if(preg_match("/laborateur/", $data[self::CSV_ATTRIBUTS])) {
+                $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_ELABORATEUR, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_ELABORATEUR));
+            }
 
-        if(preg_match("/Vinificateur/", $data[self::CSV_ATTRIBUTS])) {
-            $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_VINIFICATEUR, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_VINIFICATEUR));
-        }
+            if(preg_match("/Négoce/", $data[self::CSV_ATTRIBUTS])) {
+                $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_NEGOCE, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_NEGOCE));
+            }
 
-        if(preg_match("/Producteur de raisins en structure collective/", $data[self::CSV_ATTRIBUTS])) {
-            $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_COOPERATEUR, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_COOPERATEUR));
-        }
+            if(preg_match("/Cave coopérative/", $data[self::CSV_ATTRIBUTS])) {
+                $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_CAVE_COOPERATIVE, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_CAVE_COOPERATIVE));
+            }
 
-        if(preg_match("/Producteur/", $data[self::CSV_ATTRIBUTS])) {
-            $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_PRODUCTEUR_RAISINS, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_PRODUCTEUR_RAISINS));
-        }
+            if(preg_match("/Metteur en marché/", $data[self::CSV_ATTRIBUTS])) {
+                $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_METTEUR_EN_MARCHE, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_METTEUR_EN_MARCHE));
+            }
 
-        if(preg_match("/Distillation/", $data[self::CSV_ATTRIBUTS])) {
-            $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_DISTILLATEUR, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_DISTILLATEUR));
-        }
+            if(preg_match("/Conditionneur/", $data[self::CSV_ATTRIBUTS])) {
+                $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_CONDITIONNEUR, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_CONDITIONNEUR));
+            }
 
-        if(preg_match("/laborateur/", $data[self::CSV_ATTRIBUTS])) {
-            $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_ELABORATEUR, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_ELABORATEUR));
-        }
-
-        if(preg_match("/Négoce/", $data[self::CSV_ATTRIBUTS])) {
-            $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_NEGOCE, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_NEGOCE));
-        }
-
-        if(preg_match("/Cave coopérative/", $data[self::CSV_ATTRIBUTS])) {
-            $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_CAVE_COOPERATIVE, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_CAVE_COOPERATIVE));
-        }
-
-        if(preg_match("/Metteur en marché/", $data[self::CSV_ATTRIBUTS])) {
-            $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_METTEUR_EN_MARCHE, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_METTEUR_EN_MARCHE));
-        }
-
-        if(preg_match("/Conditionneur/", $data[self::CSV_ATTRIBUTS])) {
-            $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_CONDITIONNEUR, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_CONDITIONNEUR));
-        }
-
-        if(preg_match("/Viticulteur indépendant/", $data[self::CSV_ATTRIBUTS])) {
-            $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_VITICULTEUR_INDEPENDANT, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_VITICULTEUR_INDEPENDANT));
-        }        
+            if(preg_match("/Viticulteur indépendant/", $data[self::CSV_ATTRIBUTS])) {
+                $compte->tags->attributs->add(CompteClient::ATTRIBUT_ETABLISSEMENT_VITICULTEUR_INDEPENDANT, CompteClient::getInstance()->getAttributLibelle(CompteClient::ATTRIBUT_ETABLISSEMENT_VITICULTEUR_INDEPENDANT));
+            }
+        }     
     }
 
     protected function formatAdresse($data) {
 
-        return trim(preg_replace("/[ ]+/", " ", sprintf("%s %s %s", $data[self::CSV_ADRESSE_1], $data[self::CSV_ADRESSE_2], $data[self::CSV_ADRESSE_3])));
+        return trim(preg_replace("/[ ]+/", " ", sprintf("%s %s %s", $data[self::CSV_ADRESSE_1], $data[self::CSV_ADRESSE_2], $data[self::CSV_ADRESSE_3], $data[self::CSV_CEDEX])));
     }
 
     protected function formatPhone($numero) {
@@ -464,6 +505,16 @@ EOF;
         }
 
         return ($numero) ? $numero : null;
+    }
+
+    protected function formatDate($date) {
+        if(!$date) {
+            return null;
+        }
+
+        $dateObj = new Date($date);
+
+        return $dateObj->format('Y-m-d');
     }
 
 }
