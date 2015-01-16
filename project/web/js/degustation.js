5f6e53ab861451c1d7666929ccb1d99dc0bc8404
Vincent LAURENT <vince.laurent@gmail.com>
2015-01-16 18:45:02 +0100

    Meilleurs gestion des tableaux des opérateurs

--- project/web/js/degustation.js
+++ project/web/js/degustation.js
@@ -10,22 +10,53 @@
     /* =================================================================================== */
     _doc.ready(function()
     {
-        $("#a_choisir a.list-group-item").click(function() {
-            var element = $("#"+$(this).attr('id').replace("a_choisir", "choisi"));
-            element.removeClass("hidden");
-            $(this).addClass("hidden");
-            //$(document).scrollTo(element);
-            return false;
-        });
-        $("#choisi div.list-group-item button.trash").click(function() {
-            console.log('test');
+        $("#listes_operateurs .list-group-item .btn-success").click(function() {
             var ligne = $(this).parent().parent();
-            var element = $('#' + ligne.attr('id').replace("choisi", "a_choisir"));
-            element.removeClass("hidden");
-            ligne.addClass("hidden");
-            //$(document).scrollTo(element);
+            ligne.attr('data-state', 'active');
+            $.updateItem(ligne);
+
             return false;
         });
+
+        $("#listes_operateurs .list-group-item .btn-danger").click(function() {
+            var ligne = $(this).parent().parent();
+            ligne.attr('data-state', '');
+            $.updateItem(ligne);
+
+            return false;
+        });
+
+        $("#nav_a_prelever").click(function() {
+            $(this).parent().find('li').removeClass('active');
+            $(this).addClass('active');
+            $("#listes_operateurs .list-group-item[data-state!=active]").addClass('hidden');
+
+            return false;
+        });
+
+        $("#nav_tous").click(function() {
+            $(this).parent().find('li').removeClass('active');
+            $(this).addClass('active');
+            $("#listes_operateurs .list-group-item").removeClass('hidden');
+
+            return false;
+        });
+
+
     });
+
+    $.updateItem = function(ligne)
+    {
+        if(ligne.attr('data-state') == "active") {
+            ligne.find('button.btn-danger, select').removeClass('hidden');
+            ligne.find('button.btn-success').addClass('hidden');
+        } else {
+            ligne.find('button.btn-danger, select').addClass('hidden');
+            ligne.find('button.btn-success').removeClass('hidden');
+            if($("#nav_a_prelever").hasClass('active')) {
+                ligne.addClass('hidden');
+            }
+        }
+    }
 
 })(jQuery);
