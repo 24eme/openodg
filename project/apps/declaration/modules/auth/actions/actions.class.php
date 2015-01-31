a725356554f818098195b55fc22fb04b0d7592b7
Vincent LAURENT <vince.laurent@gmail.com>
2015-01-31 19:23:48 +0100

    Réparation du login no cas

--- project/apps/declaration/modules/auth/actions/actions.class.php
+++ project/apps/declaration/modules/auth/actions/actions.class.php
@@ -24,7 +24,7 @@ class authActions extends sfActions
             return sfView::SUCCESS;
         }
 
-        $this->getUser()->signIn($this->form->getValue('login'));
+        $this->getUser()->signIn(preg_replace("/COMPTE-[E]*/", "", $this->form->getValue('login')));
 
         return $this->redirect('home');
     }
