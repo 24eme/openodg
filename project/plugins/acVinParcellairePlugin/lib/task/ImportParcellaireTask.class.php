d1102a57ada7d1fa89bafe0b565e531ce4457d61
Tangui Morlier <tangui@tangui.eu.org>
2015-02-09 18:36:02 +0100

    Tache d'import à partir d'un CSV d'une année précise

---
+++ project/plugins/acVinParcellairePlugin/lib/task/ImportParcellaireTask.class.php
@@ -0,0 +1,80 @@
+<?php
+
+class CompteArchivageTask extends sfBaseTask
+{
+
+    protected function configure()
+    {
+        $this->addArguments(array(
+            new sfCommandArgument('annee', sfCommandArgument::REQUIRED, "Annee de declaration"),
+            new sfCommandArgument('csv', sfCommandArgument::REQUIRED, "Donnees au format CSV")
+        ));
+
+        $this->addOptions(array(
+            new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'declaration'),
+            new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+            new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
+        ));
+
+        $this->namespace = 'parcellaire';
+        $this->name = 'importFromCSV';
+        $this->briefDescription = "Importe le parcellaire depuis le CSV d'une annee";
+        $this->detailedDescription = <<<EOF
+EOF;
+    }
+
+    protected function execute($arguments = array(), $options = array())
+    {
+        // initialize the database connection
+        $databaseManager = new sfDatabaseManager($this->configuration);
+        $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+
+        $convert = array('COMMUNALE' => "AOC Alsace Communale", 'LIEUDIT' => 'aoc alsace lieudit', 'GRDCRU' => 'aoc alsace grand cru ', 'CREMANT' => 'aoc cremant dalsace');
+        $i = 0;
+        foreach(file($arguments['csv']) as $line) {
+            $i++;
+            $csv = str_getcsv($line);
+            if ($csv[0] == 'ORIGINE') {
+                continue;
+            }
+            try {
+                $p = ParcellaireClient::getInstance()->findOrCreate($csv[8], $arguments['annee']);
+            }catch(sfException $e) {
+                print "ERROR: ligne $i: ".$e->getMessage()."\n";
+                continue;
+            }
+            if (!$p->isNew()) {
+                $p->add('declarant')->add('cvi', $csv[8]);
+                $p->add('declarant')->add('nom', $csv[9]);
+                $p->add('declarant')->add('adresse', $csv[10]);
+                $p->add('declarant')->add('code_postal', $csv[11]);
+                $p->add('declarant')->add('commune', $csv[12]);
+                $p->add('declarant')->add('telephone', $csv[13]);
+                $p->add('declarant')->add('email', $csv[14]);
+            }
+            $hash = $p->getConfiguration()->identifyProduct($convert[$csv[0]], $csv[2], $csv[5]);
+            if (isset($hash['error'])) {
+                print("ERROR: ligne $i: Pas de produit pour $csv[0] / $csv[2] / $csv[5]\n");
+                continue;
+            }
+            $produit = $p->getOrAdd($hash['hash']);
+            $produit->getLibelleComplet();
+
+            $parcelle = $produit->add('detail')->add(KeyInflector::slugify($csv[1].'_'.$csv[3].'_'.$csv[4]));
+            $parcelle->commune = strtoupper($csv[1]);
+            $parcelle->section = $csv[3];
+            $parcelle->numero_parcelle = $csv[4];
+            $parcelle->superficie = str_replace(',', '.', $csv[6]) * 1;
+            $isNew = $p->isNew();
+            try{
+                $p->save();
+            }catch(sfException $e) {
+                print "ERROR: ligne $i: ".$e->getMessage()."\n";
+                continue;
+            }
+            if ($isNew) {
+                print($p->_id." created\n");
+            }
+        }
+    }
+}
