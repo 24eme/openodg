56f46fc2c9270e27107f997cf3fa7ffe2191dd19
Tangui Morlier <tangui@tangui.eu.org>
2015-02-09 18:32:55 +0100

    Ajout des méthodes abstraites

--- project/plugins/acVinParcellairePlugin/lib/model/Parcellaire/ParcellaireCertification.class.php
+++ project/plugins/acVinParcellairePlugin/lib/model/Parcellaire/ParcellaireCertification.class.php
@@ -5,5 +5,16 @@
  */
 
 class ParcellaireCertification extends BaseParcellaireCertification {
+    public function getChildrenNode() 
+    {
+        return $this->getGenres();
+    }
 
+    public function getGenres()
+    {
+        return $this->filter('^genre');
+    }
+    public function getLibelleComplet() {
+        return "";
+    }
 }
--- project/plugins/acVinParcellairePlugin/lib/model/Parcellaire/ParcellaireLieu.class.php
+++ project/plugins/acVinParcellairePlugin/lib/model/Parcellaire/ParcellaireLieu.class.php
@@ -5,5 +5,33 @@
  */
 
 class ParcellaireLieu extends BaseParcellaireLieu {
+	public function getMention() 
+    {
+        return $this->getParent();
+    }
 
+    public function getAppellation() 
+    {
+        return $this->getMention()->getParent();
+    }
+
+    public function getChildrenNode() 
+    {
+        return $this->getCouleurs();
+    }
+
+    public function getProduits($onlyActive = false) 
+    {
+        if($this->getKey() != "lieu") {
+
+            return array();
+        }
+
+        return parent::getProduits($onlyActive);
+    }
+
+    public function getCouleurs() 
+    {
+        return $this->filter('^couleur');
+    }
 }
