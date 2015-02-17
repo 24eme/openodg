8add3dd1fdf1e1bc40d12f20831b006ef642af3d
Tangui Morlier <tangui@tangui.eu.org>
2015-02-17 20:13:03 +0100

    Tri par commune dans l'édition des parcelles

--- project/apps/declaration/modules/parcellaire/actions/actions.class.php
+++ project/apps/declaration/modules/parcellaire/actions/actions.class.php
@@ -143,7 +143,7 @@ class parcellaireActions extends sfActions {
         $this->appellation = $request->getParameter('appellation');
         $this->ajoutForm = new ParcellaireAjoutParcelleForm($this->parcellaire, $this->appellation);
         $this->appellationNode = $this->parcellaire->getAppellationNodeFromAppellationKey($this->appellation, true);
-        $this->parcelles = $this->appellationNode->getDetailsSortedByParcelle();
+        $this->parcelles = $this->appellationNode->getDetailsSortedByParcelle(false);
 
         $this->form = new ParcellaireAppellationEditForm($this->parcellaire, $this->appellation, $this->parcelles);
         if ($request->isMethod(sfWebRequest::POST)) {
