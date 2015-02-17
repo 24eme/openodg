7d0982551d0dcf6f4780ff2916ca48f9de7a8df0
Vincent LAURENT <vince.laurent@gmail.com>
2015-02-17 21:12:39 +0100

    Bouton vert clair

--- project/apps/declaration/modules/parcellaire/templates/confirmationSuccess.php
+++ project/apps/declaration/modules/parcellaire/templates/confirmationSuccess.php
@@ -16,6 +16,6 @@
 
 <div class="row row-margin row-button">
     <div class="col-xs-4">
-        <a href="<?php echo url_for("home") ?>" class="btn btn-primary btn-lg btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retour</a>
+        <a href="<?php echo url_for("home") ?>" class="btn btn-default btn-lg btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retour</a>
     </div>
 </div>
