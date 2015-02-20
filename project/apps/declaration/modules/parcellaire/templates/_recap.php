0c057dca99b29bf99b52b701017a01995ee6f9a3
Vincent LAURENT <vince.laurent@gmail.com>
2015-02-20 09:46:30 +0100

    Fixe bug pas de last

--- project/apps/declaration/modules/parcellaire/templates/_recap.php
+++ project/apps/declaration/modules/parcellaire/templates/_recap.php
@@ -72,7 +72,7 @@ if ($last) {
                         <?php
                     endforeach;
 
-                    if(array_key_exists($appellation->gethash(), $lastParcellesKeysByAppellations)):
+                    if($lastParcellesKeysByAppellations && array_key_exists($appellation->gethash(), $lastParcellesKeysByAppellations)):
                     foreach ($lastParcellesKeysByAppellations[$appellation->gethash()] as $hashDetail => $detail):
                         if (!array_key_exists($hashDetail, $detailsHashes)):
                             ?>
