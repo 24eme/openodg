2f104ab4ee2e9982a75581f606daf606faf5f1cd
Vincent LAURENT <vince.laurent@gmail.com>
2015-03-02 19:21:57 +0100

    Début de dev de la tache pour ajouter des produits aux dégustateurs

---
+++ project/lib/task/import/fixDegustateurProduitsTask.class.php
@@ -0,0 +1,69 @@
+<?php
+
+class FixDegustateurProduitsTask extends sfBaseTask
+{
+
+    const CSV_EXPERT_ALSACE         = 0;
+    const CSV_EXPERT_CREMANT        = 1;
+    const CSV_EXPERT_VT_SGN         = 2;
+    const CSV_EXPERT_MAGW           = 3;
+    const CSV_EXPERT_GC             = 4;
+    const CSV_IDENTIFIANT           = 5;
+
+    protected function configure()
+    {
+        $this->addArguments(array(
+           new sfCommandArgument('csv', sfCommandArgument::REQUIRED, "Donnees au format CSV")
+        ));
+
+        $this->addOptions(array(
+            new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'declaration'),
+            new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+            new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
+        ));
+
+        $this->namespace = 'fix';
+        $this->name = 'DegustateurProduits';
+        $this->briefDescription = "Corrige le parcellaire passé en parametre";
+        $this->detailedDescription = <<<EOF
+EOF;
+    }
+
+    protected function execute($arguments = array(), $options = array())
+    {
+        // initialize the database connection
+        $databaseManager = new sfDatabaseManager($this->configuration);
+        $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+
+        foreach(file($arguments['csv']) as $line) {
+            $data = str_getcsv($line, ';');
+
+            $compte = CompteClient::getInstance()->find('COMPTE-D'.sprintf("%06d", $data[self::CSV_IDENTIFIANT]));
+
+            if(!$compte) {
+                echo sprintf("ERROR;Compte non trouvé %s;%s", $data[self::CSV_IDENTIFIANT], $line);
+                continue;
+            }
+
+            if(trim($data[self::CSV_EXPERT_ALSACE]) == "x") {
+                echo sprintf("INFO;Expert alsace;%s", $line);
+            }
+
+            if(trim($data[self::CSV_EXPERT_CREMANT])) {
+                echo sprintf("INFO;Expert crémant;%s", $line);
+            }
+
+            if(trim($data[self::CSV_EXPERT_VT_SGN])) {
+                echo sprintf("INFO;Expert vt sgn;%s", $line);
+            }
+
+            if(trim($data[self::CSV_EXPERT_MAGW])) {
+                echo sprintf("INFO;Expert marc de gewurtz;%s", $line);
+            }
+
+            if(trim($data[self::CSV_EXPERT_GC])) {
+                echo sprintf("INFO;Expert grand gru;%s", $line);
+            }
+        }
+    }
+}
