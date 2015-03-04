e974851137b7b79adb63725fef96e3247e98f0ac
Vincent LAURENT <vince.laurent@gmail.com>
2015-03-04 12:45:33 +0100

    Date avec le jour de la semaine

--- project/plugins/EmailPlugin/modules/Email/templates/_send_degustation_degustateur.php
+++ project/plugins/EmailPlugin/modules/Email/templates/_send_degustation_degustateur.php
@@ -3,7 +3,7 @@ Bonjour,
 
 En votre qualité de dégustateur expert, nous vous invitons à venir participer à une dégustation conseil :
 
-Le <?php echo format_date($degustation->date, "D", "fr_FR") ?> à <?php echo Date::francizeHeure($degustation->heure); ?>
+Le <?php echo ucfirst(format_date($degustation->date, "P", "fr_FR")) ?> à <?php echo Date::francizeHeure($degustation->heure); ?>
 
 
 A la Maison des Vins d’Alsace, 12 avenue de la Foire Aux Vins à Colmar
