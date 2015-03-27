5cdb4d1dc335f6b2435d9d97b72122c28f109c38
mathurin <mpetit@actualys.com>
2015-03-27 15:44:46 +0100

    paramétrage pdf individuels pour les courriers des notes

--- project/apps/declaration/modules/degustation/templates/_pdf.php
+++ project/apps/declaration/modules/degustation/templates/_pdf.php
@@ -8,66 +8,131 @@
 <br/>
 <br/>
 <br/>
+<br/>
 <table border="0">
     <tr>
-        <td style="width: 450px;" >&nbsp;</td>
-        <td style="width: 300px; padding-right: 40px; font-weight: bolder; ">
+        <td style="width: 350px;" >&nbsp;</td>
+        <td style="width: 200px; padding-right: 40px; font-weight: bolder; ">
             <?php echo $operateur->raison_sociale ?>
         </td>
+        <td style="width: 150px;">&nbsp;</td>
     </tr>
     <tr>
-        <td style="width: 450px;" >&nbsp;</td>
-        <td style="width: 300px; padding-right: 40px; font-weight: bolder; ">
+        <td style="width: 350px;" >&nbsp;</td>
+        <td style="width: 200px; padding-right: 40px; font-weight: bolder; ">
             <?php echo $operateur->adresse; ?>
         </td>
+        <td style="width: 150px;">&nbsp;</td>
     </tr>
     <tr>
-        <td style="width: 450px;" >&nbsp;</td>
-        <td style="width: 300px; padding-right: 40px; font-weight: bolder; ">
+        <td style="width: 350px;" >&nbsp;</td>
+        <td style="width: 200px; padding-right: 40px; font-weight: bolder; ">
             <?php echo $operateur->code_postal . ' ' . $operateur->commune; ?>
         </td>
+        <td style="width: 150px; ">&nbsp;</td>
     </tr>
 </table>
 <br/>
 <br/>
 <br/>
+<br/>
 <table>
     <tr>
-        <td style="width: 450px;" >&nbsp;</td>
-        <td style="width: 300px; padding-right: 40px; font-weight: bolder; ">            
-            <?php echo $degustation->lieu . ', le ' . date('d/m/Y'); ?>
+        <td style="width: 350px;" >&nbsp;</td>
+        <td style="width: 350px; padding-right: 40px; font-weight: bolder; ">            
+            <?php echo $degustation->lieu . ', le ' . ucfirst(format_date(date('Y-m-d'), "P", "fr_FR")); ?>
         </td>
     </tr>
 </table>
 
 <br/>
+<br/>
+<br/>
 
 <table>
     <tr>
-        <td>N/Réf.: ???? <?php echo ($prelevement->type_courrier == DegustationClient::COURRIER_TYPE_OPE) ? 'OPE' : ''; ?></td>
+        <td>N/Réf.: <?php
+            echo str_replace('-', '', $degustation->date) . '/' . $prelevement->anonymat_degustation;
+            echo ($prelevement->type_courrier == DegustationClient::COURRIER_TYPE_OPE) ? '/OPE' : '';
+            ?></td>
     </tr>
     <tr>
-        <td>Clé Identité : <?php echo $prelevement->anonymat_degustation; ?></td>
+        <td>Clé Identité : <?php echo $operateur->cvi; ?></td>
     </tr>
     <tr>
-        <td >
-            Objet : Gestion locale : dégustation conseil des <?php echo $degustation->appellation_libelle . ' ' . substr($degustation->validation, 0, 4); ?>
-        </td>
+        <td>Cuve : <?php echo $prelevement->cuve; ?></td>
+    </tr>
+    <tr>
+        <td>Objet : Gestion locale : dégustation conseil <?php echo $degustation->appellation_libelle . ' ' . substr($degustation->validation, 0, 4); ?></td>
     </tr>
 </table>
 <br/>
 <br/>
-<table>
+<p>Madame, Monsieur,</p>
+<br/>
+<p>Vous avez présenter vos vins à la degustation conseil de la Gestion locale du <strong><?php echo $degustation->appellation_libelle . ' ' . $prelevement->libelle; ?></strong>. Celle-ci a eu lieu le <strong><?php echo ucfirst(format_date($operateur->date, "P", "fr_FR")); ?></strong>.</p>
+<p>Les experts dégustateurs ont fait les commentaires suivants  sur votre vin : </p>
+
+<div><span class="h3">&nbsp;Rapport de notes&nbsp;</span></div>
+<table class="table" border="1" cellspacing=0 cellpadding=0 style="text-align: right;">
     <tr>
-        <td>Madame, Monsieur,</td>
+        <th class="th" style="text-align: left; width: 350px; font-weight: bold;">&nbsp;Produit</th>    
+        <th class="th" style="text-align: center; width: 140px; font-weight: bold;">Lot N°</th>  
+        <th class="th" style="text-align: center; width: 140px; font-weight: bold;">N° Ech</th>
     </tr>
     <tr>
-        <td>Vous avez présenter vos vins à la degustation conseil de la Gestion locale du <?php echo $degustation->appellation_libelle.' '.$prelevement->libelle; ?>. Celle-ci a eu lieu le <?php echo $operateur->date; ?>.
-        </td>
-    </tr>
-    <tr>
-        <td>Les experts dégustateurs ont fait les commentaires suivants  sur votre vin : </td>
+        <td class="td" style="text-align:left; font-weight: bold;"><?php echo tdStart() ?>&nbsp;<?php echo $prelevement->libelle; ?></td>
+        <td class="td" style="text-align:center; font-weight: bold;"><?php echo tdStart() ?>&nbsp;<?php echo $prelevement->getKey() + 1; ?></td>        
+        <td class="td" style="text-align:center; font-weight: bold;"><?php echo tdStart() ?>&nbsp;<?php echo $prelevement->anonymat_degustation; ?></td>
+
     </tr>    
+    <?php foreach ($prelevement->notes as $type_note => $note): ?>
+        <tr>
+            <td class="td" style="text-align:left;"><?php echo tdStart() ?>&nbsp;<?php echo getLibelleTypeNote($type_note) ?></td>
+            <td class="td" style="text-align:left; font-weight: bold;" colspan="2"><?php echo tdStart() ?>&nbsp;<?php echo $note->note ?></td>        
+        </tr>
+        <?php
+        $defaults = "";
+        $cpt = 0;
+        foreach ($note->defauts as $default) {
+            $defaults.=$default;
+            if ($cpt < count($note->defauts) - 1) {
+                $defaults.=",";
+            }
+            $cpt++;
+        }
+        ?>
+        <tr>
+            <td class="td" style="text-align:left;"><?php echo tdStart() ?>&nbsp;Remarque (s) :</td>
+            <td class="td" style="text-align:left; font-weight: bold;" colspan="2"><?php echo tdStart() ?>&nbsp;<?php echo $defaults ?></td>
+
+        </tr>
+    <?php endforeach; ?>
 </table>
 <br/>
+<?php echo getExplicationsPDF($prelevement); ?>
 <br/>
+<p>A votre disposition pour tout complément d'information, nous vous prions d'agréer, Madame, Monsieur, nos cordiales salutations.</p>
+
+<br/>
+<p style="width: 350px; font-size: 11pt; font-weight: bold; font-style: italic; text-align: right;">L'Appui Technique de l'Ava</p>
+<br/>
+<br/>
+<p style="width: 350px; font-weight: bold; font-style: italic">Rappel du barème des notes :</p>
+<br/>
+<table class="table" border="1" cellspacing=0 cellpadding=0 style="text-align: right;">
+    <?php
+    foreach (DegustationClient::$note_type_libelles as $noteType => $noteLibelle):
+        $notesDesc = "";
+        foreach (DegustationClient::$note_type_notes[$noteType] as $noteDesc):
+            $notesDesc.=$noteDesc . ' / ';
+        endforeach;
+        $notesDesc = substr($notesDesc, 0, strlen($notesDesc) - 2);
+        ?>
+        <tr>
+            <th class="th" style="text-align: left; width: 140px; font-weight: bold;"><small>&nbsp;<?php echo $noteLibelle; ?></small></th>    
+            <td class="td" style="text-align: left; width: 490px; font-size: 9pt;"><small>&nbsp;<?php echo $notesDesc; ?></small></td>
+
+        </tr>            
+    <?php endforeach; ?>
+</table>
--- project/lib/helper/DegustationHelper.php
+++ project/lib/helper/DegustationHelper.php
@@ -1,21 +1,51 @@
 <?php
 
-function getHeurePlus($prelevement,$heureplus) {
-   return substr($prelevement->heure,0,2)+$heureplus . ":".  substr($prelevement->heure, 3,2);
+function getHeurePlus($prelevement, $heureplus) {
+    return substr($prelevement->heure, 0, 2) + $heureplus . ":" . substr($prelevement->heure, 3, 2);
 }
 
-function getAdresseChai($prelevement){
-    return $prelevement->adresse ." ". $prelevement->code_postal . " " .$prelevement->commune;
+function getAdresseChai($prelevement) {
+    return $prelevement->adresse . " " . $prelevement->code_postal . " " . $prelevement->commune;
 }
 
-function getDatesPrelevements($degustation){
+function getDatesPrelevements($degustation) {
     setlocale(LC_ALL, 'fr_FR');
-    return 'Du '.format_date($degustation->date_prelevement_debut, "D", "fr_FR").' au '.format_date($degustation->date_prelevement_fin, "D", "fr_FR");
+    return 'Du ' . format_date($degustation->date_prelevement_debut, "D", "fr_FR") . ' au ' . format_date($degustation->date_prelevement_fin, "D", "fr_FR");
 }
 
-function getTypeCourrier($prelevement){
-    if(!$prelevement->exist('type_courrier') || ! $prelevement->type_courrier){
+function getTypeCourrier($prelevement) {
+    if (!$prelevement->exist('type_courrier') || !$prelevement->type_courrier) {
         return "<span class='glyphicon glyphicon-ban-circle' ></span>";
     }
     return DegustationClient::$types_courrier_libelle[$prelevement->type_courrier];
+}
+
+function getLibelleTypeNote($type_note) {
+    switch ($type_note) {
+        case DegustationClient::NOTE_TYPE_QUALITE_TECHNIQUE:
+            return "Note en qualité technique : ";
+        case DegustationClient::NOTE_TYPE_MATIERE:
+            return "Note en matière : ";
+        case DegustationClient::NOTE_TYPE_TYPICITE:
+            return "Note en typicité : ";
+        case DegustationClient::NOTE_TYPE_CONCENTRATION:
+            return "Note en concentration : ";
+        default:
+            break;
+    }
+}
+
+function getExplicationsPDF($prelevement) {
+        switch ($prelevement->type_courrier) {
+        case DegustationClient::COURRIER_TYPE_OK:
+            return "";
+         case DegustationClient::COURRIER_TYPE_OPE:
+            return "<p>Votre vin a fait l'objet d'une évaluation qui a mis en évidence une non-conformité. N'hésitez pas à nous solliciter si vous avez besoin d'une appui technique. Ceci dans le but de vous aider à déterminer l'origine de cette non-conformité ou simplement pour vous apporter un éclairage sur le cahier des charges.</p>";
+         case DegustationClient::COURRIER_TYPE_VISITE:
+            return "<p>Afin de dicuter avec vous des remarques attribuées à votre échantillon, nous vous proposons de vous rencontrer à votre chai</p>
+<p class='font-weight: bold;'>Le date entre X et X</p><br/>
+<p>En cas d'empêchement, merci de nous le faire savoir au 03.89.20.16.58 (Martine Parisot).</p>";
+        default:
+            break;
+    }
 }
