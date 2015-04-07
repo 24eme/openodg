33d3eeaf75cd3860090622917787882c0b870d31
mathurin <mpetit@actualys.com>
2015-04-07 17:59:24 +0200

    maj apres split du doc Degustation

--- project/apps/declaration/modules/degustation/templates/_notes.php
+++ project/apps/declaration/modules/degustation/templates/_notes.php
@@ -1,11 +1,11 @@
-<?php use_javascript("degustation.js", "last") ?>
+<?php use_javascript("tournee.js", "last") ?>
 <?php use_helper("Date"); ?>
 <?php use_helper('Degustation') ?>
 
 <?php $notes = $tournee->getNotes(); ?>
 <?php $hasForm = isset($form) && $form; ?>
     <?php if ($hasForm): ?>
-    <form action="<?php echo url_for('degustation_courriers', $degustation); ?>" method="post" class="form-horizontal">
+    <form action="<?php echo url_for('degustation_courriers', $tournee); ?>" method="post" class="form-horizontal">
         <?php echo $form->renderHiddenFields(); ?>
         <?php echo $form->renderGlobalErrors(); ?>
     <?php endif; ?>
@@ -52,21 +52,21 @@
                         <td><?php echo $note->prelevement->appreciations; ?></td> 
                         <td class="text-center">
                             <?php if ($hasForm): ?>
-                                <div class="type_courrier_for_visite" id="<?php echo $note->prelevement->getHashForKey(); ?>">
+                                <div class="type_courrier_for_visite" id="<?php echo $note->operateur->cvi.$note->prelevement->getHashForKey(); ?>">
                                    
-                                        <?php echo $form[$note->prelevement->getHashForKey()]->renderError(); ?>
-                                        <?php echo $form[$note->prelevement->getHashForKey()]->render(array('class' => 'form-control select2')); ?>
-                                    <div id="<?php echo 'visite_date_degustation_courrier_' . $note->prelevement->getHashForKey(); ?>" style="<?php echo ($note->prelevement->exist('type_courrier') && $note->prelevement->type_courrier == DegustationClient::COURRIER_TYPE_VISITE)? '' : 'display:none;' ?>" >
+                                        <?php echo $form[$note->operateur->cvi.$note->prelevement->getHashForKey()]->renderError(); ?>
+                                        <?php echo $form[$note->operateur->cvi.$note->prelevement->getHashForKey()]->render(array('class' => 'form-control select2')); ?>
+                                    <div id="<?php echo 'visite_date_tournee_courrier_' . $note->prelevement->getHashForKey(); ?>" style="<?php echo ($note->prelevement->exist('type_courrier') && $note->prelevement->type_courrier == DegustationClient::COURRIER_TYPE_VISITE)? '' : 'display:none;' ?>" >
                                     <div style="padding-top: 10px;" class="input-group date-picker" >
-                                            <?php echo $form['visite_date_' . $note->prelevement->getHashForKey()]->renderError(); ?>
-                                            <?php echo $form['visite_date_' . $note->prelevement->getHashForKey()]->render(array('class' => 'form-control')); ?>
+                                            <?php echo $form['visite_date_' . $note->operateur->cvi.$note->prelevement->getHashForKey()]->renderError(); ?>
+                                            <?php echo $form['visite_date_' . $note->operateur->cvi.$note->prelevement->getHashForKey()]->render(array('class' => 'form-control')); ?>
                                             <div class="input-group-addon">
                                                 <span class="glyphicon-calendar glyphicon"></span>
                                             </div>
                                         </div>
                                         <div style="padding-top: 10px;" class="input-group date-picker-time" >
-                                            <?php echo $form['visite_heure_' . $note->prelevement->getHashForKey()]->renderError(); ?>
-                                            <?php echo $form['visite_heure_' . $note->prelevement->getHashForKey()]->render(array('class' => 'form-control')); ?>
+                                            <?php echo $form['visite_heure_' . $note->operateur->cvi.$note->prelevement->getHashForKey()]->renderError(); ?>
+                                            <?php echo $form['visite_heure_' . $note->operateur->cvi.$note->prelevement->getHashForKey()]->render(array('class' => 'form-control')); ?>
                                             <div class="input-group-addon">
                                                 <span class="glyphicon glyphicon-time"></span>
                                             </div>
@@ -76,7 +76,7 @@
                                 </div>
                             <?php else: ?>        
                                 <?php if ($note->prelevement->exist('type_courrier') && $note->prelevement->type_courrier): ?>
-                                    <a href="<?php echo url_for('degustation_courrier_prelevement', array('id' => $degustation->_id, 'hash_prelevement' => $note->prelevement->getHashForKey())) ?>">
+                                    <a href="<?php echo url_for('degustation_courrier_prelevement', array('id' => $tournee->_id, 'hash_prelevement' => $note->prelevement->getHashForKey())) ?>">
                                     <?php endif; ?>
                                     <?php echo getTypeCourrier($note->prelevement); ?>
                                     <?php if ($note->prelevement->exist('type_courrier') && $note->prelevement->type_courrier): ?>
@@ -94,13 +94,10 @@
             <?php if ($hasForm): ?>
                 <a class="btn btn-primary btn-lg btn-upper" href="<?php echo url_for('degustation') ?>"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retour</a>
             <?php else : ?>
-                <a class="btn btn-primary btn-lg btn-upper" href="<?php echo url_for('degustation_visualisation', $degustation) ?>"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retour</a>
+                <a class="btn btn-primary btn-lg btn-upper" href="<?php echo url_for('degustation_visualisation', $tournee) ?>"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retour</a>
             <?php endif; ?>
         </div>             
         <div class="col-xs-6 text-right">
-            <?php if ($degustation->validation && $degustation->date > date('Y-m-d')): ?>
-                <a class="btn btn-warning btn-lg" href="<?php echo url_for('degustation_organisation', $degustation) ?>"><span class="glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;Modifier l'organisation des tournées</a>
-            <?php endif; ?>
             <?php if ($hasForm): ?>
                 <button type="submit" class="pull-right btn btn-default btn-md btn-upper" >Enregistrer type courrier<span class="eleganticon arrow_carrot-right"></span></button>
                 <?php endif; ?>
--- project/plugins/acVinDegustationPlugin/lib/form/DegustationCourrierForm.class.php
+++ project/plugins/acVinDegustationPlugin/lib/form/DegustationCourrierForm.class.php
@@ -9,30 +9,33 @@ class DegustationCourrierForm extends acCouchdbObjectForm {
 
     public function configure() {
         foreach ($this->getObject()->getNotes() as $note) {
-            $this->setWidget($note->prelevement->getHashForKey(), new sfWidgetFormChoice(array('choices' => $this->getTypesCourrier())));
-            $this->setValidator($note->prelevement->getHashForKey(), new sfValidatorChoice(array('choices' => array_keys($this->getTypesCourrier()), 'required' => false)));
-            
-            $this->setWidget('visite_date_'.$note->prelevement->getHashForKey(), new sfWidgetFormInput(array(), array()));
-            $this->setValidator('visite_date_'.$note->prelevement->getHashForKey(), new sfValidatorDate(array('date_output' => 'Y-m-d', 'date_format' => '~(?P<day>\d{2})/(?P<month>\d{2})/(?P<year>\d{4})~', 'required' => false)));
-            
-            $this->setWidget('visite_heure_'.$note->prelevement->getHashForKey(), new sfWidgetFormInput(array(), array()));
-            $this->setValidator('visite_heure_'.$note->prelevement->getHashForKey(), new sfValidatorTime(array('time_output' => 'H:i', 'time_format' => '~(?P<hour>\d{2}):(?P<minute>\d{2})~', 'required' => false)));
+            $keyForNote = $this->constructKeyForNote($note);
+
+            $this->setWidget($keyForNote, new sfWidgetFormChoice(array('choices' => $this->getTypesCourrier())));
+            $this->setValidator($keyForNote, new sfValidatorChoice(array('choices' => array_keys($this->getTypesCourrier()), 'required' => false)));
+
+            $this->setWidget('visite_date_' . $keyForNote, new sfWidgetFormInput(array(), array()));
+            $this->setValidator('visite_date_' . $keyForNote, new sfValidatorDate(array('date_output' => 'Y-m-d', 'date_format' => '~(?P<day>\d{2})/(?P<month>\d{2})/(?P<year>\d{4})~', 'required' => false)));
+
+            $this->setWidget('visite_heure_' . $keyForNote, new sfWidgetFormInput(array(), array()));
+            $this->setValidator('visite_heure_' . $keyForNote, new sfValidatorTime(array('time_output' => 'H:i', 'time_format' => '~(?P<hour>\d{2}):(?P<minute>\d{2})~', 'required' => false)));
         }
         $this->widgetSchema->setNameFormat('degustation_courrier[%s]');
     }
 
     protected function updateDefaults() {
         foreach ($this->getObject()->getNotes() as $note) {
+            $keyForNote = $this->constructKeyForNote($note);
             if ($note->prelevement->exist('type_courrier')) {
-                $this->setDefault($note->prelevement->getHashForKey(), $note->prelevement->type_courrier);
+                $this->setDefault($keyForNote, $note->prelevement->type_courrier);
             }
             if ($note->prelevement->exist('visite_date') && $note->prelevement->get('visite_date')) {
                 $dateArr = explode('-', $note->prelevement->visite_date);
-                $date = $dateArr[2].'/'.$dateArr[1].'/'.$dateArr[0];
-                $this->setDefault('visite_date_'.$note->prelevement->getHashForKey(), $date);
+                $date = $dateArr[2] . '/' . $dateArr[1] . '/' . $dateArr[0];
+                $this->setDefault('visite_date_' . $keyForNote, $date);
             }
-             if ($note->prelevement->visite_heure) {
-                $this->setDefault('visite_heure_'.$note->prelevement->getHashForKey(), $note->prelevement->visite_heure);
+            if ($note->prelevement->exist('visite_heure') && $note->prelevement->get('visite_heure')) {
+                $this->setDefault('visite_heure_' . $keyForNote, $note->prelevement->visite_heure);
             }
         }
     }
@@ -44,19 +47,30 @@ class DegustationCourrierForm extends acCouchdbObjectForm {
     public function update() {
         $values = $this->values;
         foreach ($values as $key => $value) {
-            if (preg_match('/^\-operateurs.*/', $key) && $value) {
-                $realKey = str_replace('-', '/', $key);
-                $this->getObject()->getOrAdd($realKey)->add('type_courrier', $value);
-                if($value == DegustationClient::COURRIER_TYPE_VISITE){
-                    $this->getObject()->getOrAdd($realKey)->add('visite_date', $values['visite_date_'.$key]);
-                    $this->getObject()->getOrAdd($realKey)->add('visite_heure', $values['visite_heure_'.$key]);
-                }else{
-                    $this->getObject()->getOrAdd($realKey)->add('visite_date', null);
-                    $this->getObject()->getOrAdd($realKey)->add('visite_heure', null);
+            $matches = array();
+            if (preg_match('/^([0-9]{10})-(.*)/', $key, $matches) && $value) {
+                $cvi_operateur = $matches[1];
+                $id_degustation = 'DEGUSTATION-' . $cvi_operateur . '-' . $this->getObject()->identifiant;
+                $degustation = DegustationClient::getInstance()->find($id_degustation);
+                $realKeyPrelevement = str_replace('-', '/', $matches[2]);
+                if(!$degustation){
+                    throw new sfException("La degustation ".$id_degustation." n'existe pas");
                 }
-                $this->getObject()->getOrAdd($realKey)->add('courrier_envoye', false);
-            }            
+                $degustation->get($realKeyPrelevement)->add('type_courrier', $value);
+                if ($value == DegustationClient::COURRIER_TYPE_VISITE) {
+                    $degustation->get($realKeyPrelevement)->add('visite_date', $values['visite_date_' . $key]);
+                    $degustation->get($realKeyPrelevement)->add('visite_heure', $values['visite_heure_' . $key]);
+                } else {
+                    $degustation->get($realKeyPrelevement)->add('visite_date', null);
+                    $degustation->get($realKeyPrelevement)->add('visite_heure', null);
+                }
+                $degustation->get($realKeyPrelevement)->add('courrier_envoye', false);
+            }
         }
+    }
+
+    public function constructKeyForNote($note) {
+        return $note->operateur->cvi . $note->prelevement->getHashForKey();
     }
 
 }
