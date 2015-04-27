d50dd55726fe6743238e0a24fde465d9bf4bc903
Vincent LAURENT <vince.laurent@gmail.com>
2015-04-27 06:22:24 +0200

    Tournée

--- project/plugins/acVinDegustationPlugin/lib/Degustation/DegustationPrelevement.class.php
+++ project/plugins/acVinDegustationPlugin/lib/Degustation/DegustationPrelevement.class.php
@@ -13,7 +13,7 @@ class DegustationPrelevement extends BaseDegustationPrelevement {
         $code_cepage = $this->getCodeCepage();
 
         if(!$code_cepage) {
-            $code_cepage = '__';
+            $code_cepage = $this->getCodeCepageEmpty();
         }
 
         $this->anonymat_prelevement_complet = sprintf("%s %03d %03X", $code_cepage, $this->anonymat_prelevement, $this->anonymat_prelevement + 2560);
@@ -27,9 +27,28 @@ class DegustationPrelevement extends BaseDegustationPrelevement {
         return $return;
     }
 
+    public function getCodeCepageEmpty() {
+        $code_cepage = '__';
+
+        if($this->vtsgn) {
+            $code_cepage .= '__';
+        }
+
+        return $code_cepage;
+    }
+
     public function getCodeCepage() {
 
-        return substr($this->hash_produit, -2);
+        $code_cepage = substr($this->hash_produit, -2);
+        if(!$code_cepage) {
+
+            return;
+        }
+        if($this->vtsgn) {
+            $code_cepage .= $this->vtsgn;
+        }
+
+        return $code_cepage;
     }
 
     public function isPreleve() {
