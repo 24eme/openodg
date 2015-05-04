e6e606b902c0fbef7bfe516f2355b5ebb0ba6e3a
Vincent LAURENT <vince.laurent@gmail.com>
2015-05-04 18:26:44 +0200

    Rollback texte du mail

--- project/plugins/EmailPlugin/modules/Email/templates/_send_degustation_note_degustateur.php
+++ project/plugins/EmailPlugin/modules/Email/templates/_send_degustation_note_degustateur.php
@@ -1,9 +1,9 @@
 <?php use_helper('Date') ?>
 Bonjour,
 
-Nous nous sommes rendu compte que le libellé du 3ème critère de notation de votre prélevement à savoir "L'équilibre" n'est pas ressorti correctement dans le courrier que nous vous avons envoyé Jeudi dernier.
+Vos vins ont été dégustés par les services de L'AVA à la dégustation conseil du <?php echo ucfirst(format_date($degustation->date_degustation, "P", "fr_FR")) ?>.
 
-Dans un soucis de bonne compréhension de vos résultats, nous nous permettons donc de vous envoyer en pièce jointe de ce mail le résultat de la dégustation conseil du <?php echo ucfirst(format_date($degustation->date_degustation, "P", "fr_FR")) ?> corrigé.
+Vous trouverez en pièce jointe les résultats de cette Dégustation.
 
 Bien cordialement,
 
