2b49d6bea452e62c367c2def15f195acaec91e0f
Vincent LAURENT <vince.laurent@gmail.com>
2015-05-26 13:06:54 +0200

    Fixe export csv des comptes avec des guilllements

--- project/apps/declaration/modules/compte/templates/rechercheCsvSuccess.php
+++ project/apps/declaration/modules/compte/templates/rechercheCsvSuccess.php
@@ -1,37 +1,39 @@
 <?php
+use_helper('Csv');
+
 printf("\xef\xbb\xbf");//UTF8 BOM (pour windows)
 echo "#nom complet ; type ; raison sociale ; civilité ; nom ; prénom ; adresse complement destinataire ; adresse ; adresse complément lieu; code postal ; commune ; pays ; téléphone bureau ; téléphone mobile ; téléphone privé ; fax ; email ; cvi ;  siret ; statut ; date de création ; date d'archivage ; identifiant interne ; id ; attributs ; syndicats ; mots clés\n";
 $allTypeCompte = CompteClient::getInstance()->getAllTypesCompteWithLibelles();
 foreach ($results as $res) {
   $data = $res->getData()->getRawValue();
   
-  echo '"'.$data['nom_a_afficher']. '";';
-  echo '"'.$allTypeCompte[$data['type_compte']]. '";';
-  echo '"'.$data['raison_sociale']. '";';
-  echo '"'.$data['civilite']. '";';
-  echo '"'.$data['prenom']. '";';
-  echo '"'.$data['nom']. '";';
-  echo '"'.$data['adresse_complement_destinataire']. '";';
-  echo '"'.$data['adresse']. '";';
-  echo '"'.$data['adresse_complement_lieu']. '";';
-  echo '"'.$data['code_postal']. '";';
-  echo '"'.$data['commune']. '";';
-  echo '"'.$data['pays']. '";';
-  echo '"'.$data['telephone_bureau']. '";';
-  echo '"'.$data['telephone_mobile']. '";';
-  echo '"'.$data['telephone_prive']. '";';
-  echo '"'.$data['fax']. '";';
-  echo '"'.$data['email']. '";';
-  echo '"'.$data['cvi']. '";';
-  echo '"'.$data['siret']. '";';
-  echo '"'.$data['statut']. '";';  
-  echo '"'.$data['date_creation']. '";';  
-  echo '"'.$data['date_archivage']. '";';  
-  echo '"'.$data['identifiant_interne']. '";';  
-  echo '"'.$data['_id']. '";';  
-  echo '"'.implode(", ", $data['infos']['attributs']). '";';  
-  echo '"'.implode(", ", $data['infos']['syndicats']). '";';  
-  echo '"'.implode(", ", $data['infos']['manuels']). '";';
-  echo '"'.str_replace("\n", '\n', $data['commentaires']).'";';  
+  echo '"' . escapeCSVValue($data['nom_a_afficher']) . '";';
+  echo '"' . escapeCSVValue($allTypeCompte[$data['type_compte']]) . '";';
+  echo '"' . escapeCSVValue($data['raison_sociale']) . '";';
+  echo '"' . escapeCSVValue($data['civilite']) . '";';
+  echo '"' . escapeCSVValue($data['prenom']) . '";';
+  echo '"' . escapeCSVValue($data['nom']) . '";';
+  echo '"' . escapeCSVValue($data['adresse_complement_destinataire']) . '";';
+  echo '"' . escapeCSVValue($data['adresse']) . '";';
+  echo '"' . escapeCSVValue($data['adresse_complement_lieu']) . '";';
+  echo '"' . escapeCSVValue($data['code_postal']) . '";';
+  echo '"' . escapeCSVValue($data['commune']) . '";';
+  echo '"' . escapeCSVValue($data['pays']) . '";';
+  echo '"' . escapeCSVValue($data['telephone_bureau']) . '";';
+  echo '"' . escapeCSVValue($data['telephone_mobile']) . '";';
+  echo '"' . escapeCSVValue($data['telephone_prive']) . '";';
+  echo '"' . escapeCSVValue($data['fax']) . '";';
+  echo '"' . escapeCSVValue($data['email']) . '";';
+  echo '"' . escapeCSVValue($data['cvi']) . '";';
+  echo '"' . escapeCSVValue($data['siret']) . '";';
+  echo '"' . escapeCSVValue($data['statut']) . '";';  
+  echo '"' . escapeCSVValue($data['date_creation']) . '";';  
+  echo '"' . escapeCSVValue($data['date_archivage']) . '";';  
+  echo '"' . escapeCSVValue($data['identifiant_interne']) . '";';  
+  echo '"' . escapeCSVValue($data['_id']) . '";';  
+  echo '"' . escapeCSVValue(implode(", ", $data['infos']['attributs'])) . '";';  
+  echo '"' . escapeCSVValue(implode(", ", $data['infos']['syndicats'])) . '";';  
+  echo '"' . escapeCSVValue(implode(", ", $data['infos']['manuels'])) . '";';
+  echo '"' . escapeCSVValue(str_replace("\n", '\n', $data['commentaires'])) . '";';  
   echo "\n";
 }
