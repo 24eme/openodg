2a38010b03b39e9b2c6426e56f9a939b584ee805
Mathurin <mathurin.petit@gmail.com>
2015-09-18 16:25:14 +0200

    coquille dans le mail

--- project/plugins/EmailPlugin/modules/Email/templates/_send_constat_approuve.php
+++ project/plugins/EmailPlugin/modules/Email/templates/_send_constat_approuve.php
@@ -1,7 +1,7 @@
 <?php use_helper('Date') ?>
 Madame, Monsieur, 
 
-Veuillez trouver ci-joint le document attestant du constat VT/SGN portant sur le <?php echo $constat->produit_libelle; ?> de <?php echo $constat->produit_libelle; echo ($constat->denomination_lieu_dit)? " (".$constat->denomination_lieu_dit.")" : ''; ?> effectué le <?php echo ucfirst(format_date($constat->date_signature, "P", "fr_FR")); ?> sur votre exploitation. 
+Veuillez trouver ci-joint le document attestant du constat VT/SGN portant sur le <?php echo $constat->produit_libelle; echo ($constat->denomination_lieu_dit)? " (".$constat->denomination_lieu_dit.")" : ''; ?> effectué le <?php echo ucfirst(format_date($constat->date_signature, "P", "fr_FR")); ?> sur votre exploitation. 
 
 En vous souhaitant bonne réception. 
 
