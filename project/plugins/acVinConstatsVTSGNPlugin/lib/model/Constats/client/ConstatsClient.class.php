89856481a807cc6d4751bea5d50c55352a41b319
Vincent LAURENT <vince.laurent@gmail.com>
2015-09-30 15:51:15 +0200

    Rendez-vous annulé

--- project/plugins/acVinConstatsVTSGNPlugin/lib/model/Constats/client/ConstatsClient.class.php
+++ project/plugins/acVinConstatsVTSGNPlugin/lib/model/Constats/client/ConstatsClient.class.php
@@ -14,6 +14,7 @@ class ConstatsClient extends acCouchdbClient {
     const CONTENANT_CAGETTE = 'CAGETTE';
     const CONTENANT_KG = 'KG';
     const CONTENANT_TYPE_PALOX = 'TYPE_PALOX';
+    const RAISON_REFUS_ANNULE = 'ANNULE';
     const RAISON_REFUS_ASSEMBLE = 'ASSEMBLE';
     const RAISON_REFUS_DEGRE_INSUFFISANT = 'DEGRE_INSUFFISANT';
     const RAISON_REFUS_MULTI_CEPAGE = 'MULTI_CEPAGE';
@@ -21,6 +22,7 @@ class ConstatsClient extends acCouchdbClient {
     const RAISON_REFUS_VENDANGES_MECANIQUE = 'VENDANGES_MECANIQUE';
 
     public static $raisons_refus_libelle = array(
+        self::RAISON_REFUS_ANNULE => 'Rendez-vous annulé',
         self::RAISON_REFUS_DEGRE_INSUFFISANT => 'Degré insuffisant',
         self::RAISON_REFUS_MULTI_CEPAGE => 'Multi-cépages',
         self::RAISON_REFUS_PRESSURAGE_EN_COURS => 'Pressurage en cours',
