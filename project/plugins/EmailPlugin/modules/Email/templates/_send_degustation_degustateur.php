c3f06e694114c89040934b418d98887dd4c9175f
Vincent LAURENT <vince.laurent@gmail.com>
2016-01-22 18:59:17 +0100

    Formatage mail

--- project/plugins/EmailPlugin/modules/Email/templates/_send_degustation_degustateur.php
+++ project/plugins/EmailPlugin/modules/Email/templates/_send_degustation_degustateur.php
@@ -5,6 +5,7 @@ En votre qualité de dégustateur expert <?php echo $degustation->appellation_li
 
 Le <?php echo ucfirst(format_date($degustation->date, "P", "fr_FR")) ?> à <?php echo Date::francizeHeure($degustation->heure); ?>
 
+
 À la Maison des Vins d’Alsace, 12 avenue de la Foire Aux Vins à Colmar.
 
 Nous vous rappelons l’importance de votre présence pour la bonne tenue des commissions de dégustation.
@@ -14,8 +15,7 @@ Merci de bien vouloir nous confirmer votre présence ou votre absence par retour
 Pour information, les autres dégustateurs conviés à cette dégustation sont :
 
 <?php foreach($degustation->degustateurs as $degustateursType): ?><?php foreach($degustateursType as $degustateur): ?> 
-- <?php echo $degustateur->nom ?> (<?php echo $degustateur->commune ?>)
-<?php endforeach; ?><?php endforeach; ?>
+- <?php echo $degustateur->nom ?> (<?php echo $degustateur->commune ?>)<?php endforeach; ?><?php endforeach; ?>
 
 
 Bien cordialement,
