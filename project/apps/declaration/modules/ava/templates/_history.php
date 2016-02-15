95ded12c26364a5367b0abec42a469616d365453
Vincent LAURENT <vince.laurent@gmail.com>
2016-02-15 21:03:16 +0100

    Liste dynamique et historique plus clair

--- project/apps/declaration/modules/ava/templates/_history.php
+++ project/apps/declaration/modules/ava/templates/_history.php
@@ -1,15 +1,15 @@
 <?php if(count($history) > 0): ?>
 <h2>Historique des déclarations</h2>
 <div class="list-group">
-<?php foreach ($history as $drev): ?>
-	<?php if ($drev->type == DRevMarcClient::TYPE_MODEL): ?>
-        <a class="list-group-item" href="<?php echo url_for('drevmarc_visualisation', $drev) ?>"><span class="glyphicon glyphicon-file"></span>&nbsp; Revendication de Marc d'Alsace Gewurztraminer <?php echo $drev->campagne ?></a>
-	<?php elseif($drev->type == DRevClient::TYPE_MODEL): ?>
-        <a class="list-group-item" href="<?php echo url_for('drev_visualisation', $drev) ?>"><span class="glyphicon glyphicon-file"></span>&nbsp;Revendication des appellations viticoles <?php echo $drev->campagne ?></a>
-    <?php elseif($drev->type == ParcellaireClient::TYPE_MODEL && strpos($drev->_id, ParcellaireClient::TYPE_COUCHDB."-") !== false): ?>
-        <a class="list-group-item" href="<?php echo url_for('parcellaire_visualisation', $drev) ?>"><span class="glyphicon glyphicon-file"></span>&nbsp;Affectation parcellaire <?php echo $drev->campagne ?></a>
-    <?php elseif($drev->type == ParcellaireClient::TYPE_MODEL && strpos($drev->_id, ParcellaireClient::TYPE_COUCHDB_PARCELLAIRE_CREMANT."-") !== false): ?>
-        <a class="list-group-item" href="<?php echo url_for('parcellaire_visualisation', $drev) ?>"><span class="glyphicon glyphicon-file"></span>&nbsp;Affectation parcellaire Crémant <?php echo $drev->campagne ?></a>
+<?php foreach ($history as $document): ?>
+	<?php if ($document->type == DRevMarcClient::TYPE_MODEL): ?>
+        <a class="list-group-item" href="<?php echo url_for('drevmarc_visualisation', $document) ?>">Revendication de Marc d'Alsace Gewurztraminer <?php echo $document->campagne ?> <small class="text-muted"><?php if($document->isPapier()): ?>(Papier)<?php else: ?>(Télédéclaration)<?php endif; ?></small></a>
+	<?php elseif($document->type == DRevClient::TYPE_MODEL): ?>
+        <a class="list-group-item" href="<?php echo url_for('drev_visualisation', $document) ?>">Revendication des appellations viticoles <?php echo $document->campagne ?> <small class="text-muted"><?php if($document->isPapier()): ?>(Papier)<?php else: ?>(Télédéclaration)<?php endif; ?></small></a>
+    <?php elseif($document->type == ParcellaireClient::TYPE_MODEL && strpos($document->_id, ParcellaireClient::TYPE_COUCHDB."-") !== false): ?>
+        <a class="list-group-item" href="<?php echo url_for('parcellaire_visualisation', $document) ?>">Affectation parcellaire <?php echo $document->campagne ?> <small class="text-muted"><?php if($document->isPapier()): ?>(Papier)<?php else: ?>(Télédéclaration)<?php endif; ?></small></a>
+    <?php elseif($document->type == ParcellaireClient::TYPE_MODEL && strpos($document->_id, ParcellaireClient::TYPE_COUCHDB_PARCELLAIRE_CREMANT."-") !== false): ?>
+        <a class="list-group-item" href="<?php echo url_for('parcellaire_visualisation', $document) ?>">Affectation parcellaire Crémant <?php echo $document->campagne ?><small class="text-muted"><?php if($document->isPapier()): ?>(Papier)<?php else: ?>(Télédéclaration)<?php endif; ?></small></a>
     <?php endif; ?>
 <?php endforeach; ?>
 </div>
