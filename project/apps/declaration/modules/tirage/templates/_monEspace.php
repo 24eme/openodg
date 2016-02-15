61f3850ac93881e0fdca61485294956e3fd02e92
Vincent LAURENT <vince.laurent@gmail.com>
2016-02-15 21:42:42 +0100

    Tirage crémant sur 2 lignes

--- project/apps/declaration/modules/tirage/templates/_monEspace.php
+++ project/apps/declaration/modules/tirage/templates/_monEspace.php
@@ -2,7 +2,7 @@
 
 <div class="col-xs-4">
     <div class="block_declaration panel panel-primary">     
-        <div class="panel-heading"><h3>Tirage Crémant 2016</h3></div>
+        <div class="panel-heading"><h3>Tirage Crémant 2016<br /><br /></h3></div>
         <div class="panel-body">
             <p>Le Téléservice sera ouvert à partir du 1er mars 2016.</p>
         </div>
