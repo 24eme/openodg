953b75508bee69af8280adeb5487a4bae287d5df
Vincent LAURENT <vince.laurent@gmail.com>
2016-02-15 20:46:35 +0100

    Nouvelle campagne dans les onglets

--- project/apps/declaration/modules/admin/actions/components.class.php
+++ project/apps/declaration/modules/admin/actions/components.class.php
@@ -26,12 +26,10 @@ class adminComponents extends sfComponents {
 
     protected function buildLists() {
         $this->lists = array();
+        $this->lists["Parcellaire 2016"] = $this->getList("Parcellaire", ConfigurationClient::getInstance()->getCampagneManager()->getCurrentNext(), function($document) { return preg_match("/PARCELLAIRE-/", $document->id); });
+        $this->lists["Parcellaire Crémant 2016"] = $this->getList("Parcellaire", ConfigurationClient::getInstance()->getCampagneManager()->getCurrentNext(), function($document) { return preg_match("/PARCELLAIRECREMANT-/", $document->id); });
         $this->lists["DRev 2015"] = $this->getList("DRev", ConfigurationClient::getInstance()->getCampagneManager()->getCurrent());
         $this->lists["DRev Marc 2015"] = $this->getList("DRevMarc", ConfigurationClient::getInstance()->getCampagneManager()->getCurrent());
-        $this->lists["DRev 2014"] = $this->getList("DRev", ConfigurationClient::getInstance()->getCampagneManager()->getCurrentPrevious());
-        $this->lists["DRev Marc 2014"] = $this->getList("DRevMarc", ConfigurationClient::getInstance()->getCampagneManager()->getCurrentPrevious());
-        $this->lists["Parcellaire 2015"] = $this->getList("Parcellaire", ConfigurationClient::getInstance()->getCampagneManager()->getCurrent(), function($document) { return preg_match("/PARCELLAIRE-/", $document->id); });
-        $this->lists["Parcellaire Crémant 2015"] = $this->getList("Parcellaire", ConfigurationClient::getInstance()->getCampagneManager()->getCurrent(), function($document) { return preg_match("/PARCELLAIRECREMANT-/", $document->id); });
     }
 
     protected function getList($type, $campagne, $filter = null) {
