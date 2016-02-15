2370190a11badff477e21f07ff54e83901c3cd23
Vincent LAURENT <vince.laurent@gmail.com>
2016-02-15 19:43:05 +0100

    Récap moins florissant

--- project/apps/declaration/modules/parcellaire/templates/_recap.php
+++ project/apps/declaration/modules/parcellaire/templates/_recap.php
@@ -34,23 +34,23 @@ if ($last) {
                             $stylesuperficie = '';
                             if (isset($diff) && $diff) {
                                 if ($last && !$last->exist($detail->getHash())) {
-                                    $classline = 'success';
+                                    #$classline = 'success';
                                     $styleline = 'border-style: solid; border-width: 1px; border-color: darkgreen;';
                                 } else {
                                     if ($last && $detail->getParcelleIdentifiant() != $last->get($detail->getHash())->getParcelleIdentifiant()) {
                                         $styleparcelle = 'border-style: solid; border-width: 1px; border-color: darkorange;';
-                                        $classparcelle = 'warning';
+                                        #$classparcelle = 'warning';
                                     }
                                     if ($last && $detail->getSuperficie() != $last->get($detail->getHash())->getSuperficie()) {
                                         $styleline = (!$detail->superficie) ? 'text-decoration: line-through; border-style: solid; border-width: 1px; border-color: darkred' : '';
                                         $classline = (!$detail->superficie) ? 'danger' : '';
-                                        $stylesuperficie = (!$detail->superficie) ? 'border-style: solid; border-width: 1px; border-color: darkred' : 'border-style: solid; border-width: 1px; border-color: darkorange';
-                                        $classsuperficie = (!$detail->superficie) ? 'danger' : 'warning';
+                                        $stylesuperficie = (!$detail->superficie) ? 'border-style: solid; border-width: 1px; border-color: darkgreen' : 'border-style: solid; border-width: 1px; border-color: darkgreen';
+                                        #$classsuperficie = (!$detail->superficie) ? 'danger' : 'warning';
                                     }
                                 }
                                 if (!$detail->getSuperficie()) {
                                     $stylesuperficie = 'border-style: solid; border-width: 1px; border-color: darkred';
-                                    $classsuperficie = 'danger';
+                                    #$classsuperficie = 'danger';
                                 }
 
                                 if (!$detail->getActive()) {
