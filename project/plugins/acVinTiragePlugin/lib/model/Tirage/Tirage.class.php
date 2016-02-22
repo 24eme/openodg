f9521aa707a382f1bf401d1b6b0f8a93129eb790
Vincent LAURENT <vince.laurent@gmail.com>
2016-02-22 10:51:15 +0100

    Classe tirage de crémant

---
+++ project/plugins/acVinTiragePlugin/lib/model/Tirage/Tirage.class.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Model for Tirage
+ *
+ */
+
+class Tirage extends BaseTirage implements InterfaceDeclarantDocument, InterfaceDeclaration {
+
+    public function __construct() {
+        parent::__construct();
+        $this->initDocuments();
+    }
+
+    public function __clone() {
+        parent::__clone();
+        $this->initDocuments();
+    }
+
+    public function __construct() {
+        parent::__construct();
+        $this->initDocuments();
+    }
+
+    public function __clone() {
+        parent::__clone();
+        $this->initDocuments();
+    }
+
+    protected function initDocuments() {
+        $this->declarant_document = new DeclarantDocument($this);
+    }
+
+    public function constructId() {
+        $this->set('_id', 'TIRAGE-' . $this->identifiant . '-' . $this->campagne . $this->numero);
+    }
+
+    public function initDoc($identifiant, $campagne, $numero) {
+        $this->identifiant = $identifiant;
+        $this->campagne = $campagne;
+        $this->numero = $numero;
+        $this->updateCepages(),
+    }
+
+    public function storeDeclarant() {
+        $this->declarant_document->storeDeclarant();
+    }
+    
+    public function storeEtape($etape) {
+        $this->add('etape', $etape);
+    }
+
+    public function getConfiguration() {
+
+        return acCouchdbManager::getClient('Configuration')->retrieveConfiguration($this->campagne);
+    }
+
+    public function getConfigurationCepages() {
+
+        return $this->getConfiguration()->declaration->get('certification/genre/appellation_CREMANT/mention/lieu/couleur')->getCepages();
+    }
+
+    public function getEtablissementObject() {
+
+        return EtablissementClient::getInstance()->findByIdentifiant($this->identifiant);
+    }
+
+    public function validate($date = null) {
+        if(is_null($date)) {
+            $date = date('Y-m-d');
+        }
+
+        $this->validation = $date;
+    }
+    
+    public function isValide() {
+        return $this->exist('validation') && $this->validation;
+    }
+
+    public function isPapier() { 
+        
+        return $this->exist('papier') && $this->get('papier');
+    }
+
+    public function isAutomatique() { 
+        
+        return $this->exist('automatique') && $this->get('automatique');
+    }
+
+    public function getValidation() {
+
+        return $this->_get('validation');
+    }
+
+    public function getValidationOdg() {
+
+        return $this->_get('validation_odg');
+    }
+
+    public function validateOdg() {
+        $this->validation_odg = date('Y-m-d');
+    }
+
+    public function updateCepages() {
+        foreach($this->getConfigurationCepages() as $cepage) {
+            $this->cepages->add($cepage->getKey())->libelle = $cepage->getLibelle();
+        }
+    }
+
+}
---
+++ project/plugins/acVinTiragePlugin/lib/model/Tirage/client/TirageClient.class.php
@@ -0,0 +1,66 @@
+<?php
+
+class TirageClient extends acCouchdbClient {
+
+    const TYPE_MODEL = "Tirage"; 
+    const TYPE_COUCHDB = "TIRAGE";
+
+    public static function getInstance()
+    {
+        
+        return acCouchdbManager::getClient("Tirage");
+    }
+
+    public function find($id, $hydrate = self::HYDRATE_DOCUMENT, $force_return_ls = false) {
+        $doc = parent::find($id, $hydrate, $force_return_ls);
+
+        if($doc && $doc->type != self::TYPE_MODEL) {
+
+            throw new sfException(sprintf("Document \"%s\" is not type of \"%s\"", $id, self::TYPE_MODEL));
+        }
+
+        return $doc;
+    }
+
+    public function createDoc($identifiant, $campagne, $papier = false) 
+    {  
+        $tirage = new Tirage();
+        $tirage->initDoc($identifiant, $campagne, "01");
+
+        if($papier) {
+            $tirage->add('papier', 1);
+        }
+
+        return $tirage;
+    }
+
+    public function getHistory($identifiant, $hydrate = acCouchdbClient::HYDRATE_DOCUMENT) {
+        $campagne_from = "0000";
+        $campagne_to = ConfigurationClient::getInstance()->getCampagneManager()->getPrevious(ConfigurationClient::getInstance()->getCampagneManager()->getCurrent())."";
+
+        return $this->startkey(sprintf("TIRAGE-%s-%s%s", $identifiant, $campagne_from, "00"))
+                    ->endkey(sprintf("TIRAGE-%s-%s%s", $identifiant, $campagne_to, "99"))
+                    ->execute($hydrate);
+    }
+
+    public function getDateOuvertureDebut() {
+        $dates = sfConfig::get('app_dates_ouverture_tirage');
+
+        return $dates['debut'];
+    }
+
+    public function getDateOuvertureFin() {
+        $dates = sfConfig::get('app_dates_ouverture_tirage');
+
+        return $dates['fin'];
+    }
+
+    public function isOpen($date = null) {
+        if(is_null($date)) {
+
+            $date = date('Y-m-d');
+        }
+
+        return $date >= $this->getDateOuvertureDebut() && $date <= $this->getDateOuvertureFin(); 
+    }
+}
