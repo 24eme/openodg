bb1b2c68bdfd7919cfccccf5303e93e346ec0047
Mathurin <mathurin.petit@gmail.com>
2016-02-22 16:50:30 +0100

    maj recap des tirages

--- project/apps/declaration/modules/tirage/templates/_recap.php
+++ project/apps/declaration/modules/tirage/templates/_recap.php
@@ -1,16 +1,25 @@
-<?php
-use_helper("Date");
-?>
-Déclaration du lot de <strong>A.O.C. Crémant <?php echo $tirage->couleur_libelle ?></strong> (
-<?php
-$cpt=1;
-foreach ($tirage->getCepagesSelectionnes() as $cepage):
-    echo $cepage->getLibelle();
-    echo ($cpt < count($tirage->getCepagesSelectionnes()))? ', ' : ' ';
-    $cpt++;
-endforeach;
-?>
-)
+<?php use_helper('Date') ?>
+<?php use_helper('Orthographe') ?>
+<p>
+    Déclaration du lot de <strong>A.O.C. Crémant <?php echo $tirage->couleur_libelle ?></strong> (
+    <?php
+    $cpt = 1;
+    foreach ($tirage->getCepagesSelectionnes() as $cepage):
+        echo $cepage->getLibelle();
+        echo ($cpt < count($tirage->getCepagesSelectionnes())) ? ', ' : ' ';
+        $cpt++;
+    endforeach;
+    ?>
+    )&nbsp;<?php echo elision('de', $tirage->millesime_libelle) ?>.</p>
+<?php if ($tirage->millesime_libelle): ?>
+    <p><strong>Millesime :</strong> Assemblé ( <?php echo $tirage->millesime_ventilation; ?> )</p>
+<?php endif; ?>
 <br/>
-de <?php echo $tirage->millesime_libelle ?>
+<p>Composition du lot :</p>
+<div class="list-group">
+    <?php foreach ($tirage->composition as $compo): ?>
+        <div class="list-group-item">
+            <?php echo $compo->nombre; ?>  bouteilles de <?php echo $compo->contenance; ?>&nbsp;</div>
+        <?php endforeach; ?>
+</div>
 
