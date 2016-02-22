cc72e3dc59e9dcfd678e6965e4b63ae79455a77b
Mathurin <mathurin.petit@gmail.com>
2016-02-22 19:29:25 +0100

    ajout du mail de confirmation

---
+++ project/plugins/EmailPlugin/modules/Email/templates/_send_tirage_confirmee.php
@@ -0,0 +1,12 @@
+Bonjour,
+
+Après vérification, votre déclaration de tirage de crémant d'Alsace <?php echo $tirage->campagne; ?> a été définitivement validée par notre service.
+
+Vous trouverez en pièce jointe le document PDF de votre déclaration.
+
+Vous pouvez aussi à tout moment revenir sur votre compte pour consulter votre document : <?php echo sfContext::getInstance()->getRouting()->generate('tirage_visualisation', $tirage,true); ?>
+
+
+Bien Cordialement,
+
+Le service Appui technique (via l'application de télédéclaration)
