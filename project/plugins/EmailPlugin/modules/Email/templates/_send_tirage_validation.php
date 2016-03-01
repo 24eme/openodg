33b74fe6fb52b9c1f833f52af5fcf23c9eda8bff
Tangui Morlier <tangui@tangui.eu.org>
2016-03-01 15:50:59 +0100

    Crémant avec une MAJUSCULE

--- project/plugins/EmailPlugin/modules/Email/templates/_send_tirage_validation.php
+++ project/plugins/EmailPlugin/modules/Email/templates/_send_tirage_validation.php
@@ -1,6 +1,6 @@
 Bonjour,
 
-Votre déclaration de tirage de crémant d'Alsace <?php echo $tirage->campagne; ?> a bien été enregistré à l'AVA.
+Votre déclaration de tirage de Crémant d'Alsace <?php echo $tirage->campagne; ?> a bien été enregistré à l'AVA.
 
 Vous pouvez à tout moment revenir sur votre compte pour consulter ou imprimer votre document : <?php echo sfContext::getInstance()->getRouting()->generate('tirage_visualisation', $tirage,true); ?>
 
