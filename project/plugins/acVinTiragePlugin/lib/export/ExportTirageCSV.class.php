6e08043e6724f0681b8327cd2e8d2f47386b5f66
Vincent LAURENT <vince.laurent@gmail.com>
2016-11-08 01:38:39 +0100

    Suppression des licenses

--- project/plugins/acVinTiragePlugin/lib/export/ExportTirageCSV.class.php
+++ project/plugins/acVinTiragePlugin/lib/export/ExportTirageCSV.class.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of ExportParcellairePdf
- *
- * @author mathurin
- */
 class ExportTirageCSV implements InterfaceDeclarationExportCsv {
 
     protected $tirage = null;
