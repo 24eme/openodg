cf3867aaa3a298d3c276287aedac206d57926bd8
Vincent LAURENT <vince.laurent@gmail.com>
2017-02-13 13:30:30 +0100

    Lien vers les pdfs de constats sur la campagne et plus sur l'année de réalisation du constat

--- project/apps/declaration/modules/constats/templates/rendezvousDeclarantSuccess.php
+++ project/apps/declaration/modules/constats/templates/rendezvousDeclarantSuccess.php
@@ -17,19 +17,19 @@
                     "data-url" => url_for('compte_recherche_json', array('type_compte' => CompteClient::TYPE_COMPTE_ETABLISSEMENT))
                 ));
                 ?>
-            </div>           
+            </div>
         </div>
 
     </form>
 </div>
 
 <h3>Prendre le 1er rendez-vous <span class="icon-raisins size-36"></span></h3>
-<div class="row">    
-    <div class="col-xs-12">        
+<div class="row">
+    <div class="col-xs-12">
         <div class="list-group">
             <?php foreach ($compte->getChais() as $keyChai => $chai): ?>
                 <div class="list-group-item">
-                    <?php include_partial('constats/rendezvousForm', array('chai' => $chai, 'form' => $formsRendezVous[$keyChai], 'rendezvous' => $formsRendezVous[$keyChai]->getObject(), 'creation' => true)); ?> 
+                    <?php include_partial('constats/rendezvousForm', array('chai' => $chai, 'form' => $formsRendezVous[$keyChai], 'rendezvous' => $formsRendezVous[$keyChai]->getObject(), 'creation' => true)); ?>
                 </div>
             <?php endforeach; ?>
 
@@ -37,7 +37,7 @@
     </div>
 </div>
 
-<div class="row">    
+<div class="row">
     <div class="col-xs-12">
         <h3>Les rendez-vous</h3>
         <table class="table table-bordered table-condensed">
@@ -45,24 +45,24 @@
                 <th>Chai</th>
                 <th>Infos</th>
                 <th>RDV</th>
-                <th>Résultat</th>  
-                <th class="col-xs-1 text-center" ></th>  
+                <th>Résultat</th>
+                <th class="col-xs-1 text-center" ></th>
             </tr>
             <?php foreach ($rendezvousConstatsDeclarant->rendezvous as $idRendezvous => $rendezvous) : ?>
 
                 <?php if ($rendezvous->statut != RendezvousClient::RENDEZVOUS_STATUT_REALISE): ?>
                     <tr style="border-bottom: 1px solid #cbcbcb;">
-                        <td class="text-center"><?php echo "" . $rendezvous->idchai + 1; ?></td>                  
+                        <td class="text-center"><?php echo "" . $rendezvous->idchai + 1; ?></td>
                         <td class="<?php if ($rendezvous->statut == RendezvousClient::RENDEZVOUS_STATUT_PRIS): ?>list-group-item-warning <?php endif; ?> <?php if ($rendezvous->statut == RendezvousClient::RENDEZVOUS_STATUT_ANNULE): ?>list-group-item-danger <?php endif; ?> "><?php echo RendezvousClient::$rendezvous_statut_libelles[$rendezvous->statut]; ?>
                             <?php
                             if ($rendezvous->statut != RendezvousClient::RENDEZVOUS_STATUT_PRIS): echo " pour " . $rendezvous->nom_agent_origine;
                             endif;
                             ?></td>
-                        <td class="text-center">                          
+                        <td class="text-center">
                             <a href="<?php echo url_for('rendezvous_modification', $rendezvous); ?>">
                                 <?php echo ucfirst(format_date($rendezvous->date, "P", "fr_FR")); ?><?php if ($rendezvous->isRendezvousRaisin()): ?> <?php echo str_replace(':', 'h', $rendezvous->heure); ?> <?php endif; ?>
                             </a>
-                        </td>                
+                        </td>
                         <td class="text-center">
                             <?php if ($rendezvous->isRendezvousRaisin()): ?>
                                 <span class="icon-raisins size-36"></span>
@@ -70,21 +70,21 @@
                             <?php if ($rendezvous->isRendezvousVolume()): ?>
                                 <span class="icon-mouts size-36"></span>
                             <?php endif; ?>
-                            &nbsp;    
-                             <!--<span class="glyphicon glyphicon-ban-circle"></span>--> 
+                            &nbsp;
+                             <!--<span class="glyphicon glyphicon-ban-circle"></span>-->
                         </td>
                         <td class="col-xs-1 text-center" >
                             <?php if ($rendezvous->statut == RendezvousClient::RENDEZVOUS_STATUT_PRIS): ?>
-                                <a class=" glyphicon glyphicon-remove-circle text-danger" href="<?php echo url_for('rendezvous_declarant_remove', array('idrendezvous' => $rendezvous->_id)); ?>" onclick="confirm('Êtes-vous sûre de vouloir supprimer ce rendez-vous?')"></a> 
+                                <a class=" glyphicon glyphicon-remove-circle text-danger" href="<?php echo url_for('rendezvous_declarant_remove', array('idrendezvous' => $rendezvous->_id)); ?>" onclick="confirm('Êtes-vous sûre de vouloir supprimer ce rendez-vous?')"></a>
                             <?php else: ?>
                                 <span class=" glyphicon glyphicon-ban-circle"></span>
                             <?php endif; ?>
                         </td>
                     </tr>
                 <?php else: ?>
-                    <?php foreach ($rendezvousConstatsDeclarant->constats[$idRendezvous]->constats as $idConstatRendezvous => $constat) : ?>                    
+                    <?php foreach ($rendezvousConstatsDeclarant->constats[$idRendezvous]->constats as $idConstatRendezvous => $constat) : ?>
                         <tr style="border-bottom: 1px solid #cbcbcb;" >
-                            <td class="text-center"><?php echo "" . $rendezvous->idchai + 1; ?></td>                  
+                            <td class="text-center"><?php echo "" . $rendezvous->idchai + 1; ?></td>
                             <td class="list-group-item-success"><?php echo RendezvousClient::$rendezvous_statut_libelles[$rendezvous->statut]; ?>
                                 <?php
                                 if ($rendezvous->statut != RendezvousClient::RENDEZVOUS_STATUT_PRIS): echo " par " . $rendezvous->getAgentNom();
@@ -92,7 +92,7 @@
                                 ?></td>
                             <td class="text-center">
                                 <?php echo ucfirst(format_date($rendezvous->date, "P", "fr_FR")); ?><?php if ($rendezvous->isRendezvousRaisin()): ?> <?php echo str_replace(':', 'h', $rendezvous->heure); ?> <?php endif; ?>
-                            </td>                
+                            </td>
                             <td class="text-center" colspan="2">
                                 <?php if (($constat->statut_raisin == ConstatsClient::STATUT_APPROUVE) && ($constat->statut_volume == ConstatsClient::STATUT_APPROUVE) && $rendezvousConstatsDeclarant->rendezvous[$idRendezvous]->isRendezvousRaisin()): ?>
                                     <span class="icon-raisins size-36"></span>&nbsp;<?php echo $constat->produit_libelle; ?>
@@ -101,7 +101,7 @@
                                 <?php elseif (($constat->statut_raisin == ConstatsClient::STATUT_APPROUVE) && ($constat->statut_volume == ConstatsClient::STATUT_APPROUVE)): ?>
                                     <span class="icon-mouts size-36"></span>&nbsp;<?php echo $constat->produit_libelle; ?>
                                     <br/>
-                                    <span class="label label-success" >Approuvé</span>&nbsp;<a href="<?php echo url_for('constat_pdf', array('identifiant' => $constat->getDocument()->getCvi(), 'campagne' => substr($constat->getKey(), 0, 4), 'identifiantconstat' => $constat->getKey())) ?>"  class="btn btn-xs btn-default btn-primary-step"><span class="glyphicon glyphicon-file"></span>&nbsp;Visualiser</a>
+                                    <span class="label label-success" >Approuvé</span>&nbsp;<a href="<?php echo url_for('constat_pdf', array('identifiant' => $constat->getDocument()->getCvi(), 'campagne' => $constat->getDocument()->campagne, 'identifiantconstat' => $constat->getKey())) ?>"  class="btn btn-xs btn-default btn-primary-step"><span class="glyphicon glyphicon-file"></span>&nbsp;Visualiser</a>
                                 <?php elseif (($constat->statut_raisin == ConstatsClient::STATUT_APPROUVE) && ($constat->statut_volume == ConstatsClient::STATUT_REFUSE) && $rendezvousConstatsDeclarant->rendezvous[$idRendezvous]->isRendezvousRaisin()): ?>
                                     <span class="icon-raisins size-36"></span>&nbsp;<?php echo $constat->produit_libelle; ?>
                                     <br/>
@@ -119,7 +119,7 @@
                                     &nbsp;A finir le <?php echo format_date(substr($constat->date_volume, 0, 4) . '-' . substr($constat->date_volume, 4, 2) . '-' . substr($constat->date_volume, 6), "P", "fr_FR"); ?>
                                 <?php elseif ($constat->statut_raisin == ConstatsClient::STATUT_REFUSE): ?>
                                     <span class="icon-raisins size-36"></span>&nbsp;<?php echo $constat->produit_libelle; ?><br/>
-                                    <span class="label label-danger">Refusé</span>                                   
+                                    <span class="label label-danger">Refusé</span>
                                 <?php else: ?>
                                     <span class="label label-default"></span>
                                 <?php endif; ?>
@@ -134,10 +134,10 @@
         </table>
     </div>
 </div>
-<div class="row">    
+<div class="row">
     <div class="col-xs-12">
         <a class="btn btn-warning btn-upper" href="<?php echo url_for('constats', array('jour' => date('Y-m-d'))) ?>"><span class="glyphicon glyphicon-arrow-left"></span>&nbsp;&nbsp;Accueil</a>
         <a href="<?php echo url_for('constats_planifications', array('date' => date('Y-m-d'))) ?>" class="btn btn-lg btn-default btn-upper pull-right"><span class="glyphicon glyphicon-calendar"></span>&nbsp;&nbsp;Accéder à la planification d'aujourd'hui</a>
         <br/>
     </div>
-</div> 
+</div>
