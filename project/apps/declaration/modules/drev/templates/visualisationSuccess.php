371430aa912ac12a8995eb3b5b45a7cef63de2ea
Vincent LAURENT <vince.laurent@gmail.com>
2017-04-27 18:11:55 +0200

    Ajout de la mention facturé dans la visualisation d'une DRev et permettre la réouverture d'une DRev non validé

--- project/apps/declaration/modules/drev/templates/visualisationSuccess.php
+++ project/apps/declaration/modules/drev/templates/visualisationSuccess.php
@@ -11,15 +11,17 @@
     <?php if($drev->isPapier()): ?>
     <small class="pull-right"><span class="glyphicon glyphicon-file"></span> Déclaration papier<?php if($drev->validation && $drev->validation !== true): ?> reçue le <?php echo format_date($drev->validation, "dd/MM/yyyy", "fr_FR"); ?><?php endif; ?><?php if($drev->isSauvegarde()): ?> <span class="text-danger">Non facturé</span><?php endif; ?></small>
     <?php elseif($drev->validation): ?>
-    <small class="pull-right">Télédéclaration<?php if($drev->validation && $drev->validation !== true): ?> validée le <?php echo format_date($drev->validation, "dd/MM/yyyy", "fr_FR"); ?><?php endif; ?><?php if($drev->isSauvegarde()): ?> <span class="text-danger">Non facturé</span><?php endif; ?></small>
+    <small class="pull-right">Télédéclaration<?php if($drev->validation && $drev->validation !== true): ?> validée le <?php echo format_date($drev->validation, "dd/MM/yyyy", "fr_FR"); ?><?php endif; ?><?php if($drev->isSauvegarde()): ?> <span class="text-danger">Non facturable</span><?php endif; ?> <?php if(!$drev->isNonFactures()): ?>
+        <span class="btn btn-default-step btn-xs">Facturé</span>
+    <?php endif; ?></small>
     <?php endif; ?>
+
     </h2>
 </div>
 
 <?php if ($sf_user->hasFlash('notice')): ?>
     <div class="alert alert-success" role="alert"><?php echo $sf_user->getFlash('notice') ?></div>
 <?php endif; ?>
-
 
 <?php if(!$drev->validation): ?>
 <div class="alert alert-warning">
@@ -46,35 +48,33 @@
 </div>
 
 <div class="row row-margin row-button">
-    <div class="col-xs-4">
+    <div class="col-xs-5">
         <a href="<?php if(isset($service)): ?><?php echo $service ?><?php else: ?><?php echo url_for("home") ?><?php endif; ?>" class="btn btn-primary btn-lg btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retour</a>
     </div>
-    <div class="col-xs-4 text-center">
+    <div class="col-xs-2 text-center">
             <a href="<?php echo url_for("drev_export_pdf", $drev) ?>" class="btn btn-warning btn-lg">
                 <span class="glyphicon glyphicon-file"></span>&nbsp;&nbsp;Visualiser
             </a>
     </div>
-    <?php if(!$drev->validation): ?>
-    <div class="col-xs-4 text-right">
-            <a href="<?php echo url_for("drev_edit", $drev) ?>" class="btn btn-warning btn-lg"><span class="glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;Continuer la saisie</a>
+
+    <div class="col-xs-2 text-right">
+        <?php if (DRevSecurity::getInstance($sf_user, $drev->getRawValue())->isAuthorized(DRevSecurity::DEVALIDATION)): ?>
+                    <a class="btn btn-xs btn-default-step pull-right" href="<?php echo url_for('drev_devalidation', $drev) ?>"><span class="glyphicon glyphicon-remove-sign"></span>&nbsp;&nbsp;Dévalider</a>
+        <?php elseif ($sf_user->isAdmin() && !$drev->isLectureSeule()): ?>
+                  <a class="btn btn-xs btn-default-step pull-right" onClick="return confirm('Attention, cette DRev a sans doute été facturée. Si vous changez un volume, pensez à en faire part au service comptable !!');" href="<?php echo url_for('drev_devalidation', $drev) ?>"><span class="glyphicon glyphicon-remove-sign"></span>&nbsp;&nbsp;Réouvrir</a>
+        <?php endif; ?>
     </div>
-    <?php elseif(!$drev->validation_odg && $sf_user->isAdmin()): ?>
-    <div class="col-xs-4 text-right">
-            <?php if($drev->hasCompleteDocuments()): ?>
-            <a href="<?php echo url_for("drev_validation_admin", array("sf_subject" => $drev, "service" => isset($service) ? $service : null)) ?>" class="btn btn-default btn-lg btn-upper"><span class="glyphicon glyphicon-ok-sign"></span>&nbsp;&nbsp;Approuver</a>
-            <?php else: ?>
-                <button type="submit" class="btn btn-default btn-lg btn-upper"><span class="glyphicon glyphicon-check"></span>&nbsp;&nbsp;Enregistrer</button>
-            <?php endif; ?>
+    <div class="col-xs-3 text-right">
+        <?php if(!$drev->validation): ?>
+                <a href="<?php echo url_for("drev_edit", $drev) ?>" class="btn btn-warning btn-lg"><span class="glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;Continuer la saisie</a>
+        <?php elseif(!$drev->validation_odg && $sf_user->isAdmin()): ?>
+                <?php if($drev->hasCompleteDocuments()): ?>
+                <a href="<?php echo url_for("drev_validation_admin", array("sf_subject" => $drev, "service" => isset($service) ? $service : null)) ?>" class="btn btn-default btn-lg btn-upper"><span class="glyphicon glyphicon-ok-sign"></span>&nbsp;&nbsp;Approuver</a>
+                <?php else: ?>
+                    <button type="submit" class="btn btn-default btn-lg btn-upper"><span class="glyphicon glyphicon-check"></span>&nbsp;&nbsp;Enregistrer</button>
+                <?php endif; ?>
+        <?php endif; ?>
     </div>
-    <?php elseif (DRevSecurity::getInstance($sf_user, $drev->getRawValue())->isAuthorized(DRevSecurity::DEVALIDATION)): ?>
-    <div class="col-xs-4 text-right">
-            <a class="btn btn-xs btn-warning pull-right" href="<?php echo url_for('drev_devalidation', $drev) ?>"><span class="glyphicon glyphicon-remove-sign"></span>&nbsp;&nbsp;Dévalider la déclaration</a>
-    </div>
-<?php elseif ($sf_user->isAdmin() && !$drev->isLectureSeule()): ?>
-      <div class="col-xs-4 text-right">
-              <a class="btn btn-xs btn-warning pull-right" onClick="return confirm('Attention, cette DRev a sans doute été facturée. Si vous changez un volume, pensez à en faire part au service comptable !!');" href="<?php echo url_for('drev_devalidation', $drev) ?>"><span class="glyphicon glyphicon-remove-sign"></span>&nbsp;&nbsp;Réouvrir la déclaration</a>
-      </div>
-    <?php endif; ?>
 </div>
 
 <?php if (isset($form)): ?>
