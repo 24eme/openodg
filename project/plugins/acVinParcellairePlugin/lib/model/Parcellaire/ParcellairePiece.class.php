dc0741359a4ae3c77d18e601ee78a005f5902c93
Vincent LAURENT <vince.laurent@gmail.com>
2017-06-02 09:47:46 +0200

    Création d'une classe parcellaire Cremant

--- project/plugins/acVinParcellairePlugin/lib/model/Parcellaire/ParcellairePiece.class.php
+++ project/plugins/acVinParcellairePlugin/lib/model/Parcellaire/ParcellairePiece.class.php
@@ -6,9 +6,4 @@
 
 class ParcellairePiece extends BaseParcellairePiece {
 
-    public static function getUrlVisualisation($id, $isadmin = false)
-    {
-
-        return Parcellaire::getUrlVisualisationPiece($id, $isadmin);
-    }
 }
