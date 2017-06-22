e68cd90a1e5fece281338ac3fe72109608406566
Vincent LAURENT <vince.laurent@gmail.com>
2017-06-22 18:47:27 +0200

    Sécurisation de l'application

--- project/apps/declaration/modules/compte/templates/visualisationAdminSuccess.php
+++ project/apps/declaration/modules/compte/templates/visualisationAdminSuccess.php
@@ -30,31 +30,31 @@
                 </div>
                 <div class="panel-body">
                     <div class="row">
-                        <label class="col-xs-6">Nom / Raison Sociale </label>    
+                        <label class="col-xs-6">Nom / Raison Sociale </label>
                         <div class="col-xs-6">
                             <?php echo $compte->nom_a_afficher; ?>
-                        </div>                
+                        </div>
                     </div>
                     <div class="row">
-                        <label class="col-xs-6">Identifiant interne </label>    
+                        <label class="col-xs-6">Identifiant interne </label>
                         <div class="col-xs-6">
                             <?php echo $compte->identifiant_interne; ?>
-                        </div>                
+                        </div>
                     </div>
                     <?php if ($compte->cvi): ?>
                         <div class="row">
-                            <label class="col-xs-6">CVI </label>    
+                            <label class="col-xs-6">CVI </label>
                             <div class="col-xs-6">
                                 <?php echo $compte->cvi; ?>
-                            </div>                
+                            </div>
                         </div>
                     <?php endif; ?>
                     <?php if ($compte->siret): ?>
                         <div class="row">
-                            <label class="col-xs-6">SIRET / SIREN</label>    
+                            <label class="col-xs-6">SIRET / SIREN</label>
                             <div class="col-xs-6">
                                 <?php echo $compte->siret; ?>
-                            </div>                
+                            </div>
                         </div>
                     <?php endif; ?>
                 </div>
@@ -69,7 +69,7 @@
                 <div class="panel-body">
                     <div class="form-group">
                         <div class="row">
-                            <label class="col-xs-6">Adresse</label>    
+                            <label class="col-xs-6">Adresse</label>
                             <div class="col-xs-6">
                                 <?php if($compte->adresse_complement_destinataire): ?>
                                     <?php echo $compte->adresse_complement_destinataire; ?><br />
@@ -80,73 +80,73 @@
                                 <?php if($compte->adresse_complement_lieu): ?>
                                     <?php echo $compte->adresse_complement_lieu; ?><br />
                                 <?php endif; ?>
-                            </div>                
+                            </div>
                         </div>
                     </div>
                     <div class="form-group">
                         <div class="row">
-                            <label class="col-xs-6">Code postal</label>    
+                            <label class="col-xs-6">Code postal</label>
                             <div class="col-xs-6">
                                 <?php echo $compte->code_postal; ?>
-                            </div>                
+                            </div>
                         </div>
-                    </div>                             
+                    </div>
                     <div class="form-group">
                         <div class="row">
-                            <label class="col-xs-6">Commune</label>    
+                            <label class="col-xs-6">Commune</label>
                             <div class="col-xs-6">
                                 <?php echo $compte->commune; ?>
-                            </div>                
+                            </div>
                         </div>
                     </div>
                     <?php if ($compte->telephone_bureau): ?>
                         <div class="form-group">
                             <div class="row">
-                                <label class="col-xs-6">Téléphone bureau</label>    
+                                <label class="col-xs-6">Téléphone bureau</label>
                                 <div class="col-xs-6">
                                     <?php echo $compte->telephone_bureau; ?>
-                                </div>                
-                            </div>                        
+                                </div>
+                            </div>
                         </div>
                     <?php endif; ?>
                     <?php if ($compte->telephone_mobile): ?>
                         <div class="form-group">
                             <div class="row">
-                                <label class="col-xs-6">Téléphone mobile</label>    
+                                <label class="col-xs-6">Téléphone mobile</label>
                                 <div class="col-xs-6">
                                     <?php echo $compte->telephone_mobile; ?>
-                                </div>                
-                            </div>     
+                                </div>
+                            </div>
                         </div>
                     <?php endif; ?>
                     <?php if ($compte->telephone_prive): ?>
                         <div class="form-group">
                             <div class="row">
-                                <label class="col-xs-6">Téléphone privé</label>    
+                                <label class="col-xs-6">Téléphone privé</label>
                                 <div class="col-xs-6">
                                     <?php echo $compte->telephone_prive; ?>
-                                </div>                
-                            </div>     
+                                </div>
+                            </div>
                         </div>
                     <?php endif; ?>
                     <?php if ($compte->fax): ?>
                         <div class="form-group">
                             <div class="row">
-                                <label class="col-xs-6">Fax</label>    
+                                <label class="col-xs-6">Fax</label>
                                 <div class="col-xs-6">
                                     <?php echo $compte->fax; ?>
-                                </div>                
-                            </div>   
+                                </div>
+                            </div>
                         </div>
                     <?php endif; ?>
                     <?php if ($compte->email): ?>
-                        <div class="form-group"> 
+                        <div class="form-group">
                             <div class="row">
-                                <label class="col-xs-6">Email</label>    
+                                <label class="col-xs-6">Email</label>
                                 <div class="col-xs-6">
                                     <a class="btn-link" href="mailto:<?php echo $compte->email; ?>"><?php echo $compte->email; ?></a>
-                                </div>                
-                            </div>     
+                                </div>
+                            </div>
                         </div>
                     <?php endif; ?>
                 </div>
@@ -172,23 +172,23 @@
                 </div>
                 <div class="panel-body">
                     <div class="form-group row">
-                        <label class="col-xs-3">Type de compte :</label> 
+                        <label class="col-xs-3">Type de compte :</label>
                         <label class="col-xs-9"><?php echo CompteClient::getInstance()->getCompteTypeLibelle($compte->getTypeCompte()); ?></label>
                     </div>
                     <?php if ($compte->hasAttributs()): ?>
                         <div class="form-group row">
-                            <label class="col-xs-3">Attributs :</label> 
-                            <div class="col-xs-9" style="line-height: 23px;">                                                   
+                            <label class="col-xs-3">Attributs :</label>
+                            <div class="col-xs-9" style="line-height: 23px;">
                                 <?php foreach ($compte->getInfosAttributs() as $attribut_code => $attribut_libelle): ?>
                                     <span class="label label-success"><?php echo $attribut_libelle ?></span>
-                                <?php endforeach; ?>                           
+                                <?php endforeach; ?>
                             </div>
                         </div>
                     <?php endif; ?>
                     <?php if ($compte->hasProduits()): ?>
                         <div class="form-group row">
-                            <label class="col-xs-3">Produits :</label> 
-                            <div class="col-xs-9" style="line-height: 23px;">                         
+                            <label class="col-xs-3">Produits :</label>
+                            <div class="col-xs-9" style="line-height: 23px;">
                                 <?php foreach ($compte->getInfosProduits() as $produit_code => $produit_libelle): ?>
                                     <span class="label label-info"><?php echo $produit_libelle ?></span>
                                 <?php endforeach; ?>
@@ -198,8 +198,8 @@
                     <?php endif; ?>
                     <?php if ($compte->hasSyndicats()): ?>
                         <div class="form-group row">
-                            <label class="col-xs-3">Syndicats :</label> 
-                            <div class="col-xs-9" style="line-height: 23px;">                         
+                            <label class="col-xs-3">Syndicats :</label>
+                            <div class="col-xs-9" style="line-height: 23px;">
                                 <?php foreach ($compte->getInfosSyndicats() as $syndicat_code => $syndicat_libelle): ?>
                                     <span class="label label-danger"><?php echo $syndicat_libelle ?></span>
                                 <?php endforeach; ?>
@@ -209,14 +209,14 @@
                     <?php endif; ?>
                     <?php if ($compte->hasManuels()): ?>
                         <div class="form-group row">
-                            <label class="col-xs-3">Mots clés :</label> 
-                            <div class="col-xs-9" style="line-height: 23px;">           
+                            <label class="col-xs-3">Mots clés :</label>
+                            <div class="col-xs-9" style="line-height: 23px;">
                                 <?php foreach ($compte->getInfosManuels() as $tag_manuel_code => $tag_manuel): ?>
                                     <span class="label label-default"><?php echo $tag_manuel ?></span>
-                                <?php endforeach; ?>                               
+                                <?php endforeach; ?>
                             </div>
                         </div>
-                    <?php endif; ?>  
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
@@ -241,7 +241,7 @@
                     </ul>
                 </div>
             </div>
-        </div>  
+        </div>
     <?php endif; ?>
 
     <?php if(count($compte->formations)): ?>
@@ -263,7 +263,7 @@
                     </ul>
             </div>
         </div>
-    </div>  
+    </div>
     <?php endif; ?>
 
     <?php if(count($abonnements)): ?>
@@ -295,7 +295,6 @@
         </div>
         <?php if ($compte->isTypeCompte(CompteClient::TYPE_COMPTE_ETABLISSEMENT) && $sf_user->isAdmin()): ?>
             <div class="col-xs-4 text-right">
-
                 <a class="btn btn-default btn-lg btn-upper" href="<?php echo url_for('declaration_etablissement', $compte->getEtablissementObj()); ?>">Espace etablissement&nbsp;&nbsp;<span class="eleganticon arrow_carrot-right"></span></a>
             </div>
         <?php endif; ?>
