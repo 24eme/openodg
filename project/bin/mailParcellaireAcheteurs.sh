85bc9814738e2aa8193244696c8d7695a6732b0b
Vincent LAURENT <vince.laurent@gmail.com>
2017-08-16 18:22:42 +0200

    Ajout des déclaration d'intention dans la tâche d'envoi de mail des déclarations

--- project/bin/mailParcellaireAcheteurs.sh
+++ project/bin/mailParcellaireAcheteurs.sh
@@ -9,7 +9,7 @@ if ! test "$CAMPAGNE"; then
     exit;
 fi
 
-curl -s http://$COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/_design/declaration/_view/tous?reduce=false | grep -E "PARCELLAIRE[A-Z]*-[0-9]+-$CAMPAGNE" | cut -d "," -f 1 | sed 's/{"id":"//' | sed 's/"//' | sort | uniq | while read id
+curl -s http://$COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/_design/declaration/_view/tous?reduce=false | grep -E "(INTENTIONCREMANT|PARCELLAIRE[A-Z]*)-[0-9]+-$CAMPAGNE" | cut -d "," -f 1 | sed 's/{"id":"//' | sed 's/"//' | sort | uniq | while read id
 do
     php symfony parcellaire:send-mail-acheteurs $id --trace
 done
