824b637d5c95bed997a20dcb3dd5fad661d8b5ac
Jb Lm <lemetayer.jb@gmail.com>
2017-09-04 15:23:17 +0200

    gestion de l'import des fichiers

--- project/lib/task/import/ImportFichierTask.class.php
+++ project/lib/task/import/ImportFichierTask.class.php
@@ -7,7 +7,7 @@ class ImportFichierTask extends sfBaseTask
     {
         $this->addArguments(array(
             new sfCommandArgument('identifiant', sfCommandArgument::REQUIRED, "Identifiant etablissment"),
-        	new sfCommandArgument('fichiers', sfCommandArgument::REQUIRED, "Chemin des fichiers, séparateur : |"),
+        	new sfCommandArgument('fichier', sfCommandArgument::REQUIRED, "Chemin du fichier"),
         ));
 
         $this->addOptions(array(
@@ -19,11 +19,14 @@ class ImportFichierTask extends sfBaseTask
         	new sfCommandOption('visibilite', null, sfCommandOption::PARAMETER_OPTIONAL, 'Libelle fichier', 1),
         	new sfCommandOption('papier', null, sfCommandOption::PARAMETER_OPTIONAL, 'Libelle fichier', false),
         	new sfCommandOption('date_depot', null, sfCommandOption::PARAMETER_OPTIONAL, 'Libelle fichier', null),
+        	new sfCommandOption('type', null, sfCommandOption::PARAMETER_OPTIONAL, 'Libelle fichier', null),
+        	new sfCommandOption('annee', null, sfCommandOption::PARAMETER_OPTIONAL, 'Libelle fichier', null),
+        	new sfCommandOption('lien_symbolique', null, sfCommandOption::PARAMETER_OPTIONAL, 'Libelle fichier', false),
         ));
 
         $this->namespace = 'import';
         $this->name = 'fichier';
-        $this->briefDescription = "Importe des fichiers";
+        $this->briefDescription = "Importe de fichier";
         $this->detailedDescription = <<<EOF
 EOF;
     }
@@ -33,14 +36,12 @@ EOF;
         $databaseManager = new sfDatabaseManager($this->configuration);
         $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
         
-        $files = explode('|', $arguments['fichiers']);
+        $file = $arguments['fichier'];
         
-        foreach ($files as $file) {
-	        if (!file_exists($file) || !is_file($file)) {
-	        	echo sprintf("ERROR;Fichier introuvable ou non valide %s\n", $file);
-	        	return;
-	        }
-        }
+	   	if (!file_exists($file) || !is_file($file)) {
+	    	echo sprintf("ERROR;Fichier introuvable ou non valide %s\n", $file);
+	        return;
+	    }
         
         $etablissement = EtablissementClient::getInstance()->findByIdentifiant($arguments['identifiant']);
         if (!$etablissement) {
@@ -55,23 +56,45 @@ EOF;
         	echo sprintf("ERROR;Format date (Y-m-d) non valide %s\n", $options['date_depot']);
         	return;
         }
-        
-        $fichier = FichierClient::getInstance()->createDoc($arguments['identifiant'], $options['papier']);
-        if ($options['libelle']) {
+
+        if ($options['lien_symbolique']) {
+        	if (!$options['type'] || !$options['annee']) {
+        		echo sprintf("ERROR;Type et année obligatoire pour la création du lien symbolique\n");
+        		return;
+        	}
+        	if (!preg_match('/^[0-9]{4}$/', $options['annee'])) {
+        		echo sprintf("ERROR;Format année (Y) non valide %s\n", $options['annee']);
+        		return;
+        	}
+        	$ls = LienSymboliqueClient::getInstance()->findByArgs($options['type'], $etablissement->identifiant,  $options['annee']);
+        	$fichier = ($ls)? $ls->getFichierObject() : null; 
+        	if (!$fichier) {
+        		$fichier = FichierClient::getInstance()->createDoc($etablissement->identifiant, $options['papier']);
+        	}
+        	
+        } else {
+        	$fichier = FichierClient::getInstance()->createDoc($etablissement->identifiant, $options['papier']);
+        }
+        if ($fichier->isNew() && $options['libelle']) {
         	$fichier->setLibelle($options['libelle']);
         }
-        if ($options['date_depot']) {
+        if ($fichier->isNew() && $options['date_depot']) {
         	$fichier->setDateDepot($options['date_depot']);
         }
-        if ($options['visibilite']) {
+        if ($fichier->isNew() && $options['visibilite']) {
         	$fichier->setVisibilite($options['visibilite']);
         }
         try {
-        	$fichier->save();
-        	foreach ($files as $file) {
-        		$fichier->storeFichier($file);
+        	if ($fichier->isNew()) {
+        		$fichier->save();
         	}
+        	$fichier->storeFichier($file);
         	$fichier->save();
+        	if ($options['lien_symbolique'] && !$ls) {
+        		$lienSymbolique = new LienSymbolique($options['type'], $etablissement->identifiant,  $options['annee']);
+        		$lienSymbolique->fichier = $fichier->_id;
+        		$lienSymbolique->save();
+        	}
         } catch (Exception $e) {
         	echo sprintf("ERROR;%s\n",$e->getMessage());
         	return;
---
+++ project/plugins/FichierPlugin/lib/model/Fichier/LienSymbolique.class.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Model for LienSymbolique
+ *
+ */
+
+class LienSymbolique extends BaseLienSymbolique {
+	
+	protected $typeLien;
+	protected $identifiant;
+	protected $annee;
+	
+    public function __construct($type, $identifiant, $annee) {
+    	$this->typeLien = $type;
+    	$this->identifiant = $identifiant;
+    	$this->annee = $annee;
+        parent::__construct();
+    }
+    public function constructId() {
+        $this->set('_id', $this->typeLien.'-' . $this->identifiant . '-' . $this->annee);
+    }
+    public function getFichierObject() {
+    	if ($this-fichier) {
+    		return FichierClient::getInstance()->find($this->fichier);
+    	}
+    	return null;
+    }
+
+}
