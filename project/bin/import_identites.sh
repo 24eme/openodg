782ee5fccc113036f0219de7dc24bcd6f023e5b4
Mathurin Petit <mathurin.petit@gmail.com>
2017-09-18 14:49:09 +0200

    maj import soc

--- project/bin/import_identites.sh
+++ project/bin/import_identites.sh
@@ -13,38 +13,45 @@ SYMFODIR=$(pwd);
 
 LOGDATE=$SYMFODIR/$(date +%Y%m%d%H%M%S_import_data.log)
 
+#
+# scp $1 $TMPDIR/ODGRHONE_IDENTITES_DATA.xml.gz
+#
+# echo "Dézippage";
+# rm -rf $TMPDIR/ODGRHONE_IDENTITES_DATA 2>/dev/null
+# mkdir $TMPDIR/ODGRHONE_IDENTITES_DATA 2> /dev/null
+# mkdir $TMPDIR/ODGRHONE_IDENTITES_DATA/IDENTITES_DATA 2> /dev/null
+# gunzip $TMPDIR/ODGRHONE_IDENTITES_DATA.xml.gz
+# mv /tmp/ODGRHONE_IDENTITES_DATA.xml $TMPDIR/ODGRHONE_IDENTITES_DATA/ODGRHONE_IDENTITES_DATA.xml
+#
 
-scp $1 $TMPDIR/ODGRHONE_IDENTITES_DATA.xml.gz
-
-echo "Dézippage";
-rm -rf $TMPDIR/ODGRHONE_IDENTITES_DATA 2>/dev/null
-mkdir $TMPDIR/ODGRHONE_IDENTITES_DATA 2> /dev/null
-mkdir $TMPDIR/ODGRHONE_IDENTITES_DATA/IDENTITES_DATA 2> /dev/null
-gunzip $TMPDIR/ODGRHONE_IDENTITES_DATA.xml.gz
-mv /tmp/ODGRHONE_IDENTITES_DATA.xml $TMPDIR/ODGRHONE_IDENTITES_DATA/ODGRHONE_IDENTITES_DATA.xml
 
 
-
-
-cat $TMPDIR/ODGRHONE_IDENTITES_DATA/ODGRHONE_IDENTITES_DATA.xml | sed -e 's|<b:Identite_Identite>|\n<b:Identite_Identite>|g' > $TMPDIR/ODGRHONE_IDENTITES_DATA/ODGRHONE_IDENTITES_DATA_N.xml
+# cat $TMPDIR/ODGRHONE_IDENTITES_DATA/ODGRHONE_IDENTITES_DATA.xml | sed -e 's|<b:Identite_Identite>|\n<b:Identite_Identite>|g' > $TMPDIR/ODGRHONE_IDENTITES_DATA/ODGRHONE_IDENTITES_DATA_N.xml
 
 ## ici retirer le head
 ## cat $TMPDIR/ODGRHONE_IDENTITES_DATA/ODGRHONE_IDENTITES_DATA_N.xml | grep -E '^<b:Identite_Identite>' | sed -r 's/(.*)<b:CleIdentite>([0-9]+)<\/b:CleIdentite>(.*)/\2###\1<b:CleIdentite>\2<\/b:CleIdentite>\3/g' > $TMPDIR/ODGRHONE_IDENTITES_DATA/ODGRHONE_IDENTITES_DATA.tmp.xml
 
-cat $TMPDIR/ODGRHONE_IDENTITES_DATA/ODGRHONE_IDENTITES_DATA_N.xml | grep -E '^<b:Identite_Identite>' | sed -r 's/(.*)<b:CleIdentite>([0-9]+)<\/b:CleIdentite>(.*)/\2###\1<b:CleIdentite>\2<\/b:CleIdentite>\3/g' > $TMPDIR/ODGRHONE_IDENTITES_DATA/ODGRHONE_IDENTITES_DATA.tmp.xml
+# cat $TMPDIR/ODGRHONE_IDENTITES_DATA/ODGRHONE_IDENTITES_DATA_N.xml | grep -E '^<b:Identite_Identite>' | sed -r 's/(.*)<b:CleIdentite>([0-9]+)<\/b:CleIdentite>(.*)/\2###\1<b:CleIdentite>\2<\/b:CleIdentite>\3/g' > $TMPDIR/ODGRHONE_IDENTITES_DATA/ODGRHONE_IDENTITES_DATA.tmp.xml
 
 echo "Création des xml entités";
 ###
-### DANS CETTE BOUCLE IL FAUDRA utiliser un identifiant unique ou répértorier les id en doublon!!!
+### DANS CETTE BOUCLE ON CHERCHE LES DEFINITIONS DE GROUPES
 ###
-cat $TMPDIR/ODGRHONE_IDENTITES_DATA/ODGRHONE_IDENTITES_DATA.tmp.xml | while read xml
-do
-  IDFIC=$(echo $xml | sed -r 's/([0-9]+)###(.*)/\1/g')
-  echo $xml | sed -r 's/([0-9]+)###(.*)/\2/g' > $TMPDIR/ODGRHONE_IDENTITES_DATA/IDENTITES_DATA/$IDFIC.xml
-done
+# cat $TMPDIR/ODGRHONE_IDENTITES_DATA/ODGRHONE_IDENTITES_DATA.tmp.xml | grep "<b:Groupes>" | while read xml
+# do
+#   IDFIC=$(echo $xml | sed -r 's/([0-9]+)###(.*)/\1/g')
+#   echo $xml | sed -r 's/([0-9]+)###(.*)/\2/g' > $TMPDIR/ODGRHONE_IDENTITES_DATA/IDENTITES_DATA/g_$IDFIC.xml
+# done
+#
+# cat $TMPDIR/ODGRHONE_IDENTITES_DATA/ODGRHONE_IDENTITES_DATA.tmp.xml | grep -v "<b:Groupes>" | while read xml
+# do
+#   IDFIC=$(echo $xml | sed -r 's/([0-9]+)###(.*)/\1/g')
+#   echo $xml | sed -r 's/([0-9]+)###(.*)/\2/g' > $TMPDIR/ODGRHONE_IDENTITES_DATA/IDENTITES_DATA/s_$IDFIC.xml
+# done
+
 
 echo "Création des entités";
 for path in $TMPDIR/ODGRHONE_IDENTITES_DATA/IDENTITES_DATA/*.xml ; do
-  php symfony import:entite-from-xml $path
+  php symfony import:entite-from-xml --trace $path
 done
 #nom num coptable et cvi + tag
