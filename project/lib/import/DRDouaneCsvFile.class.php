a49a3f78e453e4a053bf55a3244772537bfecf1d
Vincent LAURENT <vince.laurent@gmail.com>
2017-09-25 11:55:02 +0200

    Superficie en ha dans l'import des dr douane

--- project/lib/import/DRDouaneCsvFile.class.php
+++ project/lib/import/DRDouaneCsvFile.class.php
@@ -62,8 +62,8 @@ class DRDouaneCsvFile extends DouaneImportCsvFile {
         		if ($values[0] == 4) {
         			for ($i = 2; $i < count($values); $i++) {
         				if ($values[$i]) {
-        					$exploitant[$i][] = array(sprintf('%04d', $values[0]), preg_replace('/ \(ha\)/i', '', self::cleanStr($values[1])), self::numerizeVal($values[$i]), null, null, null, null);
-        					$bailleur[$i][] = array(sprintf('%04d', $values[0]), preg_replace('/ \(ha\)/i', '', self::cleanStr($values[1])), self::numerizeVal($values[$i]), null, null, null, null);
+        					$exploitant[$i][] = array(sprintf('%02d', $values[0]), preg_replace('/ \(ha\)/i', '', self::cleanStr($values[1])), self::numerizeVal($values[$i], 4), null, null, null, null);
+        					$bailleur[$i][] = array(sprintf('%02d', $values[0]), preg_replace('/ \(ha\)/i', '', self::cleanStr($values[1])), self::numerizeVal($values[$i], 4), null, null, null, null);
         				}
         			}
         			continue;
