aac38d833ebdc2eb1746c98f7c73aa28c98cf969
Tangui Morlier <tangui@tangui.eu.org>
2017-10-30 18:41:55 +0100

    Import des documents pdf précédemment récupérés

---
+++ project/bin/importDocuments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+csv=$1
+repertoire=$2
+
+if ! test "$repertoire"; then
+  echo "ERROR: besoin d'un csv et d'un repertoire en arguments";
+  exit 1;
+fi
+
+cat $csv | sed 's/ *\([0-9][0-9][0-9][0-9]\) *$/;\1/' | sed 's/ *\.pdf//' | sed 's/id=/;/' | awk -F ";" '{print "php symfony import:fichier --application=declaration  --libelle=\""$2"\" --type=\""$6"\" --date_depot=\""$5"\" --visibilite=false --papier=true --annee=\""$7"\" "sprintf("%06d01", $1)" '$repertoire'"$4".pdf"}'
