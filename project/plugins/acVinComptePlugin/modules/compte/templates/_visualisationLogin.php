ec2c359bc002b98f845853df48d1c7ef72b74693
Tangui Morlier <tangui@tangui.eu.org>
2017-10-31 18:58:51 +0100

    Affichage du code de création dans l'interface

---
+++ project/plugins/acVinComptePlugin/modules/compte/templates/_visualisationLogin.php
@@ -0,0 +1,10 @@
+<div class="row">
+<div style="margin-bottom: 5px;" class="col-xs-2  text-muted">Login&nbsp;:</div>
+<div style="margin-bottom: 5px;" class="col-xs-3"><?php echo $compte->getLogin(); ?></div>
+<?php if (preg_match('/{TEXT}(.*)/', $compte->mot_de_passe, $m)) : ?>
+<div style="margin-bottom: 5px;" class="col-xs-3 text-muted">Code de création&nbsp;:</div>
+<div style="margin-bottom: 5px;" class="col-xs-3"><?php echo $m[1]; ?></div>
+<?php else: ?>
+<div style="margin-bottom: 5px;" class="col-xs-6 text-muted">Mot de passe déjà créé</div>
+<?php endif; ?>
+</div>
