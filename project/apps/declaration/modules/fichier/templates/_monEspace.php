eca28570d0fb3b44f48b1ec57ec365b5a4ee6588
Vincent LAURENT <vince.laurent@gmail.com>
2017-11-01 21:19:50 +0100

    Bloc de document en couleur info et à la même hauteur

--- project/apps/declaration/modules/fichier/templates/_monEspace.php
+++ project/apps/declaration/modules/fichier/templates/_monEspace.php
@@ -4,13 +4,13 @@
     <?php return; ?>
 <?php endif; ?>
 <div class="col-sm-6 col-md-4 col-xs-12">
-    <div class="block_declaration panel panel-danger">
+    <div class="block_declaration panel panel-info">
         <div class="panel-heading">
             <h3 class="panel-title">Documents</h3>
         </div>
             <div class="panel-body">
                 <p>Espace de téléversement de document pour le déclarant.</p>
-                <div style="margin-top: 50px;">
+                <div style="margin-top: 50px; margin-bottom: 26px;">
                 	<a class="btn btn-block btn-default" href="<?php echo url_for('upload_fichier', $etablissement) ?>">Ajouter un document</a>
                 </div>
             </div>
