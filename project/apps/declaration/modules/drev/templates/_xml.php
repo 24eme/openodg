7e4febf0fcb63e4d85c1b01d832c95121bdf0cb3
Jb Lm <lemetayer.jb@gmail.com>
2017-11-06 11:15:47 +0100

    XML DRev complété

--- project/apps/declaration/modules/drev/templates/_xml.php
+++ project/apps/declaration/modules/drev/templates/_xml.php
@@ -1,12 +1,12 @@
 <?php echo '<?xml version="1.0" encoding="utf-8" ?>' ?>
 
 <drev xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-	<code_site value="??" />
-	<code_extravitis value="??" />
-	<numero_evv value="??" />
+	<code_site value="SGV" />
+	<code_extravitis value="<?php echo intval(substr($drev->identifiant, 0, -2)); ?>" />
+	<numero_evv value="<?php echo $drev->declarant->cvi; ?>" />
 	<rs value="<?php echo $drev->declarant->raison_sociale; ?>" />
 	<millesime value="<?php echo $drev->campagne; ?>" />
-	<date_saisie value="??" />
+	<date_saisie value="<?php echo $drev->getDateValidation() ?>" />
 	<lignes>
 <?php foreach ($drev->declaration->getProduits() as $produit): ?>
 		<ligne>
@@ -15,7 +15,7 @@
 			<code_syndicat_vin value="<?php echo $produit->getCodeCouleur(); ?>" />
 			<surface value="<?php echo $produit->superficie_revendique; ?>" />
 			<volume value="<?php echo $produit->volume_revendique_total; ?>" />
-			<vsi value="??" />
+			<vsi value="0" />
 		</ligne>
 <?php endforeach; ?>
 	</lignes>
