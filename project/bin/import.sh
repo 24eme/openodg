cfdc4c34e15387b1b4ed35de6943b3938084e74a
Tangui Morlier <tangui@tangui.eu.org>
2017-11-09 13:15:05 +0100

    On passe un param à l'import pour décoréler les contactss des documents

--- project/bin/import.sh
+++ project/bin/import.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
 
+task=$1
+
 cd ..
 git pull
 make clean
 make
 cd -
 . bin/config.inc
+
+if test $task = "CONTACTS" ; then
+
 curl -X POST -d @data/configuration/rhone/config_previous.json -H "content-type: application/json"   http://$COUCHHOST":"$COUCHDBPORT"/"$COUCHBASE
 curl -X POST -d @data/configuration/rhone/config.json -H "content-type: application/json"   http://$COUCHHOST":"$COUCHDBPORT"/"$COUCHBASE
 curl -X POST -d @data/configuration/rhone/current.json -H "content-type: application/json"   http://$COUCHHOST":"$COUCHDBPORT"/"$COUCHBASE
-
 
 php symfony cc
 bash bin/import_identites.sh ~/odgrhone_identite_antsys.xml.gz > /tmp/importEntites_$$.log
 php symfony import:etablissements-cvis  --application="declaration" ~/scrapping/data/cvi.csv > /tmp/importCvis_$$.log
 php symfony import:relations-etablissements --application="declaration" ~/scrapping/data/bailleurs_metayers_dr.csv > /tmp/importMetayers_$$.log
 php symfony import:repreneurs --application="declaration" ~/scrapping/data/reprise_repreneur.csv > /tmp/importRepreneurs_$$.log
+php symfony import:Habilitations --application="declaration" ~/scrapping/data/habilitation.csv > /tmp/habilitation_$$.log
 php symfony import:Commentaires --application="declaration" ~/scrapping/data/commentaires.csv > /tmp/importCommentaires_$$.log
+
+fi
+
+if test $task = "DOCUMENTS" ; then
 
 bash bin/import_fichiers.sh /home/actualys/prodouane_scrapy/documents/ > /tmp/importDouane_$$.log
 
-php symfony import:Chais --application=declaration  ~/scrapping/data/chais.csv > /tmp/importChais_$$.log
-php symfony import:AntsysDRev --application=declaration ~/scrapping/data/drev.csv > /tmp/importDRev_$$.log
-php symfony import:Habilitations --application=declaration ~/scrapping/data/habilitation.csv > /tmp/habilitation_$$.log
+php symfony import:Chais --application="declaration"  ~/scrapping/data/chais.csv > /tmp/importChais_$$.log
+
+php symfony import:AntsysDRev --application="declaration" ~/scrapping/data/drev.csv > /tmp/importDRev_$$.log
 
 php symfony compte:updateTagsFromHabilitations --application="declaration" > /tmp/habilitation_tags_$$.log
 
 bash bin/importDocuments.sh ~/prodouane_scrapy/phantomjs_scrapping/data/documents.csv ~/prodouane_scrapy/phantomjs_scrapping/documents/ > /tmp/documents_$$.log
+
+fi
