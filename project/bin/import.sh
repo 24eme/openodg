1c69157e0cb39d224facb65dd387be828420c226
Mathurin Petit <mathurin.petit@gmail.com>
2017-11-09 13:50:40 +0100

    on remonte les habilitations car elles créent de l'établissement

--- project/bin/import.sh
+++ project/bin/import.sh
@@ -18,10 +18,10 @@ curl -X POST -d @data/configuration/rhone/current.json -H "content-type: applica
 php symfony cc
 bash bin/import_identites.sh ~/odgrhone_identite_antsys.xml.gz > /tmp/importEntites_$$.log
 php symfony import:etablissements-cvis  --application="declaration" ~/scrapping/data/cvi.csv > /tmp/importCvis_$$.log
+php symfony import:Habilitations --application="declaration" ~/scrapping/data/habilitation.csv > /tmp/habilitation_$$.log
 php symfony import:Chais --application="declaration"  ~/scrapping/data/chais.csv > /tmp/importChais_$$.log
 php symfony import:relations-etablissements --application="declaration" ~/scrapping/data/bailleurs_metayers_dr.csv > /tmp/importMetayers_$$.log
 php symfony import:repreneurs --application="declaration" ~/scrapping/data/reprise_repreneur.csv > /tmp/importRepreneurs_$$.log
-php symfony import:Habilitations --application="declaration" ~/scrapping/data/habilitation.csv > /tmp/habilitation_$$.log
 php symfony import:Commentaires --application="declaration" ~/scrapping/data/commentaires.csv > /tmp/importCommentaires_$$.log
 
 fi
