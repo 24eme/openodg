702300b4254e00f905f9f8b2eb6a3cf764872342
Vincent LAURENT <vince.laurent@gmail.com>
2017-11-14 19:30:38 +0100

    Label dans la popup d'ajout

--- project/apps/declaration/modules/habilitation/templates/_popupAjoutForm.php
+++ project/apps/declaration/modules/habilitation/templates/_popupAjoutForm.php
@@ -9,35 +9,38 @@
 					<h4 class="modal-title" id="myModalLabel">Ajouter un produit</h4>
 				</div>
 				<div class="modal-body">
-					<div class="form-group row">
-						<span class="error"><?php echo $form['hashref']->renderError() ?></span>
-						<div class="col-xs-10 col-xs-offset-1">
-						<?php echo $form['hashref']->render(array("data-placeholder" => "Séléctionnez un produit", "class" => "form-control select2 select2-offscreen select2autocomplete", "required" => true)) ?>
+					<div class="row form-group">
+						<div class="col-xs-4 text-right control-label">
+							Produit :
+						</div>
+						<div class="col-xs-6">
+							<span class="error"><?php echo $form['hashref']->renderError() ?></span>
+							<?php echo $form['hashref']->render(array("data-placeholder" => "Séléctionnez un produit", "class" => "form-control select2 select2-offscreen select2autocomplete", "required" => true)) ?>
 						</div>
 					</div>
-					<div class="form-group row">
-						<span class="error"><?php echo $form['activites']->renderError() ?></span>
-						<div class="col-xs-10 col-xs-offset-1">
+					<div class="row form-group">
+						<div class="col-xs-4 text-right control-label">
+							Activité :
+						</div>
+						<div class="col-xs-6">
+							<span class="error"><?php echo $form['activites']->renderError() ?></span>
 							<?php $activitesWidget = $form['activites']; ?>
-							<ul class="list-unstyled" >
 								<?php foreach($activitesWidget->getWidget()->getChoices() as $key => $option): ?>
-									<li>
-										<div class="row">
-											<div class="col-xs-6 text-left">
-												<?php echo HabilitationClient::$activites_libelles[$key]; ?>
-											</div>
-											<div class="col-xs-3 text-left">
-												<input class="acheteur_checkbox" type="checkbox" id="<?php echo $activitesWidget->renderId() ?>_<?php echo $key ?>" name="<?php echo $activitesWidget->renderName() ?>[]" value="<?php echo $key ?>" <?php if(is_array($activitesWidget->getValue()) && in_array($key, $activitesWidget->getValue())): ?>checked="checked"<?php endif; ?> />
-											</div>
-										</div>
-									</li>
+									<div class="checkbox">
+										<label>
+											<input class="acheteur_checkbox" type="checkbox" id="<?php echo $activitesWidget->renderId() ?>_<?php echo $key ?>" name="<?php echo $activitesWidget->renderName() ?>[]" value="<?php echo $key ?>" <?php if(is_array($activitesWidget->getValue()) && in_array($key, $activitesWidget->getValue())): ?>checked="checked"<?php endif; ?> />&nbsp;&nbsp;<?php echo HabilitationClient::$activites_libelles[$key]; ?>
+										</label>
+								  	</div>
 								<?php endforeach; ?>
 							</ul>
 						</div>
 					</div>
-					<div class="form-group row">
-						<span class="error"><?php echo $form['date']->renderError() ?></span>
-						<div class="col-xs-10 col-xs-offset-1">
+					<div class="row form-group">
+						<div class="col-xs-4 text-right control-label">
+							Activités :
+						</div>
+						<div class="col-xs-6">
+							<span class="error"><?php echo $form['date']->renderError() ?></span>
 							<div class="input-group date-picker">
 								<?php echo $form['date']->render(array('placeholder' => "Date", "required" => false ,"class" => "form-control")) ?>
 								<div class="input-group-addon">
@@ -46,10 +49,13 @@
 							</div>
 						</div>
 					</div>
-					<div class="form-group row">
-						<span class="error"><?php echo $form['statut']->renderError() ?></span>
-						<div class="col-xs-10 col-xs-offset-1">
-						<?php echo $form['statut']->render(array("data-placeholder" => "Séléctionnez un statut", "class" => "form-control select2 select2-offscreen select2autocomplete", "required" => true)) ?>
+					<div class="row form-group">
+						<div class="col-xs-4 text-right control-label">
+							Statut :
+						</div>
+						<div class="col-xs-6">
+							<span class="error"><?php echo $form['statut']->renderError() ?></span>
+							<?php echo $form['statut']->render(array("data-placeholder" => "Séléctionnez un statut", "class" => "form-control select2 select2-offscreen select2autocomplete", "required" => true)) ?>
 						</div>
 					</div>
 				</div>
