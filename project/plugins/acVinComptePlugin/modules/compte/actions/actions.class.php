56ff2c8baeeb496c87477a7ad5f4aea8c70c12a8
Mathurin Petit <mathurin.petit@gmail.com>
2017-11-14 18:14:07 +0100

    mis a jour de l'export des contacts avec les groupes et les fonctions

--- project/plugins/acVinComptePlugin/modules/compte/actions/actions.class.php
+++ project/plugins/acVinComptePlugin/modules/compte/actions/actions.class.php
@@ -123,7 +123,16 @@ class compteActions extends sfCredentialActions {
 
     public function executeSearchcsv(sfWebRequest $request) {
       $index = acElasticaManager::getType('COMPTE');
-
+      $this->selected_rawtags = array_unique(array_diff(explode(',', $request->getParameter('tags')), array('')));
+      $this->selected_typetags = array();
+      foreach ($this->selected_rawtags as $t) {
+		      if (preg_match('/^([^:]+):(.+)$/', $t, $m)) {
+    	  		if (!isset($this->selected_typetags[$m[1]])) {
+    	    		$this->selected_typetags[$m[1]] = array();
+    	  		}
+    	  		$this->selected_typetags[$m[1]][] = $m[2];
+    		}
+      }
       $q = $this->initSearch($request);
       $q->setLimit(5000);
       $resset = $index->search($q);
