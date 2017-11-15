184bbd37362f3e62816d3a6d3a13d2f6fc745ca1
Vincent LAURENT <vince.laurent@gmail.com>
2017-11-15 19:23:27 +0100

    Correction bug syntaxe

--- project/bin/updateviews.sh
+++ project/bin/updateviews.sh
@@ -11,4 +11,4 @@ echo $$ > $TMPDIR/$COUCHBASE".updateviews.pid"
 
 curl -s "http://$COUCHHOST:$COUCHDBPORT/$COUCHBASE/_all_docs?startkey=%22_design%2F%22&endkey=%22_design0%22&include_docs=true" | sed -r 's/key.+views//' | cut -d "," -f 1,2 | cut -d ":" -f 2,3 | sed 's|","":{"|/_view/|' | sed 's/"//g' | grep "_design" | sed "s|^|curl -s http://$COUCHHOST:$COUCHDBPORT/$COUCHBASE/|" | bash > /dev/null
 
-rm $TMPDIR/$COUCHBASE".updateviews.pid
+rm $TMPDIR/$COUCHBASE".updateviews.pid"
