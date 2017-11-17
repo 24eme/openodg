300a8ce72ec4030dd5e27e76032b2da1224403ff
Tangui Morlier <tangui@tangui.eu.org>
2017-11-17 10:50:28 +0100

    Pas d'obligation de DR pour les DRev papier

--- project/plugins/acVinDRevPlugin/lib/form/DRevUploadDrForm.class.php
+++ project/plugins/acVinDRevPlugin/lib/form/DRevUploadDrForm.class.php
@@ -8,6 +8,7 @@ class DRevUploadDrForm extends FichierForm
 		$this->setWidget('visibilite', new sfWidgetFormInputHidden());
 		$this->widgetSchema->setLabel('file', 'Fichier');
 		$required = ($this->fichier->getDocumentDefinitionModel() != DRCsvFile::CSV_TYPE_DR)? false : true;
+		$required = ($required) && (!$this->options['papier']);
 		$this->setValidator('file', new sfValidatorFile(array('required' => $required, 'mime_types' => array('application/vnd.ms-office'), 'path' => sfConfig::get('sf_cache_dir')), array('mime_types' => 'Fichier de type xls attendu')));
 	}
 }
