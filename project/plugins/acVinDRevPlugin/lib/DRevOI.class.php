900d81626d89c458dbfacb86ca77ae8fe9615a64
Jb Lm <lemetayer.jb@gmail.com>
2017-11-20 10:44:58 +0100

    envoi OI

---
+++ project/plugins/acVinDRevPlugin/lib/DRevOI.class.php
@@ -0,0 +1,42 @@
+<?php
+class DRevOI
+{
+	public $drev;
+	
+	public function __construct(DRev $drev) {
+		if (!$drev->isValidee()) {
+			throw new sfException('DRev non validée');
+		}
+		$this->drev = $drev;
+	}
+	
+	public function send()
+	{
+		$headers = array(
+			"Content-Type: text/xml;charset=UTF-8",
+			"SOAPAction: http://".sfConfig::get('app_oi_domain_action')."/CreationDrev"
+		);
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, sfConfig::get('app_oi_url_http'));
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $this->getXml());
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		
+		$output = curl_exec($ch);
+		$info = curl_getinfo($ch);
+		if ($output === false || $info['http_code'] != 200) {
+			$result = "No cURL data returned (HTTP Code : ". $info['http_code']. ")";
+			if (curl_error($ch)) {
+				$result .= "\n". curl_error($ch);
+			}
+			throw new sfException($result);
+		}
+		$this->drev->add('envoi_oi', date('c'));
+		$this->drev->save();
+		curl_close($ch);
+	}
+	
+	protected function getXml() {
+		return sfContext::getInstance()->getController()->getAction('drev', 'main')->getPartial('drev/xml', array('drev' => $this->drev));
+	}
+}
