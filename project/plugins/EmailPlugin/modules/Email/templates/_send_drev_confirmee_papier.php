b917c32f3906eba562db4f73e969854e554241fe
sanso13 <33457487+sanso13@users.noreply.github.com>
2017-11-24 15:45:30 +0100

    Update _send_drev_confirmee_papier.php

--- project/plugins/EmailPlugin/modules/Email/templates/_send_drev_confirmee_papier.php
+++ project/plugins/EmailPlugin/modules/Email/templates/_send_drev_confirmee_papier.php
@@ -1,1 +1,12 @@
+Bonjour,
 
+Votre déclaration de Revendication <?php echo $drev->campagne; ?> a bien été réceptionnée et saisie par le syndicat.
+
+Votre déclaration sera transmise à l’organisme de contrôle dans les prochains jours.
+
+Pour toute question, n'hésitez pas à contacter votre syndicat.
+
+Bonne journée.
+
+Le Syndicat Général des Vignerons réunis des Côtes du Rhône
+Gestiondesdonnees@syndicat-cotesdurhone.com
