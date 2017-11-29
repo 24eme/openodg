a243a8759124ca6c82cef556fe148c7934b4badc
Vincent LAURENT <vince.laurent@gmail.com>
2017-11-29 14:04:12 +0100

    Plugin spécifique ava dans projectConfiguration

--- project/apps/ava/config/avaConfiguration.class.php
+++ project/apps/ava/config/avaConfiguration.class.php
@@ -3,24 +3,6 @@
 class avaConfiguration extends sfApplicationConfiguration
 {
 
-    public function setup() {
-        parent::setup();
-
-        $this->enablePlugins('CompteAVAPlugin');
-        $this->enablePlugins('EtablissementAVAPlugin');
-        $this->enablePlugins('acVinDRevAVAPlugin');
-        $this->enablePlugins('acVinConfigurationAVAPlugin');
-
-        $this->enablePlugins('acVinAbonnementPlugin');
-        $this->enablePlugins('acVinTiragePlugin');
-        $this->enablePlugins('acVinDRevMarcPlugin');
-        $this->enablePlugins('acVinTravauxMarcPlugin');
-        $this->enablePlugins('acVinDegustationPlugin');
-        $this->enablePlugins('acVinParcellairePlugin');
-        $this->enablePlugins('acVinTourneePlugin');
-        $this->enablePlugins('acVinConstatsVTSGNPlugin');
-    }
-
     public function configure() {
 
     }
