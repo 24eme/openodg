0804c1e300146d308c39c40b88837280acec2198
Vincent LAURENT <vince.laurent@gmail.com>
2017-12-01 11:48:48 +0100

    Retour chariot dans les mails

--- project/plugins/acVinComptePlugin/modules/compte_teledeclarant/templates/_motDePasseOublieEmail.php
+++ project/plugins/acVinComptePlugin/modules/compte_teledeclarant/templates/_motDePasseOublieEmail.php
@@ -7,6 +7,6 @@ Vous avez oublié votre mot de passe.
 
 Pour le redéfinir merci de cliquer sur le lien suivant : <?php echo $lien ?>
 
+
 Le Syndicat Général des Vignerons réunis des Côtes du Rhône
 Gestiondesdonnees@syndicat-cotesdurhone.com
-
