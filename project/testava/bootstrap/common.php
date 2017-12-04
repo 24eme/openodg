5a25a3ab78a545334d673df11c35f6a3c173e290
Vincent LAURENT <vince.laurent@gmail.com>
2017-12-04 11:33:12 +0100

    Test de l'ava dans un dossier test à part

--- project/testava/bootstrap/common.php
+++ project/testava/bootstrap/common.php
@@ -3,7 +3,7 @@
 require_once(dirname(__FILE__).'/../../config/ProjectConfiguration.class.php');
 require_once dirname(__FILE__).'/../../lib/vendor/symfony/test/bootstrap/unit.php';
 
-$application = 'rhone';
+$application = 'ava';
 
 $configuration = ProjectConfiguration::getApplicationConfiguration($application, 'dev', true);
 
