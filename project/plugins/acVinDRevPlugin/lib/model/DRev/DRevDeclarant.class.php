2480bee48750b3ea4760ef8e6156cdd8b1a5d66b
Jb Lm <lemetayer.jb@gmail.com>
2018-01-04 15:19:59 +0100

    correctif siret drev exploitant

--- project/plugins/acVinDRevPlugin/lib/model/DRev/DRevDeclarant.class.php
+++ project/plugins/acVinDRevPlugin/lib/model/DRev/DRevDeclarant.class.php
@@ -4,10 +4,18 @@ class DRevDeclarant extends BaseDRevDeclarant {
 
     public function getPpm() {
         if(!$this->_get('ppm')) {
-            return $this->getDocument()->getEtablissementObject()->ppm;
+            $this->ppm = $this->getDocument()->getEtablissementObject()->ppm;
         }
 
         return $this->_get('ppm');
+    }
+    
+    public function getSiret() {
+        if(!$this->_get('siret')) {
+            $this->siret = $this->getDocument()->getEtablissementObject()->siret;
+        }
+
+        return $this->_get('siret');
     }
 
 }
