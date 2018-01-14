ee718376952d487dc1a7f1b84b9275111a4a9004
Vincent LAURENT <vince.laurent@gmail.com>
2018-01-14 20:24:05 +0100

    Error 403 ava

---
+++ project/apps/ava/modules/global/templates/error404Success.php
@@ -0,0 +1,21 @@
+<div class="page-header no-border">
+    <h2>La page demandée est introuvable</h2>
+</div>
+
+<div class="row">
+    <div class="col-xs-12">
+        <p>
+        Ce lien n’existe plus ou vous avez saisi une adresse incorrecte.
+        </p>
+    </div>
+</div>
+
+<div class="row row-margin row-button">
+    <div class="col-xs-4">
+        <a href="<?php echo url_for('accueil') ?>" class="btn btn-sm btn-primary btn-upper btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retourner à l'accueil</a>
+        <a style="margin-top: 10px;" href="javascript:history.back()" class="btn btn-default btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retourner à la page précédente</a>
+    </div>
+    <div class="col-xs-4">
+
+    </div>
+</div>
