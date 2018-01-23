1609a4a5998bcdabf9c7ee8569d398d6bc0d3a53
Vincent LAURENT <vince.laurent@gmail.com>
2018-01-23 12:29:38 +0100

    Création de l'application provence et mutualisation de code

---
+++ project/apps/provence/config/error/error.html.php
@@ -0,0 +1,124 @@
+<!doctype html>
+<!-- ####### PLEASE KEEP ####### -->
+<!--[if lte IE 6 ]><html class="no-js ie6 ielt7 ielt8 ielt9" lang="fr"><![endif]-->
+<!--[if IE 7 ]><html class="no-js ie7 ielt8 ielt9" lang="fr"><![endif]-->
+<!--[if IE 8 ]><html class="no-js ie8 ielt9" lang="fr"><![endif]-->
+<!--[if IE 9 ]><html class="no-js ie9" lang="fr"><![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" lang="fr"><!--<![endif]-->
+<!-- ####### PLEASE KEEP ####### -->
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-Ua-Compatible" content="IE=edge" />
+        <meta name="title" content="Plateforme de télédéclaration du syndicat Côtes-du-Rhône" />
+<meta name="charset" content="utf-8" />
+<meta name="description" content="Plateforme de télédéclaration du syndicat Côtes-du-Rhône" />
+<meta name="robots" content="index,follow" />
+<meta name="content-language" content="fr-FR" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Plateforme de télédéclaration du syndicat Côtes-du-Rhône</title>
+
+        <link rel="shortcut icon" type="image/x-icon" href="/favico.ico" />
+        <link rel="icon" type="image/x-icon" href="/favico.ico" />
+        <link rel="icon" type="image/png" href="/favico.png" />
+
+        <link rel="stylesheet" type="text/css" media="all" href="/css/compile.css?201706281900" />
+<link rel="stylesheet" type="text/css" media="print" href="/css/print.css" />
+<link rel="stylesheet" type="text/css" media="all" href="/css/../components/vins/vins.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="/js/lib/leaflet/leaflet.css" />
+
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,600" rel="stylesheet" type="text/css">
+        <link href="/css/style_rhone.css" rel="stylesheet" type="text/css">
+
+        <script type="text/javascript" src="/js/lib/modernizr-2.8.2.js"></script>
+        <script type="text/javascript" src="/js/lib/device.min.js"></script>
+
+        <!--[if lt IE 9]>
+            <script type="text/javascript" src="/js/lib/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body role="document">
+
+        <!-- ####### PLEASE KEEP ####### -->
+        <!--[if lte IE 7 ]>
+        <div id="message_ie">
+            <div class="gabarit">
+                <p><strong>Vous utilisez un navigateur obsolète depuis près de 10 ans !</strong> Il est possible que l'affichage du site soit fortement altéré par l'utilisation de celui-ci.</p>
+            </div>
+        </div>
+        <![endif]-->
+        <!-- ####### PLEASE KEEP ####### -->
+
+
+                        <header id="header">
+    <div class="container" style="position: relative; background-color: #002b6f; text-align: center;">
+    	<a href="/">
+        	<img style="height: 80px; border-left: 2px solid #fff;" class="pull-right" src="/images/logo_odgrhone.png" />
+        	<img style="height: 80px;" class="pull-left" src="/images/bg/bg_rhone_site.jpg" />
+        	<span style="color: #fff; font-size: 20px; padding-right: 130px; padding-top: 7px; display: inline-block;"><span style="color: #fff; font-size: 17px;">Espace professionnel<br /></span><span style="color: #fff">syndicat-cotesdurhone<strong style="color: #9f0038;">.</strong><span style="color: #9b9b9b">com</span></span></span>
+    	</a>
+    </div>
+</header>
+
+
+ <section id="content" class="container">
+ 
+
+                <div class="page-header no-border">
+
+                <h2><span class="glyphicon glyphicon-warning-sign"></span>&nbsp;&nbsp;Une erreur est survenue</h2>
+            </div>
+
+            <div class="row">
+                <div class="col-xs-12">
+                    <p>
+                    Nous sommes désolés et vous prions de nous contacter afin de nous signaler et détailler le probleme.
+                    </p>
+                    <p>
+                    Nous tâcherons alors de le résoudre dans les plus brefs délais. 
+                    </p>
+                </div>
+            </div>
+
+            <div class="row row-margin row-button">
+                <div class="col-xs-4">
+                    <a href="/" class="btn btn-primary btn-sm btn-upper btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retourner à mes déclarations</a>
+                    <a style="margin-top: 10px;" href="javascript:history.back()" class="btn btn-default btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retourner à la page précédente</a>
+                </div>
+                <div class="col-xs-4">
+                    
+                </div>
+            </div>
+ 
+ </section>
+
+
+
+            <footer id="footer" class="container hidden-xs hidden-sm text-center" role="contentinfo" style="margin-top: 40px;">
+                <nav role="navigation">
+                    <ul class="list-inline" style="font-size: 13px;">
+                        <li><a href="/contact">Contact</a></li>
+                        <li><a href="/mentions-legales">Mentions légales</a></li>
+                    </ul>
+                </nav>
+            </footer>
+            <!-- end #footer -->
+
+        <!-- end #page -->
+
+        <div class="alert hidden alert-danger notification" id="ajax_form_error_notification">Une erreur est survenue</div>
+        <div class="alert hidden alert-success notification" id="ajax_form_progress_notification">Enregistrement en cours ...</div>
+        <script type="text/javascript" src="/js/lib/jquery/jquery.min.js"></script>
+<script type="text/javascript" src="/js/lib/jquery.plugins.min.js?201709221633"></script>
+<script type="text/javascript" src="/js/lib/bootstrap/bootstrap.min.js"></script>
+<script type="text/javascript" src="/js/lib/bootstrap3-typeahead.min.js"></script>
+<script type="text/javascript" src="/js/lib/bootstrap-switch.min.js"></script>
+<script type="text/javascript" src="/js/lib/leaflet/leaflet.js"></script>
+<script type="text/javascript" src="/js/global.js?201711052339"></script>
+<script type="text/javascript" src="/js/compte.js"></script>
+<script type="text/javascript" src="/js/drev.js?201709251013"></script>
+<script type="text/javascript" src="/js/drevmarc.js"></script>
+<script type="text/javascript" src="/js/form.js"></script>
+<script type="text/javascript" src="/js/parcellaire.js"></script>
+<script type="text/javascript" src="/js/ava.js"></script>
+    </body>
+</html>
---
+++ project/apps/provence/config/provenceConfiguration.class.php
@@ -0,0 +1,18 @@
+<?php
+
+class provenceConfiguration extends sfApplicationConfiguration
+{
+
+    public function configure()
+    {
+        $configCache = $this->getConfigCache();
+        $configCache->registerConfigHandler('config/points_aides.yml', 'sfDefineEnvironmentConfigHandler');
+        $configCache->checkConfig('config/points_aides.yml');
+
+    }
+
+    public function initialize()
+    {
+        include($this->getConfigCache()->checkConfig('config/points_aides.yml'));
+    }
+}
