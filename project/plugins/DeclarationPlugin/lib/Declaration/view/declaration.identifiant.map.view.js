0cb4237e20f834f5626fa4f11bf63cbbe1785a18
Vincent LAURENT <vince.laurent@gmail.com>
2018-01-30 03:09:19 +0100

    Suppression d'une ancienne fusion

--- project/plugins/DeclarationPlugin/lib/Declaration/view/declaration.identifiant.map.view.js
+++ project/plugins/DeclarationPlugin/lib/Declaration/view/declaration.identifiant.map.view.js
@@ -1,10 +1,6 @@
 function(doc) {
 
-<<<<<<< HEAD
-    if(doc.type != "DRev" && doc.type != "DRevMarc" && doc.type != "Parcellaire" && doc.type != "Tirage") {
-=======
     if(doc.type != "DRev" && doc.type != "DRevMarc" && doc.type != "Parcellaire" && doc.type != "Tirage" && doc.type != "TravauxMarc") {
->>>>>>> ava_master
 
         return;
     }
