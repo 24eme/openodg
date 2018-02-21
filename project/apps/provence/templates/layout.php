3bf4e62d155dab963f1d7e022c74288ec496c66a
Vincent LAURENT <vince.laurent@gmail.com>
2018-02-21 16:36:58 +0100

    Design provence

--- project/apps/provence/templates/layout.php
+++ project/apps/provence/templates/layout.php
@@ -39,32 +39,31 @@
         <![endif]-->
         <!-- ####### PLEASE KEEP ####### -->
 
-            <?php echo include_partial('global/header'); ?>
+            <div id="header">
+                <?php echo include_partial('global/header'); ?>
 
-            <?php include_partial('global/nav'); ?>
+                <?php include_partial('global/nav'); ?>
+            </div>
 
-            <section id="content" class="container">
-                <?php if(sfConfig::get('app_instance') == 'preprod' ): ?>
-                  <div><p style="color:red; text-align:center; font-weight: bold;">Preproduction (la base est succeptible d'être supprimée à tout moment)</p></div>
-                <?php endif; ?>
+                <section id="content" class="container">
+                        <?php if(sfConfig::get('app_instance') == 'preprod' ): ?>
+                          <div><p style="color:red; text-align:center; font-weight: bold;">Preproduction (la base est succeptible d'être supprimée à tout moment)</p></div>
+                        <?php endif; ?>
 
-                <?php echo $sf_content ?>
-            </section>
+                        <?php echo $sf_content ?>
+                </section>
 
-            <footer id="footer" class="container hidden-xs hidden-sm text-center" role="contentinfo" style="margin-top: 40px;">
-                <nav role="navigation">
-                    <ul class="list-inline" style="font-size: 13px;">
-                        <li><a href="<?php echo url_for('contact') ?>">Contact</a></li>
-                        <li><a href="<?php echo url_for('mentions_legales') ?>">Mentions légales</a></li>
-                    </ul>
-                </nav>
-            </footer>
-            <!-- end #footer -->
+                <footer id="footer" class="container hidden-xs hidden-sm text-center" role="contentinfo">
+                    <nav role="navigation">
+                        <ul class="list-inline" style="font-size: 13px;">
+                            <li><a href="<?php echo url_for('contact') ?>">Contact</a></li>
+                            <li><a href="<?php echo url_for('mentions_legales') ?>">Mentions légales</a></li>
+                        </ul>
+                    </nav>
+                </footer>
 
-        <!-- end #page -->
-
-        <div class="alert alert-danger notification" id="ajax_form_error_notification">Une erreur est survenue</div>
-        <div class="alert alert-success notification" id="ajax_form_progress_notification">Enregistrement en cours ...</div>
-        <?php include_javascripts() ?>
+            <div class="alert alert-danger notification" id="ajax_form_error_notification">Une erreur est survenue</div>
+            <div class="alert alert-success notification" id="ajax_form_progress_notification">Enregistrement en cours ...</div>
+            <?php include_javascripts() ?>
     </body>
 </html>
