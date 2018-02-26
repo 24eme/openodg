3ca31d80251debf1ffaaa2ab77dd00df43965414
Vincent LAURENT <vince.laurent@gmail.com>
2018-02-26 15:04:45 +0100

    Menu alignement à gauche dans la visu société

--- project/plugins/acVinSocietePlugin/modules/societe/templates/visualisationSuccess.php
+++ project/plugins/acVinSocietePlugin/modules/societe/templates/visualisationSuccess.php
@@ -14,8 +14,8 @@
                     </div>
                     <div class="col-xs-3 text-muted text-right">
                         <div class="btn-group">
-                            <a class="btn dropdown-toggle " data-toggle="dropdown" href="#">Modifier <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
+                            <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">Modifier <span class="caret"></span></a>
+                            <ul class="dropdown-menu text-left">
                                 <li<?php echo ($societe->isSuspendu()) ? ' class="disabled"' : ''; ?>><a href="<?php echo ($societe->isSuspendu()) ? 'javascript:void(0)' : url_for('societe_modification', $societe); ?>">Editer</a></li>
                                 <li<?php echo ($societe->isSuspendu())? ' class="disabled"' : ''; ?>><a href="<?php echo ($societe->isSuspendu())? 'javascript:void(0)' : url_for('societe_switch_statut', array('identifiant' => $societe->identifiant)); ?>">Archiver</a></li>
                                 <li<?php echo ($societe->isActif()   )? ' class="disabled"' : ''; ?>><a href="<?php echo ($societe->isActif())? 'javascript:void(0)' : url_for('societe_switch_statut', array('identifiant' => $societe->identifiant)); ?>">Activer</a></li>
