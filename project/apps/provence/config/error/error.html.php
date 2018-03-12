f43c3c7b38a17efe690b32517ed7a5b1ccef5b7f
Tangui Morlier <tangui@tangui.eu.org>
2018-03-12 15:05:38 +0100

    Erreur 500

--- project/apps/provence/config/error/error.html.php
+++ project/apps/provence/config/error/error.html.php
@@ -9,25 +9,25 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta http-equiv="X-Ua-Compatible" content="IE=edge" />
-        <meta name="title" content="Plateforme de télédéclaration du syndicat Côtes-du-Rhône" />
+        <meta name="title" content="DECLARATION - GAEC TERRISSE" />
 <meta name="charset" content="utf-8" />
-<meta name="description" content="Plateforme de télédéclaration du syndicat Côtes-du-Rhône" />
+<meta name="description" content="Plateforme de télédéclaration du syndicat Côtes de Provence" />
 <meta name="robots" content="index,follow" />
 <meta name="content-language" content="fr-FR" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Plateforme de télédéclaration du syndicat Côtes-du-Rhône</title>
+        <title>DECLARATION - GAEC TERRISSE</title>
 
-        <link rel="shortcut icon" type="image/x-icon" href="/favico.ico" />
-        <link rel="icon" type="image/x-icon" href="/favico.ico" />
-        <link rel="icon" type="image/png" href="/favico.png" />
+        <link rel="shortcut icon" type="image/x-icon" href="/favico_provence.ico" />
+        <link rel="icon" type="image/x-icon" href="/favico_provence.ico" />
+        <link rel="icon" type="image/png" href="/favico_provence.png" />
 
-        <link rel="stylesheet" type="text/css" media="all" href="/css/compile.css?201706281900" />
+        <link rel="stylesheet" type="text/css" media="all" href="/css/compile_provence.css" />
 <link rel="stylesheet" type="text/css" media="print" href="/css/print.css" />
 <link rel="stylesheet" type="text/css" media="all" href="/css/../components/vins/vins.css" />
 <link rel="stylesheet" type="text/css" media="screen" href="/js/lib/leaflet/leaflet.css" />
 
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,600" rel="stylesheet" type="text/css">
-        <link href="/css/style_rhone.css" rel="stylesheet" type="text/css">
+        <link href="/css/style_provence.css" rel="stylesheet" type="text/css">
 
         <script type="text/javascript" src="/js/lib/modernizr-2.8.2.js"></script>
         <script type="text/javascript" src="/js/lib/device.min.js"></script>
@@ -48,77 +48,72 @@
         <![endif]-->
         <!-- ####### PLEASE KEEP ####### -->
 
-
-                        <header id="header">
-    <div class="container" style="position: relative; background-color: #002b6f; text-align: center;">
-    	<a href="/">
-        	<img style="height: 80px; border-left: 2px solid #fff;" class="pull-right" src="/images/logo_odgrhone.png" />
-        	<img style="height: 80px;" class="pull-left" src="/images/bg/bg_rhone_site.jpg" />
-        	<span style="color: #fff; font-size: 20px; padding-right: 130px; padding-top: 7px; display: inline-block;"><span style="color: #fff; font-size: 17px;">Espace professionnel<br /></span><span style="color: #fff">syndicat-cotesdurhone<strong style="color: #9f0038;">.</strong><span style="color: #9b9b9b">com</span></span></span>
-    	</a>
-    </div>
+            <div id="header">
+                <header class="container" role="banner">
+      <div id="logo">
+          <a href="http://www.syndicat-cotesdeprovence.com/" title="Syndicat des Cotes de Provence | Retour à la page d'accueil">
+              <img src="/images/logo_provence.png" alt="Syndicat des Cotes de Provence">
+          </a>
+      </div>
+      <div id="titre"><h1>
+        <img height="35" width="207" src="/images/provence_espace_personnel.png" alt="Espace personnel"/><img height="35" width="23" src="/images/provence_tiret.png" alt=" - "/><img height="35" width="435" src="/images/provence_syndicat_des_cotes_de_provence.png" alt="Syndicat des Côtes de Provence"/>
+      </h1></div>
 </header>
 
 
- <section id="content" class="container">
- 
 
-                <div class="page-header no-border">
 
-                <h2><span class="glyphicon glyphicon-warning-sign"></span>&nbsp;&nbsp;Une erreur est survenue</h2>
+
+<nav id="menu_navigation" class="navbar navbar-default container">
+        <div class="navbar-header hidden-lg hidden-md">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/"></a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" style="padding-left: 0;">
+                        <ul class="nav navbar-nav mode-operateur" style="border: 0;">
+                <li id="nav_item_operateur" class=""><a onclick="document.location = $(this).parents('ul.mode-operateur').find('li.active a').attr('href');" href="#"><span class="glyphicon glyphicon-user"></span></a></li>
+                <li class="active"><a href="/declaration">Déclarations</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a tabindex="-1" href="/logout" title="Déconnexion"><span class="glyphicon glyphicon-log-out"></span></a></li>
+           </ul>
+        </div>
+</nav>
             </div>
 
-            <div class="row">
-                <div class="col-xs-12">
-                    <p>
-                    Nous sommes désolés et vous prions de nous contacter afin de nous signaler et détailler le probleme.
-                    </p>
-                    <p>
-                    Nous tâcherons alors de le résoudre dans les plus brefs délais. 
-                    </p>
-                </div>
-            </div>
+                <section id="content" class="container">
+                  <h2>Une erreur est survenue</h2>
+                  <p>Nous sommes désolé pour la gène occasionnée.</p>
+                </section>
 
-            <div class="row row-margin row-button">
-                <div class="col-xs-4">
-                    <a href="/" class="btn btn-primary btn-sm btn-upper btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retourner à mes déclarations</a>
-                    <a style="margin-top: 10px;" href="javascript:history.back()" class="btn btn-default btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retourner à la page précédente</a>
-                </div>
-                <div class="col-xs-4">
-                    
-                </div>
-            </div>
- 
- </section>
+               <footer id="footer" class="container hidden-xs hidden-sm text-center" role="contentinfo">
+                   <nav role="navigation">
+                       <ul class="list-inline" style="font-size: 13px;">
+                           <li><a href="/contact">Contact</a></li>
+                           <li><a href="/mentions-legales">Mentions légales</a></li>
+                       </ul>
+                   </nav>
+               </footer>
 
-
-
-            <footer id="footer" class="container hidden-xs hidden-sm text-center" role="contentinfo" style="margin-top: 40px;">
-                <nav role="navigation">
-                    <ul class="list-inline" style="font-size: 13px;">
-                        <li><a href="/contact">Contact</a></li>
-                        <li><a href="/mentions-legales">Mentions légales</a></li>
-                    </ul>
-                </nav>
-            </footer>
-            <!-- end #footer -->
-
-        <!-- end #page -->
-
-        <div class="alert hidden alert-danger notification" id="ajax_form_error_notification">Une erreur est survenue</div>
-        <div class="alert hidden alert-success notification" id="ajax_form_progress_notification">Enregistrement en cours ...</div>
-        <script type="text/javascript" src="/js/lib/jquery/jquery.min.js"></script>
+           <div class="alert alert-danger notification" id="ajax_form_error_notification">Une erreur est survenue</div>
+           <div class="alert alert-success notification" id="ajax_form_progress_notification">Enregistrement en cours ...</div>
+           <script type="text/javascript" src="/js/lib/jquery/jquery.min.js"></script>
 <script type="text/javascript" src="/js/lib/jquery.plugins.min.js?201709221633"></script>
 <script type="text/javascript" src="/js/lib/bootstrap/bootstrap.min.js"></script>
 <script type="text/javascript" src="/js/lib/bootstrap3-typeahead.min.js"></script>
 <script type="text/javascript" src="/js/lib/bootstrap-switch.min.js"></script>
 <script type="text/javascript" src="/js/lib/leaflet/leaflet.js"></script>
-<script type="text/javascript" src="/js/global.js?201711052339"></script>
+<script type="text/javascript" src="/js/global.js?201802221732"></script>
 <script type="text/javascript" src="/js/compte.js"></script>
 <script type="text/javascript" src="/js/drev.js?201709251013"></script>
 <script type="text/javascript" src="/js/drevmarc.js"></script>
-<script type="text/javascript" src="/js/form.js"></script>
+<script type="text/javascript" src="/js/form.js?201801171851"></script>
 <script type="text/javascript" src="/js/parcellaire.js"></script>
 <script type="text/javascript" src="/js/ava.js"></script>
-    </body>
+   </body>
 </html>
