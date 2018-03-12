c040487bd66bcc4cd2ebbedc0bf677aae9b0199d
Vincent LAURENT <vince.laurent@gmail.com>
2018-03-12 15:36:24 +0100

    Bouton enregistrer en brouillon

--- project/plugins/acVinParcellaireIrrigablePlugin/modules/parcellaireIrrigable/templates/parcellesSuccess.php
+++ project/plugins/acVinParcellaireIrrigablePlugin/modules/parcellaireIrrigable/templates/parcellesSuccess.php
@@ -45,7 +45,10 @@
 <?php  endforeach; ?>
 
 	<div class="row row-margin row-button">
-        <div class="col-xs-6"><a href="<?php echo url_for("parcellaireirrigable_exploitation", $parcellaireIrrigable); ?>" class="btn btn-default btn-upper"><span class="glyphicon glyphicon-chevron-left"></span> Retourner à l'étape précédente</a></div>
-        <div class="col-xs-6 text-right"><button type="submit" class="btn btn-primary btn-upper">Valider et continuer <span class="glyphicon glyphicon-chevron-right"></span></button></div>
+        <div class="col-xs-4"><a href="<?php echo url_for("parcellaireirrigable_exploitation", $parcellaireIrrigable); ?>" class="btn btn-default btn-upper"><span class="glyphicon glyphicon-chevron-left"></span> Retourner à l'étape précédente</a></div>
+        <div class="col-xs-4 text-center">
+            <button type="submit" name="saveandquit" value="1" class="btn btn-default">Enregistrer en brouillon</button>
+        </div>
+        <div class="col-xs-4 text-right"><button type="submit" class="btn btn-primary btn-upper">Valider et continuer <span class="glyphicon glyphicon-chevron-right"></span></button></div>
     </div>
 </form>
