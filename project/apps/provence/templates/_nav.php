da7d4231ac6a59017cedb1fcc156773f641c1149
Jb Lm <lemetayer.jb@gmail.com>
2018-03-13 15:41:23 +0100

    navigation onglet mon compte

--- project/apps/provence/templates/_nav.php
+++ project/apps/provence/templates/_nav.php
@@ -44,6 +44,7 @@
             <?php elseif($sf_user->isAuthenticated()): ?>
                 <ul class="nav navbar-nav <?php if($compte): ?>mode-operateur<?php endif; ?>" style="border: 0;">
                     <li class="<?php if($route instanceof InterfaceDeclarationRoute): ?>active<?php endif; ?>"><a href="<?php echo url_for('declaration_etablissement', $etablissement); ?>">Déclarations</a></li>
+                	<li class="<?php if(preg_match('/compte/', $route->getParameters()['module'])): ?>active<?php endif; ?>"><a tabindex="-1" href="<?php echo url_for("compte_teledeclarant_modification") ?>" title="Mon compte">Mon compte</a></li>
                 </ul>
             <?php endif; ?>
             <ul class="nav navbar-nav navbar-right">
