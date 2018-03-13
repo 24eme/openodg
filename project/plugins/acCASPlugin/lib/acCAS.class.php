d4121b90b994980c27e7b8661a8a9367d4433090
Tangui Morlier <tangui@tangui.eu.org>
2018-03-13 22:44:30 +0100

    Meilleure gestion du CAS

--- project/plugins/acCASPlugin/lib/acCAS.class.php
+++ project/plugins/acCASPlugin/lib/acCAS.class.php
@@ -4,11 +4,25 @@ require_once(dirname(__FILE__).'/vendor/phpCAS/CAS.php');
 
 class acCAS extends phpCAS {
 
+    public static function getConfig($str) {
+      if (isset($_SESSION[$str])) {
+        return ($_SESSION[$str]);
+      }
+      return sfConfig::get($str);
+    }
+
+    private static function initCasInfo() {
+      if (isset($_SESSION['app_cas_domain'])) {
+        return ;
+      }
+      $_SESSION['app_cas_domain'] = sfConfig::get('app_cas_domain');
+      $_SESSION['app_cas_port'] = sfConfig::get('app_cas_port');
+      $_SESSION['app_cas_path'] = sfConfig::get('app_cas_path');
+      $_SESSION['app_cas_url'] = sfConfig::get('app_cas_url');
+    }
+
     public static function processAuth() {
-        $_SESSION['app_cas_domain'] = sfConfig::get('app_cas_domain');
-        $_SESSION['app_cas_port'] = sfConfig::get('app_cas_port');
-        $_SESSION['app_cas_path'] = sfConfig::get('app_cas_path');
-        $_SESSION['app_cas_url'] = sfConfig::get('app_cas_url');
+        self::initCasInfo();
         $multidomains = sfConfig::get('app_cas_multidomains', array());
         if (isset($_GET['ticket']) && count($multidomains) && ($postfix = preg_replace('/.*-/', '', $_GET['ticket']))) {
           $_SESSION['app_cas_domain'] = $multidomains[$postfix]['domain'];
@@ -23,6 +37,7 @@ class acCAS extends phpCAS {
     }
 
     public static function processLogout($url) {
+        self::initCasInfo();
         @phpCAS::client(CAS_VERSION_2_0, $_SESSION['app_cas_domain'], $_SESSION['app_cas_port'], $_SESSION['app_cas_path'], false);
         if (@phpCas::isAuthenticated()) {
             @phpCAS::logoutWithRedirectService($url);
