491ecf1663fb069687d264fa00a310383d1c968b
Tangui Morlier <tangui@tangui.eu.org>
2018-03-13 23:02:35 +0100

    Construction de la vue login + correction erreur de copier/coller

--- project/plugins/acVinComptePlugin/lib/model/views/compte.login.map.view.js
+++ project/plugins/acVinComptePlugin/lib/model/views/compte.login.map.view.js
@@ -1,10 +1,11 @@
 function(doc) {
    if (doc.type && doc.type != "Compte") {
- 	return ;
+ 	   return ;
    }
-   for (type in doc.tags) {
-     for(idtag in doc.tags[type]) {
- 	    emit([type, doc.tags[type][idtag]], doc.origines);
-     }
+   emit([doc.identifiant], doc._id);
+   if (doc.alternative_logins) {
+      for (login in doc.alternative_logins) {
+	        emit([login], doc._id);
+      }
    }
  }
