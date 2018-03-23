7bc9f0a640adddcf18cc1f15847c2d22eb4b6c56
Mathurin Petit <mathurin.petit@gmail.com>
2018-03-23 15:58:09 +0100

    mis à jour de la tache servant à la migration

--- project/plugins/acVinSocietePlugin/lib/task/societeMigateAllComptesTask.class.php
+++ project/plugins/acVinSocietePlugin/lib/task/societeMigateAllComptesTask.class.php
@@ -4,8 +4,9 @@ class societeMigateAllComptesTask extends migrateCompteTask
 {
   protected $verbose = null;
   protected $withSave = null;
-  protected $contacts = array();
-  protected $oldContacts = array();
+  protected $comptePrincipal = null;
+  protected $comptesEtablissement = array();
+  protected $comptesInterlocuteurs = array();
 
   protected function configure()
   {
@@ -46,137 +47,131 @@ EOF;
       throw new sfException("Societe non trouvée : ".$arguments['societe_id']);
     }
     $compteSociete = $societe->getMasterCompte();
-    if(!$this->withSave && !$this->verbose){
-      echo "$societe->_id / $compteSociete->_id analyse...\n";
+
+    $this->comptePrincipal = $this->getDocWithoutIdsInfos($compteSociete->toJson());
+    foreach ($societe->getEtablissementsObj() as $idEtb => $etbStruct) {
+        $this->comptesEtablissement[$idEtb] = $this->getDocWithoutIdsInfos($etbStruct->etablissement->getMasterCompte()->toJson());
     }
-    foreach ($compteSociete->toJson() as $key => $value) {
 
-      if($key == '_id' && str_replace("COMPTE-","",$value) != str_replace("SOCIETE-","",$societe->_id)."01"){
-        throw new sfException("La societe et son compte principal ont des ids étranges : ".$value." ".$societe->_id);
-      }
-      if($key == 'identifiant' && $value != str_replace("SOCIETE-","",$societe->_id)."01"){
-        throw new sfException("La societe et son compte principal ont des ids étranges : ".$value." ".$societe->_id);
-      }
-      if($key == 'origines'){
-        if(!in_array($societe->_id,$value)){
-        throw new sfException("L'origine du compte $compteSociete->_id n'a pas la société $societe->_id !");
-        }
-        if($this->verbose){
-          echo "$societe->_id : Le compte $compteSociete->_id a pour origines : ".implode(',',$value)."\n";
-        }
-      }
-      $this->verifySocieteDuplicatedInfos($compteSociete,$societe,$key,$value,$this->verbose);
-      $this->verifyAdresseSociete($compteSociete,$societe,$key,$value,$this->verbose);
-      $this->displayGroupesTagsAndDroits($compteSociete,$societe,$key,$value,$this->verbose);
-      $this->verifyContactSociete($compteSociete,$societe,$key,$value,$this->verbose);
-      if($key == "societe_informations"){
-        $this->verifySocieteInformationNode($compteSociete,$societe,$value,$this->verbose);
-      }
-      if($key == 'compte_type'){
-        if("SOCIETE" != $value){
-          throw new sfException("La type du compte $compteSociete->_id n'est pas SOCIETE ! : ".$value);
-        }
-
-        if($this->verbose){
-          echo "$societe->_id : Le compte $compteSociete->_id a pour type : ".$value."\n";
-        }
-      }
-
-
-      if($key == 'lat'){
-        if($this->verbose){
-          echo "$societe->_id : Le compte $compteSociete->_id a pour site lat : ".$value."\n";
-        }
-      }
-      if($key == 'lon'){
-        if($this->verbose){
-          echo "$societe->_id : Le compte $compteSociete->_id a pour site lon : ".$value."\n";
-        }
-      }
-      if($key == "etablissement_informations"){
-        $fields = get_object_vars($value);
-        if($this->verbose){
-          echo "$societe->_id : Le compte $compteSociete->_id a pour etablissement informations (".implode(",",array_keys($fields)).") [cvi=$value->cvi, ppm=$value->ppm] \n";
-        }
-        if(count($fields) > 2){
-          throw new sfException("Le nombre de champs d'etablissement information du compte $compteSociete->_id est trop grand $societe->_id : ".implode(",",$fields));
-        }
-      }
-      if($key == 'interpro'){
-        if($societe->interpro != $value){
-          throw new sfException("L'interpro du compte $compteSociete->_id n'est pas la même que celle dans la société $societe->_id : ".$value);
-        }
-        if($this->verbose){
-          echo "$societe->_id : Le compte $compteSociete->_id a pour interpro : ".$value."\n";
-        }
-      }
-      if($key == 'statut'){
-        if(!is_null($societe->statut) && ($societe->statut != $value)){
-          throw new sfException("Le statut du compte $compteSociete->_id n'est pas la même que celle dans la société $societe->_id : ".$value);
-        }
-        if($this->verbose){
-          echo "$societe->_id : Le compte $compteSociete->_id a pour statut : ".$value."\n";
-        }
-      }
-
-      if($key == 'teledeclaration_active'){
-        if($this->verbose){
-          echo "$societe->_id : Le compte $compteSociete->_id a pour teledeclaration_active_compte : ".$value."\n";
-        }
-      }
-      if($key == 'date_modification'){
-        if($this->verbose){
-          echo "$societe->_id : Le compte $compteSociete->_id a pour date_modification : ".$value."\n";
-        }
-      }
-
-      if(!in_array($key,self::$list_fields_analysed)){
-        throw new sfException("Le champs $key du compte n'a pas été analysé ");
-      }
-      $compte_societe_saved = $societe->add("compte_societe_saved");
-
-      $compteSocieteJson = $compteSociete->toJson();
-      unset($compteSocieteJson->_id);
-      unset($compteSocieteJson->_rev);
-      unset($compteSocieteJson->type);
-      unset($compteSocieteJson->identifiant);
-
-      foreach ($compteSocieteJson as $key => $value) {
-        $compte_societe_saved->add($key,$value);
-      }
+    foreach ($societe->getComptesInterlocuteurs() as $idInterlocuteurs => $interlocuteur) {
+        $this->comptesInterlocuteurs[$idInterlocuteurs] = $this->getDocWithoutIdsInfos($interlocuteur->toJson());
     }
-    if($this->withSave){
-      $this->moveContacts($societe);
-      echo "Save $societe->_id avec les infos de son compte \n";
-      $societe->save();
+
+    $compteClient = CompteClient::getInstance();
+
+
+    //removes and Deletes
+    //suppression des interlocuteurs
+    foreach ($societe->getComptesInterlocuteurs() as $idInterlocuteurs => $interlocuteur) {
+        $societe->removeContact($idInterlocuteurs);
+        $societe->save(false);
+        //$compteClient->delete($interlocuteur);
+        $societe = SocieteClient::getInstance()->find($societe->_id);
     }
+
+    //suppression des comptes d'etablissements
+    foreach ($societe->getEtablissementsObj() as $idEtb => $etbStruct) {
+        $etbLocal = $etbStruct->etablissement;
+        $etbLocal->add('compte_societe_saved',$this->getDocWithoutIdsInfos($etbLocal->getMasterCompte()->toJson()));
+        $compteClient->delete($etbLocal->getMasterCompte());
+        $etbLocal->save(false);
+        $societe = SocieteClient::getInstance()->find($societe->_id);
+    }
+
+    //suppression du compte de master de la societe
+    $societe->add('compte_societe_saved',$this->getDocWithoutIdsInfos($societe->getMasterCompte()->toJson()));
+    $societe->save(false);
+    $societe = SocieteClient::getInstance()->find($societe->_id);
+
+    $masterCompteToDelete = $societe->getMasterCompte();
+    $masterCompteIdToDelete = $masterCompteToDelete->_id;
+    $compteClient->delete($masterCompteToDelete);
+    $societe->compte_societe = null;
+    $societe->contacts->remove($masterCompteIdToDelete);
+    $societe->save(false);
+    $societe = SocieteClient::getInstance()->find($societe->_id);
+
+    //réinsertion des comptes
+
+    //reinsertion du compte master
+
+
+    $compteMasterJson = $this->comptePrincipal;
+    $societe = SocieteClient::getInstance()->find($societe->_id);
+    $societe->telephone_bureau = $compteMasterJson->telephone_bureau;
+    $societe->telephone_mobile = $compteMasterJson->telephone_mobile;
+    $societe->telephone_perso = $compteMasterJson->telephone_perso;
+    $societe->fax = $compteMasterJson->fax;
+    $societe->email = $compteMasterJson->email;
+    $societe->site_internet = $compteMasterJson->site_internet;
+
+    $societe->adresse = $compteMasterJson->adresse;
+    $societe->adresse_complementaire = $compteMasterJson->adresse_complementaire;
+    $societe->code_postal = $compteMasterJson->code_postal;
+    $societe->commune = $compteMasterJson->commune;
+    $societe->insee = $compteMasterJson->insee;
+    $societe->pays = $compteMasterJson->pays;
+    $societe->save();
+
+    $societe = SocieteClient::getInstance()->find($societe->_id);
+    $compteMasterSociete = $societe->getMasterCompte();
+    foreach ($compteMasterJson as $key => $value) {
+        $compteMasterSociete->add($key,$value);
+    }
+    $compteMasterSociete->save();
+
+    $societe = SocieteClient::getInstance()->find($societe->_id);
+    $societe->remove('compte_societe_saved');
+    $societe->save();
+
+    //reinsertion des comptes d'établissement
+    foreach ($societe->getEtablissementsObj() as $idEtb => $etbStruct) {
+        $etbLocal = $etbStruct->etablissement;
+        $etbCompteLocal = $societe->findOrCreateCompteFromEtablissement($etbLocal);
+        foreach ($etbLocal->compte_societe_saved as $key => $value) {
+            $etbCompteLocal->add($key,$value);
+        }
+        $etbCompteLocal->save();
+
+        $etbLocal->telephone_bureau = $etbCompteLocal->telephone_bureau;
+        $etbLocal->telephone_mobile = $etbCompteLocal->telephone_mobile;
+        $etbLocal->telephone_perso = $etbCompteLocal->telephone_perso;
+        $etbLocal->fax = $etbCompteLocal->fax;
+        $etbLocal->email = $etbCompteLocal->email;
+        $etbLocal->site_internet = $etbCompteLocal->site_internet;
+
+        $etbLocal->adresse = $etbCompteLocal->adresse;
+        $etbLocal->adresse_complementaire = $etbCompteLocal->adresse_complementaire;
+        $etbLocal->code_postal = $etbCompteLocal->code_postal;
+        $etbLocal->commune = $etbCompteLocal->commune;
+        $etbLocal->insee = $etbCompteLocal->insee;
+        $etbLocal->pays = $etbCompteLocal->pays;
+
+
+        $etbLocal->compte = $etbCompteLocal->_id;
+        $etbLocal->remove('compte_societe_saved');
+        $etbLocal->save();
+        $societe = SocieteClient::getInstance()->find($societe->_id);
+    }
+
+    //reinsertion des interlocuteurs
+    foreach ($this->comptesInterlocuteurs as $interlocuteur) {
+        $newCompteInterlocuteur = CompteClient::getInstance()->createCompteInterlocuteurFromSociete($societe);
+        foreach ($interlocuteur as $key => $value) {
+            $newCompteInterlocuteur->add($key,$value);
+        }
+        $newCompteInterlocuteur->save();
+        $societe = SocieteClient::getInstance()->find($societe->_id);
+    }
+    echo "Société ".$societe->_id." migrée\n";
+
   }
 
-  public function moveContacts($societe){
-    foreach ($societe->getInterlocuteursWithOrdre() as $key => $contact) {
-      $compte = CompteClient::getInstance()->find($key);
-      if($compte->compte_type == "INTERLOCUTEUR"){
-        $this->oldContacts[$compte->_id] = $compte;
-        $this->contacts[$compte->_id] = clone $compte;
-        echo "L'interlocuteur $compte->_id est à déplacer \n";
-      }
-    }
-
-    foreach ($this->oldContacts as $idCompte => $oldComteToRemove) {
-      $societe->removeContact($oldComteToRemove->_id);
-      $societe->save();
-    }
-    foreach ($this->contacts as $idCompte => $compte) {
-      $newCompteInterlocuteur = CompteClient::getInstance()->createCompteInterlocuteurFromSociete($societe);
-
-      foreach ($compte as $key => $value) {
-        if($key != '_id' && $key != '_id' && $key != 'identifiant'){
-          $newCompteInterlocuteur->add($key,$value);
-        }
-      }
-
-      $newCompteInterlocuteur->save();
-      echo "L'interlocuteur $newCompteInterlocuteur->_id a été créer \n";
-    }
+  public function getDocWithoutIdsInfos($doc){
+      unset($doc->_id);
+      unset($doc->_rev);
+      unset($doc->type);
+      unset($doc->identifiant);
+      return $doc;
   }
 }
