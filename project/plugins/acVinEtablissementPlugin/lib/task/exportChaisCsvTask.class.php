de2e6fc0f1ad8980c2ac1669c4464b0e165b9017
Mathurin Petit <mathurin.petit@gmail.com>
2018-06-20 19:55:51 +0200

    Exports pour l'AVPI part 1

--- project/plugins/acVinEtablissementPlugin/lib/task/exportChaisCsvTask.class.php
+++ project/plugins/acVinEtablissementPlugin/lib/task/exportChaisCsvTask.class.php
@@ -3,12 +3,15 @@
 class exportChaisCsvTask extends sfBaseTask
 {
 
+    protected $chais = array();
+
     protected function configure()
     {
         $this->addArguments(array(
         ));
 
         $this->addOptions(array(
+            new sfCommandOption('without-liaisons', null, sfCommandOption::PARAMETER_REQUIRED, 'Sans liaisons'),
             new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
             new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
             new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
@@ -28,31 +31,99 @@ EOF;
 
         $results = EtablissementClient::getInstance()->findAll();
 
-        echo "Identifiant établissement;Raison sociale établissement;Nom;Adresse;Code postal;Commune;Partage;Attributs\n";
+        $withoutLiaisons = (isset($options['without-liaisons']) && $options['without-liaisons']);
+        echo "Identifiant chais,Identifiant établissement,Type,Chais Activites,Adresse 1,Adresse 2,Adresse 3,Code postal,Commune,Nom Contact,Tèl Contact, Carte,Position,Archivé,IdCIVP,EA1,EA2,SIRET\n";
+$cpt = 0;
+
+        if(!$withoutLiaisons){
+            foreach($results->rows as $row) {
+                $etablissement = EtablissementClient::getInstance()->find($row->id, acCouchdbClient::HYDRATE_JSON);
+                if(isset($etablissement->chais)){
+                    foreach($etablissement->chais as $numChai => $chai) {
+                        $this->chais[$etablissement->_id.'/chais/'.$numChai] = $chai;
+                    }
+                }
+            }
+        }
 
         foreach($results->rows as $row) {
             $etablissement = EtablissementClient::getInstance()->find($row->id, acCouchdbClient::HYDRATE_JSON);
+            if(isset($etablissement->chais)){
+                foreach($etablissement->chais as $numChai => $chai) {
+                    $activites = array();
+                    foreach($chai->attributs as $a) {
+                        $activites[] = $a;
+                    }
+                    sort($activites);
+                    $activites = implode(";", $activites);
 
-            foreach($etablissement->chais as $chai) {
+                    $adresses = explode(' − ', str_replace(array('"',','),array('',''),$chai->adresse));
+                    $a_comp = (isset($adresses[1]))? $adresses[1] : "";
+                    $a_comp1 = (isset($adresses[2]))? $adresses[2] : "";
 
-                $attributs = array();
-                foreach($chai->attributs as $attribut) {
-                    $attributs[] = $attribut;
+
+                    echo str_replace("ETABLISSEMENT-","",$etablissement->_id."/".$numChai).",".
+                    str_replace("ETABLISSEMENT-","",$etablissement->_id).",".
+                    "AUTRE,".
+                    $activites.",".
+                    trim(str_replace('"', '', $adresses[0])).",".
+                    trim(str_replace('"', '', $a_comp)).",".
+                    trim(str_replace('"', '', $a_comp1)).",".
+                    $chai->code_postal.",".
+                    $chai->commune.",".
+                    $etablissement->raison_sociale.",".
+                    $etablissement->telephone_bureau.",".
+                    ",,Faux,,,,,\n";
+                    }
                 }
-                sort($attributs);
-                $attributs = implode("|", $attributs);
+                if(!$withoutLiaisons && isset($etablissement->liaisons_operateurs)){
+                    foreach($etablissement->liaisons_operateurs as $numLiaison => $liaison) {
+                        if(!isset($liaison->hash_chai) || !$liaison->hash_chai){
+                            continue;
+                        }
+                        $keyL = $liaison->id_etablissement.$liaison->hash_chai;
+                        if(!array_key_exists($keyL,$this->chais)){
+                            throw new sfException("Le chai $keyL n'a pas été réfenrencé");
+                        }
 
-                echo
-                str_replace("SOCIETE-", "", $etablissement->id_societe).";".
-                $etablissement->raison_sociale.";".
-                $chai->nom.";".
-                str_replace('"', '', $chai->adresse).";".
-                $chai->code_postal.";".
-                $chai->commune.";".
-                $chai->partage.";".
-                $attributs.
-                "\n";
-            }
+                        $chaiDistant = $this->chais[$keyL];
+
+                        $activites = array();
+                        foreach($chaiDistant->attributs as $a) {
+                            $activites[] = $a;
+                        }
+                        sort($activites);
+                        $activites = implode(";", $activites);
+
+                        $attributs = array();
+                        foreach($liaison->attributs_chai as $attribut) {
+                            $attributs[] = $attribut;
+                        }
+                        $adresses = explode(' − ', str_replace(array('"',','),array('',''),$chai->adresse));
+                        $a_comp = (isset($adresses[1]))? $adresses[1] : "";
+                        $a_comp1 = (isset($adresses[2]))? $adresses[2] : "";
+
+                        sort($attributs);
+                        $attributs = implode(";", $attributs);
+
+                        $adresses = explode(' − ', str_replace(array('"',','),array('',''),$chaiDistant->adresse));
+                        $a_comp = (isset($adresses[1]))? $adresses[1] : "";
+                        $a_comp1 = (isset($adresses[2]))? $adresses[2] : "";
+
+                        echo str_replace("ETABLISSEMENT-","",str_replace("/chais/","/",$keyL)).",".
+                        str_replace("ETABLISSEMENT-","",$etablissement->_id).",".
+                        $attributs.",".
+                        $activites.",".
+                        trim(str_replace('"', '', $adresses[0])).",".
+                        trim(str_replace('"', '', $a_comp)).",".
+                        trim(str_replace('"', '', $a_comp1)).",".
+                        $chaiDistant->code_postal.",".
+                        $chaiDistant->commune.",".
+                        $etablissement->raison_sociale.",".
+                        $etablissement->telephone_bureau.",".
+                        ",,Faux,,,,,\n";
+                        }
+                }
         }
     }
 }
