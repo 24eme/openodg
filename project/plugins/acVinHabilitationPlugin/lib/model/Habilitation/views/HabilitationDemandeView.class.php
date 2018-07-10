5ee995b0a9c01dbb3472d4459be386082ff388b8
Vincent LAURENT <vince.laurent@gmail.com>
2018-07-10 23:17:58 +0200

    La date d'habilitation est stocké dans les demandes et utilisation de la variable static pour determiner les statuts qui donnent lieu à un changement de statut d'habilitation

--- project/plugins/acVinHabilitationPlugin/lib/model/Habilitation/views/HabilitationDemandeView.class.php
+++ project/plugins/acVinHabilitationPlugin/lib/model/Habilitation/views/HabilitationDemandeView.class.php
@@ -7,9 +7,10 @@ class HabilitationDemandeView extends acCouchdbView
     const KEY_PRODUIT = 2;
     const KEY_LIBELLE = 3;
     const KEY_DATE = 4;
-    const KEY_DEMANDE_KEY = 5;
-    const KEY_IDENTIFIANT = 6;
-    const KEY_COMMENTAIRE = 7;
+    const KEY_DATE_HABILITATION = 5;
+    const KEY_DEMANDE_KEY = 6;
+    const KEY_IDENTIFIANT = 7;
+    const KEY_COMMENTAIRE = 8;
 
     public static function getInstance() {
 
