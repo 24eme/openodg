76952223686b5f0f9a2bb69f970de71a31e6ceeb
Tangui Morlier <tangui@tangui.eu.org>
2018-11-26 16:51:00 +0100

    Code un peu plus générique pour la gestion des années

--- project/bin/generate_export_dr_from_douane.sh
+++ project/bin/generate_export_dr_from_douane.sh
@@ -6,13 +6,20 @@ cd $(dirname $0)/.. > /dev/null 2>&1
 
 cd ~/prodouane_scrapy/
 
+if test "$1"; then
+  ANNEE=$1 ;
+else
+  ANNEE=$(date '+%Y') ;
+fi
 
-bash bin/download_all.sh $(date '+%Y') dr
-bash bin/download_all.sh $(date '+%Y') sv11
-bash bin/download_all.sh $(date '+%Y') sv12
+bash bin/download_all.sh $ANNEE dr
+bash bin/download_all.sh $ANNEE sv11
+bash bin/download_all.sh $ANNEE sv12
 
 cd -
 
-find ~/prodouane_scrapy/documents/ -name '[ds][rv]*-2018*xls' | while read xls ; do
+find ~/prodouane_scrapy/documents/ -name '[ds][rv]*-'$ANNEE'*.xls' | while read xls ; do
   php symfony --application=provence douaneRecolte:convert2csv  $xls
-done > $EXPORTDIR"/dr_"$(date '+%Y')".csv"
+done > $EXPORTDIR"/dr_"$ANNEE".csv.tmp"
+
+mv $EXPORTDIR"/dr_"$ANNEE".csv.tmp" $EXPORTDIR"/dr_"$ANNEE".csv"
