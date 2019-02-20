cc61342b25359a5d920c5ae5c2416fcbc4cc28aa
Tangui Morlier <tangui@tangui.eu.org>
2019-02-20 18:27:14 +0100

    Export des objets en CSV

---
+++ project/bin/export_docs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd $(dirname $0)/..
+
+. bin/config.inc
+
+DOC=$1
+
+if ! test "$DOC" ; then
+	echo "USAGE: $0 DOC_TYPE";
+	exit 1
+fi
+
+header=1
+curl -s http://$COUCHHOST":"$COUCHDBPORT"/"$COUCHBASE"/_all_docs?startkey_docid="$DOC"-&endkey_docid="$DOC"-ZZZZZZ" | awk -F '"' '{print $4}' | grep $DOC | while read doc ; do
+	php symfony declaration:export-csv $SYMFONYTASKOPTIONS --header=$header $doc
+	header=0
+done
