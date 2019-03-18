554c9f6042a0e4b77ce0561f3d26a579d596589b
Tangui Morlier <tangui@tangui.eu.org>
2019-03-18 19:22:22 +0100

    Plus d'anonymisation

--- project/plugins/acVinParcellaireIrrigablePlugin/lib/model/ParcellaireIrrigable/ParcellaireIrrigableDeclarant.class.php
+++ project/plugins/acVinParcellaireIrrigablePlugin/lib/model/ParcellaireIrrigable/ParcellaireIrrigableDeclarant.class.php
@@ -5,5 +5,16 @@
  */
 
 class ParcellaireIrrigableDeclarant extends BaseParcellaireIrrigableDeclarant {
-
+    public function getTelephoneBureau(){
+        return Anonymization::hideIfNeeded($this->_get('telephone_bureau'));
+    }
+    public function getTelephoneMobile(){
+        return Anonymization::hideIfNeeded($this->_get('telephone_mobile'));
+    }
+    public function getFax(){
+        return Anonymization::hideIfNeeded($this->_get('fax'));
+    }
+    public function getEmail(){
+        return Anonymization::hideIfNeeded($this->_get('email'));
+    }
 }
