53dd23c86666833e7edc420d9b1983d0260f3ef6
Tangui Morlier <tangui@tangui.eu.org>
2019-03-19 14:12:08 +0100

    Erreur dans l'anon.

--- project/plugins/acVinGenerationPlugin/modules/generation/templates/_list.php
+++ project/plugins/acVinGenerationPlugin/modules/generation/templates/_list.php
@@ -11,7 +11,7 @@
                     <span class="col-xs-3 text-muted"><?php echo Anonymization::hideIfNeeded($generation->value[GenerationClient::HISTORY_VALUES_LIBELLE]); ?></span>
                     <span class="col-xs-2 text-muted text-right"><?php echo $generation->value[GenerationClient::HISTORY_VALUES_NBDOC]; ?> document<?php if(count($generation->value[GenerationClient::HISTORY_VALUES_NBDOC])):?>s<?php endif; ?></span>
                     <span class="col-xs-2 text-muted text-right">
-                    <?php if($generation->value[GenerationClient::HISTORY_VALUES_SOMME]): ?><?php Anonymization::hideIfNeeded(echoFloat(($generation->value[GenerationClient::HISTORY_VALUES_SOMME]) ? $generation->value[GenerationClient::HISTORY_VALUES_SOMME]: 0));?>&nbsp;€<?php endif; ?>
+                    <?php if($generation->value[GenerationClient::HISTORY_VALUES_SOMME]): ?><?php echoFloat(Anonymization::hideIfNeeded(($generation->value[GenerationClient::HISTORY_VALUES_SOMME]) ? $generation->value[GenerationClient::HISTORY_VALUES_SOMME]: 0));?>&nbsp;€<?php endif; ?>
                     </span>
                     <span class="col-xs-2 text-muted text-right"><span class="label label-<?php echo statutToCssClass($generation->value[GenerationClient::HISTORY_VALUES_STATUT]) ?>"><span class="<?php echo statutToIconCssClass($generation->value[GenerationClient::HISTORY_VALUES_STATUT]) ?>"></span>&nbsp;&nbsp;<?php echo statutToLibelle($generation->value[GenerationClient::HISTORY_VALUES_STATUT]); ?></span></span>
                 </a>
