46280aa9e9b7cdab6dee59cc36d8e55c52eab23d
Vincent LAURENT <vince.laurent@gmail.com>
2019-05-03 16:52:30 +0200

    Export des données massives

---
+++ project/bin/export_rhone.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+. bin/config.inc
+
+mkdir $EXPORTDIR 2> /dev/null
+
+bash bin/export_docs.sh DREV > $EXPORTDIR/drev.csv
+bash bin/export_docs.sh HABILITATION > $EXPORTDIR/habilitation.csv
+bash bin/export_docs.sh SV12 > $EXPORTDIR/sv12.csv
+bash bin/export_docs.sh DR > $EXPORTDIR/dr.csv
