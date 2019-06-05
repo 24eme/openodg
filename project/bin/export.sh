5f1a10a1969178983efc48fe3e5b2b549c578daf
Vincent LAURENT <vince.laurent@gmail.com>
2019-06-05 16:15:56 +0200

    On créé systématiquement un .part

--- project/bin/export.sh
+++ project/bin/export.sh
@@ -8,21 +8,29 @@ php symfony export:etablissements-csv $SYMFONYTASKOPTIONS > $EXPORTDIR/etablisse
 
 sort $EXPORTDIR/etablissements.csv.part > $EXPORTDIR/etablissements.csv.part.sorted
 mv $EXPORTDIR/etablissements.csv.part{.sorted,}
-
 cat $EXPORTDIR/etablissements.csv.part | grep -E "^IdOp" > $EXPORTDIR/etablissements.csv
 cat $EXPORTDIR/etablissements.csv.part | grep -Ev "^IdOp" >> $EXPORTDIR/etablissements.csv
+cat $EXPORTDIR/etablissements.csv | iconv -f UTF8 -t ISO88591//TRANSLIT > $EXPORTDIR/etablissements.iso8859.csv
 
 php symfony export:chais-csv $SYMFONYTASKOPTIONS > $EXPORTDIR/chais.csv.part
 mv $EXPORTDIR/chais.csv{.part,}
-
-cat $EXPORTDIR/etablissements.csv | iconv -f UTF8 -t ISO88591//TRANSLIT > $EXPORTDIR/etablissements.iso8859.csv
 cat $EXPORTDIR/chais.csv | iconv -f UTF8 -t ISO88591//TRANSLIT >$EXPORTDIR/chais.iso8859.csv
 
-bash bin/export_docs.sh DRev > $EXPORTDIR/drev.csv
-bash bin/export_docs.sh Habilitation > $EXPORTDIR/habilitation.csv
-bash bin/export_docs.sh DR > $EXPORTDIR/dr.csv
-bash bin/export_docs.sh SV12 > $EXPORTDIR/sv12.csv
-bash bin/export_docs.sh SV11 > $EXPORTDIR/sv11.csv
+bash bin/export_docs.sh DRev > $EXPORTDIR/drev.csv.part
+mv $EXPORTDIR/drev.csv{.part,}
 
-php symfony export:habilitation-demandes $SYMFONYTASKOPTIONS > $EXPORTDIR/habilitation_demandes.csv
+bash bin/export_docs.sh Habilitation > $EXPORTDIR/habilitation.csv.part
+mv $EXPORTDIR/habilitation.csv{.part,}
+php symfony export:habilitation-demandes $SYMFONYTASKOPTIONS > $EXPORTDIR/habilitation_demandes.csv.part
+mv $EXPORTDIR/habilitation_demandes.csv{.part,}
 php bin/export/export_liste_inao.php $EXPORTDIR/habilitation_demandes.csv > $EXPORTDIR/habilitation_demandes_inao.csv
+mv $EXPORTDIR/habilitation_demandes_inao.csv{.part,}
+
+bash bin/export_docs.sh DR > $EXPORTDIR/dr.csv.part
+mv $EXPORTDIR/dr.csv{.part,}
+
+bash bin/export_docs.sh SV12 > $EXPORTDIR/sv12.csv.part
+mv $EXPORTDIR/sv12.csv{.part,}
+
+bash bin/export_docs.sh SV11 > $EXPORTDIR/sv11.csv.part
+mv $EXPORTDIR/sv11.csv{.part,}
