ca3209e9259380fc6506a390bcb9b31c4067b391
mathurin petit <mathurin.petit@gmail.com>
2019-07-03 17:00:56 +0200

    custom error 500

--- project/apps/loire/config/error/error.html.php
+++ project/apps/loire/config/error/error.html.php
@@ -50,7 +50,7 @@
 
         <header id="header">
             <div class="container" style="position: relative; background-color: #fff; text-align: center;">
-            	<a href="<?php echo url_for('accueil') ?>">
+            	<a href="#">
                 	<img style="height: 33px; margin-top: 24px;" class="pull-left" src="https://teledeclaration.vinsvaldeloire.pro/images/visuels/logo_vinsdeloire_new2.png" />
                     <img style="height: 80px;" class="pull-right" src="https://teledeclaration.vinsvaldeloire.pro/data/logo_vrac_pdf.jpg" />
                     <div style="margin-left: 250px; border-left: #555353 1px solid;padding: 10px 0;margin-top: 15px;">
