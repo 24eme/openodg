2c3b1a945ba6e0b9c7fd28d070689b86e9b71ad3
Tangui Morlier <tangui@tangui.eu.org>
2019-07-30 12:08:10 +0200

    Temporise les exports

--- project/bin/export.sh
+++ project/bin/export.sh
@@ -12,17 +12,21 @@ cat $EXPORTDIR/etablissements.en.csv | sed 's/;/ø/g' | awk -F ',' 'BEGIN { OFS=
 rm $EXPORTDIR/etablissements.csv.part $EXPORTDIR/etablissements.csv.sorted.part
 ln -s etablissements.en.csv $EXPORTDIR/etablissements.iso8859.csv 2> /dev/null # Pour l'AVPI en provence
 
+sleep 60
+
 php symfony export:chais-csv $SYMFONYTASKOPTIONS > $EXPORTDIR/chais.csv.part
 iconv -f UTF8 -t ISO88591//TRANSLIT $EXPORTDIR/chais.csv.part > $EXPORTDIR/chais.en.csv
 cat $EXPORTDIR/chais.en.csv | sed 's/;/ø/g' | awk -F ',' 'BEGIN { OFS=";" }{ $1=$1; print $0 }' | sed 's/ø/,/g' > $EXPORTDIR/chais.csv
 rm $EXPORTDIR/chais.csv.part
 ln -s chais.en.csv $EXPORTDIR/chais.iso8859.csv 2> /dev/null # Pour l'AVPI en provence
 
-bash bin/export_docs.sh DRev > $EXPORTDIR/drev.csv.part
+sleep 60
+
+bash bin/export_docs.sh DRev 30 > $EXPORTDIR/drev.csv.part
 iconv -f UTF8 -t ISO88591//TRANSLIT $EXPORTDIR/drev.csv.part > $EXPORTDIR/drev.csv
 rm $EXPORTDIR/drev.csv.part
 
-bash bin/export_docs.sh Habilitation > $EXPORTDIR/habilitation.csv.part
+bash bin/export_docs.sh Habilitation 30 > $EXPORTDIR/habilitation.csv.part
 iconv -f UTF8 -t ISO88591//TRANSLIT $EXPORTDIR/habilitation.csv.part > $EXPORTDIR/habilitation.csv
 rm $EXPORTDIR/habilitation.csv.part
 
@@ -32,15 +36,17 @@ php bin/export/export_liste_inao.php $EXPORTDIR/habilitation_demandes.csv.part >
 iconv -f UTF8 -t ISO88591//TRANSLIT $EXPORTDIR/habilitation_demandes_inao.csv.part > $EXPORTDIR/habilitation_demandes_inao.csv
 rm $EXPORTDIR/habilitation_demandes.csv.part $EXPORTDIR/habilitation_demandes_inao.csv.part
 
-bash bin/export_docs.sh DR > $EXPORTDIR/dr.csv.part
+sleep 60
+
+bash bin/export_docs.sh DR 30 > $EXPORTDIR/dr.csv.part
 iconv -f UTF8 -t ISO88591//TRANSLIT $EXPORTDIR/dr.csv.part | grep -vE ";Motif de non r" > $EXPORTDIR/dr.csv
 rm $EXPORTDIR/dr.csv.part
 
-bash bin/export_docs.sh SV12 > $EXPORTDIR/sv12.csv.part
+bash bin/export_docs.sh SV12 30 > $EXPORTDIR/sv12.csv.part
 iconv -f UTF8 -t ISO88591//TRANSLIT $EXPORTDIR/sv12.csv.part > $EXPORTDIR/sv12.csv
 rm $EXPORTDIR/sv12.csv.part
 
-bash bin/export_docs.sh SV11 > $EXPORTDIR/sv11.csv.part
+bash bin/export_docs.sh SV11 30 > $EXPORTDIR/sv11.csv.part
 iconv -f UTF8 -t ISO88591//TRANSLIT $EXPORTDIR/sv11.csv.part > $EXPORTDIR/sv11.csv
 rm $EXPORTDIR/sv11.csv.part
 
