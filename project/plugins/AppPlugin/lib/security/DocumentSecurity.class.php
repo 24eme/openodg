c3ffeb67f34306bf22d88e67b93cbaf9800ca3b1
Vincent LAURENT <vince.laurent@gmail.com>
2019-07-31 10:53:58 +0200

    Correction d'un notice si pas de société principalement en mode test

--- project/plugins/AppPlugin/lib/security/DocumentSecurity.class.php
+++ project/plugins/AppPlugin/lib/security/DocumentSecurity.class.php
@@ -24,8 +24,8 @@ abstract class DocumentSecurity implements SecurityInterface {
         if(!is_array($droits)) {
             $droits = array($droits);
         }
-        
-        if(!$this->isAdmin() && !preg_match("/^".$this->user->getCompte()->getSociete()->identifiant."/", $this->doc->identifiant)) {
+
+        if(!$this->isAdmin() && $this->user->getCompte()->getSociete() && !preg_match("/^".$this->user->getCompte()->getSociete()->identifiant."/", $this->doc->identifiant)) {
 
             $lienSymbolique = DeclarationClient::getInstance()->find(str_replace($this->doc->identifiant, $this->user->getCompte()->getSociete()->getEtablissementPrincipal()->identifiant, $this->doc->_id), acCouchdbClient::HYDRATE_JSON, true);
 
