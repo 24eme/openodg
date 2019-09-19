f77cb30e1e53808fc6995e14f6d29abbbf3c4249
Vincent LAURENT <vince.laurent@gmail.com>
2019-09-19 16:04:42 +0200

    Dans la navigation ajout du menu documents pour les habilitations Pour le droit habilitation on ne voit que les documents ayant la catégorie identification

--- project/plugins/FichierPlugin/modules/fichier/actions/actions.class.php
+++ project/plugins/FichierPlugin/modules/fichier/actions/actions.class.php
@@ -115,10 +115,19 @@ class fichierActions extends sfActions
 
 		$allHistory = PieceAllView::getInstance()->getPiecesByEtablissement($this->etablissement->identifiant, $this->getUser()->hasCredential(myUser::CREDENTIAL_ADMIN));
 		$this->history = ($this->year)? PieceAllView::getInstance()->getPiecesByEtablissement($this->etablissement->identifiant, $this->getUser()->hasCredential(myUser::CREDENTIAL_ADMIN), $this->year.'-01-01', $this->year.'-12-31') : $allHistory;
+		foreach($this->history as $key => $item) {
+			if(!$this->getUser()->isAdmin() && $this->getUser()->hasCredential(myUser::CREDENTIAL_HABILITATION) && $item->key[PieceAllView::KEYS_CATEGORIE] != "Identification") {
+				unset($this->history[$key]);
+			}
+		}
+
 		$this->years = array();
 		$this->categories = array();
 		$this->decreases = 0;
 		foreach ($allHistory as $doc) {
+			if(!$this->getUser()->isAdmin() && $this->getUser()->hasCredential(myUser::CREDENTIAL_HABILITATION) && $doc->key[PieceAllView::KEYS_CATEGORIE] != "Identification") {
+				continue;
+			}
 			if (preg_match('/^([0-9]{4})-[0-9]{2}-[0-9]{2}$/', $doc->key[PieceAllView::KEYS_DATE_DEPOT], $m)) {
 				$this->years[$m[1]] = $m[1];
 			}
@@ -199,7 +208,7 @@ class fichierActions extends sfActions
 	}
 
 	protected function secureEtablissement($etablissement) {
-        if (!EtablissementSecurity::getInstance($this->getUser(), $etablissement)->isAuthorized(array())) {
+        if (!$this->getUser()->hasCredential(AppUser::CREDENTIAL_HABILITATION) && !EtablissementSecurity::getInstance($this->getUser(), $etablissement)->isAuthorized(array())) {
 
             return $this->forwardSecure();
         }
