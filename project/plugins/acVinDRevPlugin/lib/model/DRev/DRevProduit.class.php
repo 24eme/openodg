46280df92277d80863d24c6298b665f55f8e6ebd
Vincent LAURENT <vince.laurent@gmail.com>
2019-09-30 19:01:54 +0200

    Clean des produits que si pas de superficie et pas de volume

--- project/plugins/acVinDRevPlugin/lib/model/DRev/DRevProduit.class.php
+++ project/plugins/acVinDRevPlugin/lib/model/DRev/DRevProduit.class.php
@@ -83,9 +83,9 @@ class DRevProduit extends BaseDRevProduit
 
             return true;
         }
-				if ($this->recolte->superficie_total == null) {
-					return true;
-				}
+		if ($this->recolte->superficie_total == null && $this->recolte->volume_total == null) {
+			return true;
+		}
 
         return false;
     }
