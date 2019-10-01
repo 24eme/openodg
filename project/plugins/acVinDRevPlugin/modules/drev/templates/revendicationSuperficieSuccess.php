eae039db81d9553178e96fa5d4e53d40b119eed5
Tangui Morlier <tangui@tangui.eu.org>
2019-10-01 15:57:52 +0200

    Pas de VCI pour les négos

--- project/plugins/acVinDRevPlugin/modules/drev/templates/revendicationSuperficieSuccess.php
+++ project/plugins/acVinDRevPlugin/modules/drev/templates/revendicationSuperficieSuccess.php
@@ -10,8 +10,11 @@
 
     <form role="form" action="<?php echo url_for("drev_revendication_superficie", $drev) ?>" method="post" class="ajaxForm" id="form_drev_revendication_vci">
 
-    <?php echo $form->renderHiddenFields(); ?>
-    <?php echo $form->renderGlobalErrors(); ?>
+    <?php
+          $can_have_vci = !($drev->getDocumentDouanierType() == SV12CsvFile::CSV_TYPE_SV12);
+
+          echo $form->renderHiddenFields();
+          echo $form->renderGlobalErrors(); ?>
 
 	<?php if (count($form['produits']) > 0): ?>
     <table class="table table-bordered table-striped table-condensed">
@@ -19,13 +22,15 @@
         	<tr>
                 <th class="text-center col-xs-2"></th>
                 <th class="text-center info"><?php echo $drev->getDocumentDouanierTypeLibelle(); ?></th>
-                <th colspan="2" class="text-center">Déclaration de Revendication</th>
+                <th colspan="<?php echo ($can_have_vci) ? 2 : 1 ; ?>" class="text-center">Déclaration de Revendication</th>
             </tr>
             <tr>
                 <th class="text-left col-xs-4"><?php if (count($form['produits']) > 1): ?>Produits revendiqués<?php else: ?>Produit revendiqué<?php endif; ?></th>
                 <th class="text-center col-xs-2 info"><div style="position: relative;">Superficie récoltée totale<br /><small class="text-muted">(ha)</small> &nbsp;<a title="<?php echo getPointAideText('drev', 'superficie_totale') ?>" data-placement="auto" data-toggle="tooltip" class="btn-tooltip btn btn-md" style="position: absolute; bottom: -8px; right: -8px;"><span class="glyphicon glyphicon-question-sign"></span></a></div></th>
                 <th style="position: relative;" class="text-center col-xs-2">Superficie revendiquée<br /><small class="text-muted">(ha)</small>&nbsp;<a title="<?php echo getPointAideText('drev', 'superficie_revendique') ?>" data-placement="auto" data-toggle="tooltip" class="btn-tooltip btn btn-md" style="position: absolute; bottom: 0; right: 0px;"><span class="glyphicon glyphicon-question-sign"></span></a></th>
+                <?php if ($can_have_vci): ?>
                 <th style="position: relative;" class="text-center col-xs-2">Possède du stock VCI<br />&nbsp;<a title="<?php echo getPointAideText('drev', 'possede_vci') ?>" data-placement="auto" data-toggle="tooltip" class="btn-tooltip btn btn-md" style="position: absolute; bottom: 0; right: 0px;"><span class="glyphicon glyphicon-question-sign"></span></a></th>
+                <?php endif; ?>
             </tr>
         </thead>
         <tbody class="edit_vci">
@@ -38,7 +43,9 @@
                     </td>
                     <td class="info"><?php echo $formProduit['recolte']['superficie_total']->render(array( 'placeholder' => "ha")) ?></td>
                     <td><?php echo $formProduit['superficie_revendique']->render(array( 'placeholder' => "ha")) ?></td>
+                    <?php if ($can_have_vci): ?>
                     <td class="text-center pointer_checkbox"><?php echo (isset($formProduit['has_stock_vci'])) ? $formProduit['has_stock_vci']->render() : ""; ?></td>
+                    <?php endif; ?>
                 </tr>
             <?php endforeach; ?>
         </tbody>
