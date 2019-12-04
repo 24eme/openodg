836693b26c3b934e76676714da420c57a370f616
mathurin petit <mathurin.petit@gmail.com>
2019-12-04 15:45:43 +0100

    dans les etapes de DREV => pas de liens si on est une modificatrice et que l'etape est disabled

--- project/apps/loire/templates/_step.php
+++ project/apps/loire/templates/_step.php
@@ -9,13 +9,18 @@ $stepNum = $etapes->getEtapeNum($etapeMax);
         <?php $past = ($etapes->isGt($etapeMax, $k)); ?>
         <?php $disabled = ($etapes->isEtapeDisabled($k, $object)); ?>
         <li style="<?php if($disabled): ?>opacity: 0.7;<?php endif; ?>" class="<?php if($actif): ?>active<?php endif; ?> <?php if ((!$past && !$actif) || $disabled): ?>disabled<?php endif; ?> <?php if ($past && !$actif): ?>visited<?php endif; ?>">
-                <a href="<?php
-    if (isset($routeparams) && isset($routeparams[$etapes->getRouteLink($k)])) {
-       echo url_for($etapes->getRouteLink($k), $routeparams[$etapes->getRouteLink($k)]->getRawValue());
-    }else{
-       echo url_for($etapes->getRouteLink($k), $object);
-    }
-    ?>" class="<?php if(isset($ajax) && $ajax): ?>ajax<?php endif; ?>"><?php echo str_replace('%campagne%', intval($object->campagne) - 1, $etapes->getLibelle($k, $object, ESC_RAW));?></a>
+                  <a href="<?php
+                          if($disabled && $object->isModificative()){
+                            echo "#";
+                          }else {
+                            if (isset($routeparams) && isset($routeparams[$etapes->getRouteLink($k)])) {
+                               echo url_for($etapes->getRouteLink($k), $routeparams[$etapes->getRouteLink($k)]->getRawValue());
+                            }else{
+                               echo url_for($etapes->getRouteLink($k), $object);
+                            }
+                          } ?>"
+    class="<?php if(isset($ajax) && $ajax): ?>ajax<?php endif; ?>"><?php echo str_replace('%campagne%', intval($object->campagne) - 1, $etapes->getLibelle($k, $object, ESC_RAW));?></a>
+                <?php //endif; ?>
         </li>
     <?php endforeach; ?>
     </ul>
