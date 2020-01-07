9fb088ec16a807331a68b787dfbdaebc0c767ac2
Tangui Morlier <tangui@tangui.eu.org>
2020-01-07 17:37:30 +0100

    Permet la récupération de documents douaniers

---
+++ project/bin/get_csv_dr_from_douane.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd $(dirname $0)/.. > /dev/null 2>&1
+
+. bin/config.inc
+
+echo "type;année;id interne;cvi;raison sociale;;commune;tiers;tiers id;categorie;genre;denomination;mention;lieu;couleur;cepage;inao;libelle;denomination complementaire;ligne numero;ligne libelle;ligne valeur;acheteur id;acheteur raison sociale;;" 
+php symfony $SYMFONYTASKOPTIONS douaneRecolte:convert2csv  $1
