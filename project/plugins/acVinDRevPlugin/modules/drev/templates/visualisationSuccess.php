f1f4f4075402835311110b3588a36c1b75e31172
Tangui Morlier <tangui@tangui.eu.org>
2020-01-28 19:04:27 +0100

    Ajoute un message indiquant que le document est facturé

--- project/plugins/acVinDRevPlugin/modules/drev/templates/visualisationSuccess.php
+++ project/plugins/acVinDRevPlugin/modules/drev/templates/visualisationSuccess.php
@@ -71,8 +71,10 @@
     <div class="col-xs-2 text-right">
         <?php if ($drev->validation && DRevSecurity::getInstance($sf_user, $drev->getRawValue())->isAuthorized(DRevSecurity::DEVALIDATION)): ?>
                     <a class="btn btn-xs btn-default pull-right" href="<?php echo url_for('drev_devalidation', $drev) ?>" onclick="return confirm('Êtes-vous sûr de vouloir dévalider cette DRev ?');"><span class="glyphicon glyphicon-remove-sign"></span>&nbsp;&nbsp;Dévalider</a>
-        <?php elseif ($drev->validation && $sf_user->isAdmin() && !$drev->isLectureSeule()) && !$drev->isFactures(): ?>
+        <?php elseif ($drev->validation && $sf_user->isAdmin() && !$drev->isLectureSeule() && !$drev->isFactures()): ?>
                   <a class="btn btn-xs btn-default-step pull-right hidden-xs" onClick="return confirm('Attention, cette DRev a sans doute été facturée. Si vous changez un volume, pensez à en faire part au service comptable !!');" href="<?php echo url_for('drev_devalidation', $drev) ?>"><span class="glyphicon glyphicon-remove-sign"></span>&nbsp;&nbsp;Réouvrir</a>
+        <?php elseif ($drev->isFactures()): ?>
+                  <span class="text-mutted">DRev facturée.</span>
         <?php endif; ?>
     </div>
     <div class="col-xs-3 text-right">
