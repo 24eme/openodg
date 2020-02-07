b770b2481be66571b1cfb299844384f26d87cab5
Jb Lm <lemetayer.jb@gmail.com>
2020-02-07 17:59:02 +0100

    refacto

--- project/plugins/acVinParcellaireAffectationPlugin/lib/form/ParcellaireAffectationChoixDgcForm.class.php
+++ project/plugins/acVinParcellaireAffectationPlugin/lib/form/ParcellaireAffectationChoixDgcForm.class.php
@@ -16,6 +16,12 @@ class ParcellaireAffectationChoixDgcForm extends acCouchdbObjectForm {
     		$this->getWidget('date_papier')->setLabel("Date de réception du document");
     		$this->getValidator('date_papier')->setMessage("required", "La date de réception du document est requise");
     	}
+    	if (sfConfig::get('app_document_validation_signataire')) {
+    		$this->setWidget('signataire', new sfWidgetFormInput());
+    		$this->setValidator('signataire', new sfValidatorString(array('required' => true)));
+    		$this->getWidget('signataire')->setLabel("Nom et prénom :");
+    		$this->getValidator('signataire')->setMessage("required", "Le nom et prénom du signataire est requise");
+    	}
     	$dgcChoices = $this->getDgcChoices();
    		$this->setWidget('dgc', new sfWidgetFormChoice(array('multiple' => true, 'expanded' => true,'choices' => $dgcChoices)));
    		$this->setValidator('dgc', new sfValidatorChoice(array('multiple' => true, 'required' => true, 'choices' => array_keys($dgcChoices)),array('required' => "Vous devez selectionner vos dénomination complémentaire")));
