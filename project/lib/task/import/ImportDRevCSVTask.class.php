1ecd4758dddaa596222714e8734b794e3c3ab1d6
Vincent LAURENT <vince.laurent@gmail.com>
2020-02-27 23:48:53 +0100

    Reprise d'historique drev la superficie était enregistré en ares et il s'agissait de la superficie récolté total et idem pour le volume dr il s'agissait du recolte nette L15

--- project/lib/task/import/ImportDRevCSVTask.class.php
+++ project/lib/task/import/ImportDRevCSVTask.class.php
@@ -167,27 +167,22 @@ EOF;
 
             echo "Ajout d'une revendication produit ".self::$produitsKey[$produit_file][0]." à la drev $drev->_id \n";
 
-            $surface = $data[self::CSV_SURFACE] / 100.0;
-            $produit->superficie_revendique += $this->convertFloat($surface);
+            $surface = $data[self::CSV_SURFACE] / 10000.0;
+
             $produit->recolte->superficie_total += $this->convertFloat($surface);
 
             $volume_brut = $data[self::CSV_VOLUME_BRUT] / 100.00;
 
-            $produit->recolte->volume_total += $this->convertFloat($volume_brut);
-
             $produit->recolte->recolte_nette += $this->convertFloat($volume_brut);
-            $produit->recolte->volume_sur_place += $this->convertFloat($volume_brut);
 
             $volume_rev = $data[self::CSV_VOLUME] / 100.00;
-
-            $produit->volume_revendique_total += $this->convertFloat($volume_rev);
 
             $produit->volume_revendique_issu_recolte += $this->convertFloat($volume_rev);
 
         $date_reception = DateTime::createFromformat("d/m/Y",$data[self::CSV_DATE_RECEPTION]);
-        $drev->add('validation',$date_reception->format('Y-m-d'));
-        $drev->add('validation_odg',$date_reception->format('Y-m-d'));
+        $drev->update();
         $drev->validate($date_reception->format('Y-m-d'));
+        $drev->validateOdg($date_reception->format('Y-m-d'));
         $drev->save();
     }
 
