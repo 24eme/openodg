c4d25eaf424eb95dc0f3fc5fab03a675aaf42b4f
Tangui Morlier <tangui@tangui.eu.org>
2020-03-06 14:36:07 +0100

    Pas besoin du context mais de la création des répertoires parents

--- project/plugins/FichierPlugin/lib/task/DRPDFTask.class.php
+++ project/plugins/FichierPlugin/lib/task/DRPDFTask.class.php
@@ -28,16 +28,19 @@ EOF;
         // initialize the database connection
         $databaseManager = new sfDatabaseManager($this->configuration);
         $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
-        $context = sfContext::createInstance($this->configuration);
-
         $dr = FichierClient::getInstance()->find($arguments['dr_id']);
+        $file_path = $arguments['file_path'];
+
+        if (!mkdir(dirname($file_path), '0755', true)) {
+            die("could not access ".$file_path);
+        }
 
         if(!$dr) {
             die("DR ".$arguments['dr_id']." non trouvée");
         }
         foreach ($dr->_attachments as $key => $attachement) {
             if ($attachement->content_type == 'application/pdf') {
-                file_put_contents($arguments['file_path'], file_get_contents($dr->getAttachmentUri($key)));
+                file_put_contents($file_path, file_get_contents($dr->getAttachmentUri($key)));
                 return;
             }
         }
