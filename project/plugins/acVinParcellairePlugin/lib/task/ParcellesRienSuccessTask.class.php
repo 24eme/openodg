586cb9b124fa9c3cccba61113b862e2bcf1cb614
Moussa ADJI <adjimahamat@gmail.com>
2020-03-12 14:35:14 +0100

    Task import

--- project/plugins/acVinParcellairePlugin/lib/task/ParcellesRienSuccessTask.class.php
+++ project/plugins/acVinParcellairePlugin/lib/task/ParcellesRienSuccessTask.class.php
@@ -2,14 +2,14 @@
 
 class ParcellesRienSuccessTask extends sfBaseTask
 {
-    //php symfony intention-parcelles-success:import /home/moussa_24/Nextcloud/24eme/dpap2019.csv /home/moussa_24/Nextcloud/24eme/dpap2019_rien_find_perfect.csv /home/moussa_24/Nextcloud/24eme/dpap2019_perfect.csv --application="provence" --env="preprod" > ~/log_import
+    //php symfony intention-parcelles-success:import /home/moussa_24/Nextcloud/24eme/dpap2019.csv /home/moussa_24/Nextcloud/24eme/dpap2019_rien_find_perfect.csv /home/moussa_24/Nextcloud/24eme/dpap2019_rien_found_in_diff_account.csv --application="provence" --env="preprod"
 
     protected function configure()
     {
         $this->addArguments(array(
-            new sfCommandArgument('doc_name', sfCommandArgument::REQUIRED, "Document name"),
-            new sfCommandArgument('outputfileperfect', sfCommandArgument::REQUIRED, "file name of out perfect found"),
-            new sfCommandArgument('outputfile', sfCommandArgument::REQUIRED, "file name of out"),
+            new sfCommandArgument('doc_in_out', sfCommandArgument::REQUIRED, "Document name"),
+            new sfCommandArgument('input_file_perfect', sfCommandArgument::REQUIRED, "file name of out perfect found"),
+            new sfCommandArgument('input_file_diff_account', sfCommandArgument::REQUIRED, "file name of out"),
             
         ));
         $this->addOptions(array(
@@ -40,30 +40,44 @@ EOF;
     protected function execute($arguments = array(), $options = array())
     {   
         
-        $fperfect = $arguments["outputfileperfect"];
-        $filename = $arguments["doc_name"];
+        $fperfect = $arguments["input_file_perfect"];
+        $finout = $arguments["doc_in_out"];
+        $fdiff = $arguments['input_file_diff_account'];
         
         $oldDataKeys = [];
-        $oldData = $this->csvToArray($filename);
-        foreach($oldData as $data){
-            $oldDataKeys[$data[0]."-".$data[1]."-".str_replace(" ", "-",$data[3])."-".str_replace(",",".", $data[2])] = $data;
+        $oldData = $this->csvToArray($finout,",");
+        foreach($oldData as $key => $data){
+            if($key) $oldDataKeys[$data[0]."-".$data[1]."-".str_replace(" ", "-",$data[3])."-".str_replace(",",".", $data[2])] = $data;
         }
+
         $perfectData = $this->csvToArray($fperfect, ",");
         foreach($perfectData as $data){
-            $perfectDataKeys[$data[0]."-".$data[1]."-".str_replace(" ", "-",$data[3])."-".str_replace(",",".", $data[4])] = $data;
+            $perfectDataKeys[$data[0]."-".$data[1]."-".str_replace(" ", "-",$data[3])."-".str_replace(",",".", $data[5])] = $data;
+
         }
-        $fp = fopen($arguments["outputfile"], 'w');
-        foreach ($perfectDataKeys as $key => $value) {
-            if(in_array($key, array_keys($oldDataKeys))){
-                $newAccount = explode("-", $value[2]);
-                echo sprintf("Success;%s;%s\n", $oldDataKeys[$key][0], $newAccount);
+        $diffAccountData = $this->csvToArray($fdiff, ",");
+        foreach($diffAccountData as $data){
+            $diffAccountDataKeys[$data[0]."-".$data[1]."-".str_replace(" ", "-",$data[3])."-".str_replace(",",".", $data[5])] = $data;
+        }
+        $fp = fopen($finout, 'w');
+        $merge = array_merge($diffAccountDataKeys, $perfectDataKeys);
+        $nb = 0;
+        foreach ($oldDataKeys as $key => $value) {
+            if(in_array($key, array_keys($merge))){
+                $nb++;
+
+                $newAccount = explode("-", $merge[$key][2]);
+                print_r($newAccount);
+                echo sprintf("Success;%s;%s\n", $oldDataKeys[$key][0], $newAccount[1]);
+
                 $oldDataKeys[$key][0] = $newAccount[1];
-                fputcsv($fp, $oldDataKeys[$key]);
+                fputcsv($fp, $oldDataKeys[$key],";");
             }else{
-                echo sprintf("Error;%s\n", $key);
+                fputcsv($fp, $oldDataKeys[$key],";");
+                echo sprintf("Warning;non modifie;%s\n", $key);
             }
         }
         fclose($fp);
-    }
-    
+        echo sprintf("IDU Changed : %s / %s \n", $nb, count($oldData));
+    }    
 }
