11212ceea0b7aac6ee898d5646fddd8c6cae0c96
mathurin petit <mathurin.petit@gmail.com>
2020-03-26 20:20:51 +0100

    mis en place du paiements multiples dans la facturation => nantes

--- project/plugins/acVinFacturePlugin/lib/model/Facture/FacturePaiements.class.php
+++ project/plugins/acVinFacturePlugin/lib/model/Facture/FacturePaiements.class.php
@@ -6,4 +6,21 @@
 
 class FacturePaiements extends BaseFacturePaiements {
 
+
+  public function cleanPaiements() {
+      $paiementsToDelete = array();
+
+      foreach($this as $paiement) {
+          if(!$paiement->exist('date') || !$paiement->date || !$paiement->exist('montant') || !$paiement->montant) {
+              $paiementsToDelete[$paiement->getKey()] = $true;
+          }
+      }
+
+      foreach($paiementsToDelete as $key => $void) {
+          $this->remove($key);
+      }
+
+  }
+
+
 }
