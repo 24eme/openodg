330f41ba89e557e717278e850f1a9e5cf4b7934b
Vincent LAURENT <vince.laurent@gmail.com>
2020-05-05 18:43:58 +0200

    Désactivation des tests sur le potentiel de production pour les autres que provence

--- project/test/unit/55PotentielProductionFRETest.php
+++ project/test/unit/55PotentielProductionFRETest.php
@@ -4,10 +4,15 @@ require_once(dirname(__FILE__).'/../bootstrap/common.php');
 
 sfContext::createInstance($configuration);
 
+if($application != 'provence') {
+    $t = new lime_test(0);
+    exit;
+}
+
 $t = new lime_test(8);
 
 $ppmanager = new PotentielProductionManager(null);
-$generator = $ppmanager->getGenerator(); 
+$generator = $ppmanager->getGenerator();
 
 $superficiesRouge = [
     'principaux' =>
