0451dddebf5e9b9979d037518ac79d2e66ddec24
Vincent LAURENT <vince.laurent@gmail.com>
2020-05-20 00:48:43 +0200

    Mode NODELETE

--- project/test/functional/rhone/99suppressionTest.php
+++ project/test/functional/rhone/99suppressionTest.php
@@ -2,6 +2,11 @@
 
 include dirname(__FILE__).'/../../bootstrap/functional.php';
 
+if (getenv("NODELETE")) {
+    $b = new sfTestFunctional(new sfBrowser());
+    exit(0);
+}
+
 foreach (CompteTagsView::getInstance()->listByTags('test', 'test_functionnal') as $k => $v) {
     if (preg_match('/SOCIETE-([^ ]*)/', implode(' ', array_values($v->value)), $m)) {
         $soc = SocieteClient::getInstance()->findByIdentifiantSociete($m[1]);
