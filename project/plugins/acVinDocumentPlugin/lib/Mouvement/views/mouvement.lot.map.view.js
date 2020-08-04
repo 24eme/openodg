0d6416a30200821f903198e35af8b5fe2f5539e2
Tangui Morlier <tangui@tangui.eu.org>
2020-08-04 16:51:26 +0200

    La vue mouvementlot

---
+++ project/plugins/acVinDocumentPlugin/lib/Mouvement/views/mouvement.lot.map.view.js
@@ -0,0 +1,11 @@
+function (doc) {
+  if (!doc.mouvements_lots) {
+    return;
+  }
+  for(identifiant in doc.mouvements_lots) {
+    for(key in doc.mouvements_lots[identifiant]) {
+      lot = doc.mouvements_lots[identifiant][key];
+      emit([lot.prelevable, lot.preleve, lot.region, lot.date, lot.identifiant, lot.origine_document_id], lot);
+    }
+  }
+}
