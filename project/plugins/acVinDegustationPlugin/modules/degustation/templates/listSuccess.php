377c5ad6accd925aced0674748448badfaa13127
Tangui Morlier <tangui@tangui.eu.org>
2020-08-10 23:36:33 +0200

    Ajout du statut prélevé ou non

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/listSuccess.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/listSuccess.php
@@ -9,10 +9,11 @@
 <table class="table table-condensed table-striped">
 <thead>
     <th class="col-sm-2 text-center">Date</th>
-    <th class="col-sm-4">Appellation</th>
+    <th class="col-sm-3">Appellation</th>
     <th class="col-sm-1">Volume</th>
     <th class="col-sm-2 text-center">Étape</th>
-    <th class="col-sm-1 text-center"></th>
+    <th class="col-sm-1"></th>
+    <th class="col-sm-1"></th>
 </thead>
 <tbody>
 <?php foreach($lots as $l): ?>
@@ -22,6 +23,7 @@
         <td class="text-right"><strong><?php echo echoFloat($l->volume); ?>&nbsp;hl</strong></td>
         <td></td>
         <td></td>
+        <td></td>
     </tr>
     <?php foreach($l->steps as $s): ?>
         <tr>
@@ -29,6 +31,7 @@
             <td></td>
             <td></td>
             <td class="text-center"><?php echo preg_replace('/-.*/', '', $s->id_document); ?></td>
+            <td class="text-muted"><?php echo ($s->preleve) ? 'Prélevé' : '' ; ?></td>
             <td><a href="#" class="btn btn-default">Voir</a></td>
         </tr>
     <?php endforeach; ?>
