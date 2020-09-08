50ed31b2fcd698d936f496509bbc3c468f49566a
Tangui Morlier <tangui@tangui.eu.org>
2020-09-08 08:18:37 +0200

    Correction petits bugs

--- project/bin/csv2sql.py
+++ project/bin/csv2sql.py
@@ -1,34 +1,34 @@
 # -*- coding: iso-8859-1 -*
-import sys, pandas as pd
+import sys, os, pandas as pd
 from sqlalchemy import create_engine
 engine = create_engine('sqlite:///'+sys.argv[1], echo=False, encoding='iso-8859-1')
 
-if os.path.exists("etablissements.csv"):
+if os.path.exists(sys.argv[2]+"etablissements.csv"):
     sys.stderr.write("etablissements.csv\n")
     csv = pd.read_csv(sys.argv[2]+"/etablissements.csv", encoding='iso-8859-1', delimiter=";", index_col=False)
     csv.to_sql('etablissement', con=engine, if_exists='replace')
 
-if os.path.exists("chais.csv"):
+if os.path.exists(sys.argv[2]+"chais.csv"):
     sys.stderr.write("chais.csv\n")
     csv = pd.read_csv(sys.argv[2]+"/chais.csv", encoding='iso-8859-1', delimiter=";", index_col=False)
     csv.to_sql('chai', con=engine, if_exists='replace')
 
-if os.path.exists("habilitation.csv"):
+if os.path.exists(sys.argv[2]+"habilitation.csv"):
     sys.stderr.write("habilitation.csv\n")
     csv = pd.read_csv(sys.argv[2]+"/habilitation.csv", encoding='iso-8859-1', delimiter=";", index_col=False)
     csv.to_sql('habilitation', con=engine, if_exists='replace')
 
-if os.path.exists("habilitation_demandes.csv"):
+if os.path.exists(sys.argv[2]+"habilitation_demandes.csv"):
     sys.stderr.write("habilitation_demandes.csv\n")
     csv = pd.read_csv(sys.argv[2]+"/habilitation_demandes.csv", encoding='iso-8859-1', delimiter=";", index_col=False)
     csv.to_sql('habilitation_demandes', con=engine, if_exists='replace')
 
-if os.path.exists("drev.csv"):
+if os.path.exists(sys.argv[2]+"drev.csv"):
     sys.stderr.write("drev.csv\n")
     csv = pd.read_csv(sys.argv[2]+"/drev.csv", encoding='iso-8859-1', delimiter=";", decimal=",", index_col=False)
     csv.to_sql('drev', con=engine, if_exists='replace')
 
-if os.path.exists("dr.csv"):
+if os.path.exists(sys.argv[2]+"dr.csv"):
     sys.stderr.write("dr.csv\n")
     csv = pd.read_csv(sys.argv[2]+"/dr.csv", encoding='iso-8859-1', delimiter=";", decimal=",", index_col=False)
     csv.to_sql('dr', con=engine, if_exists='replace')
