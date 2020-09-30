80715f06bcf72dd1327d66bcc215ed4d52110e68
Vincent LAURENT <vince.laurent@gmail.com>
2020-09-30 18:20:49 +0200

    Le numero d'ordre est déjà incrementé

--- project/plugins/acVinParcellairePlugin/lib/model/Parcellaire/ParcellaireProduit.class.php
+++ project/plugins/acVinParcellairePlugin/lib/model/Parcellaire/ParcellaireProduit.class.php
@@ -123,9 +123,9 @@ class ParcellaireProduit extends BaseParcellaireProduit {
         if($this->detail->exist($key) && $strictNumOrdre) {
           return null;
         }
-        $sameParcelle = $this->getDocument()->countSameParcelle($commune,$section,$numero_parcelle,$lieu, $this->getHash(), $cepage, $campagne_plantation);
-        if(is_null($numero_ordre) && !$strictNumOrdre && $sameParcelle) {
-           $numero_ordre = $sameParcelle + 1;
+        $nbSameParcelle = $this->getDocument()->countSameParcelle($commune,$section,$numero_parcelle,$lieu, $this->getHash(), $cepage, $campagne_plantation);
+        if(is_null($numero_ordre) && !$strictNumOrdre && $nbSameParcelle) {
+           $numero_ordre = $nbSameParcelle;
            $key = $this->calculkey($cepage, $campagne_plantation, $commune, $section, $numero_parcelle, $lieu, $numero_ordre);
         }
 
