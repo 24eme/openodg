abeeaf6eb26931594cf9ea33aa172533d7d59e4b
mathurin petit <mathurin.petit@gmail.com>
2020-10-12 17:20:49 +0200

    ajout de l'ouverture btn de modificatrice pour les drev à lot avec noeud lots et sans lot coté noeud declaration

--- project/plugins/acVinDRevPlugin/modules/drev/templates/_recap.php
+++ project/plugins/acVinDRevPlugin/modules/drev/templates/_recap.php
@@ -109,7 +109,7 @@
         </tbody>
     </table>
 
-<?php if(($sf_user->hasDrevAdmin() || $drev->validation) && count($drev->getProduitsLots()) && $drev->isValidee() && $drev->isModifiable()): ?>
+<?php if(($sf_user->hasDrevAdmin() || $drev->validation) && (count($drev->getProduitsLots()) || count($drev->getLots())) && $drev->isValidee() && $drev->isModifiable()): ?>
 <div class="col-xs-12" style="margin-bottom: 20px;">
   <a onclick="return confirm('Êtes vous sûr de vouloir revendiquer de nouveaux lots IGP ?')" class="btn btn-default pull-right" href="<?php echo url_for('drev_modificative', $drev) ?>">Revendiquer des nouveaux lots IGP</a>
 </div>
