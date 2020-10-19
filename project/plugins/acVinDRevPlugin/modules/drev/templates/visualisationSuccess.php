d61f912d30903ee9917f2854d438b18e5ff45ae1
Tangui Morlier <tangui@tangui.eu.org>
2020-10-19 16:04:41 +0200

    Pas de dévalidation possible pour les Régions + refacto de la dévalidation/réouverture

--- project/plugins/acVinDRevPlugin/modules/drev/templates/visualisationSuccess.php
+++ project/plugins/acVinDRevPlugin/modules/drev/templates/visualisationSuccess.php
@@ -72,13 +72,17 @@
             </a>
     </div>
 
-    <div class="col-xs-offset-2 col-xs-2 text-right">
-        <?php if ($drev->validation && DRevSecurity::getInstance($sf_user, $drev->getRawValue())->isAuthorized(DRevSecurity::DEVALIDATION) && !$drev->isFactures()): ?>
-                    <a class="btn btn-xs btn-default pull-right" href="<?php echo url_for('drev_devalidation', $drev) ?>" onclick="return confirm('Êtes-vous sûr de vouloir dévalider cette DRev ?');"><span class="glyphicon glyphicon-remove-sign"></span>&nbsp;&nbsp;Dévalider</a>
-        <?php elseif ($drev->validation && $sf_user->isAdmin() && !$drev->isLectureSeule() && !$drev->isFactures()): ?>
-                  <a class="btn btn-xs btn-default-step pull-right hidden-xs" onClick="return confirm('Attention, cette DRev a sans doute été facturée. Si vous changez un volume, pensez à en faire part au service comptable !!');" href="<?php echo url_for('drev_devalidation', $drev) ?>"><span class="glyphicon glyphicon-remove-sign"></span>&nbsp;&nbsp;Réouvrir</a>
-        <?php elseif ($drev->isFactures()): ?>
-                  <span class="text-muted">DRev facturée.</span>
+    <div class="col-xs-4 text-right">
+        <?php if ($drev->validation && DRevSecurity::getInstance($sf_user, $drev->getRawValue())->isAuthorized(DRevSecurity::DEVALIDATION)):
+                if (!$drev->validation_odg): ?>
+                    <a class="btn btn-xs btn-default" href="<?php echo url_for('drev_devalidation', $drev) ?>" onclick="return confirm('Êtes-vous sûr de vouloir réouvrir cette DRev ?');"><span class="glyphicon glyphicon-remove-sign"></span>&nbsp;&nbsp;Réouvrir</a>
+            <?php elseif (!$drev->isFactures() && !$drev->isLectureSeule() && $sf_user->isAdmin()): ?>
+                    <a class="btn btn-xs btn-default" href="<?php echo url_for('drev_devalidation', $drev) ?>" onclick="return confirm('Êtes-vous sûr de vouloir dévalider cette DRev ?');"><span class="glyphicon glyphicon-remove-sign"></span>&nbsp;&nbsp;Dévalider</a>
+            <?php elseif ($drev->isFactures()): ?>
+                <span class="text-muted">DRev facturée.</span>
+            <?php elseif($sf_user->isAdmin()): ?>
+                <a class="btn btn-xs btn-default-step hidden-xs" onClick="return confirm('Attention, cette DRev a sans doute été facturée. Si vous changez un volume, pensez à en faire part au service comptable !!');" href="<?php echo url_for('drev_devalidation', $drev) ?>"><span class="glyphicon glyphicon-remove-sign"></span>&nbsp;&nbsp;Dévalider</a>
+            <?php endif; ?>
         <?php endif; ?>
         <?php if(!$drev->validation): ?>
                 <a href="<?php echo url_for("drev_edit", $drev) ?>" class="btn btn-primary"><span class="glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;Continuer la saisie</a>
