67c0c0656157e8e26f485eb3abda5c6c180d66cb
Vincent LAURENT <vince.laurent@gmail.com>
2020-10-20 14:13:12 +0200

    Bannière chemin plus absolu pour les images

--- project/apps/rhone/templates/_header.php
+++ project/apps/rhone/templates/_header.php
@@ -1,9 +1,9 @@
 <header id="header">
     <div class="container" style="position: relative; background-color: #FFF; text-align: center;">
     	<a href="<?php echo url_for('accueil') ?>">
-          <img style="float:left;position:relative" src="images/logo_odgrhone.png" alt="Syndicat des Cotes du Rhone">
+          <img style="float:left;position:relative" src="/images/logo_odgrhone.png" alt="Syndicat des Cotes du Rhone">
           <span style="font-size: 17px;position:absolute;bottom:0;left:350px;"><strong>L'ESPACE PROFESSIONNEL DES VITICULTEURS DES CÔTES DU RHÔNE</strong></span>
-          <img style="width:70%;height:96px;margin-left:70px"src="images/banniere.jpg" class="" alt="syndicat côtes du Rhône">
+          <img style="width:70%;height:96px;margin-left:70px"src="/images/banniere.jpg" class="" alt="syndicat côtes du Rhône">
     	</a>
     </div>
 </header>
