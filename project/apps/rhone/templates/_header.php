1f948abd3111570062dfed2ea950437557e56380
Mame <mngom@24eme.fr>
2020-10-20 12:29:17 +0200

    Changement fond

--- project/apps/rhone/templates/_header.php
+++ project/apps/rhone/templates/_header.php
@@ -1,5 +1,5 @@
 <header id="header">
-    <div class="container" style="position: relative; background-color: #FFF; text-align: center;">
+    <div class="container" style="position: relative; background-color: transparent; text-align: center;">
     	<a href="<?php echo url_for('accueil') ?>">
           <img style="float:left;position:relative" src="images/logo_odgrhone.png" alt="Syndicat des Cotes du Rhone">
           <span style="font-size: 17px;position:absolute;bottom:0;left:350px;"><strong>L'ESPACE PROFESSIONNEL DES VITICULTEURS DES CÔTES DU RHÔNE</strong></span>
