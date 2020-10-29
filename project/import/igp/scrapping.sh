c2397922a0ad91ff2196f7ee21940214b10907bc
Honorine-MC <honorine.mariechristy@gmail.com>
2020-10-29 12:41:12 +0100

    supprimer le dossier s'il existe déjà

--- project/import/igp/scrapping.sh
+++ project/import/igp/scrapping.sh
@@ -1,2 +1,6 @@
 mkdir -p imports
+if [ -d "imports/$(cat config.json | jq '.file_name' | sed s/\"//g)" ]; then
+  rm -r "imports/$(cat config.json | jq '.file_name' | sed s/\"//g)";
+fi
 node scrapping.js
+bash script_verify.sh
