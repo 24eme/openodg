934d506279b4c7617538c9744e9f86ab55452e3b
Honorine-MC <honorine.mariechristy@gmail.com>
2020-10-30 09:58:27 +0100

    ajout du script pour les cépages

--- project/import/igp/scrapping.sh
+++ project/import/igp/scrapping.sh
@@ -3,4 +3,5 @@ if [ -d "imports/$(cat config.json | jq '.file_name' | sed s/\"//g)" ]; then
   rm -r "imports/$(cat config.json | jq '.file_name' | sed s/\"//g)";
 fi
 node scrapping.js
+node scrapping_cepages.js
 bash script_verify.sh
