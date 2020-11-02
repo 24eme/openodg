4b9621df03b29640514634a970ac1a544a0b946e
Moussa <adjimahamat@gmail.com>
2020-11-02 10:17:07 +0100

    Revert "Revert "Drev: deplacer les champs modifiables à gauche | modifier les info tel, email""
    This reverts commit cd7e6b580ee4ad73f4766174cd6a0eff39dea108.

--- project/apps/ava/modules/etablissement/templates/_form.php
+++ project/apps/ava/modules/etablissement/templates/_form.php
@@ -1,8 +1,8 @@
 <?php echo $form->renderHiddenFields(); ?>
 <?php echo $form->renderGlobalErrors(); ?>
 <div class="row">
-    <div id="row_form_exploitation" class="row col-xs-offset-1 col-xs-10 <?php if(!$form->isBound()): ?>hidden<?php endif; ?>">
-        <div class="col-xs-5">
+    <div id="row_form_exploitation" class="row col-xs-12 <?php if(!$form->isBound()): ?>hidden<?php endif; ?>">
+        <div class="col-xs-7">
             <?php if($etablissement->cvi): ?>
             <div class="form-group">
                 <strong class="col-xs-3 text-right">N°&nbsp;CVI</strong>
@@ -29,66 +29,67 @@
             <?php endforeach; ?>
             <?php endif; ?>
             <div class="form-group">
-                <strong class="col-xs-4 text-right">Raison Sociale</strong>
-                <span class="col-xs-8">
+                <strong class="col-xs-3 text-right">Raison Sociale</strong>
+                <span class="col-xs-9">
                    <?php echo $etablissement->raison_sociale; ?>
                 </span>
             </div>
             <div class="form-group">
-                <strong class="col-xs-4 text-right">Adresse</strong>
-                <span class="col-xs-8">
+                <strong class="col-xs-3 text-right">Adresse</strong>
+                <span class="col-xs-9">
                    <?php echo $etablissement->adresse; ?>
                 </span>
             </div>
+
             <div class="form-group">
-                <strong class="col-xs-4 text-right">Commune</strong>
-                <span class="col-xs-8">
+                <strong class="col-xs-3 text-right">Commune</strong>
+                <span class="col-xs-9">
                    <?php echo $etablissement->commune; ?>
                 </span>
             </div>
             <div class="form-group">
-                <strong class="col-xs-4 text-right">Code Postal</strong>
-                <span class="col-xs-8">
+                <strong class="col-xs-3 text-right">Code postal</strong>
+                <span class="col-xs-9">
                    <?php echo $etablissement->code_postal; ?>
                 </span>
             </div>
 
         </div>
-        <div class="col-xs-7">
+        <div class="col-xs-5">
 
             <div class="form-group<?php if($form["telephone_bureau"]->hasError()): ?> has-error<?php endif; ?>">
                 <?php echo $form["telephone_bureau"]->renderError(); ?>
-                <?php echo $form["telephone_bureau"]->renderLabel(null, array("class" => "col-xs-4 control-label")); ?>
-                <div class="col-xs-8">
+                <?php echo $form["telephone_bureau"]->renderLabel(null, array("class" => "col-xs-3 control-label")); ?>
+                <div class="col-xs-9">
                     <?php echo $form["telephone_bureau"]->render(array("class" => "form-control")); ?>
                 </div>
             </div>
             <div class="form-group<?php if($form["telephone_mobile"]->hasError()): ?> has-error<?php endif; ?>">
                 <?php echo $form["telephone_mobile"]->renderError(); ?>
-                <?php echo $form["telephone_mobile"]->renderLabel(null, array("class" => "col-xs-4 control-label")); ?>
-                <div class="col-xs-8">
+                <?php echo $form["telephone_mobile"]->renderLabel(null, array("class" => "col-xs-3 control-label")); ?>
+                <div class="col-xs-9">
                     <?php echo $form["telephone_mobile"]->render(array("class" => "form-control")); ?>
                 </div>
             </div>
             <div class="form-group<?php if($form["telephone_prive"]->hasError()): ?> has-error<?php endif; ?>">
                 <?php echo $form["telephone_prive"]->renderError(); ?>
-                <?php echo $form["telephone_prive"]->renderLabel(null, array("class" => "col-xs-4 control-label")); ?>
-                <div class="col-xs-8">
+                <?php echo $form["telephone_prive"]->renderLabel(null, array("class" => "col-xs-3 control-label")); ?>
+                <div class="col-xs-9">
                     <?php echo $form["telephone_prive"]->render(array("class" => "form-control")); ?>
                 </div>
             </div>
             <div class="form-group<?php if($form["fax"]->hasError()): ?> has-error<?php endif; ?>">
                 <?php echo $form["fax"]->renderError(); ?>
-                <?php echo $form["fax"]->renderLabel(null, array("class" => "col-xs-4 control-label")); ?>
-                <div class="col-xs-8">
+                <?php echo $form["fax"]->renderLabel(null, array("class" => "col-xs-3 control-label")); ?>
+                <div class="col-xs-9">
                     <?php echo $form["fax"]->render(array("class" => "form-control")); ?>
                 </div>
             </div>
             <?php if(isset($form["email"])): ?>
             <div class="form-group<?php if($form["email"]->hasError()): ?> has-error<?php endif; ?>">
                 <?php echo $form["email"]->renderError(); ?>
-                <?php echo $form["email"]->renderLabel(null, array("class" => "col-xs-4 control-label")); ?>
-                <div class="col-xs-8">
+                <?php echo $form["email"]->renderLabel(null, array("class" => "col-xs-3 control-label")); ?>
+                <div class="col-xs-9">
                     <?php echo $form["email"]->render(array("class" => "form-control")); ?>
                 </div>
             </div>
@@ -96,7 +97,7 @@
         </div>
     </div>
     <div id="row_info_exploitation" class="row col-xs-12 <?php if($form->isBound()): ?>hidden<?php endif; ?>">
-        <div class="col-xs-5">
+        <div class="col-xs-7">
             <?php if($etablissement->cvi): ?>
             <div class="form-group">
                 <strong class="col-xs-3 text-right">N°&nbsp;CVI</strong>
@@ -122,31 +123,31 @@
         <?php endforeach; ?>
         <?php endif; ?>
         <div class="form-group">
-            <strong class="col-xs-4 text-right">Raison Sociale</strong>
-            <span class="col-xs-8">
+            <strong class="col-xs-3 text-right">Raison Sociale</strong>
+            <span class="col-xs-9">
                <?php echo $etablissement->raison_sociale; ?>
             </span>
         </div>
         <div class="form-group">
-            <strong class="col-xs-4 text-right">Adresse</strong>
-            <span class="col-xs-8">
+            <strong class="col-xs-3 text-right">Adresse</strong>
+            <span class="col-xs-9">
                <?php echo $etablissement->adresse; ?>
             </span>
         </div>
         <div class="form-group">
-            <strong class="col-xs-4 text-right">Commune</strong>
-            <span class="col-xs-8">
+            <strong class="col-xs-3 text-right">Commune</strong>
+            <span class="col-xs-9">
                <?php echo $etablissement->commune; ?>
             </span>
         </div>
         <div class="form-group">
-            <strong class="col-xs-4 text-right">Code Postal</strong>
-            <span class="col-xs-8">
+            <strong class="col-xs-3 text-right">Code Postal</strong>
+            <span class="col-xs-9">
                <?php echo $etablissement->code_postal; ?>
             </span>
         </div>
         </div>
-        <div class="col-xs-7">
+        <div class="col-xs-5">
 
             <div class="form-group">
                 <strong class="col-xs-4 text-right">Tél. Fixe</strong>
