43819ed3b7f8d6d17303969fe9f80484c6af85f4
Vincent LAURENT <vince.laurent@gmail.com>
2020-11-06 11:50:13 +0100

    Revert "Habilitation AVA: migrer l'intelligence de getProduitsConfig dans la class Habilitation"
    This reverts commit 26553303384357f94ae32f2ae2c1ae7959467968.

--- project/plugins/acVinHabilitationPlugin/lib/model/Habilitation/Habilitation.class.php
+++ project/plugins/acVinHabilitationPlugin/lib/model/Habilitation/Habilitation.class.php
@@ -69,14 +69,8 @@ class Habilitation extends BaseHabilitation implements InterfaceProduitsDocument
     }
 
     public function getProduitsConfig() {
-      $produits = array();
-      foreach ($this->getConfiguration()->declaration->getProduitsFilter(_ConfigurationDeclaration::TYPE_DECLARATION_DREV_REVENDICATION_CEPAGE) as $produit) {
-          if($produit->getNodeCahierDesCharges()){
-            $produitAppellation = $produit->getNodeCahierDesCharges();
-            $produits[$produitAppellation->getHash()] = $produitAppellation;
-          }
-      }
-      return $produits;
+
+        return $this->getConfiguration()->getProduitsCahierDesCharges();
     }
 
     public function getProduitsHabilites() {
