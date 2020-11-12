b1d84273e383a69b1304b0dbe2bea8f9a882171d
Honorine-MC <honorine.mariechristy@gmail.com>
2020-11-12 14:44:11 +0100

    script scrapping des membres innactifs

---
+++ project/import/igp/scrapping_membres_innactifs.js
@@ -0,0 +1,43 @@
+var Nightmare = require('nightmare');
+var fs = require('fs');
+const nightmare = Nightmare({ show: false
+})
+var config = require('./config.json');
+var destination_file='imports/'+config.file_name+'/';
+
+
+nightmare
+
+  //authentification
+  .goto(config.web_site)
+  .type('#LoginPhp',config.user_name)
+  .type('#PasswordPhp',config.user_password)
+  .click('#identification')
+  .wait('.menu')
+  //fin authentification
+
+
+  //list_membres_innactifs
+
+  .evaluate(()=>{
+    var elements = Array.from(document.querySelectorAll('a'))
+    elements[122].className += "list_membres_innactifs";
+  })
+  .click('.list_membres_innactifs')
+  .wait('#Button1')
+  .click('#Button1')
+  .wait('#gvMembre')
+  .evaluate(()=>{
+    var elements = document.querySelector('#gvMembre').innerText
+    return elements;
+    })
+  .end()
+  .then((text) => {
+    fs.writeFileSync(destination_file+'list_membres_innactifs.txt',text);
+  })
+
+  //fin list_membres_innactifs
+
+    .catch(error => {
+      console.error('Search failed:', error)
+    })
