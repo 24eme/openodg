d773dd96f38b0e6adf016d11fa69dac4220479c6
Honorine-MC <honorine.mariechristy@gmail.com>
2020-11-12 16:03:20 +0100

    changement de nom de fichier

--- project/import/igp/scrapping_membres_innactifs.js
+++ project/import/igp/scrapping_membres_innactifs.js
@@ -21,9 +21,9 @@ nightmare
 
   .evaluate(()=>{
     var elements = Array.from(document.querySelectorAll('a'))
-    elements[122].className += "list_membres_innactifs";
+    elements[122].className += "membres_innactifs";
   })
-  .click('.list_membres_innactifs')
+  .click('.membres_innactifs')
   .wait('#Button1')
   .click('#Button1')
   .wait('#gvMembre')
@@ -33,7 +33,7 @@ nightmare
     })
   .end()
   .then((text) => {
-    fs.writeFileSync(destination_file+'list_membres_innactifs.txt',text);
+    fs.writeFileSync(destination_file+'membres_innactifs.txt',text);
   })
 
   //fin list_membres_innactifs
