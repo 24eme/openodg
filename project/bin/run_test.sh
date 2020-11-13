5b3b8c6e56c3520a0b327e4cd9982cd759b6962a
Vincent LAURENT <vince.laurent@gmail.com>
2020-11-14 00:47:07 +0100

    Commande intégratio continue utilisé dans run test

--- project/bin/run_test.sh
+++ project/bin/run_test.sh
@@ -1,13 +1,25 @@
 . $(echo $0 | sed 's/[^\/]*$//')config.inc
 
-if ! test "$WORKINGDIR"; then
-    WORKINGDIR=$(dirname $0)"/../"
-fi
-
 if [ "$(echo $COUCHTEST | grep -E _test$)" == "" ]
 then
     echo "La base COUCHTEST ($COUCHTEST) ne semble pas être une base de test ( doit se terminer par \"_test\", par exemple http://localhost:5984/giilda_app_test )"
     exit;
+fi
+
+TYPE_TEST="unit"
+
+while getopts ":x:t:" flag
+do
+    case "${flag}" in
+        x) XMLFILE=${OPTARG};;
+        t) TYPE_TEST=${OPTARG};;
+    esac
+done
+
+shift $((OPTIND-1))
+
+if ! test "$WORKINGDIR"; then
+    WORKINGDIR=$(dirname $0)"/../"
 fi
 
 APPLICATION=$1
@@ -16,17 +28,11 @@ if [ ! $APPLICATION ]
 then
     echo "Vous devez définir une application en argument :"
     echo ;
-    echo "$0 <APPLICATION> (<NOM_TEST>) (unit|functional)";
+    echo "$0 [-t <unit|functional>] [-x <fichier xml de sortie>] [<application>] [<nom du test>]) ";
     exit;
 fi
 
 NOM_TEST=$2
-TYPE_TEST="unit"
-
-if [ $3 ]
-then
-    TYPE_TEST=$3
-fi
 
 if [ $NOM_TEST ] && [ $TYPE_TEST == "unit" ]
 then
@@ -45,20 +51,20 @@ then
     exit;
 fi
 
-curl -s -X DELETE $COUCHTEST
-curl -s -X PUT $COUCHTEST  || ( echo "connexion à $COUCHTEST impossible"  ;  exit 2 )
+curl -s -X DELETE $COUCHTEST > /dev/null
+curl -s -X PUT $COUCHTEST > /dev/null || ( echo "connexion à $COUCHTEST impossible"  ;  exit 2 )
 
 cd ..
-make clean
-make couchurl=$COUCHTEST
-cd -
+make clean > /dev/null
+make couchurl=$COUCHTEST > /dev/null
+cd - > /dev/null
 
 ls $WORKINGDIR"/data/configuration/"$APPLICATION | while read jsonFile
 do
-    curl -s -X POST -d @data/configuration/$APPLICATION/$jsonFile -H "content-type: application/json" $COUCHTEST
+    curl -s -X POST -d @data/configuration/$APPLICATION/$jsonFile -H "content-type: application/json" $COUCHTEST > /dev/null
 done
 
-rm -rf cache/*
-php symfony cc
+rm -rf cache/* > /dev/null
+php symfony cc > /dev/null
 
-APPLICATION=$APPLICATION COUCHURL=$COUCHTEST NODELETE=1 php symfony test:all
+APPLICATION=$APPLICATION COUCHURL=$COUCHTEST NODELETE=1 php symfony test:all --xml=$XMLFILE
