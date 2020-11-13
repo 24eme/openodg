2838cdc92f416c80c1f940d99f446aac75e897ed
Tangui Morlier <tangui@tangui.eu.org>
2020-11-13 10:42:50 +0100

    Liste des lots que pour la page IGP

--- project/plugins/acVinDRevPlugin/modules/drev/templates/_pdf.php
+++ project/plugins/acVinDRevPlugin/modules/drev/templates/_pdf.php
@@ -93,7 +93,7 @@ Les produits déclarés sont du millésime du VCI
 <?php endif; ?>
 <?php endif; ?>
 
-<?php if($drev->exist('lots') && count($drev->lots)): ?>
+<?php if(count($drev->declaration->getProduitsLots($region))): ?>
 <br />
 <div><span class="h3">&nbsp;Déclaration des lots&nbsp;</span></div>
 <table border="1" class="table" cellspacing=0 cellpadding=0 style="text-align: right;">
