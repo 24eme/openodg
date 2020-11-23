a04a18a2830a47fb8c7dc9ff734d863e6f7cded6
Moussa <adjimahamat@gmail.com>
2020-11-23 16:39:53 +0100

    Degustation IGP: ajouter la spécificité dans le lot et mvtLot Dégustation | afficher la spécificité à l'etape prelevement Lots

--- project/plugins/acVinDegustationPlugin/lib/Degustation/Degustation.class.php
+++ project/plugins/acVinDegustationPlugin/lib/Degustation/Degustation.class.php
@@ -178,6 +178,9 @@ class Degustation extends BaseDegustation implements InterfacePieceDocument, Int
 			$mvt->destination_date = $lot->destination_date;
 			$mvt->details = $lot->details;
 			$mvt->campagne = $this->campagne;
+			if(DRevConfiguration::getInstance()->hasSpecificiteLot()){
+				$mvt->specificite = $lot->specificite;
+			}
 			return $mvt;
 	}
 
