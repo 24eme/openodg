8feb06424ee3a85217de6d43cf678b487dd1b03c
Tangui Morlier <tangui@tangui.eu.org>
2020-11-25 10:39:29 +0100

    convertisseur vers formation inao

---
+++ project/bin/import/quali2inao.pl
@@ -0,0 +1,17 @@
+while(<STDIN>) {
+    @l = split(/;/);
+    @d = [];
+    $d[0] = $l[0];  #Produits
+    $d[3] = $l[13]; #CVI
+    $d[4] = $l[12]; #SIRET
+    $d[17] = $l[5]; #Date enregistrement
+    $d[18] = $l[6]; #date dépot
+    $d[19] = 'x' if ($l[3] =~ /A/); #Producteur de raison
+    $d[20] = 'x' if ($l[3] =~ /P/); #Producteur de mouts
+    $d[21] = 'x' if ($l[3] =~ /B/); #vinificateur
+    $d[22] = 'x' if ($l[3] =~ /D/); #conditionneur
+    $d[24] = 'x' if ($l[3] =~ /L/); #Elaboration
+    $d[40] = '';
+    print join(';', @d);
+    print "\n";
+}
