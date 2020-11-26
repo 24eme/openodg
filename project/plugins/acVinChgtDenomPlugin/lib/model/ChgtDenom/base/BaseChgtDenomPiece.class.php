6c7c8f169dce8489652d75cd6c4a3f5ad8cb140f
mathurin petit <mathurin.petit@gmail.com>
2020-11-26 14:59:16 +0100

    mis en place des mouvements de facture au sein du plugin acVinChgDenom

--- project/plugins/acVinChgtDenomPlugin/lib/model/ChgtDenom/base/BaseChgtDenomPiece.class.php
+++ project/plugins/acVinChgtDenomPlugin/lib/model/ChgtDenom/base/BaseChgtDenomPiece.class.php
@@ -1,11 +1,39 @@
 <?php
+/**
+ * BaseChgtDenomPiece
+ * 
+ * Base model for ChgtDenomPiece
 
+ * @property string $identifiant
+ * @property string $date_depot
+ * @property string $libelle
+ * @property string $mime
+ * @property integer $visibilite
+ * @property string $source
+ * @property acCouchdbJson $fichiers
+
+ * @method string getIdentifiant()
+ * @method string setIdentifiant()
+ * @method string getDateDepot()
+ * @method string setDateDepot()
+ * @method string getLibelle()
+ * @method string setLibelle()
+ * @method string getMime()
+ * @method string setMime()
+ * @method integer getVisibilite()
+ * @method integer setVisibilite()
+ * @method string getSource()
+ * @method string setSource()
+ * @method acCouchdbJson getFichiers()
+ * @method acCouchdbJson setFichiers()
+ 
+ */
 
 abstract class BaseChgtDenomPiece extends Piece {
-
+                
     public function configureTree() {
        $this->_root_class_name = 'ChgtDenom';
        $this->_tree_class_name = 'ChgtDenomPiece';
     }
-
+                
 }
