1ae4e856d7442a03ff845d80d7675fa15ed90ca9
Vincent LAURENT <vince.laurent@gmail.com>
2020-12-07 15:38:56 +0100

    Index.php des applis igp

--- project/web/igpmed_dev.php
+++ project/web/igpmed_dev.php
@@ -7,5 +7,5 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('igp13', 'dev', true);
+$configuration = ProjectConfiguration::getApplicationConfiguration('igpmed', 'dev', true);
 sfContext::createInstance($configuration)->dispatch();
