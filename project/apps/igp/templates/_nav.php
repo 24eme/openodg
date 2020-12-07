ef6963ed951f574b90d918b096e595761a08ec27
Tangui Morlier <tangui@tangui.eu.org>
2020-12-07 18:20:21 +0100

    Activation du parcellaire + _nav

--- project/apps/igp/templates/_nav.php
+++ project/apps/igp/templates/_nav.php
@@ -2,23 +2,25 @@
 <?php $etablissement = null ?>
 <?php $compte = null; ?>
 
-<?php if ($route instanceof EtablissementRoute): ?>
+<?php if($route instanceof EtablissementRoute): ?>
     <?php $etablissement = $route->getEtablissement(); ?>
-    <?php $compte =  $etablissement->getMasterCompte(); ?>
-<?php endif; ?>
-<?php if ($route instanceof SocieteRoute): ?>
-    <?php $societe = $route->getSociete(); ?>
-    <?php $compte =  $societe->getMasterCompte(); ?>
+    <?php $compte = $etablissement->getMasterCompte(); ?>
 <?php endif; ?>
 <?php if ($route instanceof FacturationDeclarantRoute || $route instanceof FactureRoute || $route instanceof CompteRoute): ?>
     <?php $compte = $route->getCompte(); ?>
     <?php $etablissement = $compte->getEtablissement(); ?>
+<?php endif; ?>
+<?php if ($route instanceof SocieteRoute): ?>
+    <?php $societe = $route->getSociete(); ?>
+    <?php $etablissement = $route->getEtablissement(); ?>
+    <?php $compte =  $societe->getMasterCompte(); ?>
 <?php endif; ?>
 
 <?php if($sf_user->isAuthenticated() && !$sf_user->hasCredential(myUser::CREDENTIAL_ADMIN) && (!$compte || !$etablissement)): ?>
     <?php $compte = $sf_user->getCompte(); ?>
     <?php $etablissement = $compte->getSociete()->getEtablissementPrincipal(); ?>
 <?php endif; ?>
+
 
 <nav id="menu_navigation" class="navbar navbar-default container">
         <div class="navbar-header hidden-lg hidden-md">
@@ -38,6 +40,7 @@
                 <li class="<?php if($route instanceof InterfaceDegustationGeneralRoute): ?>active<?php endif; ?>"><a href="<?php if($etablissement) echo url_for('degustation_etablissement_list', array('id' => $etablissement->identifiant)); else echo url_for('degustation'); ?>">Dégustation</a></li>
                 <li class="<?php if($route instanceof InterfaceHabilitationRoute): ?>active<?php endif; ?>"><a href="<?php if($etablissement  && !$route instanceof InterfaceHabilitationRoute): ?><?php echo url_for('habilitation_declarant', $etablissement); ?><?php else: ?><?php echo url_for('habilitation'); ?><?php endif; ?>">Habilitations</a></li>
                 <li class="<?php if($route instanceof InterfaceDocumentsRoute): ?>active<?php endif; ?>"><a href="<?php if($etablissement  && !$route instanceof InterfaceDocumentsRoute): ?><?php echo url_for('pieces_historique', $etablissement); ?><?php else: ?><?php echo url_for('documents'); ?><?php endif; ?>">Documents</a></li>
+                <li class="<?php if($route instanceof InterfaceParcellaireRoute): ?>active<?php endif; ?>"><a href="<?php if($etablissement && !$route instanceof InterfaceParcellaireRoute): ?><?php echo url_for('parcellaire_declarant', $etablissement); ?><?php else: ?><?php echo url_for('parcellaire'); ?><?php endif; ?>">Parcellaire</a></li>
                 <li class="<?php if($route instanceof InterfaceFacturationRoute): ?>active<?php endif; ?>"><a href="<?php if($compte  && !$route instanceof InterfaceFacturationRoute): ?><?php echo url_for('facturation_declarant', $compte); ?><?php else: ?><?php echo url_for('facturation'); ?><?php endif; ?>">Facturation</a></li>
                 <li class="<?php if($route instanceof InterfaceCompteRoute && !$route instanceof FacturationDeclarantRoute): ?>active<?php endif; ?>"><a href="<?php if($compte && !$route instanceof InterfaceCompteRoute): ?><?php echo url_for('compte_visualisation', $compte); ?><?php else: ?><?php echo url_for('compte_search'); ?><?php endif; ?>">Contacts</a></li>
             </ul>
