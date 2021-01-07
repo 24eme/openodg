80aeb1ab39446c2603ed64f4d10476b48332a70c
Vincent LAURENT <vince.laurent@gmail.com>
2021-01-07 23:35:28 +0100

    Héritage entre la tâche d'import des dégustations avec celle des lots

--- project/plugins/acVinDegustationPlugin/lib/task/ImportDegustationIATask.class.php
+++ project/plugins/acVinDegustationPlugin/lib/task/ImportDegustationIATask.class.php
@@ -1,90 +1,7 @@
 <?php
 
-class ImportDegustationIATask extends sfBaseTask
+class ImportDegustationIATask extends ImportLotsIATask
 {
-  const CSV_NUM_DOSSIER = 0;
-  const CSV_NUM_LOT_ODG = 1;
-  const CSV_CVI = 2;
-  const CSV_RAISON_SOCIALE = 3;
-  const CSV_NOM = 4;
-  const CSV_ADRESSE_1 = 5;
-  const CSV_ADRESSE_2 = 6;
-  const CSV_CODE_POSTAL = 7;
-  const CSV_VILLE = 8;
-  const CSV_FAX = 9;
-  const CSV_TELEPHONE = 10;
-  const CSV_FAMILLE = 11;
-  const CSV_NUM_LOT_OPERATEUR = 12;
-  const CSV_TYPE = 13;
-  const CSV_APPELLATION = 14;
-  const CSV_COULEUR = 15;
-  const CSV_CEPAGE_1 = 16;
-  const CSV_POURCENT_CEPAGE_1 = 17;
-  const CSV_CEPAGE_2 = 18;
-  const CSV_POURCENT_CEPAGE_2 = 19;
-  const CSV_CEPAGE_3 = 20;
-  const CSV_POURCENT_CEPAGE_3 = 21;
-  const CSV_MILLESIME = 22;
-  const CSV_CAMPAGNE = 23;
-  const CSV_VOLUME_RESIDUEL = 24;
-  const CSV_VOLUME_INITIAL = 25;
-  const CSV_DESTINATION = 26;
-  const CSV_TRANSACTION_DATE = 27;
-  const CSV_CONF = 28;
-  const CSV_PRELEVE = 29;
-  const CSV_STATUT = 30;
-  const CSV_DATE_COMMISSION = 31;
-  const CSV_DATE_VALIDATION = 32;
-  const CSV_NOM_SITE = 33;
-  const CSV_ADRESSE_1_SITE = 34;
-  const CSV_ADRESSE_2_SITE = 35;
-  const CSV_CODE_POSTAL_SITE = 36;
-  const CSV_VILLE_SITE = 37;
-  const CSV_EMAIL = 38;
-
-  const TYPE_REVENDIQUE = 'R';
-
-  const STATUT_PRELEVE = "PRELEVE";
-  const STATUT_PRELEVABLE = "PRELEVE";
-  const STATUT_DEGUSTE = "DEGUSTE";
-  const STATUT_CONFORME = "CONFORME";
-  const STATUT_NONCONFORME = "NON_CONFORME";
-  const STATUT_CHANGE = "CHANGE";
-  const STATUT_DECLASSE = "DECLASSE";
-
-  protected $date;
-  protected $convert_statut;
-  protected $convert_activites;
-  protected $etablissements;
-  protected $produits;
-  protected $cepages;
-
-  public static $types = array('B' => DRevClient::LOT_DESTINATION_CONDITIONNEMENT, 'VF' => DRevClient::LOT_DESTINATION_VRAC_FRANCE, 'VHF' => DRevClient::LOT_DESTINATION_VRAC_EXPORT);
-  public static $correspondancesCepages = array(
-    "Cabernet sauvignon N" => "CAB-SAUV-N",
-    "Chardonnay B" => "CHARDONN.B",
-    "Cinsault N" => "CINSAUT N",
-    "Clairette B" => "CLAIRET.B",
-    "Mourvèdre N" => "MOURVED.N",
-    "Muscat à petits grains B" => "MUS.PT.G.B",
-    "Muscat à petits grains Rs" => "MUS.P.G.RS",
-    "Muscat d'Hambourg N" => "MUS.HAMB.N",
-    "Muscat PG B" => "MUS.PT.G.B",
-    "Nielluccio N" => "NIELLUC.N",
-    "Sauvignon B" => "SAUVIGN.B",
-    "Savagnin Blanc B" => "SAVAGN.B",
-    "Vermentino B" => "VERMENT.B"
-  );
-    public static $correspondancesStatuts = array(
-      "Conforme" => self::STATUT_CONFORME,
-      "Déclassé" => self::STATUT_DECLASSE,
-      "Non Conforme" => self::STATUT_NONCONFORME,
-      "Prélevé A" => self::STATUT_PRELEVE,
-      "Prélevé NA" => self::STATUT_PRELEVE,
-      "Prévu" => self::STATUT_PRELEVE,
-      "Revendiqué C" => array(self::STATUT_PRELEVABLE, self::STATUT_PRELEVE),
-      "Revendiqué NC" => array(self::STATUT_PRELEVABLE, self::STATUT_PRELEVE)
-    );
 
     protected function configure()
     {
@@ -236,46 +153,5 @@ EOF;
       return $d;
     }
 
-    protected function clearProduitKey($key) {
-      $key = str_replace('PAYS-DES-', '', $key);
-      return $key;
-    }
 
-    protected function identifyEtablissement($data) {
-        foreach ($this->etablissements as $etab) {
-            if (isset($data[self::CSV_CVI]) && trim($data[self::CSV_CVI]) && $etab->key[EtablissementAllView::KEY_CVI] == trim($data[self::CSV_CVI])) {
-                return EtablissementClient::getInstance()->find($etab->id);
-                break;
-            }
-            if (isset($data[self::CSV_RAISON_SOCIALE]) && trim($data[self::CSV_RAISON_SOCIALE]) && KeyInflector::slugify($etab->key[EtablissementAllView::KEY_NOM]) == KeyInflector::slugify(trim($data[self::CSV_RAISON_SOCIALE]))) {
-                return EtablissementClient::getInstance()->find($etab->id);
-                break;
-            }
-            if (isset($data[self::CSV_RAISON_SOCIALE]) && trim($data[self::CSV_RAISON_SOCIALE]) && KeyInflector::slugify($etab->value[EtablissementAllView::VALUE_RAISON_SOCIALE]) == KeyInflector::slugify(trim($data[self::CSV_RAISON_SOCIALE]))) {
-                return EtablissementClient::getInstance()->find($etab->id);
-                break;
-            }
-            if (isset($data[self::CSV_NOM]) && trim($data[self::CSV_NOM]) && KeyInflector::slugify($etab->key[EtablissementAllView::KEY_NOM]) == KeyInflector::slugify(trim($data[self::CSV_NOM]))) {
-                return EtablissementClient::getInstance()->find($etab->id);
-                break;
-            }
-            if (isset($data[self::CSV_NOM]) && trim($data[self::CSV_NOM]) && KeyInflector::slugify($etab->value[EtablissementAllView::VALUE_RAISON_SOCIALE]) == KeyInflector::slugify(trim($data[self::CSV_NOM]))) {
-                return EtablissementClient::getInstance()->find($etab->id);
-                break;
-            }
-        }
-        return null;
-    }
-
-    public function initProduitsCepages() {
-      $this->produits = array();
-      $this->cepages = array();
-      $produits = ConfigurationClient::getInstance()->getConfiguration()->declaration->getProduits();
-      foreach ($produits as $key => $produit) {
-        $this->produits[KeyInflector::slugify($produit->getLibelleFormat())] = $produit;
-        foreach($produit->getCepagesAutorises() as $ca) {
-          $this->cepages[KeyInflector::slugify($ca)] = $ca;
-        }
-      }
-    }
 }
