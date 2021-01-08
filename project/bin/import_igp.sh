8ed7ce71754b13c6eec8c5c8495684aab9e3acdc
Vincent LAURENT <vince.laurent@gmail.com>
2021-01-08 23:23:24 +0100

    Import des lots de dégustations

--- project/bin/import_igp.sh
+++ project/bin/import_igp.sh
@@ -90,11 +90,11 @@ echo "Lots"
 xlsx2csv -l '\r\n' -d ";" $DATA_DIR/lots.xlsx | tr -d "\n" | tr "\r" "\n" | sort -t ";" -k 3,4 -k 24 > $DATA_DIR/lots.csv
 php symfony import:lots-ia $DATA_DIR/lots.csv --application="$ODG" --trace
 
-
 echo "Degustations"
 # trie des lots par date de commission pour dire qu'une date correspond à une degustation.
 sort -t";" -k32.7,32.10 -k32.4,32.5 -k32.1,32.2 $DATA_DIR/lots.csv  > $DATA_DIR/lots_sort_by_date.csv
-# j'ai commenté l'import car il y a un problème avec le schema des mouvement de lots dans une DREV car certains comporte le champs numero_cuve et d'autres non or j'identifie la DREV en question pour une Degustation
-# par le numéro de cuve il y a donc des erreurs ça serait bien d'identifié la DREV d'une Degustation grace à la campagne, numéro de dossier et numéro de lot et non seulement avec le numéro de cuve (ligne 180 de ImportDegustationIATask)
 # il y a également un problème avec le nom de lieu j'ai mis SYNDICAT-VIGNERONS-ARLES par défaut mais il faudra le changer en fonction de ce qu'il y a en base pour chaque lot ce champs n'est pas dans le csv exporté.
-# php symfony import:degustations-ia $DATA_DIR/lots_sort_by_date.csv --application="$ODG" --trace
+php symfony import:degustations-ia $DATA_DIR/lots_sort_by_date.csv --application="$ODG" --trace
+
+xlsx2csv -l '\r\n' -d ";" $DATA_DIR/gestion_nc.xlsx | tr -d "\n" | tr "\r" "\n" > $DATA_DIR/gestion_nc.csv
+php symfony import:degustations-non-conformite-ia $DATA_DIR/gestion_nc.csv --application="$ODG" --trace
