cc7c17c45f4722b06b5d5cf75755ef79360d9aae
2maj <adjimahamat@gmail.com>
2021-01-12 11:18:39 +0100

    Degust prélèvement:total lots prélevés sans leurre

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/_synthese.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/_synthese.php
@@ -1,11 +1,11 @@
-<div class="alert alert-info" role="alert" style="padding:10px;padding-top:5px;"> 
+<div class="alert alert-info" role="alert" style="padding:10px;padding-top:5px;">
   <h3><?php echo ucfirst(format_date($degustation->date, "P", "fr_FR"))." à ".format_date($degustation->date, "H")."h".format_date($degustation->date, "mm") ?></h3>
   <h4>Lieu : <?php echo $degustation->getLieuNom(); ?></h4>
   <table class="table table-condensed">
     <tbody>
       <tr class="vertical-center">
         <td class="col-xs-3" >Nombre total de <strong>lots prévus&nbsp;:</strong></td>
-        <td class="col-xs-9"><strong><?php echo $infosDegustation["nbLots"]; ?></strong></td>
+        <td class="col-xs-9"><strong><?php echo $infosDegustation["nbLotsSansLeurre"]; ?></strong></td>
       </tr>
       <tr class="vertical-center">
         <td class="col-xs-3" >Nombre total <strong>d'adhérents prélevés&nbsp;:</strong></td>
