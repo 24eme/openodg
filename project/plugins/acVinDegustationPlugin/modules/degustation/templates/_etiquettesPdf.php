7bbff7a31ede143bd11a6a0263189a112c7e99a7
Honorine-MC <honorine.mariechristy@gmail.com>
2021-01-25 15:06:46 +0100

    maj du pdf pour ISO + si pas de CVI -> SIRET

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/_etiquettesPdf.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/_etiquettesPdf.php
@@ -5,41 +5,45 @@
     <?php foreach($plancheLots as $lotInfo): ?>
         <tr>
           <?php for($i=0; $i <3 ; $i++): ?>
-            <td style="text-align: left;">
-                <table cellspacing=0 cellpadding=0 style="font-size:8px;padding:0px;">
-                  <tr style="line-height:10px;">
+            <td style="text-align: left;width:245px;">
+                <table cellspacing=0 cellpadding=0 style="font-size:8px;">
+                  <tr style="line-height:8px;">
                     <td style="overflow-wrap:break-word;">
-                      <?php echo tdStart() ?>&nbsp;N°&nbsp;Lot&nbsp;ODG:&nbsp;<strong style="font-size:10px;"><?php echo (int)$lotInfo->lot->numero_archive;  ?></strong>
+                      <?php echo tdStart() ?>&nbsp;&nbsp;<strong style="font-size:10px;"><?php echo (int)$lotInfo->lot->numero_archive;  ?></strong>
                     </td>
                     <td style="overflow-wrap:break-word;text-align: right;">
                       <?php echo tdStart() ?>&nbsp;N°Dos:<strong><?php echo (int)$lotInfo->lot->numero_dossier;  ?></strong>
                     </td>
                   </tr>
-                  <tr style="line-height:10px;">
+                  <tr style="line-height:12px;">
                     <td colspan="2" style="overflow-wrap:break-word;text-align:center;line-height:8px;" >
                       <?php echo tdStart() ?>&nbsp;<strong><?php echo ($lotInfo->lot->declarant_nom)? $lotInfo->lot->declarant_nom : "Leurre";  ?></strong>
                     </td>
                   </tr>
-                  <tr style="line-height:8px;">
+                  <tr style="line-height:12px;">
                     <td colspan="2" style="text-align: center;">
-                      <?php echo tdStart() ?><strong><?php echo ($lotInfo->etablissement->cvi);  ?></strong>
+                      <?php echo tdStart() ?><strong><?php if($lotInfo->etablissement->cvi):echo ($lotInfo->etablissement->cvi);
+                        elseif ($lotInfo->etablissement->siret):echo (substr($lotInfo->etablissement->siret,0,3)." ".substr($lotInfo->etablissement->siret,3,3)." ".substr($lotInfo->etablissement->siret,6,3)." ".substr($lotInfo->etablissement->siret,9,5));
+                        endif;
+                       ?></strong>
                     </td>
                   </tr>
-                  <tr style="line-height:10px;">
+                  <tr style="line-height:12px;">
                     <td colspan="2" style="overflow-wrap:break-word; text-align: center;">
                       <?php echo tdStart() ?><strong>&nbsp;IGP&nbsp;<?php echo $lotInfo->lot->produit_libelle .' '.  $lotInfo->lot->millesime;  ?></strong>
                     </td>
                   </tr>
-                  <tr style="line-height:10px;">
-                    <td colspan="2" style="overflow-wrap:break-word;text-align:center;line-height:6px;">
+                  <tr style="line-height:12px;">
+                    <td colspan="2" style="overflow-wrap:break-word;text-align:center;line-height:7px;">
                       <?php echo tdStart() ?>
                       <?php if ($lotInfo->lot->details): ?>
                       <strong><?php echo $lotInfo->lot->details;  ?></strong>
+                    <?php else: echo (""); ?>
                       <?php endif; ?>
                     </td>
                   </tr>
                   <tr>
-                    <td style="overflow-wrap:break-word;">
+                    <td style="overflow-wrap:break-word;"style="line-height:12px;">
                       &nbsp;Lgt&nbsp;:&nbsp;<strong><?php echo $lotInfo->lot->numero_cuve;  ?></strong>
                     </td>
                     <td style="text-align: right;">
