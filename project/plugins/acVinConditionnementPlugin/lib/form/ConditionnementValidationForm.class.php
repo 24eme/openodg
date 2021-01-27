f5e7a0f8cb797a305506bd5713d024129ddc4dae
Honorine-MC <honorine.mariechristy@gmail.com>
2021-01-27 11:02:17 +0100

    date de degustation souhaite replacer par date automatique (plus dans le formulaire)

--- project/plugins/acVinConditionnementPlugin/lib/form/ConditionnementValidationForm.class.php
+++ project/plugins/acVinConditionnementPlugin/lib/form/ConditionnementValidationForm.class.php
@@ -12,17 +12,6 @@ class ConditionnementValidationForm extends acCouchdbForm
                     $this->getValidator('engagement_'.$engagement->getCode())->setOption('required', false);
                 }
             }
-
-            if (DrevConfiguration::getInstance()->hasDegustation()) {
-                $this->setWidget('date_degustation_voulue', new sfWidgetFormInput(array(), array()));
-                $this->setValidator('date_degustation_voulue', new sfValidatorDate(array('with_time' => false, 'datetime_output' => 'Y-m-d', 'date_format' => '~(?<day>\d{2})/(?P<month>\d{2})/(?P<year>\d{4})~', 'required' => true)));
-
-                if ($this->getDocument()->exist('date_degustation_voulue') && $this->getDocument()->date_degustation_voulue !== null) {
-                    $this->setDefault('date_degustation_voulue', DateTime::createFromFormat('Y-m-d', $this->getDocument()->date_degustation_voulue)->format('d/m/Y'));
-                } else {
-                    $this->setDefault('date_degustation_voulue', (new DateTime())->format('d/m/Y'));
-                }
-            }
         }
 
         if($this->getDocument()->isPapier()) {
@@ -36,9 +25,8 @@ class ConditionnementValidationForm extends acCouchdbForm
     }
 
     public function save() {
-  		$values = $this->getValues();
   	   $this->getDocument()->getOrAdd("date_degustation_voulue");
-       $this->getDocument()->date_degustation_voulue = $values["date_degustation_voulue"];
+       $this->getDocument()->date_degustation_voulue = date("d/m/y");
        $this->getDocument()->save();
   	}
 }
