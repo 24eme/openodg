8577e76eb8736edb56c593e2de37b7b4981df996
Moussa <adjimahamat@gmail.com>
2021-02-22 13:16:26 +0100

    Condit°: controle validation si pas de vol et pas de produit alors aucun pt bloquant

--- project/plugins/acVinConditionnementPlugin/lib/controle/ConditionnementValidation.class.php
+++ project/plugins/acVinConditionnementPlugin/lib/controle/ConditionnementValidation.class.php
@@ -41,15 +41,16 @@ class ConditionnementValidation extends DocumentValidation
         $produits = [];
 
       foreach ($this->document->lots as $key => $lot) {
+        if((!$lot->exist('produit_hash') || !$lot->produit_hash) && (!$lot->exist('volume') || !$lot->volume)){
+          continue;
+        }
         if(!$lot->exist('produit_hash') || !$lot->produit_hash){
           $this->addPoint(self::TYPE_ERROR, 'lot_produit_non_saisi', "Lot n° ".($key+1), $this->generateUrl('conditionnement_lots', array("id" => $this->document->_id)));
         }
         if(!$lot->exist('volume') || !$lot->volume){
           $this->addPoint(self::TYPE_ERROR, 'lot_volume_non_saisi', "Lot n° ".($key+1), $this->generateUrl('conditionnement_lots', array("id" => $this->document->_id)));
-        }
-
+        }        
       }
-
     }
 
     protected function controleLots(){
