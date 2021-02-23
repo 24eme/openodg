8fc742cc0d31ebc0bece82cc8b8c34c2339b0d71
Moussa <adjimahamat@gmail.com>
2021-02-23 18:06:44 +0100

    Transact°: etape Lot ajout coche degustation

--- project/plugins/acVinTransactionPlugin/modules/transaction/templates/lotsSuccess.php
+++ project/plugins/acVinTransactionPlugin/modules/transaction/templates/lotsSuccess.php
@@ -147,6 +147,14 @@
                         </div>
                     </div>
                 </div>
+                <div class="row">
+                  <div class="col-md-6 col-md-offset-6">
+                    <label>
+                      <?php echo $lot['degustable']->render() ?>
+                      <?php echo $lot['degustable']->renderLabel('Lot prévu pour la dégustation') ?>
+                    </label>
+                  </div>
+                </div>
             </div>
         </div>
         <div class="modal fade modal_lot_cepages" data-lot=<?php echo $key ?> id="<?php echo $lot->renderId() ?>_cepages" role="dialog" aria-labelledby="Répartition des cépages" aria-hidden="true">
