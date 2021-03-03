b50865f94c9e85cd5a8c7ba718b2bfa19e93c430
Gabriel Poma <gpoma@24eme.fr>
2021-03-03 17:23:41 +0100

    test seconde degustation

--- project/test/unit/76SecondeDegustationTest.php
+++ project/test/unit/76SecondeDegustationTest.php
@@ -9,26 +9,32 @@ if ($application != 'igp13') {
 }
 
 
-$t = new lime_test(5);
+$t = new lime_test(2);
 
 //Début des tests
 $t->comment("Création d'un second passage");
 
-$lots_a_redeguster = DegustationClient::getInstance()->getManquements();
-$lot_a_redeguster = array_pop($lots_a_redeguster);
-$specificite_originale = $lot_a_redeguster->specificite;
+foreach (DegustationClient::getInstance()->getHistory(1) as $d) {
+    $degustation = $d;
+}
 
-$lot_a_redeguster->prelevable = Lot::STATUT_PRELEVABLE;
-$t->is($lot_a_redeguster->prelevable, Lot::STATUT_PRELEVABLE, 'Le statut est à prélevable');
+$lot = $degustation->lots[0];
+$new_mvmt = $lot->redegustation();
 
-DegustationClient::updatedSpecificite($lot_a_redeguster);
-$t->is($lot_a_redeguster->specificite, $specificite_originale.', 2ème passage', 'Il s\'agit d\'un second passage');
+$t->is($new_mvmt->isSecondPassage(), true, "C'est le deuxième passage du lot");
+$t->is($new_mvmt->statut, Lot::STATUT_PRELEVABLE, 'Le status est changé');
+$t->is($new_mvmt->id_document, $degustation->_id, "L'id du doc est la même degustation");
+$t->is($new_mvmt->numero_archive, $lot->numero_archive, "Le numero archive n'a pas changé");
+$t->is($new_mvmt->numero_dossier, $lot->numero_dossier, "Le numero dossier n'a pas changé");
 
-DegustationClient::updatedSpecificite($lot_a_redeguster);
-$t->is($lot_a_redeguster->specificite, $specificite_originale.', 3ème passage', "Il s'agit d'un troisième passage");
+$t->is($lot->statut, Lot::STATUT_NONCONFORME, "Le statut n'as pas bougé pour le lot originel");
 
-$lot_a_redeguster->specificite = null;
-$t->is($lot_a_redeguster->specificite, null, 'On reset la spécificité');
+$new_Degustation = new Degustation();
+$new_Degustation->addLot($new_mvmt);
+$new_Degustation->generateMouvementsLots();
 
-DegustationClient::updatedSpecificite($lot_a_redeguster);
-$t->is($lot_a_redeguster->specificite, '2ème passage', 'Il s\'agit d\'un second passage');
+$lots = $new_Degustation->getLots();
+$t->is(count($lots), 1, "Il y a un lot dans la nouvelle dégustation");
+
+$new_lot = $lots[0];
+$t->is($new_lot->statut, Lot::STATUT_ATTENTE_PRELEVEMENT, "Le statut est en attente de prélèvement");
