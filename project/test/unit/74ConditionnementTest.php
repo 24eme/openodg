9bbe8764a2464d87f2d3ce71e863632c052c401c
Tangui Morlier <tangui@tangui.eu.org>
2021-03-03 19:51:01 +0100

    Test qui fonctionnement (avec un peu de clarification)

--- project/test/unit/74ConditionnementTest.php
+++ project/test/unit/74ConditionnementTest.php
@@ -9,7 +9,7 @@ if ($application != 'igp13') {
 }
 
 
-$t = new lime_test(7);
+$t = new lime_test(11);
 
 $viti =  CompteTagsView::getInstance()->findOneCompteByTag('test', 'test_viti')->getEtablissement();
 $centilisations = ConditionnementConfiguration::getInstance()->getContenances();
@@ -22,49 +22,57 @@ foreach(ConditionnementClient::getInstance()->getHistory($viti->identifiant, acC
 }
 
 $campagne = (date('Y')-1)."";
+$mydate = $campagne.'-'.date('m-d');
 //Début des tests
 $t->comment("Création d'un Conditionnement");
 
-$conditionnement = ConditionnementClient::getInstance()->createDoc($viti->identifiant, $campagne);
+$conditionnement = ConditionnementClient::getInstance()->createDoc($viti->identifiant, $campagne, $mydate);
 
 $conditionnement->storeDeclarant();
 $conditionnement->save();
+
+$t->comment($conditionnement->_id);
+$t->is($conditionnement->date, $mydate, "La date est bien la date fournie ($mydate)");
+$t->is($conditionnement->_id, "CONDITIONNEMENT-".$viti->identifiant."-".preg_replace('/-/', '', $mydate), "L'identifiant est bien constituté de la date");
 
 $produits = $conditionnement->getConfigProduits();
 
 foreach ($produits as $key => $produit) {
   break;
 }
-$lot = $conditionnement->addLot();
+$t->comment("création du lot 1");
+$lot1 = $conditionnement->addLot();
+$t->is($lot1->millesime, $conditionnement->campagne, "Le millésime est intialisé avec la campagne");
+$t->is($lot1->specificite, Lot::SPECIFICITE_UNDEFINED, "La spécificité est nul à la création du lot");
+$t->ok($lot1->isEmpty(), "Le lot est vide sans numéro et sans produit");
+$lot1->add('numero', "1");
+$t->ok(!$lot1->isEmpty(), "Le lot n'est plus vide avec un numéro");
+$lot1->volume = 12;
 
-$t->is($lot->millesime, $conditionnement->campagne, "Le millésime est intialisé");
-$t->is($lot->specificite, Lot::SPECIFICITE_UNDEFINED, "La spécificité est nul à la création du lot");
-
-$t->ok($lot->isEmpty(), "Le lot est vide");
-$lot->add('numero', "1");
-$t->ok(!$lot->isEmpty(), "Le lot n'est plus vide");
-
-$lot->volume = 12;
-
-$lot = $conditionnement->addLot();
-$lot->produit_hash = $produit->getHash();
+$t->comment("création du lot 2");
+$lot2 = $conditionnement->addLot();
+$lot2->produit_hash = $produit->getHash();
+$t->ok(!$lot2->isEmpty(), "Le lot n'est plus vide avec juste un produit");
 $conditionnement->save();
-
 $validation = new ConditionnementValidation($conditionnement);
+$t->is(count($validation->getPointsByCode(ConditionnementValidation::TYPE_ERROR, "lot_incomplet")), 2, "Point bloquant: Aucun produit saisi lors de l'etape Lot");
 
-$t->is(count($validation->getPointsByCode(ConditionnementValidation::TYPE_ERROR, "lot_incomplet")), 2, "Point bloquant:Aucun produit saisi lors de l'etape Lot");
-$lot = $conditionnement->addLot();
-$lot->produit_hash = $produit->getHash();
-$lot->volume = 15;
-$lot->add('numero', 'C12');
-$lot->add('specificite', "");
-$lot->add('centilisation', $centilisations_bib_key);
+$t->comment("création du lot 3");
+$lot3 = $conditionnement->addLot();
+$lot3->produit_hash = $produit->getHash();
+$lot3->volume = 15;
+$lot3->add('numero', 'C12');
+$lot3->add('specificite', "");
+$lot3->add('centilisation', $centilisations_bib_key);
+$conditionnement->save();
 
 $validation = new ConditionnementValidation($conditionnement);
 $t->is(count($validation->getPointsByCode(ConditionnementValidation::TYPE_WARNING, "lot_a_completer")), 1, "Point vigilance: la date de destinantion n'a pas été saisie");
 
 
-$lot = $conditionnement->addLot();
+$t->comment("création du lot 4");
+$lot4 = $conditionnement->addLot();
+$conditionnement->save();
 
 $t->is(count($conditionnement->lots), 4, "4 lots avant le clean");
 $conditionnement->cleanLots();
