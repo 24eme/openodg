ae8eeba16edbd884212b083355b172bfd77159db
Tangui Morlier <tangui@tangui.eu.org>
2021-03-04 18:00:34 +0100

    L'identifiant n'existe pas lorsqu'on vient d'adder le noeud parent

--- project/plugins/acVinConditionnementPlugin/lib/model/Conditionnement/Conditionnement.class.php
+++ project/plugins/acVinConditionnementPlugin/lib/model/Conditionnement/Conditionnement.class.php
@@ -492,7 +492,7 @@ class Conditionnement extends BaseConditionnement implements InterfaceVersionDoc
 
     public function generateAndAddMouvementLotsFromLot($lot, $key) {
         $mvt = $this->generateMouvementLotsFromLot($lot, $key);
-        return $this->add('mouvements_lots')->get($this->identifiant)->add($key, $mvt);
+        return $this->add('mouvements_lots')->add($this->identifiant)->add($key, $mvt);
     }
 
     public function generateMouvementsLots() {
