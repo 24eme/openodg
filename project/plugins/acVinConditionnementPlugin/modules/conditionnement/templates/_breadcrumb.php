485735011d0222ceae3d278e7a92d7c9f93b47b6
Jb Lm <lemetayer.jb@gmail.com>
2021-03-04 17:44:53 +0100

    bug variable dans template

--- project/plugins/acVinConditionnementPlugin/modules/conditionnement/templates/_breadcrumb.php
+++ project/plugins/acVinConditionnementPlugin/modules/conditionnement/templates/_breadcrumb.php
@@ -5,5 +5,5 @@
   <li><a href="<?php echo url_for('accueil'); ?>"><?php echo $sf_user->getTeledeclarationConditionnementRegion(); ?></a></li>
   <?php endif; ?>
   <li><a href="<?php echo url_for('declaration_etablissement', array('identifiant' => $conditionnement->identifiant, 'campagne' => $conditionnement->campagne)); ?>"><?php echo $conditionnement->getEtablissementObject()->getNom() ?> (<?php echo $conditionnement->getEtablissementObject()->identifiant ?> - <?php echo $conditionnement->getEtablissementObject()->cvi ?>)</a></li>
-  <li class="active"><a href="">Conditionnement du <?php echo format_date($transaction->getDate(), 'dd/MM/yyyy'); ?></a></li>
+  <li class="active"><a href="">Conditionnement du <?php echo format_date($conditionnement->getDate(), 'dd/MM/yyyy'); ?></a></li>
 </ol>
