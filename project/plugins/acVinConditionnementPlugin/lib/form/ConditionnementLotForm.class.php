c89eea6fb9e50edb5dcef3fb91aa0a197893e63b
Moussa <adjimahamat@gmail.com>
2021-03-10 16:43:25 +0100

    Transact°: ne pas laisser les champs degust, num, spécif à require false

--- project/plugins/acVinConditionnementPlugin/lib/form/ConditionnementLotForm.class.php
+++ project/plugins/acVinConditionnementPlugin/lib/form/ConditionnementLotForm.class.php
@@ -92,7 +92,6 @@ class ConditionnementLotForm extends acCouchdbObjectForm
 
             $this->getObject()->addCepage($values['cepage_'.$i], $values['repartition_'.$i]);
         }
-        $this->getObject()->getOrAdd("degustable");
         $this->getObject()->set("degustable",true);
     }
 
