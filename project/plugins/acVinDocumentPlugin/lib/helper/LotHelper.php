7c159f1ade67b6c5c94bf19b40523853d3f8edf2
Gabriel Poma <gpoma@24eme.fr>
2021-03-15 16:14:44 +0100

    on enleve l'info provenance du helper

--- project/plugins/acVinDocumentPlugin/lib/helper/LotHelper.php
+++ project/plugins/acVinDocumentPlugin/lib/helper/LotHelper.php
@@ -15,17 +15,6 @@ function showProduitLot($lot)
     $text .= "<small class=\"text-muted\">".$lot->getCepagesToStr()."</small>";
   }
 
-  // check car dans /prelevement-lots il s'agit de stdClass
-  $text .= '<span class="pull-right text-muted">';
-
-  if (get_class($lot) === stdClass::class) {
-    $text .= substr($lot->id_document, 0, 4);
-  } else {
-    $text .= $lot->getProvenance();
-  }
-
-  $text .= '</span>';
-
   return $text;
 
 }
