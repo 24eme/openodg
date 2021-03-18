799d63e7a3854ae76c651735d124ef7e9a7e20d6
Gabriel Poma <gpoma@24eme.fr>
2021-03-18 16:51:32 +0100

    fix type prevonance partial rowTable

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/_rowTablePrelevable.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/_rowTablePrelevable.php
@@ -1,5 +1,5 @@
 <td><?php echo $lot->declarant_nom; ?></td>
-<td><?php echo $lot->prevonance ?></td>
+<td><?php echo $lot->provenance ?></td>
 <td><?php echo $lot->numero_logement_operateur; ?></td>
 <td><?php echo showProduitLot($lot->getRawValue()) ?></td>
 <td class="text-right"><?php echoFloat($lot->volume); ?><small class="text-muted">&nbsp;hl</small></td>
