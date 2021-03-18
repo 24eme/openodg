89bfb2ec856130720957129fc32cb1c0d2341921
Vincent LAURENT <vince.laurent@gmail.com>
2021-03-19 00:00:48 +0100

    Plus besoin du statut pour l'import des lots

--- project/plugins/acVinDRevPlugin/lib/task/ImportLotsIATask.class.php
+++ project/plugins/acVinDRevPlugin/lib/task/ImportLotsIATask.class.php
@@ -191,17 +191,6 @@ EOF;
             $date = (preg_match('/^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/', trim($data[self::CSV_DATE_VALIDATION]), $m))? $m[3].'-'.$m[2].'-'.$m[1] : null;
 
             $prelevable = (strtolower(trim($data[self::CSV_PRELEVE])) == 'oui');
-            $statut = null;
-            if(isset($data[self::CSV_STATUT])){
-              $statut = trim($data[self::CSV_STATUT]);
-           }
-
-           if (!isset(self::$correspondancesStatuts[$statut])) {
-              echo "WARNING;statut inconnu ".$statut.";pas d'import;$line\n";
-              continue;
-           }
-
-           $statut = self::$correspondancesStatuts[$statut];
 
            $previousdoc = $document;
            $document = $this->getDocument($type, $document, $etablissement, $campagne, $date, $numeroDossier);
@@ -248,18 +237,10 @@ EOF;
             }
             $lot->destination_date = $destinationDate;
             $lot->date = $date;
-            $lot->statut = Lot::STATUT_NONPRELEVABLE;
+            $lot->affectable = $prelevable;
             $lot->specificite = null;
             $lot->add('document_fils', true);
-            if ($statut == self::STATUT_NONCONFORME) {
-              $lot->specificite = "2ème passage $lot->specificite";
-            }
-            if($prelevable) {
-                $lot->remove('document_fils');
-            }
-            if($lot->elevage) {
-                $lot->statut = Lot::STATUT_ELEVAGE;
-            }
+
             if ($data[self::CSV_TYPE] == self::TYPE_CONDITIONNEMENT) {
                 $lot->centilisation = "donnée non présente dans l'import";
             }
