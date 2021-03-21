f81136673b8959e7d4f7da1926bbf1a888b6fbe4
Tangui Morlier <tangui@tangui.eu.org>
2021-03-21 19:45:19 +0100

    Oubli bete

--- project/plugins/acVinFacturePlugin/lib/export/ExportFactureCSV_nantes.class.php
+++ project/plugins/acVinFacturePlugin/lib/export/ExportFactureCSV_nantes.class.php
@@ -95,6 +95,8 @@ class ExportFactureCSV_nantes implements InterfaceDeclarationExportCsv {
           $csv .= $this->floatHelper->formatFr(($odg_ou_forfait->montant_ht-$montant_covid_odg), 2, 2).";";
           $csv .= $this->floatHelper->formatFr($montant_covid_odg_ht, 2, 2).";";
           $odg_ou_forfait_inao_total += $odg_ou_forfait->montant_ht-$montant_covid_odg;
+        }else{
+          $csv .= ";;";
         }
         // inao
         $inao = $this->getCotisationNode('inao');
