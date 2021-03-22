7cee4cd3db6bafb25195cb49d7a842a1e05996b2
2maj <adjimahamat@gmail.com>
2021-03-22 10:52:18 +0100

    Drev: pas de btn retour/continue à l'import douane

--- project/plugins/acVinDRevPlugin/modules/drev/templates/drSuccess.php
+++ project/plugins/acVinDRevPlugin/modules/drev/templates/drSuccess.php
@@ -17,25 +17,11 @@
     </span></p>
 
     <form action="<?php echo url_for('drev_scrape_dr', $drev); ?>" method="get" id="form">
-    	<div style="margin-top: 20px;" class="row row-margin row-button">
-        	<div class="col-xs-6">
-            	<a href="<?php echo url_for('drev_exploitation', $drev) ?>" class="btn btn-default btn-upper"><span class="glyphicon glyphicon-chevron-left"></span> Retourner à l'étape précédente</a>
-        	</div>
-        	<div class="col-xs-6 text-right">
-                <button type="submit" class="btn btn-primary btn-upper" <?php if (!$drev->hasDocumentDouanier()): ?>disabled="disabled"<?php endif; ?>>Valider et continuer  <span class="glyphicon glyphicon-chevron-right"></span></button>
-            </div>
-		</div>
+
     </form>
     <?php else: ?>
         <p class="text-center" style="margin-top: 20px; padding: 150px;">Les données de la <?php echo $drev->getDocumentDouanierTypeLibelle() ?> ont correctement été importées <?php if($sf_user->isAdmin()): ?><small>(<a href="<?php echo url_for('drev_dr_upload', array('sf_subject' => $drev, 'force' => true)) ?>">changer le fichier</a>)</small><?php endif; ?>. </p>
-        <div style="margin-top: 20px;" class="row row-margin row-button">
-        	<div class="col-xs-6">
-            	<a href="<?php echo url_for('drev_exploitation', $drev) ?>" class="btn btn-default btn-upper"><span class="glyphicon glyphicon-chevron-left"></span> Retourner à l'étape précédente</a>
-        	</div>
-        	<div class="col-xs-6 text-right">
-                <a href="<?php echo url_for('drev_revendication_superficie', $drev) ?>" class="btn btn-primary btn-upper">Valider et continuer  <span class="glyphicon glyphicon-chevron-right"></span></a>
-            </div>
-		</div>
+
     <?php endif; ?>
 </div>
 <?php if (!$drev->hasDocumentDouanier()): ?>
