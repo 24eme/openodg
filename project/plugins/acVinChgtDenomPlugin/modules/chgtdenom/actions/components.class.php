60454c050888cda8784a6e5a85ea7b6ea567019a
Vincent LAURENT <vince.laurent@gmail.com>
2021-03-23 15:30:14 +0100

    Mise en forme des blocs

--- project/plugins/acVinChgtDenomPlugin/modules/chgtdenom/actions/components.class.php
+++ project/plugins/acVinChgtDenomPlugin/modules/chgtdenom/actions/components.class.php
@@ -4,15 +4,12 @@ class chgtdenomComponents extends sfComponents {
 
     public function executeMonEspace(sfWebRequest $request) {
         $this->campagne = $request->getParameter('campagne',ConfigurationClient::getInstance()->getCampagneManager()->getCurrent());
-        $this->hasLots = (count(MouvementLotView::getInstance()->getByIdentifiant($this->etablissement->identifiant)->rows) > 0);
         $this->enCours = null;
-        if ($this->hasLots) {
-            $chgts = ChgtDenomClient::getInstance()->getHistory($this->etablissement->identifiant);
-            foreach($chgts as $chgt) {
-                if (!$chgt->isValide() && $chgt->campagne == $this->campagne) {
-                    $this->enCours = $chgt;
-                    break;
-                }
+        $chgts = ChgtDenomClient::getInstance()->getHistory($this->etablissement->identifiant);
+        foreach($chgts as $chgt) {
+            if (!$chgt->isValide() && $chgt->campagne == $this->campagne) {
+                $this->enCours = $chgt;
+                break;
             }
         }
     }
