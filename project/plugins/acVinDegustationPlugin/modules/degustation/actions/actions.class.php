200f62766dcfd6f38a9d7e10207751dfd82a652b
2maj <adjimahamat@gmail.com>
2021-03-23 15:47:31 +0100

    Revert "convocation en etape"
    This reverts commit e04283dccfb9fdd94112de4c246520a5549c8db0.

--- project/plugins/acVinDegustationPlugin/modules/degustation/actions/actions.class.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/actions/actions.class.php
@@ -195,15 +195,6 @@ class degustationActions extends sfActions {
         }
     }
 
-    public function executeConvocationsEtape(sfWebRequest $request) {
-        $this->degustation = $this->getRoute()->getDegustation();
-        $this->redirectIfIsAnonymized();
-        $this->infosDegustation = $this->degustation->getInfosDegustation();
-        if ($this->degustation->storeEtape($this->getEtape($this->degustation, DegustationEtapes::ETAPE_PRELEVEMENTS))) {
-            $this->degustation->save();
-        }
-    }
-
     public function executeTablesEtape(sfWebRequest $request) {
         $this->degustation = $this->getRoute()->getDegustation();
         $this->redirectIfIsAnonymized();
