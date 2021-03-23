49e0cf3c3b5623b558061492231409adcd9970c0
Vincent LAURENT <vince.laurent@gmail.com>
2021-03-23 16:32:31 +0100

    Liste tables récap correction bug lot et sim=

--- project/plugins/acVinDegustationPlugin/lib/Degustation/Degustation.class.php
+++ project/plugins/acVinDegustationPlugin/lib/Degustation/Degustation.class.php
@@ -1003,7 +1003,7 @@ class Degustation extends BaseDegustation implements InterfacePieceDocument, Int
 
 		public function getLotsByNumDossier(){
 			$lots = array();
-			foreach ($this->getLotsTablesByUniqId() as $numTab => $lotTable) {
+			foreach ($this->getLotsSortByTables() as $numTab => $lotTable) {
 				foreach ($lotTable as $numAnonyme => $lot) {
 					$lots[$lot->numero_dossier][$numAnonyme] = $lot;
 				}
@@ -1056,14 +1056,14 @@ class Degustation extends BaseDegustation implements InterfacePieceDocument, Int
 			return sfConfig::get('sf_app');
 		}
 
-		public function getLotsTablesByUniqId(){
+		public function getLotsSortByTables(){
 			$lots = array();
 			for($numTab=1; $numTab <= $this->getLastNumeroTable(); $numTab++) {
 				$table = chr($numTab+64);
 
 				foreach ($this->getLotsByTable($numTab) as $key => $lot) {
 
-					$lots[$numTab][$lot->unique_id] = $lot;
+					$lots[] = $lot;
 				}
 			}
 
