38809c42d5396a3210a842938540816df018b5b6
Gabriel Poma <gpoma@24eme.fr>
2021-03-25 17:38:33 +0100

    partial liste degustation

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/indexSuccess.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/indexSuccess.php
@@ -63,31 +63,9 @@
 <div class="page-header no-border">
     <h2>Les dernières dégustations</h2>
 </div>
-<?php if (count($degustations)): ?>
-<table class="table table-condensed table-striped">
-<thead>
-    <th class="col-sm-2">Date de dégustation</th>
-    <th class="col-sm-7">Lieu de la dégustation</th>
-    <th class="col-sm-2">Infos</th>
-    <th class="col-sm-2 text-center"></th>
-</thead>
-<tbody>
-<?php foreach($degustations as $d): ?>
-    <tr>
-        <td class="col-sm-2"><?php echo $d->date; ?></td>
-        <td class="col-sm-"><?php echo $d->lieu; ?></td>
-        <td class="col-sm-2">
-            <?php echo ($d->lots) ? count($d->lots) : '0'; ?> <span class="text-muted">lots</span> -
-            <?php echo ($d->degustateurs) ? count($d->degustateurs) : '0'; ?> <span class="text-muted">degust.</span>
-        </td>
-        <td class="col-sm-1 text-right">
-            <a href="<?php echo url_for('degustation_visualisation', $d)?>"class="btn btn-default"><?= ($d->etape)? DegustationEtapes::$libelles_short[$d->etape] : 'Lots'  ?></a>
-        </td>
-    </tr>
-<?php endforeach; ?>
-<tbody>
-</table>
-<?php endif; ?>
+
+<?php include_partial('degustation/liste', ['degustations' => $degustations]) ?>
+
 <div class="page-header no-border">
     <h2>Elevages</h2>
 </div>
