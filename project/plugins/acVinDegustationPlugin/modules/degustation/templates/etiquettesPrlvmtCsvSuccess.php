cb010f959ad0a8b4227a160baa6a738d7043746a
Tangui Morlier <tangui@tangui.eu.org>
2021-03-25 11:59:38 +0100

    Encodage pour tableur MS

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/etiquettesPrlvmtCsvSuccess.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/etiquettesPrlvmtCsvSuccess.php
@@ -1,17 +1,20 @@
 numero_archive;numero_dossier;declarant_nom;cvi;siret;produit_libelle;millesime;details;centilisation;numero_logement_operateur;volume
 <?php
+
+
 foreach ($degustation->getEtiquettesFromLots(7) as $plancheLots) {
     foreach($plancheLots as $lotInfo) {
-        echo $lotInfo->lot->numero_archive.";";
-        echo $lotInfo->lot->numero_dossier.";";
-        echo $lotInfo->lot->declarant_nom.";";
-        echo $lotInfo->etablissement->cvi.";";
-        echo $lotInfo->etablissement->siret.";";
-        echo $lotInfo->lot->produit_libelle.";";
-        echo $lotInfo->lot->millesime.";";
-        echo $lotInfo->lot->details.";";
-        echo $lotInfo->lot->centilisation.";";
-        echo $lotInfo->lot->numero_logement_operateur.";";
-        echo $lotInfo->lot->volume."\n";
+        $lotInfo = $lotInfo->getRawValue();
+        echo str_replace(';', ' ', $lotInfo->lot->numero_archive).";";
+        echo str_replace(';', ' ', $lotInfo->lot->numero_dossier).";";
+        echo iconv("UTF-8", "ISO-8859-1", str_replace(';', ' ', $lotInfo->lot->declarant_nom)).";";
+        echo str_replace(';', ' ', $lotInfo->etablissement->cvi).";";
+        echo str_replace(';', ' ', $lotInfo->etablissement->siret).";";
+        echo iconv("UTF-8", "ISO-8859-1", str_replace(';', ' ', $lotInfo->lot->produit_libelle)).";";
+        echo str_replace(';', ' ', $lotInfo->lot->millesime).";";
+        echo iconv("UTF-8", "ISO-8859-1", str_replace(';', ' ', $lotInfo->lot->details)).";";
+        echo str_replace(';', ' ', $lotInfo->lot->centilisation).";";
+        echo iconv("UTF-8", "ISO-8859-1", str_replace(';', ' ', $lotInfo->lot->numero_logement_operateur)).";";
+        echo str_replace(';', ' ', $lotInfo->lot->volume)."\n";
     }
 }
