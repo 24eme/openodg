920b8262537b4866bb2bcae1f62e727c41ab18cb
Gabriel Poma <gpoma@24eme.fr>
2021-03-25 19:27:27 +0100

    oubli de fermer une balise

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/listeSuccess.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/listeSuccess.php
@@ -1,5 +1,5 @@
 <div class="page-header no-border">
-    <h2>Toutes les dégustations de <?php echo $campagne ?>
+    <h2>Toutes les dégustations de <?php echo $campagne ?></h2>
 </div>
 
 <?php include_partial('degustation/liste', ['degustations' => $degustations]); ?>
