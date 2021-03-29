ef3006e33e99ba172847835416c68111550ac8da
Vincent LAURENT <vince.laurent@gmail.com>
2021-03-29 13:10:35 +0200

    Parsing des cépages

---
+++ project/import/igp/cepage2csv.sh
@@ -0,0 +1,4 @@
+DATADIR=$1
+
+echo "appellation;cepage" > $DATADIR/cepages.csv
+ls $DATADIR/cepages_*.html | while read file; do nodejs parse_cepages.js $file; done | sort -t ";" -k1.7,1.10 -k1.4,1.5 -k1.1,1.2 -k2,2 >> $DATADIR/cepages.csv
