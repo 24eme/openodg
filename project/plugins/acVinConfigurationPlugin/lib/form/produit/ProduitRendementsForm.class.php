4a1e771f1b933165146932e50e93be4cfe2a8a9a
Gabriel Poma <gpoma@24eme.fr>
2021-03-31 23:23:23 +0200

    modif rendement admin

--- project/plugins/acVinConfigurationPlugin/lib/form/produit/ProduitRendementsForm.class.php
+++ project/plugins/acVinConfigurationPlugin/lib/form/produit/ProduitRendementsForm.class.php
@@ -5,18 +5,24 @@ class ProduitRendementsForm extends acCouchdbObjectForm {
             $this->setWidget('rendement', new bsWidgetFormInput());
             $this->setWidget('rendement_conseille', new bsWidgetFormInput());
             $this->setWidget('rendement_dr', new bsWidgetFormInput());
+            $this->setWidget('rendement_dr_l5', new bsWidgetFormInput());
+            $this->setWidget('rendement_dr_l15', new bsWidgetFormInput());
             $this->setWidget('rendement_vci', new bsWidgetFormInput());
             $this->setWidget('rendement_vci_total', new bsWidgetFormInput());
 
             $this->getWidget('rendement')->setLabel("Rendemt Maximum :");
             $this->getWidget('rendement_conseille')->setLabel("Rendemt Conseillé :");
             $this->getWidget('rendement_dr')->setLabel("Rendemt DR :");
+            $this->getWidget('rendement_dr_l5')->setLabel("Rendemt DR L5 :");
+            $this->getWidget('rendement_dr_l15')->setLabel("Rendemt DR L15 :");
             $this->getWidget('rendement_vci')->setLabel("Rendemt VCI :");
             $this->getWidget('rendement_vci_total')->setLabel("Rendemt VCI Total :");
 
             $this->setValidator('rendement', new sfValidatorNumber(array('required' => false)));
             $this->setValidator('rendement_conseille', new sfValidatorNumber(array('required' => false)));
             $this->setValidator('rendement_dr', new sfValidatorNumber(array('required' => false)));
+            $this->setValidator('rendement_dr_l5', new sfValidatorNumber(array('required' => false)));
+            $this->setValidator('rendement_dr_l15', new sfValidatorNumber(array('required' => false)));
             $this->setValidator('rendement_vci', new sfValidatorNumber(array('required' => false)));
             $this->setValidator('rendement_vci_total', new sfValidatorNumber(array('required' => false)));
         $this->widgetSchema->setNameFormat('%s');
