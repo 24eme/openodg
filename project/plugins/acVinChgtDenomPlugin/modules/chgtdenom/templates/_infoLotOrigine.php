78c8494bd9764c1f851d0e1984e9790a2d8f33ee
Tangui Morlier <tangui@tangui.eu.org>
2021-03-31 22:50:11 +0200

    Lien vers le détail d'un lot

--- project/plugins/acVinChgtDenomPlugin/modules/chgtdenom/templates/_infoLotOrigine.php
+++ project/plugins/acVinChgtDenomPlugin/modules/chgtdenom/templates/_infoLotOrigine.php
@@ -1,5 +1,9 @@
 <div class="alert col-sm-12" role="alert" style="background-color: #f8f8f8; border: 1px solid #e7e7e7; <?php if($opacity): ?>opacity: 0.7;<?php endif; ?>">
-  <h4>Dossier <?php echo $lot->campagne; ?> n°&nbsp;<strong><?php echo $lot->numero_dossier; ?></strong> – Lot n°&nbsp;<strong><?php echo $lot->numero_archive; ?></strong></h4>
+  <h4>
+      <a href="<?php echo url_for('degustation_lot_historique',array('identifiant' => $lot->declarant_identifiant, 'unique_id' => $lot->unique_id)); ?>">
+      Dossier <?php echo $lot->campagne; ?> n°&nbsp;<strong><?php echo $lot->numero_dossier; ?></strong> – Lot n°&nbsp;<strong><?php echo $lot->numero_archive; ?></strong>
+      </a>
+  </h4>
   <table class="table table-condensed" style="margin: 0;">
     <tbody>
       <tr>
