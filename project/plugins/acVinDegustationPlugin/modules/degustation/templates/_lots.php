3346d27202785e018438079c9608327a4c24529e
Tangui Morlier <tangui@tangui.eu.org>
2021-03-31 13:16:01 +0200

    Refacto des listings de lots (en fonction de leur statut)

---
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/_lots.php
@@ -0,0 +1,38 @@
+<table class="table table-condensed table-striped">
+<thead>
+    <th class="col-xs-1">Date</th>
+    <th class="col-xs-2 text-center">Dossier / Archive / Prov.</th>
+    <th class="col-xs-2">Opérateur</th>
+    <th class="col-xs-1 text-right">Volume</th>
+    <th class="col-xs-4">Produit</th>
+    <th class="col-xs-1">&nbsp;</th>
+    <th class="col-xs-1">&nbsp;</th>
+</thead>
+<tbody>
+<?php foreach($lots as $key => $lot): ?>
+  <tr>
+      <td><?php echo format_date($lot->date, "dd/MM/yyyy", "fr_FR");  ?></td>
+      <td class="text-center">
+          <?php echo $lot->numero_dossier;  ?> /
+          <?php echo $lot->numero_archive;  ?> /
+          <?php echo substr($lot->id_document, 0, 4); ?>
+      </td>
+      <td><?php echo $lot->declarant_nom; ?></td>
+      <td class="text-right"><?php echo $lot->volume; ?>&nbsp;hl</td>
+      <td>
+          <?php echo showProduitLot($lot->getRawValue(), false) ?><br/>
+          <span class="text-muted">Lot opérateur n°&nbsp;<?php echo $lot->numero_logement_operateur; ?></span>
+      </td>
+      <td>
+          <?php if (isset(Lot::$libellesStatuts[$lot->statut])) : ?>
+          <span class="label label-<?php echo isset(Lot::$statut2label[$lot->statut]) ? Lot::$statut2label[$lot->statut] : "default"; ?>">
+          <?php echo Lot::$libellesStatuts[$lot->statut]; ?>
+          <?php endif; ?>
+      </td>
+      <td>
+            <a class="btn btn-default btn-xs" href="<?php echo url_for('degustation_lot_historique', array('identifiant' => $lot->declarant_identifiant, 'unique_id' => $lot->unique_id)) ?>">Historique&nbsp;<span class="glyphicon glyphicon-chevron-right"></span></a>
+      </td>
+  </tr>
+<?php endforeach; ?>
+</tbody>
+</table>
--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/prelevementLotsSuccess.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/prelevementLotsSuccess.php
@@ -47,7 +47,20 @@
         <?php $dates = $form->getDateDegustParDrev(); foreach ($form['lots'] as $key => $lotForm): ?>
           <tr class="vertical-center cursor-pointer" data-adherent="<?php echo $form->getLot($key)->numero_dossier ?>">
             <td><?php echo DateTime::createFromFormat('Ymd', $dates[$form->getLot($key)->id_document])->format('d/m/Y') ?></td>
-            <?php include_partial('degustation/rowTablePrelevable', ['lot' => $form->getLot($key)]) ?>
+            <td><?php echo $lot->declarant_nom; ?></td>
+            <td>
+              <a href="<?php  echo url_for(strtolower(strtok($lot->id_document, '-')).'_visualisation', array('id' => $lot->id_document));  ?>">
+                <?php echo $lot->type_document; ?>
+              </a>
+            </td>
+            <td><?php echo $lot->numero_logement_operateur; ?></td>
+            <td><?php echo showProduitLot($lot->getRawValue(), false) ?></td>
+            <td class="edit text-right">
+              <?php echoFloat($lot->volume); ?><small class="text-muted">&nbsp;hl</small>
+              <a title="Modifier le volume" href="<?php echo url_for("declaration_doc", ['id' => $lot->id_document]); ?>">
+                <i class="glyphicon glyphicon-share-alt"></i>
+              </a>
+            </td>
             <td class="text-center" data-hash="<?php echo $form->getLot($key)->declarant_nom; ?>">
               <div style="margin-bottom: 0;" class="form-group <?php if($form['lots'][$key]['preleve']->hasError()): ?>has-error<?php endif; ?>">
                 <?php echo $form['lots'][$key]['preleve']->renderError() ?>
