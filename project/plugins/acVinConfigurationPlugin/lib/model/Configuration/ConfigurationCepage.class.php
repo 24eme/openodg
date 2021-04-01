4117daa3812221167b71eff866d4b342614505b7
mathurin petit <mathurin.petit@gmail.com>
2021-04-01 15:17:49 +0200

    Revert "pouvoir inserer des cepages_autorisé au noeud cepage"
    This reverts commit 787a0203594bf5019dda6f619428260541afbaba.

--- project/plugins/acVinConfigurationPlugin/lib/model/Configuration/ConfigurationCepage.class.php
+++ project/plugins/acVinConfigurationPlugin/lib/model/Configuration/ConfigurationCepage.class.php
@@ -237,7 +237,7 @@ class ConfigurationCepage extends BaseConfigurationCepage {
     }
 
     public function hasCepagesAutorises(){
-        return $this->exist('cepages_autorises');
+        return $this->exist('cepages_autorises') && count($this->_get('cepages_autorises')->toArray(true, false));
     }
 
     public function getCepagesAutorises() {
