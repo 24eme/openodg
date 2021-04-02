6ffe55d4e7851ab127c8aa1188330cde93a19c1f
mathurin petit <mathurin.petit@gmail.com>
2021-04-02 17:11:50 +0200

    allNotification sans le statut lot

--- project/plugins/acVinDegustationPlugin/lib/export/ExportDegustationAllNotificationsPDF.class.php
+++ project/plugins/acVinDegustationPlugin/lib/export/ExportDegustationAllNotificationsPDF.class.php
@@ -3,24 +3,23 @@
 class ExportDegustationAllNotificationsPDF extends ExportDeclarationLotsPDF {
 
     protected $degustation = null;
-    protected $etablissement = null;
+    protected $etablissements = array();
     protected $courrierInfos;
 
     public function __construct($degustation, $type = 'pdf', $use_cache = false, $file_dir = null, $filename = null) {
         $this->degustation = $degustation;
-        parent::__construct($degustation,$type, $use_cache, $file_dir, $filename);
+        parent::__construct($degustation, $type, $use_cache, $file_dir, $filename);
     }
 
     public function create() {
         $lots = array();
         $etablissements = [];
-
         foreach ($this->degustation->getLots() as $lot) {
             if (in_array($lot->declarant_identifiant, $etablissements) === false) {
                 $etablissements[$lot->declarant_identifiant] = EtablissementClient::getInstance()->findByIdentifiant($lot->declarant_identifiant);
             }
 
-            if (($lot->conformite == Lot::CONFORMITE_CONFORME || !$lot->conformite) && ($lot->statut == Lot::STATUT_PRELEVE || $lot->statut == Lot::STATUT_CONFORME) ) {
+            if ($lot->conformite == Lot::CONFORMITE_CONFORME) {
                 $lots[$lot->declarant_identifiant]['conforme'][] = $lot;
             } elseif ($lot->statut == Lot::STATUT_NONCONFORME) {
                 $lots[$lot->declarant_identifiant]['nonconforme'][] = $lot;
@@ -28,7 +27,6 @@ class ExportDegustationAllNotificationsPDF extends ExportDeclarationLotsPDF {
         }
 
         foreach ($lots as $declarant => $lots_declarant) {
-            $this->etablissement = $etablissements[$declarant];
             if (isset($lots_declarant['conforme']) && count($lots_declarant['conforme'])) {
                 $this->printable_document->addPage($this->getPartial('degustation/degustationConformitePDF', array('degustation' => $this->degustation, 'etablissement' => $etablissements[$declarant], 'lots' => $lots_declarant['conforme'], 'courrierInfos' => $this->courrierInfos)));
             }
@@ -37,8 +35,9 @@ class ExportDegustationAllNotificationsPDF extends ExportDeclarationLotsPDF {
                 foreach ($lots_declarant['nonconforme'] as $lot_nonconforme) {
                     $this->printable_document->addPage($this->getPartial('degustation/degustationNonConformitePDF_page1', array('degustation' => $this->degustation, 'etablissement' => $etablissements[$declarant], "lot" => $lot_nonconforme, 'courrierInfos' => $this->courrierInfos)));
                     $this->printable_document->addPage($this->getPartial('degustation/degustationNonConformitePDF_page2', array('degustation' => $this->degustation, 'etablissement' => $etablissements[$declarant], "lot" => $lot_nonconforme)));
+                    }
                 }
-            }
+
         }
     }
 
