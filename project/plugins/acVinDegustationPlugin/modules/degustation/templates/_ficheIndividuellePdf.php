d182fd7efc931b31134deeb4a015122874ec5c21
Tangui Morlier <tangui@tangui.eu.org>
2021-04-06 19:45:49 +0200

    Organisation de la fiche de dégustation + refacto de la pagination

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/_ficheIndividuellePdf.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/_ficheIndividuellePdf.php
@@ -32,49 +32,24 @@
       </p>
     </table>
 
-<table border="1px" class="table" cellspacing=0 cellpadding=0 style="text-align: center;border-collapse:collapse;" scope="colgroup" >
-  <tr style="line-height:20px;">
-     <th class="topempty bg-white"style="width:7%; "><?php echo tdStart() ?><strong>Anon</strong></th>
-     <th class="topempty bg-white" style="width:10%; "><?php echo tdStart() ?><strong>Couleur</strong></th>
-     <th class="topempty bg-white"style="width:14%;"><?php echo tdStart() ?><strong>IGP/ Millesime</strong></th>
-     <th class="topempty bg-white"style="width:14%;"><?php echo tdStart() ?><strong>Cépage</strong></th>
-    <th colspan="4"style="width:16%;"><?php echo tdStart() ?><strong>NOTATION</strong></th>
-     <th class="bg-white" colspan="2"style="width:10%;"><?php echo tdStart() ?><strong>Avis</strong></th>
-     <th class="bg-white"  colspan="2"style="width:10%;"><?php echo tdStart() ?><strong>Typicité cépage</strong></th>
-     <th class="topempty bg-white" style="width:22%;"><strong>Motifs (si non conforme)</strong></th>
-  </tr>
-  <tr style="line-height:13px;">
-    <th class="empty bg-white"></th>
-    <th class="empty bg-white"></th>
-    <th class="empty bg-white"></th>
-    <th class="empty bg-white"></th>
-    <th style="width:4%;"><?php echo tdStart() ?><strong><small>Visuel <br>/12</small></strong></th>
-    <th style="width:4%;"><?php echo tdStart() ?><strong><small>Oifactif <br>/12</small></strong></th>
-    <th style="width:4%;"><?php echo tdStart() ?><strong><small>Gustatif  <br> /24</small></strong></th>
-    <th style="width:4%;"><?php echo tdStart() ?><strong><small>NOTE TOTALE /48</small></strong></th>
-    <th class="bg-white" style="width:5%;" ><?php echo tdStart() ?><strong><small>C</small></strong></th>
-    <th class="bg-white" style="width:5%;"><?php echo tdStart() ?><strong><small>NC</small></strong></th>
-    <th class="bg-white" style="width:5%;"><?php echo tdStart() ?><strong><small>C</small></strong></th>
-    <th class="bg-white" style="width:5%;"><?php echo tdStart() ?><strong><small>NC</small></strong></th>
-    <th class="empty bg-white"></th>
-  </tr>
-
-<?php $i=0;
+<?php $i = 9; $table_header = true;
  foreach($lots as $lotInfo): ?>
-   <?php if($i == 16 || ($i - 16) % 19 > 17): ?>
+   <?php if ($i % 30 == 0 ) : $table_header = true; ?>
 </table>
      <br pagebreak="true" />
      <p>Suite des lots table <?php echo $lotInfo->getNumeroTableStr(); ?><p/>
      <br/>
+   <?php endif; ?>
+   <?php if ($table_header): $table_header = false; ?>
      <table border="1px" class="table" cellspacing=0 cellpadding=0 style="text-align: center;border-collapse:collapse;" scope="colgroup" >
        <tr style="line-height:20px;">
           <th class="topempty bg-white"style="width:7%; "><?php echo tdStart() ?><strong>Anon</strong></th>
           <th class="topempty bg-white" style="width:10%; "><?php echo tdStart() ?><strong>Couleur</strong></th>
-          <th class="topempty bg-white"style="width:14%;"><?php echo tdStart() ?><strong>IGP</strong></th>
+          <th class="topempty bg-white"style="width:14%;"><?php echo tdStart() ?><strong>IGP/ Millesime</strong></th>
           <th class="topempty bg-white"style="width:14%;"><?php echo tdStart() ?><strong>Cépage</strong></th>
-         <th colspan="4"style="width:16%;"><?php echo tdStart() ?><strong>NOTATION</strong></th>
-          <th class="bg-white" colspan="2"style="width:10%;"><?php echo tdStart() ?><strong>Avis</strong></th>
-          <th class="bg-white"  colspan="2"style="width:10%;"><?php echo tdStart() ?><strong>Typicité cépage</strong></th>
+         <th colspan="4"style="width:20%;"><?php echo tdStart() ?><strong>NOTATION</strong></th>
+          <th class="bg-white" colspan="2"style="width:8%;"><?php echo tdStart() ?><strong>Avis</strong></th>
+          <th class="bg-white"  colspan="2"style="width:8%;"><?php echo tdStart() ?><strong>Typicité cépage</strong></th>
           <th class="topempty bg-white" style="width:22%;"><strong>Motifs (si non conforme)</strong></th>
        </tr>
        <tr style="line-height:13px;">
@@ -82,14 +57,14 @@
          <th class="empty bg-white"></th>
          <th class="empty bg-white"></th>
          <th class="empty bg-white"></th>
-         <th style="width:4%;"><?php echo tdStart() ?><strong><small>Visuel <br>/12</small></strong></th>
-         <th style="width:4%;"><?php echo tdStart() ?><strong><small>Oifactif <br>/12</small></strong></th>
-         <th style="width:4%;"><?php echo tdStart() ?><strong><small>Gustatif  <br> /24</small></strong></th>
-         <th style="width:4%;"><?php echo tdStart() ?><strong><small>NOTE TOTALE /48</small></strong></th>
-         <th class="bg-white" style="width:5%;" ><?php echo tdStart() ?><strong><small>C</small></strong></th>
-         <th class="bg-white" style="width:5%;"><?php echo tdStart() ?><strong><small>NC</small></strong></th>
-         <th class="bg-white" style="width:5%;"><?php echo tdStart() ?><strong><small>C</small></strong></th>
-         <th class="bg-white" style="width:5%;"><?php echo tdStart() ?><strong><small>NC</small></strong></th>
+         <th style="width:5%;"><?php echo tdStart() ?><strong><small>Visuel<br>/12</small></strong></th>
+         <th style="width:5%;"><?php echo tdStart() ?><strong><small>Olfactif<br>/12</small></strong></th>
+         <th style="width:5%;"><?php echo tdStart() ?><strong><small>Gustatif <br> /24</small></strong></th>
+         <th style="width:5%;"><?php echo tdStart() ?><strong><small>TOTAL<br/>/48</small></strong></th>
+         <th class="bg-white" style="width:4%;" ><?php echo tdStart() ?><strong><small>C</small></strong></th>
+         <th class="bg-white" style="width:4%;"><?php echo tdStart() ?><strong><small>NC</small></strong></th>
+         <th class="bg-white" style="width:4%;"><?php echo tdStart() ?><strong><small>C</small></strong></th>
+         <th class="bg-white" style="width:4%;"><?php echo tdStart() ?><strong><small>NC</small></strong></th>
          <th class="empty bg-white"></th>
        </tr>
    <?php endif;?>
