5e431c63975313efc78b61a66f5bb77a998b065d
Gabriel Poma <gpoma@24eme.fr>
2021-04-06 12:33:38 +0200

    message d'erreur date non remplie DRev papier

--- project/plugins/acVinDRevPlugin/lib/form/DRevValidationForm.class.php
+++ project/plugins/acVinDRevPlugin/lib/form/DRevValidationForm.class.php
@@ -35,10 +35,12 @@ class DRevValidationForm extends acCouchdbForm
         }
 
         if($this->getDocument()->isPapier()) {
-            $this->setWidget('date', new sfWidgetFormInput());
-            $this->setValidator('date', new sfValidatorDate(array('date_output' => 'Y-m-d', 'date_format' => '~(?P<day>\d{2})/(?P<month>\d{2})/(?P<year>\d{4})~', 'required' => true)));
-            $this->getWidget('date')->setLabel("Date de réception du document");
-            $this->getValidator('date')->setMessage("required", "La date de réception du document est requise");
+            $this->setWidget("date", new sfWidgetFormInput());
+            $this->setValidator("date", new sfValidatorDate(
+                array('date_output' => 'Y-m-d', 'date_format' => '~(?P<day>\d{2})/(?P<month>\d{2})/(?P<year>\d{4})~', 'required' => true),
+                array('required' => 'La date de réception du document est requise')
+            ));
+            $this->getWidget("date")->setLabel("Date de réception du document");
         }
 
         $formDegustable = new BaseForm();
