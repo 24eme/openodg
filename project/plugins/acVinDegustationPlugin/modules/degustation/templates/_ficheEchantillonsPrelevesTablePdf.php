72bdc36fa88fa2c58928dffde718c6946ea57a6b
2maj <adjimahamat@gmail.com>
2021-04-07 15:59:45 +0200

    Degust: fiche lot ventilés par table correction pagination et header

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/_ficheEchantillonsPrelevesTablePdf.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/_ficheEchantillonsPrelevesTablePdf.php
@@ -52,44 +52,59 @@
     </div>
 
     <p>Nombre total de lots : <?php echo $nbLots;?></p>
-    <table border="0.5px" class="table"style="text-align: center;">
-      <tr style="line-height:20px;">
-        <th rowspan="2" class="topempty bg-white"style="width:5%;"><?php echo tdStart() ?><strong>Table</strong></th>
-         <th rowspan="2" class="topempty bg-white"style="width:10%;"><?php echo tdStart() ?><strong>N° Dossier</strong></th>
-         <th rowspan="2" class="topempty bg-white" style="width:20%; "><?php echo tdStart() ?><strong>Raison Sociale<br>N°CVI</strong></th>
-         <th class="bg-white" colspan="6"style="width:65%;"><?php echo tdStart() ?><strong>Liste des lots</strong></th>
-      </tr>
-      <tr style="line-height:13px;">
-        <th class="bg-white" style="width:7%;"><?php echo tdStart() ?><strong><small>N°Lot ODG</small></strong></th>
-        <th class="bg-white" style="width:5%;"><?php echo tdStart() ?><strong><small>N°Anon</small></strong></th>
-        <th class="bg-white" style="width:9%;"><?php echo tdStart() ?><strong><small>Cuve</small></strong></th>
-        <th class="bg-white" style="width:9%;"><?php echo tdStart() ?><strong><small>Vol (hl)</small></strong></th>
-        <th class="bg-white" style="width:20%;"><?php echo tdStart() ?><strong><small>IGP/Couleur/Millésime</small></strong></th>
-        <th class="bg-white" style="width:15%;"><?php echo tdStart() ?><strong><small>Cepage</small></strong></th>
-      </tr>
-     <?php  foreach($lots as $numTab => $lotTable):  ?>
-       <?php $firstDisplay = true; $class = "bg-tab$numTab"?>
-        <?php foreach ($lotTable as $numero_dossier => $lotInfo): ?>
-          <?php $firstDisplayTab = true; $firstDisplayOp = true; ?>
-          <?php foreach ($lotInfo as $uniqueId => $lot): ?>
 
+     <?php $affiche = 0; $reste = 0; $i = 8; $table_header = true;
+      foreach($lots as $numTab => $lotTable):  ?>
+       <?php $firstDisplay = true; $reste = 0; $class = "bg-tab$numTab"?>
+        <?php foreach ($lotTable as $numero_dossier => $lotInfo): ?>
+          <?php $firstDisplayTab = true; $firstDisplayOp = true; $reste = 0; ?>
+          <?php foreach ($lotInfo as $uniqueId => $lot): ?>
+            <?php if ($i % 20 == 0 ) : $table_header = true; $firstDisplayTab = $firstDisplayOp = true;?>
+             </table>
+              <br pagebreak="true" />
+              <p>Suite des lots<p/>
+              <br/>
+            <?php $i = 0; endif; ?>
+            <?php if ($table_header): $table_header = false; ?>
+              <table border="0.5px" class="table"style="text-align: center;">
+                <tr style="line-height:20px;">
+                  <th rowspan="2" class="topempty bg-white"style="width:5%;"><?php echo tdStart() ?><strong>Table</strong></th>
+                   <th rowspan="2" class="topempty bg-white"style="width:10%;"><?php echo tdStart() ?><strong>N° Dossier</strong></th>
+                   <th rowspan="2" class="topempty bg-white" style="width:23%; "><?php echo tdStart() ?><strong>Raison Sociale<br>N°CVI</strong></th>
+                   <th class="bg-white" colspan="6"style="width:62%;"><?php echo tdStart() ?><strong>Liste des lots</strong></th>
+                </tr>
+                <tr style="line-height:13px;">
+                  <th class="bg-white" style="width:7%;"><?php echo tdStart() ?><strong><small>N°Lot ODG</small></strong></th>
+                  <th class="bg-white" style="width:5%;"><?php echo tdStart() ?><strong><small>N°Anon</small></strong></th>
+                  <th class="bg-white" style="width:11%;"><?php echo tdStart() ?><strong><small>Cuve</small></strong></th>
+                  <th class="bg-white" style="width:9%;"><?php echo tdStart() ?><strong><small>Vol (hl)</small></strong></th>
+                  <th class="bg-white" style="width:30%;"><?php echo tdStart() ?><strong><small>Produit millesime cépage</small></strong></th>
+                </tr>
+            <?php endif; ?>
             <tr class="<?php echo $class; ?>" >
-              <?php if($firstDisplayTab == true ): ?>
-                <td rowspan="<?php echo count($lotInfo); ?>" ><small><?php echo DegustationClient::getNumeroTableStr($numTab) ?></small></td>
+              <?php if($firstDisplayTab == true):
+                $affiche = count($lotInfo) - $reste;
+                if((20 - $i) > 0 && (20 - $i) < count($lotInfo)){
+                  $reste = (20 - $i);
+                  $affiche = (20 - $i);
+                }
+                ?>
+                <td rowspan="<?php echo $affiche; ?>" ><small><?php echo DegustationClient::getNumeroTableStr($numTab) ?></small></td>
               <?php $firstDisplayTab= false; endif; ?>
+
                 <td ><small><?php echo ($lot->numero_dossier) ? $lot->numero_dossier : "Leurre" ; ?></small></td>
+
               <?php if($firstDisplayOp == true ): ?>
-                <td rowspan="<?php echo count($lotInfo); ?>" ><small><?php echo $lot->declarant_nom."<br>".$lot->declarant_identifiant;?></small></td>
+                <td rowspan="<?php echo $affiche; ?>" ><small><?php echo $lot->declarant_nom."<br>".$lot->declarant_identifiant;?></small></td>
               <?php $firstDisplayOp= false; endif; ?>
 
               <td><small><?php echo $lot->numero_archive ?></small></td>
               <td><small><?php echo $lot->numero_anonymat ?></small></td>
               <td><small><?php echo $lot->numero_logement_operateur ?></small></td>
-              <td style="text-align:right;"><small><?php echo number_format($lot->volume, 2) ?></small></td>
-              <td><small><?php echo $lot->produit_libelle." ".$lot->getMillesime(); ?></small></td>
-              <td><small><?php echo showOnlyCepages($lot); ?></small></td>
+              <td style="text-align:center;"><small><?php echo number_format($lot->volume, 2) ?></small></td>
+              <td><small><?php echo showProduitLot($lot); ?></small></td>
             </tr>
-          <?php endforeach; ?>
+          <?php $i++; endforeach; ?>
         <?php endforeach; ?>
     <?php endforeach; ?>
   </table>
