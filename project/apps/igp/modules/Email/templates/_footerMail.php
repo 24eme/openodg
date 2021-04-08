14cbcf9b8277060e2404b93cac3c7a9f1a477da9
Tangui Morlier <tangui@tangui.eu.org>
2021-04-08 16:03:25 +0200

    Amélioration du footer des mails

--- project/apps/igp/modules/Email/templates/_footerMail.php
+++ project/apps/igp/modules/Email/templates/_footerMail.php
@@ -1,4 +1,11 @@
 <?php
+$infos = sfConfig::get('app_facture_emetteur');
+$app = strtoupper(sfConfig::get('sf_app'));
 $signature = $infos[$app]['service_facturation'];
+$telephone = $infos[$app]['telephone'];
+$email = $infos[$app]['email'];
 ?>
 <?php echo $signature; ?>
+--
+mailto:<?php echo $email ;?>
+<?echo $telephone; ?>
