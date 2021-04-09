d14d25f405c9fe47936c97a1065482436b11bf56
Gabriel Poma <gpoma@24eme.fr>
2021-04-09 15:48:50 +0200

    adresse oi pdf nc majeur / 2nd passage

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/_degustationNonConformitePDF_page1.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/_degustationNonConformitePDF_page1.php
@@ -38,7 +38,7 @@
     <ul>
       <li><strong>Soit d’abandonner volontairement la dénomination correspondante</strong> en nous communiquant par fax, courrier ou mail, une déclaration de déclassement au moyen de la fiche jointe.</li><br/>
       <?php if ($lot->isSecondPassage() || $lot->conformite == Lot::CONFORMITE_NONCONFORME_MAJEUR): ?>
-      <li>Soit de vous opposer aux conclusions de ce contrôle. Dans ce cas, ainsi que le prévoit le plan de contrôle de l'IGP, vous êtes dans l'obligation de transmettre le dossier à l'organisme de contrôle/d'inspection qui diligentera un nouveau contrôle entièrement à votre charge.</li>
+      <li>Soit de vous opposer aux conclusions de ce contrôle. Dans ce cas, ainsi que le prévoit le plan de contrôle de l'IGP, vous êtes dans l'obligation de transmettre le dossier à l'organisme de contrôle/d'inspection qui diligentera un nouveau contrôle entièrement à votre charge. <?php if (isset($courrierInfos['oi'])): ?><strong>(<?php echo $courrierInfos['oi'] ?>)</strong><?php endif ?></li>
       <?php else: ?>
       <li>Soit d’exécuter l’action corrective recommandée dans la fiche de non-conformité ci jointe. Dans cette hypothèse, votre vin pourra être soumis à une deuxième dégustation après que nous ayons procédé à un nouveau prélèvement, et ce dans un délai minimum de 15 jours à compter de la réception de votre demande de nouvelle présentation. Notez qu’après travail le vin concerné par la deuxième présentation peut être relogé&nbsp;:
         <ul>
