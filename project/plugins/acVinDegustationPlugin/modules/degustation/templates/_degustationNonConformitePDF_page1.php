f1ff5252bcede3567ab1ffe059fe81dfc6a03cb2
Gabriel Poma <gpoma@24eme.fr>
2021-04-09 14:56:03 +0200

    typo

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/_degustationNonConformitePDF_page1.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/_degustationNonConformitePDF_page1.php
@@ -25,22 +25,20 @@
 <table><tr><td style="width: 324px;"><?php echo 'Le ' . format_date(date('Y-m-d'), "P", "fr_FR"); ?></td></tr></table>
 <br/><br/>
 
-<table><tr><td><strong>Objet :</strong> Résultats contrôles organoleptiques <?php echo $lot->getTextPassage() ?><strong>non conformes</strong></td></tr></table>
+<table><tr><td><strong>Objet :</strong> Résultats contrôles organoleptiques <?php echo $lot->getTextPassage() ?> <strong>non conformes</strong></td></tr></table>
 <br/><br/>
 
 
 <table>
   <tr><td>Madame, Monsieur,</td></tr>
   <br/>
-  <tr><td>Lors de la séance de dégustation du <strong><?php echo format_date($degustation->date, "P", "fr_FR"); ?></strong>, certains de vos vins dont la liste figure dans les fiches de non conformité ci-jointes ont été ajournés<?php echo ($lot->isSecondPassage()) ? ' pour la 2ème fois' : '' ?>.</td></tr>
+  <tr><td>Lors de la séance de dégustation du <strong><?php echo format_date($degustation->date, "P", "fr_FR"); ?></strong>, le lot dont les informations figurent dans la fiche de non conformité ci-jointe a été ajourné<?php echo ($lot->isSecondPassage()) ? ' pour la 2ème fois' : '' ?>.<br/></td></tr>
   <tr><td>Compte tenu de ce résultat, vous pouvez décider :</td></tr><br/>
   <tr><td>
     <ul>
       <li><strong>Soit d’abandonner volontairement la dénomination correspondante</strong> en nous communiquant par fax, courrier ou mail, une déclaration de déclassement au moyen de la fiche jointe.</li><br/>
       <?php if ($lot->isSecondPassage() || $lot->conformite == Lot::CONFORMITE_NONCONFORME_MAJEUR): ?>
-      <li>
-        Soit de vous opposer aux conclusions de ce conttôle. Dans ce cas, ainsi que le prévoit le plan de contrôle de l'IGP, vous êtes dans l'obligation de transmettre le dossier à l'organisme de contrôle/d'inspection qui diligentera un nouveau contrôle entièrement à votre charge.
-      </li>
+      <li>Soit de vous opposer aux conclusions de ce contrôle. Dans ce cas, ainsi que le prévoit le plan de contrôle de l'IGP, vous êtes dans l'obligation de transmettre le dossier à l'organisme de contrôle/d'inspection qui diligentera un nouveau contrôle entièrement à votre charge.</li>
       <?php else: ?>
       <li>Soit d’exécuter l’action corrective recommandée dans la fiche de non-conformité ci jointe. Dans cette hypothèse, votre vin pourra être soumis à une deuxième dégustation après que nous ayons procédé à un nouveau prélèvement, et ce dans un délai minimum de 15 jours à compter de la réception de votre demande de nouvelle présentation. Notez qu’après travail le vin concerné par la deuxième présentation peut être relogé&nbsp;:
         <ul>
