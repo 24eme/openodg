f55c4e6b9c6f1dfcf22c07c769d58221d62e7028
Tangui Morlier <tangui@tangui.eu.org>
2021-04-13 17:32:17 +0200

    Taille des blocs mon espace

--- project/plugins/acVinParcellaireAffectationPlugin/modules/parcellaireIntentionAffectation/templates/_monEspace.php
+++ project/plugins/acVinParcellaireAffectationPlugin/modules/parcellaireIntentionAffectation/templates/_monEspace.php
@@ -5,8 +5,8 @@
             <h3 class="panel-title">Identification parcellaire</h3>
         </div>
           <div class="panel-body">
-			  <p><?php if ($intentionParcellaireAffectation): ?>Mettre à jour<?php else: ?>Saisir<?php endif; ?> l'identification parcellaire<br />&nbsp;</p>
-              <div style="margin: 50px 0 29px 0;">
+			  <p class="explications"><?php if ($intentionParcellaireAffectation): ?>Mettre à jour<?php else: ?>Saisir<?php endif; ?> l'identification parcellaire<br />&nbsp;</p>
+              <div class="actions">
               	<a class="btn btn-default btn-block" href="<?php echo url_for('parcellaireintentionaffectation_edit', array('sf_subject' => $etablissement, 'periode' => $periode)) ?>"><span class="glyphicon glyphicon-file"></span>&nbsp;&nbsp;<?php if ($intentionParcellaireAffectation): ?>Mettre à jour<?php else: ?>Saisir<?php endif; ?> l'identification</a>
               </div>
           </div>
