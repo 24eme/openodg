d195f0f5b287c2839e4ae49547025a14afed836e
Vincent LAURENT <vince.laurent@gmail.com>
2021-04-14 11:12:11 +0200

    Code de giilda pour les sous génération et envoi de mail des factures

--- project/plugins/acVinGenerationPlugin/lib/model/Generation/Generation.class.php
+++ project/plugins/acVinGenerationPlugin/lib/model/Generation/Generation.class.php
@@ -7,17 +7,29 @@
 class Generation extends BaseGeneration {
 
   public function constructId() {
-    $this->setDateEmission(date('YmdHis'));
+      if(!$this->date_emission) {
+          $this->setDateEmission(date('YmdHis'));
+      }
     $this->setIdentifiant($this->type_document.'-'.$this->date_emission);
     $this->set_id('GENERATION-'.$this->identifiant);
-    $this->setStatut(GenerationClient::GENERATION_STATUT_ENATTENTE);
+    if(!$this->statut) {
+        $this->setStatut(GenerationClient::GENERATION_STATUT_ENATTENTE);
+    }
+
+    if(count(GenerationConfiguration::getInstance()->getSousGeneration($this->type_document))) {
+        $this->add('sous_generation_types', GenerationConfiguration::getInstance()->getSousGeneration($this->type_document));
+    }
   }
 
   public function save() {
     $this->nb_documents = count($this->documents);
     if (count($this->fichiers) && $this->statut != GenerationClient::GENERATION_STATUT_ENERREUR) {
       $this->setStatut(GenerationClient::GENERATION_STATUT_GENERE);
-    } 
+    }
+    if (!$this->nb_documents && $this->statut == GenerationClient::GENERATION_STATUT_GENERE) {
+	$this->nb_documents = count($this->fichiers);
+    }
+
     $this->setDateMaj(date('YmdHis'));
     return parent::save();
   }
@@ -34,8 +46,34 @@ class Generation extends BaseGeneration {
     if($statut == GenerationClient::GENERATION_STATUT_ENERREUR) {
       $this->message = "";
     }
-    
+
     return $this->_set('statut', $statut);
+  }
+
+  public function getOrCreateSubGeneration($typeDocument) {
+      $subGeneration = GenerationClient::getInstance()->find($this->_id."-".$typeDocument);
+
+      if($subGeneration) {
+          return $subGeneration;
+      }
+
+      $subGeneration = new Generation();
+      $subGeneration->_id = $this->_id."-".$typeDocument;
+      $subGeneration->type_document = $typeDocument;
+      $subGeneration->date_emission = date('YmdHis');
+      $subGeneration->statut = GenerationClient::GENERATION_STATUT_ENATTENTE;
+
+      return $subGeneration;
+  }
+
+  public function getMasterGeneration() {
+
+      return GenerationClient::getInstance()->find(preg_replace("/-[^-]*$/", "", $this->_id));
+  }
+
+  public function getSubGenerations() {
+
+      return GenerationClient::getInstance()->findSubGeneration($this->_id);
   }
 
   public function reload() {
