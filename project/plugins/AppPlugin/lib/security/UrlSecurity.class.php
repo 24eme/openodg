bf14e148f3a396edaba2b5db55b743e2e7513b12
Vincent LAURENT <vince.laurent@gmail.com>
2021-04-15 12:34:05 +0200

    Compatibilité avec md5 pour l'ava

--- project/plugins/AppPlugin/lib/security/UrlSecurity.class.php
+++ project/plugins/AppPlugin/lib/security/UrlSecurity.class.php
@@ -9,6 +9,11 @@ class UrlSecurity
 
     public static function verifyAuthKey($askedAuthKey, $id, $discriminant = null)
     {
+        // Pour assurer une compatibilité temporaire avec des mails qui ont été généré en md5 pour l'ava en mars 2021. (peut être supprimé dés la prochaine récolte)
+        if(substr(hash_hmac('md5', $id.$discriminant, sfConfig::get('app_secret')), 0, 10) == substr($askedAuthKey, 0, 10)) {
+
+            return true;
+        }
 
         return self::generateAuthKey($id, $discriminant) == substr($askedAuthKey, 0, 10);
     }
