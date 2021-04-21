30534b1ffb42bc400d0dff376fd193188d9f91ed
Vincent LAURENT <vince.laurent@gmail.com>
2021-04-21 11:39:43 +0200

    Facture au lieu Relevé de cotisation

--- project/apps/igp/modules/facturation/templates/_latexContent.php
+++ project/apps/igp/modules/facturation/templates/_latexContent.php
@@ -30,7 +30,7 @@
 \definecolor{vertfonce}{rgb}{0.17,0.29,0.28}
 \definecolor{vertmedium}{rgb}{0.63,0.73,0.22}
 \def\LOGO{<?php echo sfConfig::get('sf_web_dir'); ?>/images/logo_<?php echo strtolower($facture->region); ?>.png}
-\def\TYPEFACTURE{<?php if($facture->isAvoir()): ?>Avoir<?php else:?>Relevé de Cotisations<?php endif; ?>}
+\def\TYPEFACTURE{<?php if($facture->isAvoir()): ?>Avoir<?php else:?>Facture<?php endif; ?>}
 \def\NUMFACTURE{<?php echo $facture->numero_ava; ?>}
 \def\NUMADHERENT{<?php echo $facture->numero_adherent; ?>}
 \def\CAMPAGNE{<?php echo ($facture->getCampageTemplate() + 1).""; ?>}
