bebe4cf27f7e7de3add8391df88f9ec19d03d813
Gabriel Poma <gpoma@24eme.fr>
2021-04-22 17:47:53 +0200

    ajout test téléchargement pdf drev 03 fonctionnel

--- project/test/functional/app/03drevTest.php
+++ project/test/functional/app/03drevTest.php
@@ -140,6 +140,10 @@ $t->is($b->getResponse()->getStatuscode(), 200, "Page de confirmation");
 preg_match("|/drev/visualisation/([^/]+)|", $b->getRequest()->getUri(), $matches);
 $drevId = $matches[1];
 
+$b->click('a#lien-telechargement-pdf-drev');
+$b->isForwardedTo('drev', 'PDF');
+$t->is($b->getResponse()->getStatuscode(), 200, "Pdf de la drev");
+
 $t->comment('En mode habilitation');
 
 $b->get('/logout');
