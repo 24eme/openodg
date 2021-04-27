c96d83d6f59618ee7e5c92c16ccc504bd3f4f6d0
Vincent LAURENT <vince.laurent@gmail.com>
2021-04-27 13:25:34 +0200

    Plus de place dans le pdf pour pouvoir mettre 10 lignes au lieu de 4 dans la première page, Réorganisation du tableau de saisie de l'heure; la date et le labo et les signataires

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/_ficheIndividuelleLotsAPreleverPdf.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/_ficheIndividuelleLotsAPreleverPdf.php
@@ -1,6 +1,7 @@
 <?php use_helper('TemplatingPDF'); ?>
 <?php use_helper('Lot') ?>
 <?php use_helper('Float') ?>
+<?php use_helper('Text') ?>
 <?php $adresseLgt = splitLogementAdresse($adresseLogement); ?>
 <style>
 <?php echo style(); ?>
@@ -29,7 +30,7 @@ p, div {
   </tr>
   <tr>
     <td border="1px" class="border">
-        <p><span>&nbsp;&nbsp;<strong>Raison sociale :</strong> <?php echo $etablissement->raison_sociale ?></span></p>
+        <p><span>&nbsp;&nbsp;<strong>Raison sociale :</strong> <?php echo truncate_text($etablissement->getRawValue()->raison_sociale, 43, '...') ?></span></p>
         <p>
           <span><strong>Siret :</strong> <?php echo $etablissement->siret ?></span>
           <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>N° CVI :</strong> <?php echo $etablissement->cvi ?></span>
@@ -44,8 +45,8 @@ p, div {
         <p>
           <span><strong>Téléphone :</strong> <?php echo $etablissement->telephone_bureau ?></span>
           <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Portable :</strong> <?php echo $etablissement->telephone_mobile ?></span>
+          <br />
         </p>
-        <p></p>
     </td>
     <td border="1px" class="border">
         <p>
@@ -56,62 +57,56 @@ p, div {
         </p>
         <p>
           <span><strong>Code postal : </strong><?php echo $adresseLgt['code_postal'] ?></span>
-          <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ville : <?php echo $adresseLgt['commune'] ?></span>
+          <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Ville</strong> : <?php echo $adresseLgt['commune'] ?></span>
         </p>
         <p>
           <span><strong>Téléphone :</strong> <?php echo $etablissement->telephone_bureau ?></span>
       </p>
     </td>
   </tr>
+</table>
+<br />
+<br />
+<table class="table" cellspacing=0 cellpadding=0 style="border-collapse:collapse;" scope="colgroup">
   <tr>
-    <br/>
-    <td style="width:30%;">
-      <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Prélèvement&nbsp;date : _ _ _ _ </span>
-      <br/>
-    </td>
-    <td style="width:20%;">
-      <span>&nbsp;&nbsp;Heure : _ _ _ _</span>
-    </td>
-    <td style="width:50%; text-align: center;">
-      <span>&nbsp;&nbsp;&nbsp;&nbsp;Labo : <?php echo $etablissement->getLaboLibelle(); ?> </span>
-    </td>
-  </tr>
-  <tr>
-    <th border="1px" class="border" style="width: 50%; text-align: center;"><strong>Nom et signature préleveur</strong></th>
-    <th border="1px" class="border" style="width: 50%; text-align: center;"><strong>Nom et signature personne présente</strong></th>
+    <th border="1px" class="border" style="width: 10%; text-align: center;"><strong>Date</strong></th>
+    <th border="1px" class="border" style="width: 10%; text-align: center;"><strong>Heure</strong></th>
+    <th border="1px" class="border" style="width: 14%; text-align: center;"><strong>Labo</strong></th>
+    <th border="1px" class="border" style="width: 33%; text-align: center;"><strong>Nom et signature préleveur</strong></th>
+    <th border="1px" class="border" style="width: 33%; text-align: center;"><strong>Nom et signature personne présente</strong></th>
   </tr>
   <tr>
     <td border="1px" class="border"><br/><br/></td>
     <td border="1px" class="border"></td>
-  </tr>
-  <tr>
-    <td><br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nombre des lots à prélever : <?php echo count($lots) ?></td>
-    <td><br/><br/>Volume total : <?php echo $volumeLotTotal ?> hl</td>
+    <td border="1px" class="border"></td>
+    <td border="1px" class="border"></td>
+    <td border="1px" class="border"></td>
   </tr>
 </table>
-<div>
-  <?php $i = 6; $table_header = true; foreach($lots as $key => $lot): ?>
-   <?php if($i % 10 == 0 ): $table_header = true; ?>
+<br />
+<p><strong><?php echo count($lots) ?> lots à prélever</strong> <small>(pour un volume total de <?php echo $volumeLotTotal ?> hl)</small> :</p>
+  <?php $i = 10; $table_header = true; foreach($lots as $key => $lot): ?>
+   <?php if($i % 20 == 0 ): $table_header = true; ?>
     </table>
      <br pagebreak="true" />
-     <p>Suite des lots<p/>
+     <p><strong>Suite des lots :</strong><p/>
      <br/>
      <?php endif; ?>
     <?php if ($table_header): $table_header = false; ?>
      <table border="1px" class="table" cellspacing=0 cellpadding=0 style="text-align: center;border-collapse:collapse;" scope="colgroup" >
        <tr style="line-height:20px;">
-         <th style="width: 10%"><?php echo tdStart() ?><small>N° Dossier/Lot</small></th>
-         <th style="width: 30%"><?php echo tdStart() ?><small>Produit/Cépage/Millésime</small></th>
+         <th style="width: 8%"><?php echo tdStart() ?><small>N° Dossier/Lot</small></th>
+         <th style="width: 33%"><?php echo tdStart() ?><small>Produit/Cépage/Millésime</small></th>
          <th style="width: 5%"><?php echo tdStart() ?><small>Volume<br/>(hl)</small></th>
          <th style="width:8%"><?php echo tdStart() ?><small>N°Lot<br/>Opérateur</small></th>
          <th style="width: 10%"><?php echo tdStart() ?><small>Passage (spécificité)</small></th>
          <th style="width: 15%"><?php echo tdStart() ?><small>Destination</small></th>
-         <th style="width: 22%"><?php echo tdStart() ?><small>Obs préleveur <br/>Obs opérateurs</small></th>
+         <th style="width: 21%"><?php echo tdStart() ?><small>Obs préleveur <br/>Obs opérateurs</small></th>
        </tr>
    <?php endif;?>
    <tr style="line-height:17px;">
      <td><?php echo tdStart() ?><small><?php echo $lot->numero_dossier.' / '.$lot->numero_archive ?></small></td>
-     <td><?php echo tdStart() ?>
+     <td style="text-align:left;"><?php echo tdStart() ?>
        <small><?php echo showProduitCepagesLot($lot); ?></small>
      </td>
     <td><?php echo tdStart() ?>
@@ -134,4 +129,3 @@ p, div {
    <?php $i++; ?>
   <?php endforeach; ?>
   </table>
-</div>
