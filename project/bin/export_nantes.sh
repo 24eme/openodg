5e54d320795b4d39cf15f90bf914a81cdf048974
Tangui Morlier <tangui@tangui.eu.org>
2021-04-29 18:14:36 +0200

    Oups debug oublié

--- project/bin/export_nantes.sh
+++ project/bin/export_nantes.sh
@@ -2,7 +2,7 @@
 
 . bin/config.inc
 
-#bash bin/export.sh
+bash bin/export.sh
 
 php symfony export:facture $SYMFONYTASKOPTIONS >  $EXPORTDIR/factures.csv.part
 iconv -f UTF8 -t ISO88591//TRANSLIT $EXPORTDIR/factures.csv.part > $EXPORTDIR/factures.csv
