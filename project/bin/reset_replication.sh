bb2b9006b41fb77585b12d033642bd9aab1eb6ac
Vincent LAURENT <vince.laurent@gmail.com>
2021-05-03 15:47:47 +0200

    Il manquait un exit

--- project/bin/reset_replication.sh
+++ project/bin/reset_replication.sh
@@ -5,6 +5,7 @@ if ! test -f $(echo $0 | sed 's/[^\/]*$//')config.inc && ! test $1 ; then
     ls . $(echo $0 | sed 's/[^\/]*$//') | grep "config_" | grep ".inc$" | sed 's/config_//' | sed 's/\.inc//' | while read app; do
         bash $(echo $0 | sed 's/[^\/]*$//')reset_replication.sh $app;
     done
+    exit 0;
 fi
 
 if ! test $1 ; then
