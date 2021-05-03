717d2effa681248cca020408ab74fafce8adc9d0
Vincent LAURENT <vince.laurent@gmail.com>
2021-05-03 11:31:29 +0200

    Mode multi app dans generation.sh directement

--- project/bin/cron/generation.sh
+++ project/bin/cron/generation.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Mode multi app
+if ! test -f $(echo $0 | sed 's/[^\/]*$//')../config.inc && ! test $1 ; then
+    ls . $(echo $0 | sed 's/[^\/]*$//')../ | grep "config_" | grep ".inc$" | sed 's/config_//' | sed 's/\.inc//' | while read app; do
+        echo "running generation $app :"
+        bash $(echo $0 | sed 's/[^\/]*$//')generation.sh $app
+    done
+    exit 0;
+fi
+
 if ! test $1 ; then
     . $(echo $0 | sed 's/[^\/]*$//')../config.inc
 else
