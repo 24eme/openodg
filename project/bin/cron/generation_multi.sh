f7e4a11bc59e6577d25f9d13ff0758b6b38312b8
Vincent LAURENT <vince.laurent@gmail.com>
2021-05-03 11:08:54 +0200

    Génération multiapp

--- project/bin/cron/generation_multi.sh
+++ project/bin/cron/generation_multi.sh
@@ -1,3 +1,4 @@
 ls . $(echo $0 | sed 's/[^\/]*$//')../ | grep "config_" | grep ".inc$" | sed 's/config_//' | sed 's/\.inc//' | while read app; do
-    echo bash $(echo $0 | sed 's/[^\/]*$//')generation.sh $app
+    echo "running generation $app :"
+    bash $(echo $0 | sed 's/[^\/]*$//')generation.sh $app
 done
