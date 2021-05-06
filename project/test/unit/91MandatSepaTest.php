f34cb966f808f27b7e2e372ed6c143c071a59497
Honorine-MC <honorine.mariechristy@gmail.com>
2021-05-06 12:02:51 +0200

    maj du test de jb sur les mandatsepa

--- project/test/unit/91MandatSepaTest.php
+++ project/test/unit/91MandatSepaTest.php
@@ -8,18 +8,21 @@ if ($application != 'igp13') {
     return;
 }
 
-$t = new lime_test();
+$viti =  CompteTagsView::getInstance()->findOneCompteByTag('test', 'test_viti')->getEtablissement();
+$societe = $viti->getSociete();
+$socVitiCompte = $societe->getMasterCompte();
 
-$societe = SocieteClient::getInstance()->createSociete("Chateau 24ème", SocieteClient::TYPE_OPERATEUR);
-$societe->siege->adresse = "2 place Sainte Opportune";
-$societe->siege->code_postal = "75001";
-$societe->siege->commune = "Paris";
+$oldmandat = MandatSepaClient::getInstance()->findLastBySociete($societe->identifiant);
+if ($oldmandat) {
+    MandatSepaClient::getInstance()->delete($oldmandat);
+}
+foreach(FactureClient::getInstance()->getFacturesByCompte($socVitiCompte->identifiant) as $k => $f) {
+    FactureClient::getInstance()->delete($f);
+}
 
-$mandatSepa = MandatSepaClient::getInstance()->createDoc($societe);
 
-$t->is($mandatSepa->date, date('Y-m-d'), 'date de creation = date du jour');
-$t->is($mandatSepa->is_signe, 0, 'mandat non signe');
-$t->is($mandatSepa->debiteur->frequence_prelevement, MandatSepaClient::FREQUENCE_PRELEVEMENT_RECURRENT, 'fréquence = récurrent');
+$t = new lime_test(34);
+
 $t->is(MandatSepaConfiguration::getInstance()->getMentionAutorisation(), 'En signant ce formulaire de mandat, vous autorisez (A) le Syndicat des Vins IGP à envoyer des instructions à votre banque pour débiter votre compte, et (B) votre banque à débiter votre compte conformément aux instructions du Syndicat des Vins IGP.', 'autorisation correctement configuré');
 $t->is(MandatSepaConfiguration::getInstance()->getMentionRemboursement(), 'Vous bénéficiez d\'un droit à remboursement par votre banque selon les conditions décrites dans la convention que vous avez passée avec elle. Toute demande de remboursement doit être présentée dans les 8 semaines suivant la date de débit de votre compte ou sans tarder et au plus tard dans les 13 mois en cas de prélèvement non autorisé.', 'remboursement correctement configuré');
 $t->is(MandatSepaConfiguration::getInstance()->getMentionDroits(), 'Vos droits concernant le présent mandat sont expliqués dans un document que vous pouvez obtenir auprès de votre banque.', 'droits correctement configuré');
@@ -29,6 +32,16 @@ $t->is(MandatSepaConfiguration::getInstance()->getMandatSepaNom(), 'Syndicat des
 $t->is(MandatSepaConfiguration::getInstance()->getMandatSepaAdresse(), '22 Avenue Henri Pontier', 'adresse correctement configuré');
 $t->is(MandatSepaConfiguration::getInstance()->getMandatSepaCodePostal(), '13626', 'cp correctement configuré');
 $t->is(MandatSepaConfiguration::getInstance()->getMandatSepaCommune(), 'Aix-en-Provence', 'commune correctement configuré');
+
+$mandatSepa = MandatSepaClient::getInstance()->createDoc($societe);
+$mandatSepa->constructId();
+$mandatSepa->save();
+$mandaid = $mandatSepa->_id;
+$mandatSepa = MandatSepaClient::getInstance()->find($mandaid);
+
+$t->is($mandatSepa->date, date('Y-m-d'), 'date de creation = date du jour');
+$t->is($mandatSepa->is_signe, 0, 'mandat non signe');
+$t->is($mandatSepa->debiteur->frequence_prelevement, MandatSepaClient::FREQUENCE_PRELEVEMENT_RECURRENT, 'fréquence = récurrent');
 
 $t->is($mandatSepa->mention_autorisation, MandatSepaConfiguration::getInstance()->getMentionAutorisation(), 'autorisation conforme à la configuration');
 $t->is($mandatSepa->mention_remboursement, MandatSepaConfiguration::getInstance()->getMentionRemboursement(), 'remboursement conforme à la configuration');
