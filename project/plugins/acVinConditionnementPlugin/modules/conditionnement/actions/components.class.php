d48e51e6f9a1d5cfa5ce42f1795351b15e8fc347
Tangui Morlier <tangui@tangui.eu.org>
2021-05-10 16:19:13 +0200

    Un seul conditionnement par jour

--- project/plugins/acVinConditionnementPlugin/modules/conditionnement/actions/components.class.php
+++ project/plugins/acVinConditionnementPlugin/modules/conditionnement/actions/components.class.php
@@ -4,6 +4,9 @@ class conditionnementComponents extends sfComponents {
 
     public function executeMonEspace(sfWebRequest $request) {
         $this->conditionnement = ConditionnementClient::getInstance()->findBrouillon($this->etablissement->identifiant);
+        if (!$this->conditionnement) {
+            $this->conditionnement = ConditionnementClient::getInstance()->findByIdentifiantAndDate($this->etablissement->identifiant, date('Y-m-d'));
+        }
     }
 
 }
