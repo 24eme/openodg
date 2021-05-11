b136d8d4039bfe71eb1384d87e0505118353f2e9
Mame <mngom@24eme.fr>
2021-05-11 16:55:23 +0200

    Condition si le lot hasDocumentOrigine() -> permission de modifier

--- project/plugins/acVinChgtDenomPlugin/modules/chgtdenom/templates/_infoLotOrigine.php
+++ project/plugins/acVinChgtDenomPlugin/modules/chgtdenom/templates/_infoLotOrigine.php
@@ -1,7 +1,9 @@
 <?php use_helper('Lot') ?>
 
 <div class="alert col-sm-12 mb-4" role="alert" style="position: relative; background-color: #f8f8f8; border: 1px solid #e7e7e7; <?php if($opacity): ?>opacity: 0.7;<?php endif; ?>">
+<?php if ($lot->hasDocumentOrigine()): ?>
   <a href="<?php  echo url_for('degustation_lot_modification', array('identifiant' => $lot->declarant_identifiant, 'unique_id' => $lot->unique_id));  ?>" style="position: absolute; right:6px; top: 12px;" class="btn btn-sm btn-link transparence-sm btn-modifier-lot" title="Modifier le lot"><span class="glyphicon glyphicon-pencil"></span></a>
+<?php endif; ?>
   <h4>
       <a href="<?php echo url_for('degustation_lot_historique',array('identifiant' => $lot->declarant_identifiant, 'unique_id' => $lot->unique_id)); ?>">
       Dossier <?php echo $lot->campagne; ?> n°&nbsp;<strong><?php echo $lot->numero_dossier; ?></strong> – Lot n°&nbsp;<strong><?php echo $lot->numero_archive; ?></strong>
