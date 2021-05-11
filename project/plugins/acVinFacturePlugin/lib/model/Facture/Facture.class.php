ce8e2796b5209568cec37b000d2619eb4195a776
mathurin petit <mathurin.petit@gmail.com>
2021-05-11 12:34:23 +0200

    dans les template de facturation on utilisera %detail_identifiant% pour ordonner les lignes de facturation

--- project/plugins/acVinFacturePlugin/lib/model/Facture/Facture.class.php
+++ project/plugins/acVinFacturePlugin/lib/model/Facture/Facture.class.php
@@ -245,7 +245,7 @@ class Facture extends BaseFacture implements InterfaceArchivageDocument, Interfa
 
     /** facturation par mvts **/
     public function storeLignesByMouvementsView($mouvement) {
-            $keyLigne = str_replace("%numero_dossier%",$mouvement->value->detail_identifiant,$mouvement->value->categorie);
+            $keyLigne = str_replace("%detail_identifiant%",$mouvement->value->detail_identifiant,$mouvement->value->categorie);
             $ligne = $this->lignes->add($keyLigne);
             $ligne->libelle = $mouvement->value->type_libelle;
             $ligne->origine_mouvements->add($mouvement->id)->add(null, $mouvement->key[MouvementFactureView::KEY_ORIGIN]);
