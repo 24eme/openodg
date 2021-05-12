f6c13ac9efb4b4cc07d2e498404c4b1e0b68d2c6
Vincent LAURENT <vince.laurent@gmail.com>
2021-05-12 18:36:34 +0200

    Page statut pour toutes les applis

---
+++ project/web/statuts/igparles.php
@@ -0,0 +1,3 @@
+<?php
+    $application = 'igparles';
+    require_once(dirname(__FILE__).'/app.php');
---
+++ project/web/statuts/igpmed.php
@@ -0,0 +1,3 @@
+<?php
+    $application = 'igpmed';
+    require_once(dirname(__FILE__).'/app.php');
