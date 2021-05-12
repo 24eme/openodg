31f40d97e956d57d4fad7ec1098b1c92d5ae0f47
Tangui Morlier <tangui@tangui.eu.org>
2021-05-12 20:25:04 +0200

    Première version du versement comptable des paiements (isacompta)

---
+++ project/bin/facture/csvpaiement2isacompta.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PAIEMENT_CSV_FILE=$2
+
+echo "date;identifiant analytique;journal;piece;raison sociale;libelle ligne;;quantite;debit;credit;;igp";
+cat $PAIEMENT_CSV_FILE | awk -F ';' '{
+	//compte client
+	print $1";411"$3";22;"$4";"$2";"$7";;;;"$6";;";
+	//depot
+        print $1";58000000;22;"$4";"$2";"$7";;;"$6";;;";
+}'
