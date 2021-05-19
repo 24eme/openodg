238a93b70d0bdba15cea6c45a74b737a75cd58fc
Tangui Morlier <tangui@tangui.eu.org>
2021-05-19 18:41:35 +0200

    Bon journal + longueur du champ libellé

--- project/bin/facture/csvfacture2isacompta.sh
+++ project/bin/facture/csvfacture2isacompta.sh
@@ -3,7 +3,7 @@
 FACTURE_CSV_FILE=$1
 
 #echo "date;identifiant analytique;journal;piece;raison sociale;libelle mouvement;;quantite;debit;credit;code tva;igp";
-cat $FACTURE_CSV_FILE | tail -n +2 | sed 's/\([0-9]*\)-\([0-9]*\)-\([0-9]*\);/\3\/\2\/\1;/' | awk -F ';' '{
+cat $FACTURE_CSV_FILE | tail -n +2 | sed 's/\([0-9]*\)-\([0-9]*\)-\([0-9]*\);/\3\/\2\/\1;/' | sed 's/ - N. / - /' | awk -F ';' '{
     tva = ""
     tva_prix = $17
     sub(",", ".", tva_prix) * 1.0
@@ -12,12 +12,12 @@ cat $FACTURE_CSV_FILE | tail -n +2 | sed 's/\([0-9]*\)-\([0-9]*\)-\([0-9]*\);/\3
     }
 	if ($14 && $15) {
 		//Credit ligne
-		print $1";"$12";"70";"$11";"$4";"$13";0;"$15";;"$16";"tva";"$13;
+		print $1";"$12";"71";"$11";"$4";"$13";0;"$15";;"$16";"tva";"$13;
 	} else {
 		//export debit client
-        print $1";411"$3";"70";"$11";"$4";REV "$4";0;"$15";"$18";;;"$13;
+        print $1";411"$3";"71";"$11";"$4";REV "$4";0;"$15";"$18";;;"$13;
         if (tva_prix * 1 != 0.00) {
-			print $1";44571251;"70";"$11";"$4";TVA "$13";0;"$15";;"$17";"tva";"$13;
+			print $1";44571251;"71";"$11";"$4";TVA "$13";0;"$15";;"$17";"tva";"$13;
 		}
 	}
 }'
--- project/bin/facture/csvpaiement2isacompta.sh
+++ project/bin/facture/csvpaiement2isacompta.sh
@@ -5,7 +5,7 @@ PAIEMENT_CSV_FILE=$1
 #echo "date;identifiant analytique;journal;piece;raison sociale;libelle ligne;;quantite;debit;credit;;igp";
 cat $PAIEMENT_CSV_FILE | sed 's/_REV_/R/' | tail -n +2 | awk -F ';' '{
 	//compte client
-	print $5";411"$3";22;"$4";"$2";"$7";;;;"$6";;";
+	print $5";411"$3";25;"$4";"$2";"$7";;;;"$6";;";
 	//depot
-    print $5";58000000;22;"$4";"$2";"$7";;;"$6";;;";
+    print $5";58000000;25;"$4";"$2";"$7";;;"$6";;;";
 }' | grep -v '^;' | sed 's/\([0-9]*\)-\([0-9]*\)-\([0-9]*\);/\3\/\2\/\1;/'
