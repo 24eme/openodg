198a9dd07a826fabacb559d4c74ead361abd2b57
Tangui Morlier <tangui@tangui.eu.org>
2021-05-19 14:32:20 +0200

    Prix tva peut etre négatif ou inférieur à 0

--- project/bin/facture/csvfacture2isacompta.sh
+++ project/bin/facture/csvfacture2isacompta.sh
@@ -6,12 +6,14 @@ echo "date;identifiant analytique;journal;piece;raison sociale;libelle mouvement
 cat $FACTURE_CSV_FILE | tail -n +2 | sed 's/\([0-9]*\)-\([0-9]*\)-\([0-9]*\);/\3\/\2\/\1;/' | awk -F ';' '{
 	if ($14 && $15) {
         tva = ""
-        if ($17 * 1 > 0) {
+        tva_prix = $17
+        sub(",", ".", tva_prix) * 1.0
+        if (tva_prix != 0) {
             tva = "r5"
         }
 		//Credit ligne
 		print $1";"$12";"70";"$11";"$4";"$13";0;"$15";;"$16";"tva";"$13;
-		if ($17 * 1 > 0) {
+		if (tva_prix != 0) {
 			print $1";44571251;"70";"$11";"$4";TVA "$13";0;"$15";;"$17";"tva";"$13;
 		}
 	} else {
