799ee435db6df912cfad29aa701197be05071716
Vincent LAURENT <vince.laurent@gmail.com>
2021-05-21 02:25:17 +0200

    Export CSV des affections parcellaire par cave

--- project/plugins/acVinParcellaireAffectationPlugin/modules/parcellaireAffectationCoop/actions/actions.class.php
+++ project/plugins/acVinParcellaireAffectationPlugin/modules/parcellaireAffectationCoop/actions/actions.class.php
@@ -84,4 +84,28 @@ class parcellaireAffectationCoopActions extends sfActions {
         $this->parcellaireAffectation = ParcellaireAffectationClient::getInstance()->find($request->getParameter('id_document'));
     }
 
+    public function executeExportcsv(sfWebRequest $request) {
+        $this->etablissement = $this->getRoute()->getObject();
+        $this->periode = $request->getParameter('periode');
+
+        $this->apporteurs = $this->etablissement->getLiaisonOfType(EtablissementClient::TYPE_LIAISON_COOPERATEUR);
+        $header = true;
+
+        foreach($this->apporteurs as $liaison) {
+            $doc = ParcellaireAffectationClient::getInstance()->find(ParcellaireAffectationClient::TYPE_COUCHDB."-".$liaison->getEtablissementIdentifiant()."-".$this->periode);
+            if(!$doc) {
+                continue;
+            }
+            $export = new ExportParcellaireAffectationCSV($doc, $header);
+            $this->renderText($export->export());
+            $header = false;
+        }
+
+        $attachement = sprintf("attachment; filename=export_affectation_parcellaire_%s_%s_%s.csv", $this->etablissement->identifiant, $this->periode, date('YmdHis'));
+        $this->response->setContentType('text/csv');
+        $this->response->setHttpHeader('Content-Disposition',$attachement );
+
+        return sfView::NONE;
+    }
+
 }
