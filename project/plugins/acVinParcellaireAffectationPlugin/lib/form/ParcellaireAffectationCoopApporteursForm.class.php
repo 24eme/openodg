5aaa0099735ea3ed462c2ae924f51400360ba62a
Vincent LAURENT <vince.laurent@gmail.com>
2021-05-27 19:14:23 +0200

    buildApporteurs dans la méthode create, mise en forme des apporteurs sans identification dans le tableau

--- project/plugins/acVinParcellaireAffectationPlugin/lib/form/ParcellaireAffectationCoopApporteursForm.class.php
+++ project/plugins/acVinParcellaireAffectationPlugin/lib/form/ParcellaireAffectationCoopApporteursForm.class.php
@@ -1,30 +1,30 @@
 <?php
 
-class ParcellaireAffectationCoopApporteursForm extends acCouchdbObjectForm {
+class ParcellaireAffectationCoopApporteursForm extends acCouchdbForm {
+
+    public function __construct($doc) {
+        $defaults = array();
+        foreach($doc->getApporteursChoisis() as $id => $apporteur) {
+            $defaults[$id] = 1;
+        }
+      parent::__construct($doc, $defaults);
+    }
 
     public function configure() {
-        foreach ($this->getObject()->apporteurs as $id => $apporteur) {
-            $this->setWidget($id , new WidgetFormInputCheckbox());
+        foreach ($this->getDocument()->apporteurs as $id => $apporteur) {
+            $this->setWidget($id, new WidgetFormInputCheckbox());
             $this->setValidator($id, new ValidatorBoolean());
         }
         $this->widgetSchema->setNameFormat('apporteurs[%s]');
     }
 
-    protected function updateDefaultsFromObject() {
-        parent::updateDefaultsFromObject();
-    }
-
-    protected function doUpdateObject($values) {
-      parent::doUpdateObject($values);
-      foreach ($this->getObject()->getApporteurs() as $id => $apporteur) {
-          $apporteur->apporteur = boolval($values[$id]);
-      }
-      $this->getObject()->updateApporteurs();
-    }
-
-    protected function doSave($con = null) {
-        $this->updateObject();
-        $this->object->getCouchdbDocument()->save();
+    public function save() {
+        $values = $this->getValues();
+        foreach ($this->getDocument()->getApporteurs() as $id => $apporteur) {
+            $apporteur->apporteur = boolval($values[$id]);
+        }
+        $this->getDocument()->updateApporteurs();
+        $this->getDocument()->save();
     }
 
 }
