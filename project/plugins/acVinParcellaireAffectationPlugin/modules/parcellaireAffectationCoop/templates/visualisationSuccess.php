0b6fc1a2682094decc5d72eceb41268aa4def705
mathurin petit <mathurin.petit@gmail.com>
2021-05-27 18:38:11 +0200

    Observations dans le formulaire de saisie de l'intention parcellaire cave coop

--- project/plugins/acVinParcellaireAffectationPlugin/modules/parcellaireAffectationCoop/templates/visualisationSuccess.php
+++ project/plugins/acVinParcellaireAffectationPlugin/modules/parcellaireAffectationCoop/templates/visualisationSuccess.php
@@ -4,6 +4,21 @@
 
 <?php include_partial('parcellaireAffectation/recap', array('parcellaireAffectation' => $parcellaireAffectation)); ?>
 
+<?php if($parcellaireAffectation->observations): ?>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <div class="row">
+                    <div class="col-xs-3">
+                        <h3>Observations :</h3>
+                    </div>
+                     <div class="col-xs-9">
+                        <?php echo nl2br($parcellaireAffectation->observations); ?>
+                     </div>
+            </div>
+        </div>
+   </div>
+<?php endif; ?>
+
 <div class="row row-margin row-button">
     <div class="col-xs-4">
         <a href="<?php echo url_for("parcellaireaffectationcoop_liste", array('sf_subject' => $etablissement, 'periode' => $periode)) ?>" class="btn btn-default btn-upper"><span class="glyphicon glyphicon-chevron-left"></span> Retour</a>
