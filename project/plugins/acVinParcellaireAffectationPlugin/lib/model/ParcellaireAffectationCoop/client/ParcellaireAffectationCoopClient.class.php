f12d2605455bfe0472da76c10aafd1aa8268d0b2
mathurin petit <mathurin.petit@gmail.com>
2021-05-27 15:07:05 +0200

    optimisation des test pour le formulaire

--- project/plugins/acVinParcellaireAffectationPlugin/lib/model/ParcellaireAffectationCoop/client/ParcellaireAffectationCoopClient.class.php
+++ project/plugins/acVinParcellaireAffectationPlugin/lib/model/ParcellaireAffectationCoop/client/ParcellaireAffectationCoopClient.class.php
@@ -34,4 +34,14 @@ class ParcellaireAffectationCoopClient extends acCouchdbClient {
 
     }
 
+    public function getHistory($identifiant, $max_annee = '9999', $hydrate = acCouchdbClient::HYDRATE_DOCUMENT) {
+        $periode_from = "0000";
+        $periode_to = $max_annee;
+
+        $id = self::TYPE_COUCHDB.'-%s-%s';
+        return $this->startkey(sprintf($id, $identifiant, $periode_from))
+        ->endkey(sprintf($id, $identifiant, $periode_to))
+        ->execute($hydrate);
+    }
+
 }
