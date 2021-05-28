9e15d4b15ff151d7c18ee8fbc01a3ede4f43faac
Vincent LAURENT <vince.laurent@gmail.com>
2021-05-28 22:31:16 +0200

    Texte avec "."

--- project/plugins/acVinParcellaireAffectationPlugin/modules/parcellaireAffectationCoop/templates/_monEspace.php
+++ project/plugins/acVinParcellaireAffectationPlugin/modules/parcellaireAffectationCoop/templates/_monEspace.php
@@ -5,7 +5,7 @@
             <h3 class="panel-title">Affectation parcellaire de vos apporteurs</h3>
         </div>
         <div class="panel-body">
-            <p class="explications">Vous pouvez déclarer les affectations parcellaires de vos apporteurs</p>
+            <p class="explications">Vous pouvez déclarer les affectations parcellaires de vos apporteurs.</p>
             <div class="actions">
                 <a id="btn_affection_parcellaire_coop" class="btn btn-block btn-default" href="<?php echo url_for('parcellaireaffectationcoop_create', array('sf_subject' => $etablissement, 'periode' => $periode)) ?>">Déclarer pour vos apporteurs</a>
             </div>
