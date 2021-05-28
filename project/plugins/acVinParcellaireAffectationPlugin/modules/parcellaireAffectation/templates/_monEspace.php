72e04fa537fb357409fabc0388e759f1f23e97a1
Vincent LAURENT <vince.laurent@gmail.com>
2021-05-28 23:23:45 +0200

    Conditionnement du bloc coop aux caves coop uniquement

--- project/plugins/acVinParcellaireAffectationPlugin/modules/parcellaireAffectation/templates/_monEspace.php
+++ project/plugins/acVinParcellaireAffectationPlugin/modules/parcellaireAffectation/templates/_monEspace.php
@@ -1,3 +1,4 @@
+<?php if($etablissement->hasFamille(EtablissementFamilles::FAMILLE_COOPERATIVE)): return; endif; ?>
 <?php use_helper('Date'); ?>
 <div class="col-sm-6 col-md-4 col-xs-12">
     <div class="block_declaration panel  <?php if ($parcellaireAffectation && $parcellaireAffectation->validation): ?>panel-success<?php elseif (($parcellaireAffectation) || ($intentionParcellaireAffectation && ParcellaireAffectationClient::getInstance()->isOpen())): ?>panel-primary<?php else: ?>panel-default<?php endif; ?>">
--- project/plugins/acVinParcellaireAffectationPlugin/modules/parcellaireAffectationCoop/templates/_monEspace.php
+++ project/plugins/acVinParcellaireAffectationPlugin/modules/parcellaireAffectationCoop/templates/_monEspace.php
@@ -1,3 +1,4 @@
+<?php if(!$etablissement->hasFamille(EtablissementFamilles::FAMILLE_COOPERATIVE)): return; endif; ?>
 <?php use_helper('Date'); ?>
 <?php $parcellaireAffectationCoop = ParcellaireAffectationCoopClient::getInstance()->find(ParcellaireAffectationCoopClient::getInstance()->buildId($etablissement->identifiant, $periode), acCouchdbClient::HYDRATE_JSON); ?>
 <div class="col-sm-6 col-md-4 col-xs-12">
