e87688f9b111af88422bc1a73c42088b08bbe036
Mame <mngom@24eme.fr>
2021-05-31 16:05:32 +0200

    Correction Test AffectationLot

--- project/test/unit/82AffectationLotDegustationTest.php
+++ project/test/unit/82AffectationLotDegustationTest.php
@@ -66,6 +66,9 @@ $lotDrev = $drev->lots[0];
 $dateDegustation = new DateTime();
 $dateDegustation = $dateDegustation->modify('+5 days')->format('Y-m-d');
 $degustation = DegustationClient::getInstance()->createDoc($dateDegustation.' 09:24:00');
+$degustation->etape = DegustationEtapes::ETAPE_LOTS;
+$degustation->numero_archive = 2021102;
+$degustation->lieu = "Syndicat des Vignerons";
 $degustation->save();
 $degustationTerminee = DegustationClient::getInstance()->createDoc($dateDegustation.' 10:48:00');
 $degustationTerminee->etape = DegustationEtapes::ETAPE_RESULTATS;
@@ -80,27 +83,43 @@ $degustations = DegustationClient::getInstance()->getHistoryEncours();
 $t->comment('Récupération de la dégustation');
 
 $t->is(count($degustations),1,"Il y a une dégustation en cours");
-$degustationTeste = $degustations[0];
+$degustationTeste = current($degustations);
 $t->is($degustationTeste->_id,$degustation->_id,"La dégustation récupérée correspond à celle crée");
 
 $t->comment('Création du formulaire');
 
-$form = new DegustationAffectionLotForm($lot,$degustationTeste);
+$form = new DegustationAffectionLotForm($lot);
+
+$nom = "Degustation du ".$degustationTeste->date." au ".$degustationTeste->lieu;
+$t->is($form->getDegustationChoices(),array($degustationTeste->_id => $nom),"Exemple : Degustation n°2021102 du 21/05/2021 à 10h30 au Syndicat des Vignerons");
 
 $values = array();
 $values['degustation'] = $degustationTeste->_id;
-$values['prelevement'] = false;
-$values['table'] = 2;
+$values['preleve'] = 1;
+$values['numero_table'] = 1;
 
 $form->bind($values);
-$form->save();
-
 $t->ok($form->isValid(),"Le formulaire est valide");
 
-$degustations = DegustationClient::getInstance()->find($degustationTeste->_id);
+$form->save();
 
-$t->is(count($degustationTeste->lots),1,"La dégustation à un lot");
-$t->is($lots[0]->unique_id,$degustationTeste->lots->getFirst()->unique_id,"Le lot de la dégustation correspond au lot crée");
-$lotAjoutee = $degustationTeste->lots->getFirst();
-$t->is($lotAjoutee->prelevee,null,"Le lot n'a pas été prelevé");
-$t->is($lotAjoutee->numero_table,2,"Le lot est bien assigné à la table 2");
+$degustation = DegustationClient::getInstance()->find($degustationTeste->_id);
+$t->is(count($degustation->getLots()),1,"La dégustation a un lot");
+$t->is($lot->unique_id,$degustation->lots->getFirst()->unique_id,"Le lot de la dégustation correspond au lot crée");
+$lotAjoutee = $degustation->lots->getFirst();
+$t->is($lotAjoutee->preleve,date('Y-m-d'),"Le lot a une date de prelevement");
+$t->is($lotAjoutee->numero_table,1,"Le lot est bien assigné à la table 2");
+$t->is($lotAjoutee->statut, Lot::STATUT_ATTABLE, "Le 1er lot est attablé");
+
+$degustationAnonyme = DegustationClient::getInstance()->find($degustation->_id);
+$degustationAnonyme->anonymize();
+$degustationAnonyme->save();
+$t->ok($degustationAnonyme->isAnonymized(),"La dégustation a été anonymisé");
+
+$t->comment('On ajoute un leurre');
+$leurre = $degustationAnonyme->addLeurre($produitLeurreHash, 'Cepage leurre', 1);
+
+$t->is($leurre->leurre, true, 'C\'est un leurre');
+$t->is($leurre->getProduitHash(), $produitLeurreHash, 'Le hash est le même');
+$t->is($leurre->numero_table, 1, 'Le numéro de table est le 1');
+$t->is($leurre->details, 'Cepage leurre', 'Le cepage du leurre est "Cepage leurre"');
