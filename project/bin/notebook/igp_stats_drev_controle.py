a205d597148baaa0505afeb71b2f005fab607286
Tangui Morlier <tangui@tangui.eu.org>
2021-06-17 16:30:00 +0200

    stats de revendication IGP

--- project/bin/notebook/igp_stats_drev_controle.py
+++ project/bin/notebook/igp_stats_drev_controle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[8]:
+# In[ ]:
 
 
 import pandas as pd
@@ -9,40 +9,61 @@ import pandas as pd
 lots = pd.read_csv("../../web/exports_igp/lots.csv", encoding="iso8859_15", delimiter=";", decimal=",", 
                    dtype={'Code postal Opérateur': 'str', 'Campagne': 'str', 'Num dossier': 'str', 
                           'Num lot': 'str', 'Millésime': 'str'}, low_memory=False)
-lots.columns
+lots['Lieu'].fillna('', inplace=True)
+#lots.columns
 
 
-# In[52]:
+# In[ ]:
+
+
+historique = pd.read_csv("../../web/exports_igp/lots-historique.csv", encoding="iso8859_15", delimiter=";", decimal=",", 
+                   dtype={'Campagne': 'str', 'Num dossier': 'str', 
+                          'Num lot': 'str', 'Millésime': 'str'}, low_memory=False)
+#historique['Doc Type'].unique()
+
+
+# In[ ]:
+
+
+uniq_id = historique[ 
+    (historique['Doc Type'] != 'Conditionnement') & 
+    (historique['Doc Type'] != 'Transaction') & 
+    (historique['Campagne'] >= '2019-2020')
+]['Lot unique Id'].unique()
+lots = lots[lots['Lot unique Id'].isin(uniq_id)]
+#lots = lots[lots['Appellation'] == 'MED']
+
+
+# In[ ]:
 
 
 lots_2020 = lots[(lots['Millésime'] == '2020') & (lots['Date lot'] >= '2020-08-01') & (lots['Date lot'] <= '2021-06-15')]
 lots_2019 = lots[(lots['Millésime'] == '2019') & (lots['Date lot'] >= '2019-08-01') & (lots['Date lot'] <= '2020-06-15')]
 
-stat_igp = lots_2020.groupby('Produit')[['Volume']].sum().rename(columns={"Volume": "VRT 2020"})
-stat_igp['VRT 2019'] = lots_2019.groupby('Produit')[['Volume']].sum()
+#group = ['Produit']
+group = ['Produit', 'Appellation', 'Couleur', 'Lieu']
 
-stat_igp
+stat_igp = lots_2020.groupby(group)[['Volume']].sum().rename(columns={"Volume": "VRT 2020"})
+stat_igp['VRT 2019'] = lots_2019.groupby(group)[['Volume']].sum()
 
-
-# In[45]:
-
-
-historique = pd.read_csv("../../web/exports_igp/lots-historique.csv", encoding="iso8859_15", delimiter=";", decimal=",", 
-                   dtype={'Code postal Opérateur': 'str', 'Campagne': 'str', 'Num dossier': 'str', 
-                          'Num lot': 'str', 'Millésime': 'str'}, low_memory=False)
-lots_controles = historique[(historique['Statut'] == 'Réputé conforme') | (historique['Statut'] == 'Conforme')]
-lots_controles.columns
-
-
-# In[53]:
-
-
-lots_controles_2020 = lots_controles[(lots_controles['Campagne'] == '2020-2021') & (lots_controles['Date lot'] >= '2020-08-01') & (lots_controles['Date lot'] <= '2021-06-15')]
-lots_controles_2019 = lots_controles[(lots_controles['Campagne'] == '2019-2020') & (lots_controles['Date lot'] >= '2019-08-01') & (lots_controles['Date lot'] <= '2020-06-15')]
-
-lots_controles_2020.groupby('Produit')[['Volume']].sum()
-
-stat_igp['VRC 2020'] = lots_controles_2020.groupby('Produit')[['Volume']].sum()
-#stat_igp['VRC 2019'] = lots_controles_2019.groupby('Produit')[['Volume']].sum()
 #stat_igp
+
+
+# In[ ]:
+
+
+lots_conformes = lots[(lots['Statut de lot'] == 'Conforme') | (lots['Statut de lot'] == 'Réputé conforme')]
+lots_conformes_2020 = lots_conformes[(lots_conformes['Millésime'] == '2020') & (lots_conformes['Date lot'] >= '2020-08-01') & (lots_conformes['Date lot'] <= '2021-06-15')]
+lots_conformes_2019 = lots_conformes[(lots_conformes['Millésime'] == '2019') & (lots_conformes['Date lot'] >= '2019-08-01') & (lots_conformes['Date lot'] <= '2020-06-15')]
+
+stat_igp['VRC 2020'] = lots_conformes_2020.groupby(group)[['Volume']].sum()
+stat_igp['VRC 2019'] = lots_conformes_2019.groupby(group)[['Volume']].sum()
+
+#stat_igp
+
+
+# In[ ]:
+
+
+stat_igp.reset_index().to_csv("../../web/exports_igp/igp_stats_vrc-vrt_2020.csv", encoding="iso8859_15", sep=";", index=False, decimal=",")
 
