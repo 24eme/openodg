e6c0f0d6d33fb5a2513355999deb053ba958dab4
Vincent LAURENT <vince.laurent@gmail.com>
2021-06-18 16:50:33 +0200

    Bloc parcellaire crémant

--- project/plugins/DeclarationPlugin/modules/declaration/templates/etablissementSuccess.php
+++ project/plugins/DeclarationPlugin/modules/declaration/templates/etablissementSuccess.php
@@ -85,8 +85,8 @@
     <?php if(class_exists("ParcellaireAffectation") && in_array('parcellaireAffectation', sfConfig::get('sf_enabled_modules'))): ?>
     <?php include_component('parcellaireAffectation', 'monEspace', array('etablissement' => $etablissement, 'campagne' => ConfigurationClient::getInstance()->getCampagneManager()->getNext($campagne))); ?>
     <?php endif; ?>
-    <?php if(class_exists("ParcellaireCremant") && in_array('parcellaireCremant', sfConfig::get('sf_enabled_modules'))): ?>
-    <?php include_component('parcellaireCremant', 'monEspace', array('etablissement' => $etablissement, 'campagne' => ConfigurationClient::getInstance()->getCampagneManager()->getNext($campagne))); ?>
+    <?php if(class_exists("ParcellaireAffectation") && in_array('parcellaireAffectationCremant', sfConfig::get('sf_enabled_modules'))): ?>
+    <?php include_component('parcellaireAffectationCremant', 'monEspace', array('etablissement' => $etablissement, 'periode' => $periode)); ?>
     <?php endif; ?>
     <?php if(class_exists("IntentionCremant") && in_array('intentionCremant', sfConfig::get('sf_enabled_modules'))): ?>
     <?php include_component('intentionCremant', 'monEspace', array('etablissement' => $etablissement, 'campagne' => ConfigurationClient::getInstance()->getCampagneManager()->getNext($campagne))); ?>
