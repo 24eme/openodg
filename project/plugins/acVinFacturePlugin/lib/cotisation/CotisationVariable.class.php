df5566c8f9e17cff71be9c07341065e65824e6e2
Vincent LAURENT <vince.laurent@gmail.com>
2021-06-25 16:54:20 +0200

    Permet de filtrer sur le millésime courant ou non pour la facturation

--- project/plugins/acVinFacturePlugin/lib/cotisation/CotisationVariable.class.php
+++ project/plugins/acVinFacturePlugin/lib/cotisation/CotisationVariable.class.php
@@ -14,10 +14,4 @@ class CotisationVariable extends CotisationFixe
 		return str_replace('%callback%', $this->getCallbackValue(), parent::getLibelle());
 	}
 
-	public function getCallbackValue()
-	{
-
-		return round(call_user_func_array(array($this->getDoc(), $this->getConfigCallback()), $this->getConfigCallbackParameters()), self::PRECISION);
-	}
-
 }
