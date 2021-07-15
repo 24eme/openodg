a312600d0b4f86ac0ab17338c95b68a52baa648b
Honorine-MC <honorine.mariechristy@gmail.com>
2021-07-15 22:41:03 +0200

    export produit ajout de deux colonnes pour faire une jointure avec les stats

--- project/plugins/acVinConfigurationPlugin/lib/task/exportCSVConfigurationTask.class.php
+++ project/plugins/acVinConfigurationPlugin/lib/task/exportCSVConfigurationTask.class.php
@@ -35,7 +35,7 @@ EOF;
 
     $produits = ConfigurationClient::getCurrent()->getProduits();
 
-    echo sprintf("hash;libelle;catégorie;genre;dénomination;mention;lieu;couleur;cépage;code douane;réserve interpro;rend conseil;rend;rend DR;Rend VCI;Rend VCI total;code OI;code produit;code comptable;cvo\n");
+    echo sprintf("hash;libelle;catégorie;genre;appellation;dénomination;mention;lieu;lieu libelle;couleur;cépage;code douane;réserve interpro;rend conseil;rend;rend DR;Rend VCI;Rend VCI total;code OI;code produit;code comptable;cvo\n");
 
     foreach($produits as $hash => $produit) {
         try {
@@ -43,13 +43,15 @@ EOF;
         } catch(Exception $e) {
             $droit_cvo = null;
         }
-        echo sprintf("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n",
+        echo sprintf("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n",
         $hash,
         $produit->getLibelleFormat(),
         $produit->getCertification()->getLibelle(),
         $produit->getGenre()->getLibelle(),
+        $produit->getAppellation()->getKey(),
         $produit->getAppellation()->getLibelle(),
         $produit->getMention()->getLibelle(),
+        $produit->getLieu()->getKey(),
         $produit->getLieu()->getLibelle(),
         $produit->getCouleur()->getLibelle(),
         $produit->getCepage()->getLibelle(),
