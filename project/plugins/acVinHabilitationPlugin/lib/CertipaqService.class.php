cc45032a9144702bcb67d7d7c5e69272df5affc9
Gabriel Poma <gpoma@24eme.fr>
2021-07-15 12:58:39 +0200

    refacto pour marcher avec les token JWT

--- project/plugins/acVinHabilitationPlugin/lib/CertipaqService.class.php
+++ project/plugins/acVinHabilitationPlugin/lib/CertipaqService.class.php
@@ -44,25 +44,25 @@ class CertipaqService
 
     public function sign()
     {
-        $encrypted = '';
-        $key = openssl_pkey_get_private('file://'.$this->configuration['keypath'], $this->configuration['keypass']);
-        if (!$key) {
-            throw new sfException('CertipaqService Error : Openssl get private key failed : '.openssl_error_string());
-        }
-        $datas = $this->getDatas();
-        if (!openssl_sign($datas, $encrypted, $key, 'SHA256')) {
-            throw new sfException('CertipaqService Error : '.openssl_error_string());
-        }
-        $oauth = $datas.'.'.$this->base64SafeEncode($encrypted);
-        $data = array('grant_type'=> 'urn:ietf:params:oauth:grant-type:jwt-bearer' , 'assertion' => $oauth);
-        $result = json_decode($this->httpQuery($this->configuration['urltoken'], array('http' => $this->getOauthHttpRequest($data))), true);
+        $payload = [
+            'username' => $this->configuration['username'],
+            'password' => $this->configuration['password']
+        ];
+
+        $result = json_decode($this->httpQuery(
+            $this->configuration['urltoken'],
+            ['http' => $this->getJWTHttpRequest($payload)]
+        ), true);
+
         if (isset($result['error'])) {
             throw new sfException('CertipaqService Error : '.$result['error'].' : '.$result['message']);
         }
-        if (!isset($result['access_token'])) {
+
+        if (! isset($result['token'])) {
             throw new sfException('CertipaqService Error : '.json_encode($result));
         }
-        return $result['access_token'];
+
+        return $result['token'];
     }
 
     public function transfer($xml = null, $token = null)
@@ -72,39 +72,6 @@ class CertipaqService
         }
         $result = $this->httpQuery($this->configuration['urlapp'], array('http' => $this->getTransferHttpRequest($token, $xml)));
         return $result;
-    }
-
-    /**
-     * @deprecated
-     */
-    public function storeXmlAsAttachement($drm, $xml) {
-        $tmp = tempnam('/tmp', 'attachement');
-        file_put_contents($tmp, $xml);
-        $drm->storeAttachment($tmp, 'text/xml', 'drm_transmise.xml');
-        unlink($tmp);
-    }
-
-    /**
-     * @deprecated
-     */
-    public function transferAndStore($drm, $xml, $token = null) {
-        $certipaqResponse = "";
-        try {
-            $certipaqResponse = $this->transfer($xml, $token);
-        } catch (sfException $e) {
-            $certipaqResponse = $e->getMessage();
-        }
-        $this->storeXmlAsAttachement($drm, $xml);
-        $drm->add('transmission_douane')->add('xml', $certipaqResponse);
-        $drm->add('transmission_douane')->add('success', false);
-
-        if (preg_match('/identifiant-declaration>([^<]*)<.*horodatage-depot>([^<]+)</', $certipaqResponse, $m)) {
-            $drm->add('transmission_douane')->add('success', true);
-            $drm->add('transmission_douane')->add('horodatage', $m[2]);
-            $drm->add('transmission_douane')->add('id_declaration', $m[1]);
-        }
-
-        $drm->save();
     }
 
     protected function needNewToken()
@@ -133,17 +100,15 @@ class CertipaqService
         return sfConfig::get('sf_cache_dir').'/'.self::TOKEN_CACHE_FILENAME;
     }
 
-    protected function getOauthHttpRequest($content)
+    protected function getJWTHttpRequest($content)
     {
         return array(
             'headers'  => array(
                 "Host: ".$this->configuration['host'],
                 "Content-Type: application/json"
             ),
-            'method'  => 'POST',
-            'protocol_version' => 1.1,
-            'ignore_errors' => true,
-            'content' => http_build_query($content)
+            'method'  => 'GET',
+            'content' => json_encode($content)
         );
     }
 
@@ -184,32 +149,32 @@ class CertipaqService
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
-        if (isset($options['http']['content']) || (isset($options['http']['method']) && $options['http']['method'] == 'POST')) {
-            curl_setopt($ch, CURLOPT_POST, 1);
+
+        if (isset($options['http']['method'])) {
+            if ($options['http']['method'] == "GET") {
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+            } else {
+                curl_setopt($ch, CURLOPT_POST, 1);
+            }
+        }
+
+        if (isset($options['http']['content'])) {
             curl_setopt($ch, CURLOPT_POSTFIELDS, $options['http']['content']);
         }
+
         if (isset($options['http']['headers'])) {
             curl_setopt($ch, CURLOPT_HTTPHEADER, $options['http']['headers']);
         }
+
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $server_output = curl_exec ($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close ($ch);
+
         if ($httpCode < 200 || $httpCode >= 300 ) {
             throw new sfException('HTTP Error '.$httpCode.' : '.$server_output);
         }
+
         return $server_output;
-    }
-
-    protected function getDatas()
-    {
-        $entete = '{"alg":"RS256"}';
-        $corps = '{"iss":"'.$this->configuration['iss'].'","scope":"'.$this->configuration['service'].'","aud":"'.$this->configuration['url'].'","iat":'.time().'000}';
-        return $this->base64SafeEncode($entete).'.'.$this->base64SafeEncode($corps);
-    }
-
-    protected function base64SafeEncode($input)
-    {
-        return str_replace('=', '', strtr(base64_encode($input), '+/', '-_'));
     }
 }
