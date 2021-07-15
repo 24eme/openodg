5e8d2f72ff29ebf3efb90f2bc98879ca0fd9451a
Gabriel Poma <gpoma@24eme.fr>
2021-07-15 20:06:21 +0200

    certipaq drev

--- project/test/unit/44HabilitationCertipaqTest.php
+++ project/test/unit/44HabilitationCertipaqTest.php
@@ -2,10 +2,11 @@
 
 require_once(dirname(__FILE__).'/../bootstrap/common.php');
 
-$t = new lime_test(4);
+$t = new lime_test(6);
 
 $certipaq = CertipaqService::getInstance();
 $deroulant = CertipaqDeroulant::getInstance();
+$drev = CertipaqDRev::getInstance();
 
 $token = $certipaq->getToken();
 
@@ -13,3 +14,12 @@ $t->ok($token, "Il y a un token");
 $t->ok($certipaq->getProfil(), "On récupère l'info du profil");
 $t->cmp_ok(count($deroulant->getListeActivitesOperateurs()), '>=', 1, "On récupère la liste d'activité");
 $t->cmp_ok(count($deroulant->getListeTypeControle()), '>=', 1, "On récupère la liste des types controle");
+
+$t->comment("DRev");
+$t->ok($drev->list(['date' => ['2019-01-01', '2022-01-01']]));
+try {
+    var_dump($drev->find($i));
+    $t->fail();
+} catch (Exception $e) {
+    $t->pass($e->getMessage());
+}
