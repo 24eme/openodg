5237db4887c10d4ca717b9c17b14264f9580e411
Gabriel Poma <gpoma@24eme.fr>
2021-07-15 19:47:27 +0200

    2eme classe pour les appels à certipaq

--- project/test/unit/44HabilitationCertipaqTest.php
+++ project/test/unit/44HabilitationCertipaqTest.php
@@ -2,9 +2,14 @@
 
 require_once(dirname(__FILE__).'/../bootstrap/common.php');
 
-$t = new lime_test();
+$t = new lime_test(4);
 
 $certipaq = CertipaqService::getInstance();
+$deroulant = CertipaqDeroulant::getInstance();
+
 $token = $certipaq->getToken();
 
-$t->ok($token, "Il y a un token: $token");
+$t->ok($token, "Il y a un token");
+$t->ok($certipaq->getProfil(), "On récupère l'info du profil");
+$t->cmp_ok(count($deroulant->getListeActivitesOperateurs()), '>=', 1, "On récupère la liste d'activité");
+$t->cmp_ok(count($deroulant->getListeTypeControle()), '>=', 1, "On récupère la liste des types controle");
