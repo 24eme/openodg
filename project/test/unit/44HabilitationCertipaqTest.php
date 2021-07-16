9df65107206247fe97d0b04a663fe8c2a2ac19cd
Gabriel Poma <gpoma@24eme.fr>
2021-07-16 13:05:59 +0200

    recuperation liste habilitation

--- project/test/unit/44HabilitationCertipaqTest.php
+++ project/test/unit/44HabilitationCertipaqTest.php
@@ -4,19 +4,21 @@ require_once(dirname(__FILE__).'/../bootstrap/common.php');
 
 $t = new lime_test(10);
 
-$cvi_test = "0000000000";
+$cvi_test = "0708900950";
 
 $certipaq = CertipaqService::getInstance();
 $deroulant = CertipaqDeroulant::getInstance();
 $drev = CertipaqDRev::getInstance();
 $operateur = CertipaqOperateur::getInstance();
 
+$statuts_habilitations = $deroulant->getListeStatutHabilitation();
 $token = $certipaq->getToken();
 
 $t->ok($token, "Il y a un token");
 $t->ok($certipaq->getProfil(), "On récupère l'info du profil");
 $t->cmp_ok(count($deroulant->getListeActivitesOperateurs()), '>=', 1, "On récupère la liste d'activité");
 $t->cmp_ok(count($deroulant->getListeTypeControle()), '>=', 1, "On récupère la liste des types controle");
+$t->cmp_ok(count($deroulant->getListeStatutHabilitation()), '>=', 1, "On récupère la liste des statuts d'habilitation");
 
 $t->comment("DRev");
 $t->ok($drev->list(['date' => ['2019-01-01', '2022-01-01']]));
