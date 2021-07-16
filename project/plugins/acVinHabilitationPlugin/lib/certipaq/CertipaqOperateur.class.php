6c5ef7a1fe6313b41dfc24183534efcb31652ba4
Gabriel Poma <gpoma@24eme.fr>
2021-07-16 12:14:37 +0200

    recherche opérateur

---
+++ project/plugins/acVinHabilitationPlugin/lib/certipaq/CertipaqOperateur.class.php
@@ -0,0 +1,42 @@
+<?php
+
+class CertipaqOperateur extends CertipaqService
+{
+    const ENDPOINT_RECHERCHE = 'operateur';
+    const ENDPOINT_RECUPERATION = 'operateur/{id_operateur}';
+
+    private function query($endpoint, $method = 'GET', $payload = null)
+    {
+        $result = $this->httpQuery(
+            $this->configuration['apiurl'].$endpoint,
+            [
+                'http' => $this->getQueryHttpRequest($this->getToken(), $method, $payload)
+            ]
+        );
+
+        $result = json_decode($result);
+
+        return $result->results;
+    }
+
+    /**
+     * @param Array $params Il faut renseigner au moins : raison_sociale et
+     * code_postal, ou siret ou cvi ou dr_cdc_famille ou dr_cdc ou
+     * dr_activites_operateurs ou date_decision ou dr_statut_habilitation.
+     *
+     */
+    public function recherche($params = [])
+    {
+        if (empty($params)) {
+            throw new Exception("CertipaqOperateur Error : this function needs params");
+        }
+
+        if ((isset($params['raison_sociale']) && empty($params['code_postal']))
+            || isset($params['code_postal']) && empty($params['raison_sociale']))
+        {
+            throw new Exception("CertipaqOperateur Error : la raison sociale et le cp doivent être renseignés ensemble");
+        }
+
+        return $this->query(self::ENDPOINT_RECHERCHE, 'GET', $params);
+    }
+}
--- project/test/unit/44HabilitationCertipaqTest.php
+++ project/test/unit/44HabilitationCertipaqTest.php
@@ -2,11 +2,14 @@
 
 require_once(dirname(__FILE__).'/../bootstrap/common.php');
 
-$t = new lime_test(6);
+$t = new lime_test(10);
+
+$cvi_test = "0000000000";
 
 $certipaq = CertipaqService::getInstance();
 $deroulant = CertipaqDeroulant::getInstance();
 $drev = CertipaqDRev::getInstance();
+$operateur = CertipaqOperateur::getInstance();
 
 $token = $certipaq->getToken();
 
@@ -18,8 +21,26 @@ $t->cmp_ok(count($deroulant->getListeTypeControle()), '>=', 1, "On récupère la
 $t->comment("DRev");
 $t->ok($drev->list(['date' => ['2019-01-01', '2022-01-01']]));
 try {
-    var_dump($drev->find($i));
+    $drev->find(1);
     $t->fail();
 } catch (Exception $e) {
     $t->pass($e->getMessage());
 }
+
+try {
+    $operateur->recherche();
+    $t->fail();
+} catch (Exception $e) {
+    $t->pass($e->getMessage());
+}
+
+try {
+    $operateur->recherche(['raison_sociale' => 'test']);
+    $t->fail();
+} catch (Exception $e) {
+    $t->pass($e->getMessage());
+}
+
+$operateur = $operateur->recherche(['cvi' => $cvi_test]);
+$t->is(count($operateur), 1, "On récupère les infos du viti");
+$t->is($operateur[0]->cvi, $cvi_test, "C'est le cvi qu'on a demandé");
