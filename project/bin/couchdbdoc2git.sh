73b577e77625144c218ed40551009e0bc6977993
Tangui Morlier <tangui@tangui.eu.org>
2021-07-22 18:19:28 +0200

    versionnage des documents couchdb depuis une url

--- project/bin/couchdbdoc2git.sh
+++ project/bin/couchdbdoc2git.sh
@@ -3,15 +3,21 @@
 . bin/config.inc
 
 doc=$1
+url=$doc
+if ! echo $url | grep 'http://' > /dev/null; then
+    url="http://$COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/$doc"
+else
+    doc=$(echo $doc | sed 's|http://[^/]*/[^/]*/||')
+fi
 
-i=0 
+i=0
 mkdir  -p couchdbdoc2git && cd couchdbdoc2git && git init .
-curl "http://$COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/$doc?open_revs=all&revs=true" | grep '^\{' | jq ._revisions.ids  | tac | grep ' "' | awk -F '"' '{print $2}'  | while read rev ; do 
-	i=$(( $i + 1 )) ; 
-	echo $i"-"$rev ; 
-	curl -s http://$COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/$doc"?rev="$i"-"$rev | jq . > $doc".json" ; 
-	git add $doc".json" ; 
-	git commit -m "version "$i"-"$rev ; 
+curl "$url?open_revs=all&revs=true" | grep '^\{' | jq ._revisions.ids  | tac | grep ' "' | awk -F '"' '{print $2}'  | while read rev ; do
+	i=$(( $i + 1 )) ;
+	echo $i"-"$rev ;
+	curl -s $url"?rev="$i"-"$rev | jq . > $doc".json" ;
+	git add $doc".json" ;
+	git commit -m "version "$i"-"$rev ;
 done
 
 git log -p $doc".json"
