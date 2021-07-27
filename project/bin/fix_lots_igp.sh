99c0dfc7aba5d103bded6124ad502c1f2387c107
Vincent LAURENT <vince.laurent@gmail.com>
2021-07-27 18:39:23 +0200

    Oubli

--- project/bin/fix_lots_igp.sh
+++ project/bin/fix_lots_igp.sh
@@ -11,7 +11,7 @@ ODG=$1
 
 curl -s http://$COUCHHOST:$COUCHPORT/$COUCHBASE/_design/archivage/_view/all?reduce=false | grep "Lot" | cut -d "," -f 1 | sed 's/{"id":"//' | sed 's/"//' | sort | uniq > /tmp/all_docs_$COUCHBASE
 # Mise à jour des changements de dénominations
-cat /tmp/all_docs_igparlespreprod | grep CHGTDENOM | while read id; do echo $id; echo php symfony chgtdenom:regenerate-lots $id $SYMFONYTASKOPTIONS; done
+cat /tmp/all_docs_$COUCHBASE | grep CHGTDENOM | while read id; do echo $id; echo php symfony chgtdenom:regenerate-lots $id $SYMFONYTASKOPTIONS; done
 # Première correction des lots
 cat /tmp/all_docs_$COUCHBASE | while read id; do echo "$id"; php symfony fix:document-lots $id $SYMFONYTASKOPTIONS; done
 # Second passage pour cohérence des numéros de lots
