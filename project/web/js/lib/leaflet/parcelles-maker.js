ccace4ee1281d339be534f6978f6cf92e0a42781
Tangui Morlier <tangui@tangui.eu.org>
2021-08-02 21:00:43 +0200

    Bonne attribution (cadastre + ign)

--- project/web/js/lib/leaflet/parcelles-maker.js
+++ project/web/js/lib/leaflet/parcelles-maker.js
@@ -26,9 +26,10 @@ var map = L.map('map');
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 30,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> creator: ' +
+    attribution: 'Map data &copy;' +
         '<a href="https://www.24eme.fr/">24eme Société coopérative</a>, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        '<a href="https://cadastre.data.gouv.fr/">Cadastre</a>, ' +
+        'Imagery © <a href="https://www.igp.fr/">IGN</a>',
     id: 'mapbox.light'
 }).addTo(map);
 
