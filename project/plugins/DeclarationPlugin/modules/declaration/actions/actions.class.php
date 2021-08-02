f7effb4f48249f841ddc9ad25f780481eadbb373
Tangui Morlier <tangui@tangui.eu.org>
2021-08-02 22:02:45 +0200

    Pour mon espace en nouvelle campagne

--- project/plugins/DeclarationPlugin/modules/declaration/actions/actions.class.php
+++ project/plugins/DeclarationPlugin/modules/declaration/actions/actions.class.php
@@ -181,9 +181,9 @@ class declarationActions extends sfActions {
             $this->form = new EtablissementChoiceForm(sfConfig::get('app_interpro', 'INTERPRO-declaration'), array('identifiant' => $this->etablissement->identifiant), true);
         }
 
-        $this->campagne = $request->getParameter('campagne', ConfigurationClient::getInstance()->getCampagneVinicole()->getCurrent());
+        $this->campagne = $request->getParameter('campagne', ConfigurationClient::getInstance()->getCampagneManager()->getCurrent());
         $this->periode = preg_replace('/-.*/', '', $this->campagne);
-        if(!$this->getUser()->hasDrevAdmin() && $this->campagne != ConfigurationClient::getInstance()->getCampagneVinicole()->getCurrent()) {
+        if(!$this->getUser()->hasDrevAdmin() && $this->campagne != ConfigurationClient::getInstance()->getCampagneManager()->getCurrent()) {
 
             return $this->forwardSecure();
         }
--- project/plugins/acVinDRevAVAPlugin/modules/drev/actions/components.class.php
+++ project/plugins/acVinDRevAVAPlugin/modules/drev/actions/components.class.php
@@ -3,6 +3,7 @@
 class drevComponents extends sfComponents {
 
     public function executeMonEspace(sfWebRequest $request) {
+        $this->periode = substr($this->campagne, 0, 5);
         $this->drev = DRevClient::getInstance()->findMasterByIdentifiantAndCampagne($this->etablissement->identifiant, $this->campagne);
     }
 
