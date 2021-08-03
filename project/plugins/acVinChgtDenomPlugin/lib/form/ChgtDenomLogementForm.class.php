db18717c2d0acc2bf859bddbd662dc9d8a88fb60
Tangui Morlier <tangui@tangui.eu.org>
2021-08-03 15:08:14 +0200

    Tant que les chngt n'est pas validé, il n'y a pas de lots de sauver. Il faut donc les simuler (sans qu'ils soient sauvés)

--- project/plugins/acVinChgtDenomPlugin/lib/form/ChgtDenomLogementForm.class.php
+++ project/plugins/acVinChgtDenomPlugin/lib/form/ChgtDenomLogementForm.class.php
@@ -5,6 +5,11 @@ class ChgtDenomLogementForm extends acCouchdbObjectForm
     public function configure()
     {
         $chgt = $this->getObject();
+        $this->has_lots = true;
+        if (!$chgt->exist('lots') || !count($chgt->lots)) {
+            $this->has_lots = false;
+            $chgt->generateLots();
+        }
 
         foreach ($chgt->lots as $lot) {
             if ($lot->isLogementEditable() && $lot->isLotOrigine()) {
@@ -16,6 +21,9 @@ class ChgtDenomLogementForm extends acCouchdbObjectForm
                 $this->setWidget('changement_numero_logement_operateur', new bsWidgetFormInput());
                 $this->setValidator('changement_numero_logement_operateur', new sfValidatorString());
             }
+        }
+        if (!$this->has_lots) {
+            $chgt->clearLots();
         }
 
         $this->widgetSchema->setNameFormat('chgt_denom_logement[%s]');
