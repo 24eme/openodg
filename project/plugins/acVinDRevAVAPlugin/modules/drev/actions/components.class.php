01d66c42acd034533e7c278b8b1839aa695072e8
Tangui Morlier <tangui@tangui.eu.org>
2021-08-04 12:13:48 +0200

    Sécurité sur la période dans mon espace drev de l'ava

--- project/plugins/acVinDRevAVAPlugin/modules/drev/actions/components.class.php
+++ project/plugins/acVinDRevAVAPlugin/modules/drev/actions/components.class.php
@@ -3,6 +3,9 @@
 class drevComponents extends sfComponents {
 
     public function executeMonEspace(sfWebRequest $request) {
+        if (!$this->periode) {
+            $this->periode = substr($this->campagne, 0, 4);
+        }
         $this->campagne = $this->periode.'-'.($this->periode + 1);
         $this->drev = DRevClient::getInstance()->findMasterByIdentifiantAndCampagne($this->etablissement->identifiant, $this->periode);
     }
