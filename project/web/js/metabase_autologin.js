2e635199430036d1a3d3f73b4a0121e7f04654e5
Vincent LAURENT <vince.laurent@gmail.com>
2021-09-04 00:30:40 +0200

    Metabase autologin in function

--- project/web/js/metabase_autologin.js
+++ project/web/js/metabase_autologin.js
@@ -1,16 +1,15 @@
-// var metabase_webpath = /webpath_to_metabase;
-// var metabase_authscript = /webpath_to_metabase_auth.php;
-
-var xhttp = new XMLHttpRequest();
-xhttp.open("GET", metabase_webpath + "/api/user/current", true);
-xhttp.send();
-xhttp.onload = function() {
-	if(xhttp.status != 401) {
-		return;
-	}
+function metabase_redirect_to_external_auth(metabase_webpath, metabase_external_auth_page) {
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", metabase_webpath + "/api/user/current", true);
+	xhttp.send();
+	xhttp.onload = function() {
+		if(xhttp.status != 401) {
+			return;
+		}
 		var url = new URL(document.location.href);
 		if(!url.search.match(/noautologin/)) {
         		url.searchParams.append('noautologin', '1');
-        		document.location.href = metabase_authscript + '?redirect=' + encodeURIComponent(url);
+            document.location.href = metabase_external_auth_page + '?redirect=' + encodeURIComponent(url);
 		}
+	}
 }
