c62c2d9dc135dd903588f1b56ba3cef3d18d64c0
Tangui Morlier <tangui@tangui.eu.org>
2021-09-24 18:46:32 +0200

    Test d'envoi d'une ligne de DR

--- project/plugins/acVinHabilitationPlugin/lib/certipaq/CertipaqDRev.class.php
+++ project/plugins/acVinHabilitationPlugin/lib/certipaq/CertipaqDRev.class.php
@@ -12,4 +12,28 @@ class CertipaqDRev extends CertipaqService
         $endpoint = str_replace('{id_declaration}', $id, self::ENDPOINT_RECUPERATION);
         return $this->query($endpoint);
     }
+
+    public function createUneLigne($etablissement, $produit_conf, $millesime, $superficie, $volume) {
+        $operateur = CertipaqOperateur::getInstance()->findByEtablissement($etablissement);
+        if (!$operateur) {
+            throw new sfException('Opérateur non reconnu pour '.$etablissement->cvi." / ".$etablissement->siret);
+        }
+        $produit = CertipaqDeroulant::getInstance()->getCertipaqProduitFromConfigurationProduit($produit_conf);
+        if (!$produit) {
+            throw new sfException('Produit non reconnu pour '.$produit_conf->getLibelleComplet());
+        }
+        $habilitation = CertipaqOperateur::getInstance()->getHabilitationFromOperateurProduitAndActivite($operateur, $produit, CertipaqDeroulant::ACTIVITE_PRODUCTEUR);
+
+        $params = array();
+        $params['operateur_id'] = $operateur->id;
+        $params['operateurs_sites_id'] = $habilitation->site_id;
+        $params['dr_cdc_famille_id'] = $habilitation->dr_cdc_famille_id;
+        $params['dr_cdc_id'] = $habilitation->dr_cdc->id;
+        $params['millesime'] = "$millesime";
+        $params['volume_hl'] = floatval($volume);
+        $params['surface_ha'] = floatval($superficie);
+        $params['dr_cdc_produit_id'] = $habilitation->dr_cdc_produit_id;
+
+        return $this->query('declaration/revendication', 'POST', $params);
+    }
 }
