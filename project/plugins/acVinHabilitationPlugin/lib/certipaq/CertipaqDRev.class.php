68f3369726e54d3379334e28ded9ff42ca1ecf24
Tangui Morlier <tangui@tangui.eu.org>
2021-09-24 18:45:09 +0200

    Simplification

--- project/plugins/acVinHabilitationPlugin/lib/certipaq/CertipaqDRev.class.php
+++ project/plugins/acVinHabilitationPlugin/lib/certipaq/CertipaqDRev.class.php
@@ -2,17 +2,9 @@
 
 class CertipaqDRev extends CertipaqService
 {
-    const ENDPOINT_REVENDICATION = 'declaration/revendication';
-    const ENDPOINT_RECUPERATION = 'declaration/revendication/{id_declaration}';
-
     public function list($params = [])
     {
-        return $this->query(self::ENDPOINT_REVENDICATION, 'GET', $params);
-    }
-
-    public function create($params = [])
-    {
-        return $this->query(self::ENDPOINT_REVENDICATION, 'POST', $params);
+        return $this->query('declaration/revendication', 'GET', $params);
     }
 
     public function find($id)
