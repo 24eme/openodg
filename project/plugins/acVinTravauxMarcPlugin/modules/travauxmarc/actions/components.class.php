830cae96f0b8ab1e9cc0f02855303f9a242846f7
Vincent LAURENT <vince.laurent@gmail.com>
2021-10-01 17:17:32 +0200

    Compatibilité période / campagne

--- project/plugins/acVinTravauxMarcPlugin/modules/travauxmarc/actions/components.class.php
+++ project/plugins/acVinTravauxMarcPlugin/modules/travauxmarc/actions/components.class.php
@@ -3,6 +3,9 @@
 class travauxmarcComponents extends sfComponents {
 
     public function executeMonEspace(sfWebRequest $request) {
+        if(!$this->periode) {
+            $this->periode = $this->campagne;
+        }
         $this->travauxmarc = TravauxMarcClient::getInstance()->find(TravauxMarcClient::TYPE_COUCHDB.'-' . $this->etablissement->identifiant . '-' . $this->periode);
     }
 
