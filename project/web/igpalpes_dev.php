202906a68cf3fed54c4cfc985dc854bef85692e6
Tangui Morlier <tangui@tangui.eu.org>
2021-10-05 11:08:39 +0200

    Introduction de Alpes

---
+++ project/web/igpalpes_dev.php
@@ -0,0 +1,10 @@
+<?php
+require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
+$configuration = ProjectConfiguration::getApplicationConfiguration('igpalpes', 'dev', true);
+
+if (!in_array(@$_SERVER["REMOTE_ADDR"], sfConfig::get("app_debug_authorized_ip", array("127.0.0.1", "::1")) ))
+{
+  die('You ('.$_SERVER['REMOTE_ADDR'].') are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
+
+sfContext::createInstance($configuration)->dispatch();
