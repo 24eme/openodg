09b36ae3b1cbf355fa48709395baffccaa70e7c5
Vincent LAURENT <vince.laurent@gmail.com>
2021-10-08 14:58:51 +0200

    application passé en paramètre du notebook

--- project/bin/notebook/igp_stats_drev_controle.py
+++ project/bin/notebook/igp_stats_drev_controle.py
@@ -9,17 +9,22 @@ import sys
 import re
 from datetime import datetime
 
+path_igp = "../../web/exports_igp"
+
+if(re.search("^igp", sys.argv[1])):
+     path_igp = "../../web/exports_"+sys.argv[1]
+
 millesime = str(datetime.now().year - 1)
 
-if(re.search("^[0-9]{4}#", sys.argv[1])):
-    millesime = sys.argv[1]
+if(re.search("^[0-9]{4}#", sys.argv[2])):
+    millesime = sys.argv[2]
     
 millesime_precedent = str(int(millesime) - 1)
 
 moisjour = "07-31"
     
-if(re.search("^[0-9]{2}-[0-9]{2}#", sys.argv[2])):
-    moisjour = sys.argv[2]
+if(len(sys.argv) > 3 and re.search("^[0-9]{2}-[0-9]{2}#", sys.argv[3])):
+    moisjour = sys.argv[3]
     
 date_debut_courant = millesime + '-08-01'
 date_fin_courant = str(int(millesime) + 1) + '-' + moisjour
@@ -31,7 +36,7 @@ date_fin_precedent = str(int(millesime_precedent) + 1) + '-' + moisjour
 # In[ ]:
 
 
-lots = pd.read_csv("../../web/exports_igp/lots.csv", encoding="iso8859_15", delimiter=";", decimal=",",
+lots = pd.read_csv(path_igp+"/lots.csv", encoding="iso8859_15", delimiter=";", decimal=",",
                    dtype={'Code postal Opérateur': 'str', 'Campagne': 'str', 'Num dossier': 'str',
                           'Num lot': 'str', 'Millésime': 'str'}, low_memory=False)
 lots['Lieu'].fillna('', inplace=True)
@@ -75,5 +80,11 @@ stat_igp = stat_igp.round({'VRC ' + millesime: 2, 'VRC ' + millesime_precedent: 
 # In[ ]:
 
 
-stat_igp.reset_index().to_csv("../../web/exports_igp/igp_stats_vrc-vrt_2020.csv", encoding="iso8859_15", sep=";", index=False, decimal=",")
+stat_igp.reset_index().to_csv(path_igp+"/igp_stats_vrc-vrt_2020.csv", encoding="iso8859_15", sep=";", index=False, decimal=",")
+
+
+# In[ ]:
+
+
+
 
