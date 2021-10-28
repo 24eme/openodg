de9acda5f6b0bb8f52e0aa5f8a30548c0a03ff8e
Tangui Morlier <tangui@tangui.eu.org>
2021-10-28 11:48:34 +0200

    Nouveau logo de provence

--- project/apps/provence/templates/_header.php
+++ project/apps/provence/templates/_header.php
@@ -5,6 +5,6 @@
           </a>
       </div>
       <div id="titre"><h1>
-        <img height="35" width="207" src="/images/provence_espace_personnel.png" alt="Espace personnel"/><img height="35" width="23" src="/images/provence_tiret.png" alt=" - "/><img height="35" width="435" src="/images/provence_syndicat_des_cotes_de_provence.png" alt="Syndicat des Côtes de Provence"/>
+        <img height="35" width="207" src="/images/provence_espace_personnel.png" alt="Espace personnel"/>
       </h1></div>
 </header>
