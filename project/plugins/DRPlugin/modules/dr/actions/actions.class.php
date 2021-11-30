453030ed9901eeec5c322d52889c230108cbe962
Gabriel Poma <gpoma@24eme.fr>
2021-11-30 16:45:56 +0100

    DR visualisation: squelette

---
+++ project/plugins/DRPlugin/modules/dr/actions/actions.class.php
@@ -0,0 +1,14 @@
+<?php
+
+class drActions extends sfActions
+{
+    public function executeVisualisation(sfWebRequest $request)
+    {
+
+    }
+
+    public function executeApprobation(sfWebRequest $request)
+    {
+
+    }
+}
