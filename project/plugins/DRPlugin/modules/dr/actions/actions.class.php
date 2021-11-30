72e5d1166e847d17618a8d500db3383d59ef4b28
Gabriel Poma <gpoma@24eme.fr>
2021-11-30 18:34:10 +0100

    DR: squelette page visualisation

--- project/plugins/DRPlugin/modules/dr/actions/actions.class.php
+++ project/plugins/DRPlugin/modules/dr/actions/actions.class.php
@@ -4,7 +4,7 @@ class drActions extends sfActions
 {
     public function executeVisualisation(sfWebRequest $request)
     {
-
+        $this->dr = $this->getRoute()->getDR();
     }
 
     public function executeApprobation(sfWebRequest $request)
---
+++ project/plugins/DRPlugin/modules/dr/templates/visualisationSuccess.php
@@ -0,0 +1,51 @@
+<?php use_helper('Date'); ?>
+
+<?php include_partial('global/flash'); ?>
+
+<div class="page-header no-border clearfix">
+    <h2>
+        Déclaration de Récolte <?= $dr->campagne ?>
+        <small class="pull-right">
+            <i class="glyphicon glyphicon-file"></i>
+            Déclaration importé le <?= format_date($dr->date_import, "dd/MM/yyyy", "fr_FR") ?>
+        </small>
+    </h2>
+</div>
+
+<?php if($dr->exist('validation') && $dr->validation): ?>
+    <div class="text-right text-success">
+        DR validée le <?= format_date($dr->validation, "dd/MM/yyyy", "fr_FR") ?>
+    </div>
+<?php endif ?>
+
+<div class="well mb-5">
+    <?php include_partial('etablissement/blocDeclaration', ['etablissement' => $dr->getEtablissementObject()]); ?>
+</div>
+
+<?php include_partial('dr/recap', ['dr' => $dr]) ?>
+
+<div class="row row-margin row-button">
+    <div class="col-xs-4">
+        <a href="<?= ($service) ?: url_for('declaration_etablissement', ['identifiant' => $dr->identifiant, 'campagne' => $dr->campagne]) ?>"
+            class="btn btn-default"
+        >
+            <i class="glyphicon glyphicon-chevron-left"></i> Retour
+        </a>
+    </div>
+
+    <div class="col-xs-4 text-center">
+        <a class="btn btn-default" href="#">
+            <i class="glyphicon glyphicon-file"></i> DR
+        </a>
+    </div>
+
+    <div class="col-xs-4 text-right">
+        <?php if($dr->exist('validation') && $dr->validation): ?>
+            DR approuvée le <?= format_date($dr->validation, "dd/MM/yyyy", "fr_FR") ?>
+        <?php else : ?>
+            <a href="<?= url_for('dr_approbation', ['id' => $dr->_id]) ?>" class="btn btn-default">
+                Approuver la DR
+            </a>
+        <?php endif ?>
+    </div>
+</div>
