db863b5581ed8ca2004959f7af4cfe8dc01331d8
Vincent LAURENT <vince.laurent@gmail.com>
2021-12-03 16:03:12 +0100

    Comparaison des numéros de cuve slugifié

--- project/plugins/acVinDRevPlugin/lib/task/DrevImportTask.class.php
+++ project/plugins/acVinDRevPlugin/lib/task/DrevImportTask.class.php
@@ -248,7 +248,7 @@ EOF;
         // Check si le Volume est le même que celui d'un autre Lot
         foreach ($drev->getLots() as $lot) {
             if (  ($lot->volume == $volume) &&
-                  ( ($ligne[ExportDRevCSV::CSV_DATE_VALIDATION_ODG] < '2021-08-00') || ($numero_cuve == $lot->numero_logement_operateur) ) ) {
+                  ( ($ligne[ExportDRevCSV::CSV_DATE_VALIDATION_ODG] < '2021-08-00') || (KeyInflector::slugify(trim($numero_cuve)) == KeyInflector::slugify(trim($lot->numero_logement_operateur)) ) )) {
                 return true;
             }
         }
