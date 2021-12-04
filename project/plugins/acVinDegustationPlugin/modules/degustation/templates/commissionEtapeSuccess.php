3d7a506b107be93f02654bdfabc5efcb6379b9ab
Vincent LAURENT <vince.laurent@gmail.com>
2021-12-04 13:53:42 +0100

    Épuration des divs et amélioration de la mise en forme de l'étape commission

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/commissionEtapeSuccess.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/commissionEtapeSuccess.php
@@ -3,101 +3,54 @@
 <?php include_partial('degustation/breadcrumb', array('degustation' => $degustation)); ?>
 <?php include_partial('degustation/step', array('degustation' => $degustation, 'active' => DegustationEtapes::ETAPE_COMMISSION)); ?>
 
-
 <div class="page-header no-border">
-  <h2>Commission</h2>
+    <h2>Commission</h2>
 </div>
 
-<div class="row">
-  <div class="col-xs-12">
-    <div class="panel panel-default" style="min-height: 160px">
-      <div class="panel-heading">
-        <h2 class="panel-title">
-          <div class="row">
-            <div class="col-xs-12">Documents nécessaires à l'organisation d'une commission</div>
-          </div>
-        </h2>
-      </div>
-      <div class="panel-body">
-          <div class="row">
-            <div class="col-xs-12">
-              <strong>Pdf des étiquettes pour l'organisation des tables</strong>
-              <br/>
-            </div>
-          </div>
-          <div class="row">
-            <?php if($degustation->isAnonymized()): ?>
-              <div class="col-xs-12">
-                <br/>
-                <ul class="list-group">
-                  <li class="list-group-item"><a id="btn_degustation_fiche_tables_echantillons_par_dossier_pdf" href="<?php echo url_for('degustation_fiche_tables_echantillons_par_dossier_pdf', $degustation) ?>" ><span class="glyphicon glyphicon-file"></span>&nbsp;Fiche des lots ventilés (triés par Numéro de dossier)</a></li>
-                  <li class="list-group-item"><a id="btn_pdf_degustation_fiche_tables_echantillons_par_anonymat_pdf" href="<?php echo url_for('degustation_fiche_tables_echantillons_par_anonymat_pdf', $degustation) ?>"><span class="glyphicon glyphicon-file"></span>&nbsp;Fiche des lots ventilés (triés par Numéro d'anonymat)</a></li>
-                  <li class="list-group-item">
-                      <span class="glyphicon glyphicon-file"></span>&nbsp;Étiquettes pour tables (
-                      <a id="btn_pdf_degustation_etiquettes_tables_echantillons_par_anonymat_pdf" href="<?php echo url_for('degustation_etiquettes_tables_echantillons_par_anonymat_pdf', $degustation) ?>">triées par Numéro d'anonymat </a>
-                       -
-                      <a id="btn_pdf_degustation_etiquettes_tables_echantillons_par_unique_id_pdf" href="<?php echo url_for('degustation_etiquettes_tables_echantillons_par_unique_id_pdf', $degustation) ?>">triées par Numéro de dossier</a>
-                      )
-                  </li>
-                </ul>
-                <br/>
-            </div>
-          <?php else: ?>
-            <div class="col-xs-12">
-              La dégustation n'est <strong>pas encore anonymisée</strong> actuellement.<br/>
-            </div>
-          <?php endif; ?>
-          </div>
-          <div class="row">
-            <div class="col-xs-12">
-              <br/>
-              <strong>Pdf des présences et notations de la commissions</strong>
-              <br/>
-            </div>
-          </div>
-          <div class="row">
-          <div class="col-xs-12">
-            <br/>
-            <ul class="list-group">
-              <li class="list-group-item"><a id="btn_pdf_presence_degustateurs" href="<?php echo url_for('degustation_fiche_presence_degustateurs_pdf', $degustation) ?>"><span class="glyphicon glyphicon-file"></span>&nbsp;Feuille de présence des dégustateurs</a></li>
-              <li class="list-group-item"><a id="btn_pdf_fiche_individuelle_degustateurs" href="<?php echo url_for('degustation_fiche_individuelle_pdf', $degustation) ?>"><span class="glyphicon glyphicon-file"></span>&nbsp;Fiche individuelle des dégustateurs (<?php echo $infosDegustation["nbDegustateursConfirmes"] ?>)</a></li>
-              <li class="list-group-item"><a id="btn_pdf_fiche_resultats_table" href="<?php echo url_for('degustation_fiche_recap_tables_pdf', $degustation) ?>"><span class="glyphicon glyphicon-file"></span>&nbsp;Fiche résultats par table (<?php echo $infosDegustation["nbTables"] ?>)</a></li>
-            </ul>
-            <br/>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-12">
-            <br/>
-            <strong>Documents tiers</strong>
-            <br/>
-          </div>
-        </div>
-        <div class="row">
-        <div class="col-xs-12">
-          <br/>
-          <ul class="list-group">
-            <li class="list-group-item">
-                <a id="btn_csv_etiquette" href="<?php echo url_for('degustation_etiquette_csv', $degustation) ?>"><span class="glyphicon glyphicon-list"></span>&nbsp;Tableur des lots pour labo</a>
-            </li>
-          </ul>
-          <br/>
-        </div>
-      </div>
-      </div>
+<div class="panel panel-default" style="min-height: 160px">
+    <div class="panel-heading">
+        <h2 class="panel-title">Documents nécessaires à l'organisation d'une commission</h2>
     </div>
-  </div>
+    <div class="panel-body">
+        <h4>Organisation des tables</h4>
+        <ul class="list-group">
+            <li class="list-group-item">
+                <span class="glyphicon glyphicon-file"></span>&nbsp;Fiche des lots ventilés (
+                <a id="btn_pdf_degustation_fiche_tables_echantillons_par_anonymat_pdf" href="<?php echo url_for('degustation_fiche_tables_echantillons_par_anonymat_pdf', $degustation) ?>">triées par Numéro d'anonymat </a>
+                -
+                <a id="btn_degustation_fiche_tables_echantillons_par_dossier_pdf" href="<?php echo url_for('degustation_fiche_tables_echantillons_par_dossier_pdf', $degustation) ?>">triées par Numéro de dossier</a>
+                )
+            </li>
+            <li class="list-group-item">
+                <span class="glyphicon glyphicon-th"></span>&nbsp;Étiquettes pour tables (
+                <a id="btn_pdf_degustation_etiquettes_tables_echantillons_par_anonymat_pdf" href="<?php echo url_for('degustation_etiquettes_tables_echantillons_par_anonymat_pdf', $degustation) ?>">triées par Numéro d'anonymat </a>
+                -
+                <a id="btn_pdf_degustation_etiquettes_tables_echantillons_par_unique_id_pdf" href="<?php echo url_for('degustation_etiquettes_tables_echantillons_par_unique_id_pdf', $degustation) ?>">triées par Numéro de dossier</a>
+                )
+            </li>
+        </ul>
+
+        <h4>Présences et notations de la commissions</h4>
+        <ul class="list-group">
+            <li class="list-group-item"><a id="btn_pdf_presence_degustateurs" href="<?php echo url_for('degustation_fiche_presence_degustateurs_pdf', $degustation) ?>"><span class="glyphicon glyphicon-file"></span>&nbsp;Feuille de présence des dégustateurs</a></li>
+            <li class="list-group-item"><a id="btn_pdf_fiche_individuelle_degustateurs" href="<?php echo url_for('degustation_fiche_individuelle_pdf', $degustation) ?>"><span class="glyphicon glyphicon-file"></span>&nbsp;Fiche individuelle des dégustateurs</a></li>
+            <li class="list-group-item"><a id="btn_pdf_fiche_resultats_table" href="<?php echo url_for('degustation_fiche_recap_tables_pdf', $degustation) ?>"><span class="glyphicon glyphicon-file"></span>&nbsp;Fiche des résultats par table</a></li>
+        </ul>
+
+        <h4>Tiers</h4>
+        <ul class="list-group">
+            <li class="list-group-item">
+                <a id="btn_csv_etiquette" href="<?php echo url_for('degustation_etiquette_csv', $degustation) ?>"><span class="glyphicon glyphicon-list"></span>&nbsp;Tableur des lots pour les laboratoires</a>
+            </li>
+        </ul>
+    </div>
 </div>
 
-<div class="row">
-  <div class="col-xs-12">
-        <?php include_partial('degustation/convocationDegustateurs', array('degustation' => $degustation, 'infosDegustation' => $infosDegustation)) ?>
-  </div>
-</div>
+<?php include_partial('degustation/convocationDegustateurs', array('degustation' => $degustation, 'infosDegustation' => $infosDegustation)) ?>
 
 <div class="row row-button">
-  <div class="col-xs-4"><a href="<?php echo url_for('degustation_tables_etape', $degustation) ?>" class="btn btn-default btn-upper"><span class="glyphicon glyphicon-chevron-left"></span> Retour</a></div>
-  <div class="col-xs-4 text-center">
-  </div>
-  <div class="col-xs-4 text-right"><a id="btn_suivant" class="btn btn-primary btn-upper" href="<?php echo url_for('degustation_resultats_etape', $degustation) ?>" >Valider&nbsp;<span class="glyphicon glyphicon-chevron-right"></span></a></div>
+    <div class="col-xs-4"><a href="<?php echo url_for('degustation_tables_etape', $degustation) ?>" class="btn btn-default btn-upper"><span class="glyphicon glyphicon-chevron-left"></span> Retour</a></div>
+    <div class="col-xs-4 text-center">
+    </div>
+    <div class="col-xs-4 text-right"><a id="btn_suivant" class="btn btn-primary btn-upper" href="<?php echo url_for('degustation_resultats_etape', $degustation) ?>" >Valider&nbsp;<span class="glyphicon glyphicon-chevron-right"></span></a></div>
 </div>
