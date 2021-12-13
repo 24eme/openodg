b1156b341755b5bbe1ad5b40ca50667fa4453470
Tangui Morlier <tangui@tangui.eu.org>
2021-12-13 17:20:33 +0100

    On génère le production.csv et ses déscendant qu'une seule fois par global

--- project/bin/export_globalisefichiers.sh
+++ project/bin/export_globalisefichiers.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Mode multi app
-if ! test -f $(echo $0 | sed 's/[^\/]*$//')config.inc && ! test $1 ; then
-    ls . $(echo $0 | sed 's/[^\/]*$//') | grep "config_" | grep ".inc$" | sed 's/config_//' | sed 's/\.inc//' | while read app; do
-        bash $(echo $0 | sed 's/[^\/]*$//')export_globalisefichiers.sh $app;
-    done
-    exit 0;
-fi
-
 if ! test $1 ; then
     . $(echo $0 | sed 's/[^\/]*$//')config.inc
 else
@@ -17,6 +9,25 @@ fi
 EXPORTGLOBALDIR=web/exports_igp
 mkdir $EXPORTGLOBALDIR 2> /dev/null
 
+# Mode multi app
+if ! test -f $(echo $0 | sed 's/[^\/]*$//')config.inc && ! test $1 ; then
+    ls . $(echo $0 | sed 's/[^\/]*$//') | grep "config_" | grep ".inc$" | sed 's/config_//' | sed 's/\.inc//' | while read app; do
+        bash $(echo $0 | sed 's/[^\/]*$//')export_globalisefichiers.sh $app;
+    done
+
+    head -n 1 $EXPORTGLOBALDIR/production.csv | iconv -f ISO88591 -t UTF8 > $EXPORTGLOBALDIR/production.csv.part
+    tail -n +2 $EXPORTGLOBALDIR/production.csv | iconv -f ISO88591 -t UTF8 | awk -F ';' '{uniq = $1"-"$2"-"$4"-"$17"-"$18"-"$19"-"$20"-"$29 ; if ( ! unicite[uniq] ) { print $0  ; unicite[uniq] = 1 } }'  >> $EXPORTGLOBALDIR/production.csv.part
+    cat $EXPORTGLOBALDIR/production.csv.part | iconv -f UTF8 -t ISO88591 > $EXPORTGLOBALDIR/production.csv
+
+    for type in dr sv11 sv12 ; do
+        head -n 1 $EXPORTGLOBALDIR/production.csv.part  > $EXPORTGLOBALDIR/$type".csv.part"
+        cat $EXPORTGLOBALDIR/production.csv.part | grep -i "^"$type";" >> $EXPORTGLOBALDIR/$type".csv.part"
+        cat $EXPORTGLOBALDIR/$type".csv.part" | iconv -f UTF8 -t ISO88591 > $EXPORTGLOBALDIR/$type".csv"
+    done
+
+    exit 0;
+fi
+
 for file in $(find "$EXPORTDIR" -maxdepth 1 -type f -name "*.csv")
 do
 	FILENAME=$(basename $file)
@@ -25,14 +36,4 @@ do
 	else
 		cat $file > $EXPORTGLOBALDIR/$FILENAME
 	fi
-done
-
-head -n 1 $EXPORTGLOBALDIR/production.csv | iconv -f ISO88591 -t UTF8 > $EXPORTGLOBALDIR/production.csv.part
-tail -n +2 $EXPORTGLOBALDIR/production.csv | iconv -f ISO88591 -t UTF8 | awk -F ';' '{uniq = $1"-"$2"-"$4"-"$17"-"$18"-"$19"-"$20"-"$29 ; if ( ! unicite[uniq] ) { print uniq";"$0  ; unicite[uniq] = 1 } }'  >> $EXPORTGLOBALDIR/production.csv.part
-cat $EXPORTGLOBALDIR/production.csv.part | iconv -f UTF8 -t ISO88591 > $EXPORTGLOBALDIR/production.csv
-
-for type in dr sv11 sv12 ; do
-    head -n 1 $EXPORTGLOBALDIR/production.csv.part  > $EXPORTGLOBALDIR/$type".csv.part"
-    cat $EXPORTGLOBALDIR/production.csv.part | grep -i "^"$type";" >> $EXPORTGLOBALDIR/$type".csv.part"
-    cat $EXPORTGLOBALDIR/$type".csv.part" | iconv -f UTF8 -t ISO88591 > $EXPORTGLOBALDIR/$type".csv"
 done
