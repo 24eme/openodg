d7e4ad3df0d89ba5ae6a406663dffcd5323866e1
Tangui Morlier <tangui@tangui.eu.org>
2021-12-14 11:19:35 +0100

    Tentative de désambuguisation du répertoire IGPGlobal et du sous répertoire GOBAL de l'instance

--- project/bin/export_distribueparproduits.sh
+++ project/bin/export_distribueparproduits.sh
@@ -14,45 +14,45 @@ else
     . $(echo $0 | sed 's/[^\/]*$//')config_"$1".inc
 fi
 
-EXPORTGLOBALDIR=web/exports_igp
+GLOBALDIR=web/exports_igp
 EXPORTAPPDIR=web/exports_"$1"
-EXPORTDIRFORGLOBAL=$EXPORTAPPDIR/GLOBAL
+EXPORTFORAPPGLOBALSUBDIR=$EXPORTAPPDIR/GLOBAL
 
-if [ -d "$EXPORTDIRFORGLOBAL" ]; then
-  rm -rf $EXPORTDIRFORGLOBAL
+if [ -d "$EXPORTFORAPPGLOBALSUBDIR" ]; then
+  rm -rf $EXPORTFORAPPGLOBALSUBDIR
 fi
 
-mkdir $EXPORTDIRFORGLOBAL 2> /dev/null
+mkdir $EXPORTFORAPPGLOBALSUBDIR 2> /dev/null
 
-for file in $(find "$EXPORTGLOBALDIR" -maxdepth 1 -type f -name "*.csv")
+for file in $(find "$GLOBALDIR" -maxdepth 1 -type f -name "*.csv")
 do
   FILENAME="$(basename $file)"
-  if [ ! -f "$EXPORTDIRFORGLOBAL/$FILENAME" ]; then
-    head -n 1 $file > $EXPORTDIRFORGLOBAL/$FILENAME
+  if [ ! -f "$EXPORTFORAPPGLOBALSUBDIR/$FILENAME" ]; then
+    head -n 1 $file > $EXPORTFORAPPGLOBALSUBDIR/$FILENAME
   fi
-  cat $file | grep -E $HASHPRODUIT --binary-files=text >> $EXPORTDIRFORGLOBAL/$FILENAME
+  cat $file | grep -E $HASHPRODUIT --binary-files=text >> $EXPORTFORAPPGLOBALSUBDIR/$FILENAME
 done
 
-cut -d ";" -f 2 $EXPORTDIRFORGLOBAL/lots.csv $EXPORTDIRFORGLOBAL/habilitation.csv | sed 's/"//g' | sort -u | sed -r 's/[0-9]{2}$//' > $EXPORTDIRFORGLOBAL/etablissements_ids.tmp
+cut -d ";" -f 2 $EXPORTFORAPPGLOBALSUBDIR/lots.csv $EXPORTFORAPPGLOBALSUBDIR/habilitation.csv | sed 's/"//g' | sort -u | sed -r 's/[0-9]{2}$//' > $EXPORTFORAPPGLOBALSUBDIR/etablissements_ids.tmp
 
-head -n 1 $EXPORTGLOBALDIR"/etablissements.csv" > $EXPORTDIRFORGLOBAL/etablissements.csv
-cat $EXPORTGLOBALDIR"/etablissements.csv" | sort -t ";" -k 1,1 | join -t ";" -1 1 -2 1 - $EXPORTDIRFORGLOBAL/etablissements_ids.tmp >> $EXPORTDIRFORGLOBAL/etablissements.csv
+head -n 1 $GLOBALDIR"/etablissements.csv" > $EXPORTFORAPPGLOBALSUBDIR/etablissements.csv
+cat $GLOBALDIR"/etablissements.csv" | sort -t ";" -k 1,1 | join -t ";" -1 1 -2 1 - $EXPORTFORAPPGLOBALSUBDIR/etablissements_ids.tmp >> $EXPORTFORAPPGLOBALSUBDIR/etablissements.csv
 
-rm $EXPORTDIRFORGLOBAL/etablissements_ids.tmp
+rm $EXPORTFORAPPGLOBALSUBDIR/etablissements_ids.tmp
 
-head -n 1 $EXPORTGLOBALDIR/production.csv | iconv -f ISO88591 -t UTF8 > $EXPORTDIRFORGLOBAL/production.csv.part
-awk -F ';' '{print $9}' $EXPORTDIRFORGLOBAL/etablissements.csv | grep '[0-9]' | sort -u | while read cvi ; do
-    cat $EXPORTGLOBALDIR/production.csv | grep -a ";"$cvi";" >> $EXPORTDIRFORGLOBAL/production.csv.part
+head -n 1 $GLOBALDIR/production.csv | iconv -f ISO88591 -t UTF8 > $EXPORTFORAPPGLOBALSUBDIR/production.csv.part
+awk -F ';' '{print $9}' $EXPORTFORAPPGLOBALSUBDIR/etablissements.csv | grep '[0-9]' | sort -u | while read cvi ; do
+    cat $GLOBALDIR/production.csv | grep -a ";"$cvi";" >> $EXPORTFORAPPGLOBALSUBDIR/production.csv.part
 done
-cat $EXPORTDIRFORGLOBAL/production.csv.part | iconv -f UTF8 -t ISO88591 > $EXPORTDIRFORGLOBAL/production.csv
+cat $EXPORTFORAPPGLOBALSUBDIR/production.csv.part | iconv -f UTF8 -t ISO88591 > $EXPORTFORAPPGLOBALSUBDIR/production.csv
 
 for type in dr sv11 sv12 ; do
-    head -n 1 $EXPORTDIRFORGLOBAL/production.csv.part  > $EXPORTDIRFORGLOBAL/$type".csv.part"
-    cat $EXPORTDIRFORGLOBAL/production.csv.part | grep -i "^"$type";" >> $EXPORTDIRFORGLOBAL/$type".csv.part"
-    cat $EXPORTDIRFORGLOBAL/$type".csv.part" | iconv -f UTF8 -t ISO88591 > $EXPORTDIRFORGLOBAL/$type".csv"
+    head -n 1 $EXPORTFORAPPGLOBALSUBDIR/production.csv.part  > $EXPORTFORAPPGLOBALSUBDIR/$type".csv.part"
+    cat $EXPORTFORAPPGLOBALSUBDIR/production.csv.part | grep -i "^"$type";" >> $EXPORTFORAPPGLOBALSUBDIR/$type".csv.part"
+    cat $EXPORTFORAPPGLOBALSUBDIR/$type".csv.part" | iconv -f UTF8 -t ISO88591 > $EXPORTFORAPPGLOBALSUBDIR/$type".csv"
 done
 
-rm $EXPORTDIRFORGLOBAL/dr.csv.part
-rm $EXPORTDIRFORGLOBAL/sv11.csv.part
-rm $EXPORTDIRFORGLOBAL/sv12.csv.part
-rm $EXPORTDIRFORGLOBAL/production.csv.part
+rm $EXPORTFORAPPGLOBALSUBDIR/dr.csv.part
+rm $EXPORTFORAPPGLOBALSUBDIR/sv11.csv.part
+rm $EXPORTFORAPPGLOBALSUBDIR/sv12.csv.part
+rm $EXPORTFORAPPGLOBALSUBDIR/production.csv.part
