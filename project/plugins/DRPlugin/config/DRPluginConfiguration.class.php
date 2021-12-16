33fa4a7bd2aaf94f1d4ae6b07edfe4feb9761a90
Gabriel Poma <gpoma@24eme.fr>
2021-12-16 12:34:07 +0100

    config validation dr

--- project/plugins/DRPlugin/config/DRPluginConfiguration.class.php
+++ project/plugins/DRPlugin/config/DRPluginConfiguration.class.php
@@ -2,8 +2,18 @@
 
 class DRPluginConfiguration extends sfPluginConfiguration
 {
-  public function initialize()
-  {
-      
-  }
+    public function setup() {
+        if ($this->configuration instanceof sfApplicationConfiguration) {
+            $configCache = $this->configuration->getConfigCache();
+            $configCache->registerConfigHandler('config/dr.yml', 'sfDefineEnvironmentConfigHandler', array('prefix' => 'dr_'));
+            $configCache->checkConfig('config/dr.yml');
+        }
+    }
+
+    public function initialize() {
+        if ($this->configuration instanceof sfApplicationConfiguration) {
+            $configCache = $this->configuration->getConfigCache();
+            include($configCache->checkConfig('config/dr.yml'));
+        }
+    }
 }
