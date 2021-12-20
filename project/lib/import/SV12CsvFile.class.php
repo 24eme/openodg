ae8fff3ab6fe21b42430bc714ff1e67195f05e9b
Tangui Morlier <tangui@tangui.eu.org>
2021-12-20 17:58:57 +0100

    Correction tests document de production douaniers

--- project/lib/import/SV12CsvFile.class.php
+++ project/lib/import/SV12CsvFile.class.php
@@ -4,6 +4,9 @@ class SV12CsvFile extends DouaneCsvFile
 {
   const CSV_TYPE_SV12 = 'SV12';
   const CSV_LIGNE_CODE_SUPERFICIE = "4";
+  const CSV_LIGNE_CODE_RECOLTE_RAISINS = "06kg";
+  const CSV_LIGNE_CODE_RECOLTE_MOUTS = "07";
   const CSV_LIGNE_CODE_VOLUME_RAISINS = "15VF";
   const CSV_LIGNE_CODE_VOLUME_MOUTS = "15L";
+  const CSV_LIGNE_CODE_VOLUME_TOTAL = "15";
 }
