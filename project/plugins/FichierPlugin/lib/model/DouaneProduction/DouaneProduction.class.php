b224507fbeac68c2e0481f7efb8b7c644dd1ef3d
Jb Lm <lemetayer.jb@gmail.com>
2021-12-21 13:08:51 +0100

    generer erreur pour certaines familles notamment pour le cas des apporteurs lors de la facturation

--- project/plugins/FichierPlugin/lib/model/DouaneProduction/DouaneProduction.class.php
+++ project/plugins/FichierPlugin/lib/model/DouaneProduction/DouaneProduction.class.php
@@ -370,7 +370,7 @@ class DouaneProduction extends Fichier implements InterfaceMouvementFacturesDocu
         return eval("return $calcul;");
     }
 
-    public function getTotalValeur($numLigne, $famille = null, $produitFilter = null, $famille_exclue = null) {
+    public function getTotalValeur($numLigne, $famille = null, $produitFilter = null, $famille_exclue = null, $throw_familles = array()) {
         $value = 0;
 
         $produitFilter = preg_replace("/^NOT /", "", $produitFilter, -1, $produitExclude);
@@ -383,6 +383,9 @@ class DouaneProduction extends Fichier implements InterfaceMouvementFacturesDocu
             }
             if ($famille_exclue && $donnee->colonne_famille == $famille_exclue) {
                 continue;
+            }
+            if (in_array($donnee->colonne_famille, $throw_familles)) {
+                throw new sfException("Famille ".donnee->colonne_famille." non permise");
             }
             if($produitFilter && !$produitExclude && !preg_match($regexpFilter, $donnee->produit)) {
                 continue;
