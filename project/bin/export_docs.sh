b536ecb3183f3d03f330f5af523fbbe5a67e2dfe
Tangui Morlier <tangui@tangui.eu.org>
2021-12-31 14:57:53 +0100

    Export doc config

--- project/bin/export_docs.sh
+++ project/bin/export_docs.sh
@@ -26,7 +26,7 @@ fi
 header=1
 curl -s http://$COUCHHOST":"$COUCHDBPORT"/"$COUCHBASE"/_design/declaration/_view/export?reduce=true&group_level=2" | awk -F '"' '{print $4 " " $6}' | grep "^$DOC_TYPE " > /tmp/$$.docs
 cat /tmp/$$.docs | while read doctype ; do
-	php symfony declarations:export-csv $SYMFONYTASKOPTIONS --header=$header $OPTIONS $doctype
+	php symfony declarations:export-csv $SYMFONYTASKOPTIONS --header=$header $OPTIONS $EXPORTDOC_FILTER $doctype
 	header=0
 	if test "$WAITSLEEP" ; then
 		sleep $WAITSLEEP
