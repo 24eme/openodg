a79807dedcea3913d1c5a685a7960e78ca6c9a5c
Gabriel Poma <gpoma@24eme.fr>
2022-01-14 14:35:24 +0100

    on ne cherche pas les etab suspendu import

--- project/plugins/acVinDRevPlugin/lib/task/DrevImportTask.class.php
+++ project/plugins/acVinDRevPlugin/lib/task/DrevImportTask.class.php
@@ -51,7 +51,7 @@ EOF;
                 $cvi = $cviParts[0];
                 $campagne = $cviParts[1];
 
-                $etablissement = EtablissementClient::getInstance()->findByCvi($cvi,true);
+                $etablissement = EtablissementClient::getInstance()->findByCvi($cvi);
 
                 if(!$etablissement) {
                     echo "DREV;ERREUR;$cvi;cvi non trouvé\n";
