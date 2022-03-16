01bbfb18a226970f5bdadd5957384e59f2cea809
Vincent LAURENT <vince.laurent@gmail.com>
2022-03-16 10:44:01 +0100

    Calcul thoérique de la date de commission

--- project/plugins/acVinDRevPlugin/lib/model/DRev/DRev.class.php
+++ project/plugins/acVinDRevPlugin/lib/model/DRev/DRev.class.php
@@ -1521,6 +1521,23 @@ class DRev extends BaseDRev implements InterfaceProduitsDocument, InterfaceVersi
         return $this->_get('date_commission');
     }
 
+    public function getTheoriticalDateCommission() {
+        $date_commission = null;
+        foreach($this->getCurrentLots() as $lot) {
+            if(!$lot->date_commission) {
+                continue;
+            }
+
+            if($date_commission && $lot->date_commission != $date_commission) {
+
+                return null;
+            }
+            $date_commission = $lot->date_commission;
+        }
+
+        return $date_commission;
+    }
+
 	public function getDateValidation($format = 'Y-m-d')
 	{
 		if ($this->validation) {
