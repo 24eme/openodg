bc6297cb9a0b85aee3306f257e3d7fbfe66c7389
Tangui Morlier <tangui@tangui.eu.org>
2022-03-17 12:02:55 +0100

    Nouveau format pour viticonnect

--- project/plugins/acVinComptePlugin/modules/compte_teledeclarant/templates/viticonnectSuccess.php
+++ project/plugins/acVinComptePlugin/modules/compte_teledeclarant/templates/viticonnectSuccess.php
@@ -1,22 +1,9 @@
-<cas:entities>
-<?php foreach($compte->getSociete()->getEtablissementsObj() as $e): ?>
-    <cas:entity>
-        <cas:raison_sociale><?php echo htmlspecialchars($e->etablissement->raison_sociale, ENT_XML1, 'UTF-8'); ?></cas:raison_sociale>
-<?php if ($e->etablissement->cvi): ?>
-        <cas:cvi><?php echo $e->etablissement->cvi; ?></cas:cvi>
-<?php endif; ?>
-<?php if ($compte->societe_informations->siret): ?>
-        <cas:siret><?php echo $compte->societe_informations->siret; ?></cas:siret>
-<?php endif; ?>
-<?php if ($e->etablissement->ppm): ?>
-        <cas:ppm><?php echo $e->etablissement->ppm; ?></cas:ppm>
-<?php endif; ?>
-<?php if ($e->etablissement->no_accises): ?>
-        <cas:accise><?php echo $e->etablissement->no_accises; ?></cas:accise>
-<?php endif; ?>
-<?php if ($compte->getSociete()->no_tva_intracommunautaire): ?>
-        <cas:tva><?php echo $compte->getSociete()->no_tva_intracommunautaire; ?></cas:tva>
-<?php endif; ?>
-    </cas:entity>
+<cas:viticonnect_entities_number><?php echo $entities_number; ?></cas:viticonnect_entities_number>
+<?php foreach($entities as $k => $a): $i = 0; ?>
+<?php foreach($a as $v) : $i++ ; if ($v): ?>
+<cas:viticonnect_entity_<?php echo $i; ?>_<?php echo $k; ?>><?php echo $v ?></cas:viticonnect_<?php echo $i; ?>_<?php echo $k; ?>>
+<?php endif; endforeach; ?>
 <?php endforeach; ?>
-</cas:entities>
+<?php foreach($entities as $k => $a): $a = array_filter($a->getRawValue()); if (count($a)):  ?>
+<cas:viticonnect_entities_all_<?php echo $k; ?>><?php echo implode('|', $a); ?></cas:viticonnect_all_<?php echo $k; ?>>
+<?php endif; endforeach; ?>
