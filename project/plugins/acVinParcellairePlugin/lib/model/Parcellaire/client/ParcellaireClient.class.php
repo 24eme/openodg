53a0f9832336dcd16457e46a03421d838360cba0
Tangui Morlier <tangui@tangui.eu.org>
2022-03-22 17:51:06 +0100

    On ne dépend plus d'une variable pas défaut qui est potentiellement pas en configuration

--- project/plugins/acVinParcellairePlugin/lib/model/Parcellaire/client/ParcellaireClient.class.php
+++ project/plugins/acVinParcellairePlugin/lib/model/Parcellaire/client/ParcellaireClient.class.php
@@ -68,10 +68,18 @@ class ParcellaireClient extends acCouchdbClient {
         return $aires;
     }
 
-    public function getAire($communes, $jsonFolder = "communes") {
+    public function getDefaultCommune() {
+        return array_keys(ParcellaireConfiguration::getInstance()->getAiresName())[0];
+    }
+
+    public function getAire($communes, $jsonFolder = null) {
+        if (!$jsonFolder) {
+            $jsonFolder = $this->getDefaultCommune();
+        }
         $scrapydocs = ProdouaneScrappyClient::getDocumentPath();
         $geojson = [];
         $files = '';
+
         foreach ($communes as $id => $commune) {
             $file_name = $scrapydocs.'/../'.$jsonFolder.'/delimitation-'.$commune.'.json';
             $files = glob($file_name);
