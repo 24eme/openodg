befabdf05c1e489dfb0265914537cbd43e375bb5
Vincent LAURENT <vince.laurent@gmail.com>
2022-03-23 01:04:26 +0100

    Hamzastyle dans le parcellaire multiple

--- project/plugins/acVinParcellairePlugin/modules/parcellaire/templates/parcellaireSuccess.php
+++ project/plugins/acVinParcellairePlugin/modules/parcellaire/templates/parcellaireSuccess.php
@@ -67,19 +67,19 @@ $list_idu = [];
     $import = $parcellaire->getGeoJson(); ?>
 
     <?php if($parcellaire && $parcellaire->getGeoJson() != false): ?>
-        <a name="carte"></a>
-        <div>
+        <div id="jump">
+            <a name="carte"></a>
             <?php include_partial('parcellaire/parcellaireMap', array('parcellaire' => $parcellaire)); ?>
         </div>
     <?php endif; ?>
 
 
     <?php if(!empty($import)): ?>
-    <div class="row" id="jump">
+    <div class="row">
         <div class="col-xs-12">
             <h3>Filtrer</h3>
             <div class="form-group">
-                <input id="hamzastyle" onchange="filterMapOn(this);" type="hidden" data-placeholder="Saisissez un Cépage, un numéro parcelle ou une compagne :" data-hamzastyle-container=".tableParcellaire" data-mode="OR" class="hamzastyle form-control" />
+                <input id="hamzastyle" onchange="filterMap()" type="hidden" data-placeholder="Saisissez un Cépage, un numéro parcelle ou une compagne :" data-hamzastyle-container=".tableParcellaire" data-mode="OR" class="hamzastyle form-control" />
             </div>
         </div>
     </div>
@@ -192,10 +192,8 @@ $list_idu = [];
                                 <td class="<?php echo $classecart; ?>" style="text-align: center;" ><?php echo $ecart_pieds; ?> / <?php echo $ecart_rang; ?></td>
                                 <?php if(!empty($import)): ?>
                                 <td style="text-align: center;">
-                                    <div id="<?php echo $detail->idu; ?>" class="liencarto">
-                                        <a onclick="showParcelle('<?php echo $detail->idu; ?>')"> &nbsp;
-                                            <i class="glyphicon glyphicon-map-marker"></i> &nbsp;
-                                        </a>
+                                    <div id="<?php echo $detail->idu; ?>">
+                                        <button class="btn btn-link" onclick="showParcelle('<?php echo $detail->idu; ?>')"><i class="glyphicon glyphicon-map-marker"></i></button>
                                     </div>
                                 </td>
                                 <?php endif; ?>
