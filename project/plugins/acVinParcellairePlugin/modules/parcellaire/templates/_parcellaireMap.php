4a443354ddb84de8ffc5749dec0107d529d96674
Vincent LAURENT <vince.laurent@gmail.com>
2022-03-23 16:21:57 +0100

    Changement de la transparence au rollover

--- project/plugins/acVinParcellairePlugin/modules/parcellaire/templates/_parcellaireMap.php
+++ project/plugins/acVinParcellairePlugin/modules/parcellaire/templates/_parcellaireMap.php
@@ -11,7 +11,7 @@
 	var parcelles = '<?php echo addslashes($parcellaire->getRawValue()->getGeoJson()) ?>';
     var aires = [];
     <?php foreach(ParcellaireConfiguration::getInstance()->getAiresInfos() as $jsonFolder => $infos): $json = addslashes(implode("|", $parcellaire->getAire($jsonFolder)->getRawValue())); if ($json): ?>
-        aires.push({'geojson': '<?php echo $json ?>', 'color': '<?php $infos['color'] ?>', 'name': '<?php echo addslashes($infos['name']) ?>'});
+        aires.push({'geojson': '<?php echo $json ?>', 'color': '<?php echo $infos['color'] ?>', 'name': '<?php echo addslashes($infos['name']) ?>'});
     <?php endif; endforeach; ?>
     console.log(aires);
 </script>
