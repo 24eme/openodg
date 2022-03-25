168e6a69126469ce5f63b4188fe4c6c19fa64f03
Vincent LAURENT <vince.laurent@gmail.com>
2022-03-25 15:16:39 +0100

    Pause de 2 seconde entre chaque envoi de mails

--- project/bin/sendmail_facture_withgeneration.sh
+++ project/bin/sendmail_facture_withgeneration.sh
@@ -11,4 +11,5 @@ fi
 
 cat | while read generation_id; do curl -s "http://$COUCHDBDOMAIN:$COUCHDBPORT/$COUCHDBBASE/$generation_id" | jq '.documents' | grep "FACTURE" ; done | sed 's/"//g' | cut -d "-" -f 2 | sed 's/^/COMPTE-/' | sort | uniq | while read compte_id; do
     echo php symfony facture:send-mail "$compte_id" "$CAMPAGNE" "$SYMFONYTASKOPTIONS";
+    sleep 2
 done;
