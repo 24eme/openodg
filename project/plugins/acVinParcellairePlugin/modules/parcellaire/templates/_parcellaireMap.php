815d79aa4ef7a27e362b58af4755b3edc3b3ea1c
Vincent LAURENT <vince.laurent@gmail.com>
2022-03-29 17:22:54 +0200

    Modif pas intégré du merge 822957951c1a65ced7bcadf179c2aca58e5e287f

--- project/plugins/acVinParcellairePlugin/modules/parcellaire/templates/_parcellaireMap.php
+++ project/plugins/acVinParcellairePlugin/modules/parcellaire/templates/_parcellaireMap.php
@@ -8,7 +8,7 @@
 </div>
 
 <script type="text/javascript">
-	var parcelles = '<?php echo addslashes($parcellaire->getRawValue()->getGeoJson()) ?>';
+	var parcelles = '<?php echo addslashes(json_encode($parcellaire->getRawValue()->getGeoJson())) ?>';
     var aires = [];
     <?php foreach(ParcellaireConfiguration::getInstance()->getAiresInfos() as $jsonFolder => $infos): $json = addslashes(implode("|", $parcellaire->getAire($jsonFolder)->getRawValue())); if ($json): ?>
         aires.push({'geojson': '<?php echo $json ?>', 'color': '<?php echo $infos['color'] ?>', 'name': '<?php echo addslashes($infos['name']) ?>'});
