33b16e0efc848233e3cfc708862217ea0b711967
Vincent LAURENT <vince.laurent@gmail.com>
2022-04-12 16:25:39 +0200

    Parcellaire accessible pour les télédéclarants

--- project/plugins/acVinParcellairePlugin/modules/parcellaire/actions/actions.class.php
+++ project/plugins/acVinParcellairePlugin/modules/parcellaire/actions/actions.class.php
@@ -33,29 +33,29 @@ class parcellaireActions extends sfActions {
     }
 
     public function executeDeclarant(sfWebRequest $request) {
-          $this->etablissement = $this->getRoute()->getEtablissement();
-          $this->parcellaire = ParcellaireClient::getInstance()->getLast($this->etablissement->identifiant);
+        $this->secureTeledeclarant();
+        
+        $this->etablissement = $this->getRoute()->getEtablissement();
+        $this->parcellaire = ParcellaireClient::getInstance()->getLast($this->etablissement->identifiant);
 
-          $this->form = new EtablissementChoiceForm('INTERPRO-declaration', array(), true);
+        $this->form = new EtablissementChoiceForm('INTERPRO-declaration', array(), true);
 
-          $this->setTemplate('parcellaire');
+        $this->setTemplate('parcellaire');
     }
 
     public function executeVisualisation(sfWebRequest $request) {
+        $this->secureTeledeclarant();
+        
         $this->parcellaire = $this->getRoute()->getParcellaire();
         $this->etablissement = $this->getRoute()->getEtablissement();
-        $this->secureEtablissement($this->etablissement);
         $this->setTemplate('parcellaire');
     }
 
     public function executeScrape(sfWebRequest $request)
     {
+        $this->secureTeledeclarant();
+        
         $this->etablissement = $this->getRoute()->getEtablissement();
-    }
-
-    public function executeMap(sfWebRequest $request){
-        $parcellaire_client = ParcellaireClient::getInstance();
-        //TODO if we popup the map
     }
 
     public function executeImport(sfWebRequest $request)
@@ -86,20 +86,9 @@ class parcellaireActions extends sfActions {
         $this->redirect('parcellaire_declarant', $this->etablissement);
     }
 
-    protected function secureEtablissement($etablissement) {
-        if (!EtablissementSecurity::getInstance($this->getUser(), $etablissement)->isAuthorized(array())) {
-
-            return $this->forwardSecure();
-        }
-    }
-
-    protected function forwardSecure() {
-        $this->context->getController()->forward(sfConfig::get('sf_secure_module'), sfConfig::get('sf_secure_action'));
-
-        throw new sfStopException();
-    }
-
     public function executeParcellairePDF(sfWebRequest $request) {
+        $this->secureTeledeclarant();
+        
         $parcellaire = $this->getRoute()->getParcellaire();
         $this->forward404Unless($parcellaire);
 
@@ -112,5 +101,11 @@ class parcellaireActions extends sfActions {
         $this->content = $parcellaire->getParcellairePDF();
         echo $this->content;
         exit;
+    }
+    
+    public function secureTeledeclarant() {
+        if(!$this->getUser()->isAdmin() && (!class_exists("SocieteConfiguration") || !SocieteConfiguration::getInstance()->isVisualisationTeledeclaration())) {
+            throw new sfError403Exception();
+        }
     }
 }
--- project/plugins/acVinParcellairePlugin/modules/parcellaire/templates/parcellaireSuccess.php
+++ project/plugins/acVinParcellairePlugin/modules/parcellaire/templates/parcellaireSuccess.php
@@ -9,11 +9,10 @@ $superficie_multiplicateur = (ParcellaireConfiguration::getInstance()->isAres())
 ?>
 
 <?php if($sf_user->hasTeledeclaration()): ?>
-    <ol class="breadcrumb">
-      <li><a href="<?php echo url_for('accueil'); ?>">Déclarations</a></li>
-      <li><a href="<?php echo url_for('declaration_etablissement', array('identifiant' => $parcellaire->identifiant)); ?>"><?php echo $parcellaire->getEtablissementObject()->getNom() ?> (<?php echo $parcellaire->getEtablissementObject()->identifiant ?>)</a></li>
-      <li class="active"><a href="">Parcellaire au <?php echo $parcellaire->getDateFr(); ?></a></li>
-    </ol>
+<ol class="breadcrumb">
+  <li><a href="<?php echo url_for('parcellaire_declarant', $parcellaire->getEtablissementObject()); ?>">Parcellaire</a></li>
+  <?php if($parcellaire): ?><li><a href="<?php echo url_for('parcellaire_declarant', $parcellaire->getEtablissementObject()); ?>">Parcellaire de <?php echo $parcellaire->getEtablissementObject()->getNom() ?> (<?php echo $parcellaire->getEtablissementObject()->identifiant ?>) </a></li><?php endif;?>
+</ol>
 <?php else: ?>
 <ol class="breadcrumb">
   <li><a href="<?php echo url_for('parcellaire'); ?>">Parcellaire</a></li>
@@ -43,14 +42,6 @@ $superficie_multiplicateur = (ParcellaireConfiguration::getInstance()->isAres())
 <?php if ($sf_user->hasFlash('success_import')): ?>
 <div class="alert alert-success" role="alert">
     <strong>Succès :</strong> <?= $sf_user->getFlash('success_import') ?>
-</div>
-<?php endif; ?>
-
-<?php if(isset($form)): ?>
-<div class="row row-margin">
-    <div class="col-xs-12">
-        <?php include_partial('etablissement/formChoice', array('form' => $form, 'action' => url_for('parcellaire_etablissement_selection'),  'noautofocus' => true)); ?>
-    </div>
 </div>
 <?php endif; ?>
 
