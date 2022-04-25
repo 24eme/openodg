8b2ae49584e26f9eecf33200bc483cb33cf40f8c
Gabriel Poma <gpoma@24eme.fr>
2022-04-25 17:28:59 +0200

    igploire: maj courrier non conformité

--- project/apps/igploire/modules/degustation/templates/_degustationNonConformitePDF_page1.php
+++ project/apps/igploire/modules/degustation/templates/_degustationNonConformitePDF_page1.php
@@ -39,22 +39,22 @@
 <table>
   <tr><td>Madame, Monsieur,</td></tr>
   <br/>
-  <tr><td>Lors de la séance de dégustation du <strong><?php echo format_date($degustation->date, "P", "fr_FR"); ?></strong>, le lot dont les informations figurent dans la fiche de non conformité ci-jointe a été ajourné<?php echo ($lot->isSecondPassage()) ? ' pour la 2ème fois' : '' ?>.<br/></td></tr>
+  <tr><td>Lors de la séance de dégustation du <strong><?php echo format_date($degustation->date, "P", "fr_FR"); ?></strong>, le lot dont les informations figurent dans la fiche de non conformité ci-jointe fait l'objet d'un manquement majeur<?php echo ($lot->isSecondPassage()) ? ' pour la 2ème fois' : '' ?>.<br/></td></tr>
   <tr><td>Compte tenu de ce résultat, vous pouvez décider :</td></tr><br/>
   <tr><td>
     <ul>
-      <li><strong>Soit d’abandonner volontairement la dénomination correspondante</strong> en nous communiquant par mail ou courrier, une déclaration de déclassement au moyen de la fiche jointe.</li><br/>
       <?php if ($lot->isSecondPassage() || $lot->conformite == Lot::CONFORMITE_NONCONFORME_MAJEUR): ?>
-      <li>Soit de vous opposer aux conclusions de ce contrôle. Dans ce cas, ainsi que le prévoit le plan de contrôle de l'IGP, vous êtes dans l'obligation de transmettre le dossier à l'organisme de contrôle/d'inspection qui diligentera un nouveau contrôle entièrement à votre charge. <?php if (Organisme::getInstance(null, 'degustation')->getOi()): ?><strong>(<?php echo Organisme::getInstance(null, 'degustation')->getOi() ?>)</strong><?php endif ?></li>
+        <li><strong>Soit de vous opposer aux conclusions de ce contrôle et de demander un reprélèvement du même lot.</strong> Dans ce cas, veuillez nous retourner la fiche jointe dûment complétée. Le dossier sera transmis à l'ASJVAS (organisme d'inspection) sur notre demande ou sous quinzaine à compter de la date du présent courrier. (Frais à votre charge : 50€ HT)<br/>Attention : Durant cette période, votre lot NE peut PAS être commercialisé.</li>
       <?php else: ?>
-      <li>Soit d’exécuter l’action corrective recommandée dans la fiche de non-conformité ci jointe. Dans cette hypothèse, votre vin pourra être soumis à une deuxième dégustation après que nous ayons procédé à un nouveau prélèvement, et ce dans un délai minimum de 15 jours à compter de la réception de votre demande de nouvelle présentation. Notez qu’après travail le vin concerné par la deuxième présentation peut être relogé&nbsp;:
-        <ul>
-          <li>Dans la même cuve</li>
-          <li>Dans une autre cuve d’un volume inférieur ou égal au volume initial,</li>
-          <li>Dans d’autres cuves d’un volume total inférieur ou égal au volume initial.</li>
-        </ul>
-      </li>
+        <li>Soit d’exécuter l’action corrective recommandée dans la fiche de non-conformité ci jointe. Dans cette hypothèse, votre vin pourra être soumis à une deuxième dégustation après que nous ayons procédé à un nouveau prélèvement, et ce dans un délai minimum de 15 jours à compter de la réception de votre demande de nouvelle présentation. Notez qu’après travail le vin concerné par la deuxième présentation peut être relogé&nbsp;:
+          <ul>
+            <li>Dans la même cuve</li>
+            <li>Dans une autre cuve d’un volume inférieur ou égal au volume initial,</li>
+            <li>Dans d’autres cuves d’un volume total inférieur ou égal au volume initial.</li>
+          </ul>
+        </li>
       <?php endif ?>
+      <li><strong>Soit d’abandonner volontairement la dénomination correspondante</strong> en nous communiquant par mail ou courrier, une déclaration de déclassement au moyen de la fiche jointe (DICD).</li><br/>
     </ul>
   </td></tr>
 </table><br/><br/>
@@ -68,6 +68,6 @@
 <br/><br/>
 <br/><br/>
 <table style="width:1100px;padding-left:400px;" >
-  <tr><td><?php echo Organisme::getInstance(null, 'degustation')->getResponsable() ?></td></tr>
+  <tr><td>Directeur<br/><?php echo Organisme::getInstance(null, 'degustation')->getResponsable() ?></td></tr>
   <tr><td><?php if(file_exists(Organisme::getInstance(null, 'degustation')->getImageSignaturePath())): ?><img src="<?php echo Organisme::getInstance(null, 'degustation')->getImageSignaturePath() ?>"/><?php endif; ?></td></tr>
 </table>
