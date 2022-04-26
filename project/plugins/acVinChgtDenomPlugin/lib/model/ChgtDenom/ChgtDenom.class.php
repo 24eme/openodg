78b9964cedf135950cb9aab5362649ed90b45439
Tangui Morlier <tangui@tangui.eu.org>
2022-04-26 10:57:27 +0200

    Petite opti + refacto

--- project/plugins/acVinChgtDenomPlugin/lib/model/ChgtDenom/ChgtDenom.class.php
+++ project/plugins/acVinChgtDenomPlugin/lib/model/ChgtDenom/ChgtDenom.class.php
@@ -812,9 +812,21 @@ class ChgtDenom extends BaseChgtDenom implements InterfaceDeclarantDocument, Int
 
     public function getFirstChgtDenomFacturable($produitFilter = null)
     {
-      $chgtdenom = $this->getChgtDenomToday($produitFilter);
-      $first = current($chgtdenom);
-      return $first !== false && $first->_id == $this->_id;
+      $chgtdenoms = ChgtDenomClient::getInstance()->getHistoryCampagne(
+          $this->identifiant,
+          substr($this->validation, 0, 7)
+      );
+
+      $today = [];
+      foreach ($chgtdenoms as $chgt) {
+          if ($chgt->validation_odg && substr($chgt->validation, 0, 10) === substr($this->validation, 0, 10) && $this->matchFilter($produitFilter, $chgt)) {
+              $today[$chgt->_id] = $chgt->_id;
+          }
+      }
+      ksort($today);
+
+      $first = current($today);
+      return $first !== false && $first == $this->_id;
     }
 
     public function getSecondChgtDenomFacturable($produitFilter = null)
@@ -902,24 +914,6 @@ class ChgtDenom extends BaseChgtDenom implements InterfaceDeclarantDocument, Int
         }
 
         return false;
-    }
-
-    private function getChgtDenomToday($produitFilter = null)
-    {
-        $chgtdenoms = ChgtDenomClient::getInstance()->getHistoryCampagne(
-            $this->identifiant,
-            substr($this->validation, 0, 4)
-        );
-
-        $today = [];
-        foreach ($chgtdenoms as $chgt) {
-            if ($chgt->validation_odg && substr($chgt->validation, 0, 10) === substr($this->validation, 0, 10) && $this->matchFilter($produitFilter, $chgt)) {
-                $today[$chgt->_id] = $chgt;
-            }
-        }
-        ksort($today);
-
-        return $today;
     }
 
     private function origineFilter($filter)
