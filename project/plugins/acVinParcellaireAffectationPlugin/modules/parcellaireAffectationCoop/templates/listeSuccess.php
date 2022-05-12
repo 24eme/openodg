2c682e69c4c8a4b04c7554ba39ec206ef2706a4d
Tangui Morlier <tangui@tangui.eu.org>
2022-05-12 14:45:01 +0200

    Rappel que seule les affectations validées sont exportables

--- project/plugins/acVinParcellaireAffectationPlugin/modules/parcellaireAffectationCoop/templates/listeSuccess.php
+++ project/plugins/acVinParcellaireAffectationPlugin/modules/parcellaireAffectationCoop/templates/listeSuccess.php
@@ -48,7 +48,7 @@
     <div class="row row-margin row-button">
         <div class="col-xs-4"><a href="<?php echo url_for("parcellaireaffectationcoop_apporteurs", $parcellaireAffectationCoop) ?>" class="btn btn-default btn-upper"><span class="glyphicon glyphicon-chevron-left"></span> Retour</a></div>
         <div class="col-xs-4 text-center">
-            <a href="<?php echo url_for("parcellaireaffectationcoop_exportcsv", $parcellaireAffectationCoop) ?>" class="btn btn-primary">Export CSV</a>
+            <a href="<?php echo url_for("parcellaireaffectationcoop_exportcsv", $parcellaireAffectationCoop) ?>" class="btn btn-primary">Export CSV des affectations validées</a>
         </div>
         <div class="col-xs-4 text-right">
         </div>
