d41a449fe33e82ccd87fdabebcb2dd5dc5ac56c7
Tangui Morlier <tangui@tangui.eu.org>
2022-05-13 16:49:55 +0200

    Export des habilitations pour les crus

--- project/bin/export.sh
+++ project/bin/export.sh
@@ -112,6 +112,14 @@ php bin/export/export_liste_inao.php $EXPORTDIR/habilitation_demandes.csv.part |
 iconv -f UTF8 -t ISO88591//TRANSLIT $EXPORTDIR/habilitation_demandes_inao.csv.part > $EXPORTDIR/habilitation_demandes_inao.csv
 rm $EXPORTDIR/habilitation_demandes.csv.part $EXPORTDIR/habilitation_demandes_inao.csv.part
 
+echo $EXPORT_SUB_HABILITATION | tr '|' '\n' | while read subhab; do
+    eval 'SUBDIR=$EXPORT_SUB_HABILITATION_'$subhab'_DIR'
+    eval 'SUBFILTRE=$EXPORT_SUB_HABILITATION_'$subhab'_FILTRE'
+    mkdir -p $SUBDIR
+    head -n 1 $EXPORTDIR/habilitation.csv > $SUBDIR/habilitation.csv
+    cat $EXPORTDIR/habilitation.csv | grep "$SUBFILTRE" >> $SUBDIR/habilitation.csv
+done
+
 sleep $EXPORTSLEEP
 
 if [ -z $IS_NO_VINIF ]; then
