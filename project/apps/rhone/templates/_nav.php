0c6853c27f7ede497027dd853e7b61f6ed5fed94
Tangui Morlier <tangui@tangui.eu.org>
2022-05-27 12:18:20 +0200

    Ajout des lien de stats export crus

--- project/apps/rhone/templates/_nav.php
+++ project/apps/rhone/templates/_nav.php
@@ -71,11 +71,11 @@
             <?php endif; ?>
             <ul class="nav navbar-nav navbar-right">
                 <?php if($sf_user->hasCredential(myUser::CREDENTIAL_ADMIN)): ?>
-                <?php if(sfConfig::get('app_nav_stats_'.sfConfig::get('sf_app'))): ?>
+                <?php if(NavConfiguration::getInstance()->hasStatLinks()): ?>
                 <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-stats"></span><span class="caret"></span></a>
                   <ul class="dropdown-menu">
-                    <?php foreach(sfConfig::get('app_nav_stats_'.sfConfig::get('sf_app')) as $i => $navItem): ?>
+                    <?php foreach(NavConfiguration::getInstance()->getStatLinks() as $i => $navItem): ?>
                      <?php if($i > 0 && isset($navItem['title'])): ?><li role="separator" class="divider"></li><?php endif; ?>
                      <li>
                          <a href="<?php echo $navItem['url'] ?>">
