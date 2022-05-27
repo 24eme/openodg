bacf3d38dfa5dcaa89d85b7ed6e2ed33aaf2201c
Tangui Morlier <tangui@tangui.eu.org>
2022-05-27 12:18:20 +0200

    Ajout des lien de stats export crus

---
+++ project/lib/NavConfiguration.class.php
@@ -0,0 +1,46 @@
+<?php
+
+class NavConfiguration {
+
+    private static $_instance = null;
+    protected $configuration;
+
+    public static function getInstance() {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new NavConfiguration();
+        }
+        return self::$_instance;
+    }
+
+    public function getAppPath() {
+        return 'app_nav_stats_'.sfConfig::get('sf_app');
+    }
+
+    public function load() {
+        $this->nav_configuration = array();
+        $this->nav_configuration['stats'] = sfConfig::get($this->getAppPath(), array());
+        $regions = array(sfContext::getInstance()->getUser()->getTeledeclarationDrevRegion());
+        if (sfContext::getInstance()->getUser()->hasCredential(myUser::CREDENTIAL_ADMIN)) {
+            $regions = RegionConfiguration::getInstance()->getOdgRegions();
+        }
+        if (count($regions)) {
+            $this->nav_configuration['stats'][] = array('name' => 'Export Crus', 'url' => '#', 'title' => 1);
+            foreach($regions as $r) {
+                $this->nav_configuration['stats'][] = array('name' => 'Tableurs '.$r, 'url' => '/exports_'.$r.'/');
+            }
+        }
+        return ;
+    }
+
+    public function __construct() {
+        $this->load();
+    }
+    
+    public function getStatLinks() {
+        return $this->nav_configuration['stats'];
+    }
+    
+    public function hasStatLinks() {
+        return count($this->nav_configuration['stats']) > 1;
+    }
+}
