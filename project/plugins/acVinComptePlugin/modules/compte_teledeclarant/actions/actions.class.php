88d99505263b8bd926bf8392e8ba121118723f85
Tangui Morlier <tangui@tangui.eu.org>
2022-07-05 19:13:11 +0200

    Vérifie l'existance d'un compte

--- project/plugins/acVinComptePlugin/modules/compte_teledeclarant/actions/actions.class.php
+++ project/plugins/acVinComptePlugin/modules/compte_teledeclarant/actions/actions.class.php
@@ -233,8 +233,7 @@ class compte_teledeclarantActions extends sfActions {
         return $this->renderText(file_get_contents($path));
     }
 
-    public function executeViticonnect(sfWebRequest $request)
-    {
+    private function checkApiAccess(sfWebRequest $request) {
         $secret = sfConfig::get('app_viticonnect_secret');;
 
         $login = $request->getParameter('login');
@@ -251,7 +250,12 @@ class compte_teledeclarantActions extends sfActions {
             http_response_code(401);
             die("Unauthorized");
         }
+    }
 
+    public function executeViticonnectApi(sfWebRequest $request)
+    {
+        $this->checkApiAccess($request);
+        $login = $request->getParameter('login');
         $compte = acCouchdbManager::getClient('Compte')->retrieveByLogin(strtolower($login));
         if (!$compte) {
             http_response_code(401);
@@ -268,10 +272,24 @@ class compte_teledeclarantActions extends sfActions {
             $this->entities['tva'][] = str_replace(' ', '', $compte->getSociete()->no_tva_intracommunautaire);
             $this->entities_number++;
         }
-    
+
         $this->setLayout(false);
         $this->getResponse()->setHttpHeader('Content-Type', 'text/plain');
-        
+
+    }
+
+    public function executeViticonnectCheck(sfWebRequest $request)
+    {
+        $this->checkApiAccess($request);
+        $login = $request->getParameter('login');
+        $comptes = EtablissementAllView::getInstance()->findByInterproAndStatut('INTERPRO-declaration', EtablissementClient::STATUT_ACTIF, $login);
+        if(count($comptes) == 1) {
+            echo "Found";
+            exit;
+        }
+        http_response_code(404);
+        die('Not found');
+
     }
 
 }
