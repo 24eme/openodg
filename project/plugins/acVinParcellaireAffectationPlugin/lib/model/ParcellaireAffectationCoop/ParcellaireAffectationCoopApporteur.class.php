3907002925666fe20574a9993c6eff31b7c5bf75
Tangui Morlier <tangui@tangui.eu.org>
2022-08-30 16:20:20 +0200

    Différentie les apporteurs désactivés de ceux qui n'ont pas de parcelles

--- project/plugins/acVinParcellaireAffectationPlugin/lib/model/ParcellaireAffectationCoop/ParcellaireAffectationCoopApporteur.class.php
+++ project/plugins/acVinParcellaireAffectationPlugin/lib/model/ParcellaireAffectationCoop/ParcellaireAffectationCoopApporteur.class.php
@@ -5,6 +5,7 @@ class ParcellaireAffectationCoopApporteur extends BaseParcellaireAffectationCoop
     protected $affectationParcellaire = null;
 
     const STATUT_NON_IDENTIFIEE = "NON_IDENTIFIEE";
+    const STATUT_DESACTIVE = "DESACTIVE";
     const STATUT_A_SAISIR = "A_SAISIR";
     const STATUT_EN_COURS = "EN_COURS";
     const STATUT_VALIDE = "VALIDE";
@@ -37,17 +38,21 @@ class ParcellaireAffectationCoopApporteur extends BaseParcellaireAffectationCoop
             return self::STATUT_EN_COURS;
         }
 
-        if(!$this->intention) {
+        if(!$this->getNbParcelles()) {
 
             return self::STATUT_NON_IDENTIFIEE;
         }
+        if(!$this->intention) {
 
+            return self::STATUT_DESACTIVE;
+        }
         return self::STATUT_A_SAISIR;
     }
 
     public function getStatutLibelle() {
         $libelles = array(
             self::STATUT_NON_IDENTIFIEE => "Aucune parcelle identifiée",
+            self::STATUT_DESACTIVE => "Coopérateur désactivé",
             self::STATUT_A_SAISIR => "À saisir",
             self::STATUT_EN_COURS => "En cours de saisie",
             self::STATUT_VALIDE => "Validé",
@@ -76,5 +81,20 @@ class ParcellaireAffectationCoopApporteur extends BaseParcellaireAffectationCoop
         $this->affectationParcellaire = ParcellaireAffectationClient::getInstance()->createDoc($this->getEtablissementIdentifiant(), substr($this->getDocument()->campagne, 0, 4));
 
         return $this->affectationParcellaire;
+    }
+
+    public function updateParcelles() {
+        $this->nb_parcelles_identifiees = 0;
+        $intention = ParcellaireIntentionAffectationClient::getInstance()->getLast($this->getEtablissementIdentifiant());
+        if ($intention) {
+            $this->nb_parcelles_identifiees = count($intention->getParcelles());
+        }
+    }
+
+    public function getNbParcelles() {
+        if (!$this->exist('nb_parcelles_identifiees') || is_null($this->_get('nb_parcelles_identifiees'))) {
+            $this->updateParcelles();
+        }
+        return $this->_get('nb_parcelles_identifiees');
     }
 }
