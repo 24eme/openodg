b87f575d1f589295b05e5230983bb488c384526f
Tangui Morlier <tangui@tangui.eu.org>
2022-09-05 12:37:54 +0200

    Indépendant de la version de couchdb

--- project/bin/comptes_update_ldap.sh
+++ project/bin/comptes_update_ldap.sh
@@ -28,7 +28,7 @@ fi
 curl -s "http://$COUCHHOST:$COUCHPORT/$COUCHBASE/_changes?feed=continuous&timeout=590000&since="$(cat $SEQ | sed 's/[^0-9]//g') | grep "COMPTE" | while read ligne
 do
     echo $ligne | awk -F '"' '{print $3}' | sed 's/[^0-9]//g' > $SEQ
-    php symfony compte:ldap-update $SYMFONYTASKOPTIONS $(echo $ligne | awk -F '"' '{print $6}')
+    php symfony compte:ldap-update $SYMFONYTASKOPTIONS $(echo $ligne | sed 's/.*"id":"//' | sed 's/".*//')
 done
 
 rm $LOCK
