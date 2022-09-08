a4752395098cca9eedd79b3578e090e0d47ad986
Tangui Morlier <tangui@tangui.eu.org>
2022-09-08 17:32:07 +0200

    Priorise les brouillons existant sur la période au document du jour

--- project/plugins/acVinTransactionPlugin/modules/transaction/actions/components.class.php
+++ project/plugins/acVinTransactionPlugin/modules/transaction/actions/components.class.php
@@ -3,7 +3,17 @@
 class transactionComponents extends sfComponents {
 
     public function executeMonEspace(sfWebRequest $request) {
-        $this->transaction = TransactionClient::getInstance()->findByIdentifiantAndDate($this->etablissement->identifiant, date('Ymd'));
+        $campagne = $request->getParameter('campagne');
+        $date = date('Y-m-d');
+        if ($campagne != ConfigurationClient::getInstance()->getCampagneVinicole()->getCampagneByDate($date)) {
+            $date = substr($request->getParameter('campagne'), 5, 4).'-07-31';
+        }
+
+        $this->transaction = TransactionClient::getInstance()->findBrouillon($this->etablissement->identifiant, $campagne);
+        if (!$this->transaction) {
+            $this->transaction = TransactionClient::getInstance()->findByIdentifiantAndDate($this->etablissement->identifiant, $date);
+        }
+
     }
 
 }
