44c113aff1cee8da0c4cc8bbf62a4a98f9409cb0
Tangui Morlier <tangui@tangui.eu.org>
2022-09-13 17:40:22 +0200

    Bug :(

--- project/bin/sync_instances.sh
+++ project/bin/sync_instances.sh
@@ -3,7 +3,7 @@
 # Mode multi app
 if ! test -f $(echo $0 | sed 's/[^\/]*$//')config.inc && ! test $1 ; then
     ls . $(echo $0 | sed 's/[^\/]*$//') | grep "config_" | grep ".inc$" | sed 's/config_//' | sed 's/\.inc//' | while read app; do
-        bash $(echo $0 | sed 's/[^\/]*$//')sync_instances $app;
+        bash $(echo $0 | sed 's/[^\/]*$//')sync_instances.sh $app;
     done
     exit 0
 fi
@@ -16,6 +16,6 @@ fi
 
 rsync -aO $WORKINGDIR"/web/generation/" $COUCHDISTANTHOST":"$WORKINGDIR"/web/generation"
 rsync -aO $WORKINGDIR"/"$EXPORTDIR"/" $COUCHDISTANTHOST":"$WORKINGDIR"/"$EXPORTDIR
-if test "$EXTRA_SYNC" then
-    rsync -aO $EXTRA_SYNC"/" $COUCHDISTANTHOST":"$EXTRA_SYNC"
+if test "$EXTRA_SYNC" ; then
+    rsync -aO $EXTRA_SYNC"/" $COUCHDISTANTHOST":"$EXTRA_SYNC
 fi
