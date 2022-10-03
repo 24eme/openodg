2bf49c5a5f49cb6d04f183e7ee53d2f1351cc493
Honorine-MC <honorine.mariechristy@gmail.com>
2022-10-03 17:13:36 +0200

    volume proposé dans le contrat

--- project/plugins/acVinDRevPlugin/lib/controle/DRevValidation.class.php
+++ project/plugins/acVinDRevPlugin/lib/controle/DRevValidation.class.php
@@ -81,7 +81,7 @@ class DRevValidation extends DeclarationLotsValidation
         $contrats = $this->document->getContratsFromAPI();
 
         foreach($contrats as $k=>$v){
-            $this->addControle(self::TYPE_ENGAGEMENT, DRevDocuments::DOC_VIP2C_OUEX_CONTRAT_VENTE_EN_VRAC."_".$k,DRevDocuments::getEngagementLibelle(DRevDocuments::DOC_VIP2C_OUEX_CONTRAT_VENTE_EN_VRAC).'<strong>'.$v['numero']."</strong> avec <strong>".$v['acheteur']."</strong>.");
+            $this->addControle(self::TYPE_ENGAGEMENT, DRevDocuments::DOC_VIP2C_OUEX_CONTRAT_VENTE_EN_VRAC."_".$k,DRevDocuments::getEngagementLibelle(DRevDocuments::DOC_VIP2C_OUEX_CONTRAT_VENTE_EN_VRAC).'<strong>'.$v['numero']."</strong> avec un volume proposé de <strong>".$v['volume']." hl</strong>.");
         }
 
         /* Lots */
