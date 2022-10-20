677cff90913add337e270abc163a5c7ec1ba972b
Tangui Morlier <tangui@tangui.eu.org>
2022-10-20 15:25:47 +0200

    Problème de l'engagement "je n'ai pas l'info" qui n'avait pas été inclu dans le _OUEX_ et donc était obligatoire cc @Honorine-MC

--- project/plugins/acVinDRevPlugin/modules/drev/templates/_engagements.php
+++ project/plugins/acVinDRevPlugin/modules/drev/templates/_engagements.php
@@ -2,28 +2,14 @@
 
 <div class="alert" role="alert" id="engagements">
     <div class="form-group">
-
+<?php print_r($form->getErrors()); ?>
         <div class="alert alert-danger <?php if(!$form->hasErrors()): ?>hidden<?php endif; ?>" role="alert">
     	    <ul class="error_list">
-                <?php
-                    foreach($validation->getEngagements() as $engagement):
-                        if(($engagement->getCode() == "VIP2C_OUEX_CONDITIONNEMENT") or preg_match('/VIP2C_OUEX_CONTRAT_VENTE_EN_VRAC/',$engagement->getCode())):
-                            $VIP2C = true;
-                        else:
-                            $VIP2C = false;
-                            continue;
-                        endif;
-                    endforeach;
-                    if($VIP2C):?> <li class="text-left" style="list-style-type: none;">Merci de ne sélectionner qu' <strong>1</strong> seul engagement qui justifie le dépassement du volume de Méditerrannée rosé</li>
-                    <?php else: ?><li class="text-left" style="list-style-type: none;">Merci de sélectionner vos engagements.</li>
-                    <?php endif; ?>
+                <li class="text-left" style="list-style-type: none;">Merci de sélectionner vos engagements.</li>
     		</ul>
     	</div>
 
         <?php foreach ($validation->getEngagements() as $engagement):
-            if(!$sf_user->isAdmin() && $engagement->getCode()== DRevDocuments::DOC_VIP2C_PAS_INFORMATION){
-                continue;
-            }
             if (isset($form['engagement_' . $engagement->getCode()])): ?>
         <div class="checkbox-container <?php if ($form['engagement_' . $engagement->getCode()]->hasError()): ?>has-error<?php endif; ?>">
             <div class="checkbox">
