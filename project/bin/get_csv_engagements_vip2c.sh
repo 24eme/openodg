9d00caa6f0b429cae805228f8a0a07d865629296
Honorine-MC <honorine.mariechristy@gmail.com>
2022-11-04 15:05:02 +0100

    opti script avec tangui

--- project/bin/get_csv_engagements_vip2c.sh
+++ project/bin/get_csv_engagements_vip2c.sh
@@ -8,12 +8,10 @@ grep -a VIP2C $EXPORTGLOBALDIR/engagements.csv >> $EXPORTIVSEDIR/engagements_vip
 if [ -e $EXPORTIVSEDIR/engagements_vip2c.csv ]
 then
     DIFF=$(diff $EXPORTIVSEDIR/engagements_vip2c.csv $EXPORTIVSEDIR/engagements_vip2c.new.csv)
-    if [ "$DIFF" != "" ]
+    if ! test "$DIFF" != "" ;
     then
-        mv $EXPORTIVSEDIR/engagements_vip2c.new.csv $EXPORTIVSEDIR/engagements_vip2c.csv
-    else
         rm $EXPORTIVSEDIR/engagements_vip2c.new.csv
+        exit 0
     fi
-else
-    mv $EXPORTIVSEDIR/engagements_vip2c.new.csv $EXPORTIVSEDIR/engagements_vip2c.csv
 fi
+mv $EXPORTIVSEDIR/engagements_vip2c.new.csv $EXPORTIVSEDIR/engagements_vip2c.csv
