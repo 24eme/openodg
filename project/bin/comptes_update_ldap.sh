0e7482f1584a0542764fe31205ba5e3a29e98307
Tangui Morlier <tangui@tangui.eu.org>
2022-11-08 14:20:27 +0100

    update ldap plus résilient

--- project/bin/comptes_update_ldap.sh
+++ project/bin/comptes_update_ldap.sh
@@ -7,14 +7,14 @@ if test -f $LOCK ; then
     exit 1
 fi
 touch $LOCK
-if ! test -s $SEQ; then
+if ! grep '[0-9]' $SEQ > /dev/null ; then
     echo 0 > $SEQ
 fi
 
-curl -s "http://$COUCHHOST:$COUCHPORT/$COUCHBASE/_changes?feed=continuous&timeout=590000&since="$(cat $SEQ | sed 's/[^0-9]//g') | grep "COMPTE" | while read ligne
+curl -s "http://$COUCHHOST:$COUCHPORT/$COUCHBASE/_changes?feed=continuous&timeout=590000&since="$(cat $SEQ ) | grep "COMPTE" | while read ligne
 do
-    echo $ligne | awk -F '"' '{print $3}' | sed 's/[^0-9]//g' > $SEQ
-    php symfony compte:ldap-update $SYMFONYTASKOPTIONS $(echo $ligne | awk -F '"' '{print $6}')
+    echo $ligne | sed 's/.*"seq":"//' | sed 's/".*//' > $SEQ
+    php symfony compte:ldap-update $SYMFONYTASKOPTIONS $(echo $ligne | sed 's/.*"id":"//' | sed 's/".*//')
 done
 
 rm $LOCK
