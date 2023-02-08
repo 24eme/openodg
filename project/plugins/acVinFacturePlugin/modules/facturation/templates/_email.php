0e939b5ef75140a55a44a6f2c04f0eb6f20f03ac
Vincent LAURENT <vince.laurent@gmail.com>
2023-02-08 17:47:24 +0100

    "Sur ce lien"

--- project/plugins/acVinFacturePlugin/modules/facturation/templates/_email.php
+++ project/plugins/acVinFacturePlugin/modules/facturation/templates/_email.php
@@ -1,6 +1,6 @@
 Bonjour,
 
-Une nouvelle facture est disponible, vous pouvez la télécharger directement en cliquant sur le lien : <?php echo ProjectConfiguration::getAppRouting()->generate('piece_public_view', array('doc_id' => $id, 'piece_id' => '0', 'auth' => UrlSecurity::generateAuthKey($id.'0')), true) ?>
+Une nouvelle facture est disponible, vous pouvez la télécharger directement en cliquant sur ce lien : <?php echo ProjectConfiguration::getAppRouting()->generate('piece_public_view', array('doc_id' => $id, 'piece_id' => '0', 'auth' => UrlSecurity::generateAuthKey($id.'0')), true) ?>
 
 
 Bien cordialement,
