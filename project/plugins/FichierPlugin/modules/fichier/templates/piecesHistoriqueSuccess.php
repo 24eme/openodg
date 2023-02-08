91126d948006fa35bf2e947c428fc1f931443f83
Vincent LAURENT <vince.laurent@gmail.com>
2023-02-08 16:59:47 +0100

    Téléchargement automatique

--- project/plugins/FichierPlugin/modules/fichier/templates/piecesHistoriqueSuccess.php
+++ project/plugins/FichierPlugin/modules/fichier/templates/piecesHistoriqueSuccess.php
@@ -48,11 +48,11 @@
             				<a href="<?php echo url_for('get_piece', array('doc_id' => $document->id, 'piece_id' => $document->value[PieceAllView::VALUES_KEY])) ?>"><?php echo $document->key[PieceAllView::KEYS_LIBELLE] ?></a>
             				<?php endif; ?>
             			<?php endif; ?>
-                        <?php if (Piece::hasUrlPublic($document->id)): ?>
-                        <a title="Lien public pour partage" class="pull-right" href="<?php echo url_for('piece_public_view', array('doc_id' => $document->id, 'piece_id' => $document->value[PieceAllView::VALUES_KEY], 'auth' => UrlSecurity::generateAuthKey($document->id.$document->value[PieceAllView::VALUES_KEY]))) ?>"><span class="glyphicon glyphicon-link"></span></a>
-                        <?php endif; ?>
                     </td>
                     <td class="text-right">
+                        <?php if (Piece::hasUrlPublic($document->id)): ?>
+                        <a style="opacity: 0.3" onclick="navigator.clipboard.writeText(this.href); alert('Le lien a été copié dans le presse papier !'); return false;" title="Lien public pour partage" class="pull-left" href="<?php echo url_for('piece_public_view', array('doc_id' => $document->id, 'piece_id' => $document->value[PieceAllView::VALUES_KEY], 'auth' => UrlSecurity::generateAuthKey($document->id.$document->value[PieceAllView::VALUES_KEY]))) ?>"><span class="glyphicon glyphicon-link"></span></a>
+                        <?php endif; ?>
                         <?php if (Piece::isPieceEditable($document->id, $sf_user->hasCredential(myUser::CREDENTIAL_ADMIN))): ?>
                 			<a href="<?php echo url_for('edit_fichier', array('id' => $document->id)) ?>"><span class="glyphicon glyphicon-user"></span></a>
                 		<?php endif; ?>
--- project/plugins/acVinDocumentPlugin/modules/piece/templates/publicViewSuccess.php
+++ project/plugins/acVinDocumentPlugin/modules/piece/templates/publicViewSuccess.php
@@ -10,14 +10,46 @@
         <?php else: ?>
             <link href="<?php echo public_path("/css/compile_default.css").'?'.$gitcommit; ?>" rel="stylesheet">
         <?php endif; ?>
+        <style>
+
+        .loader {
+            width: 18px;
+            height: 18px;
+            border: 2px solid #fff;
+            border-bottom-color: transparent;
+            border-radius: 50%;
+            display: inline-block;
+            box-sizing: border-box;
+            animation: rotation 1s linear infinite;
+            }
+            @keyframes rotation {
+            0% {
+                transform: rotate(0deg);
+            }
+            100% {
+                transform: rotate(360deg);
+            }
+        }
+        </style>
     </head>
     <body>
         <div style="padding-top: 40px;" class="container text-center">
             <img src="/images/logo_<?php echo Organisme::getCurrentOrganisme() ?>.png" alt="<?php echo Organisme::getInstance()->getNom() ?>">
 
             <p style="margin-top: 30px;" class="lead"><?php echo $piece->libelle ?></p>
-
-            <a style="margin-top: 20px;" class="btn btn-primary btn-lg" href="<?php echo $piece->getUrlPublic() ?>">Télécharger ce document</a>
+            <a id="btn_loading" style="margin-top: 20px;" class="btn btn-primary btn-lg hidden" href="<?php echo $piece->getUrlPublic() ?>"><span class="loader" style="margin-right: 10px;"></span>Téléchargement cours ...</a>
+            <a style="margin-top: 20px;" id="btn_download" class="btn btn-primary btn-lg" href="<?php echo $piece->getUrlPublic() ?>">Télécharger ce document</a>
         </div>
+        <script>
+            document.getElementById('btn_loading').classList.remove('hidden');
+            document.getElementById('btn_download').classList.add('hidden');
+            window.setTimeout(function() {
+                document.getElementById('btn_download').click()
+            }, 700);
+            window.setTimeout(function() {
+                document.getElementById('btn_loading').classList.add('hidden');
+                document.getElementById('btn_download').classList.remove('hidden');
+            }, 5000);
+        </script>
     </body>
 </html>
