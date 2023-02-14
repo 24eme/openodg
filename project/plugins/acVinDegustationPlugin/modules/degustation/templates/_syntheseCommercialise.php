a4e06d54b97da23321f5cb1e5611c673447da350
Gabriel Poma <gpoma@24eme.fr>
2023-02-14 18:32:14 +0100

    vip2c: mise en page synthese

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/_syntheseCommercialise.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/_syntheseCommercialise.php
@@ -3,19 +3,17 @@
 <table class="table table-bordered table-striped">
     <thead>
         <tr>
-            <th class="text-center col-xs-6">Produit</th>
+            <th class="text-center col-xs-8">Produit</th>
             <th class="text-center col-xs-2">Volume revendiqué</th>
             <th class="text-center col-xs-2">Volume commercialisé</th>
-            <th class="text-center col-xs-2">VIP2C</th>
         </tr>
     </thead>
     <tbody>
         <?php foreach ($syntheseLots as $libelle => $produit): ?>
-            <tr<?php echo ($produit['vip2c'] && $produit['volume_commercialise'] > $produit['vip2c']) ? " class='danger'" : '' ?>>
-                <td><?php echo $libelle ?></td>
-                <td><?php echo $produit['volume_revendique'] ?></td>
-                <td><?php echo $produit['volume_commercialise'] ?></td>
-                <td><?php echo $produit['vip2c'] ?? '-' ?></td>
+            <tr>
+                <td><strong><?php echo $libelle ?></strong></td>
+                <td class="text-right"><?php echo echoFloat($produit['volume_revendique']) ?> <small class="text-muted">hl</small></td>
+                <td class="text-right"><?php echo echoFloat($produit['volume_commercialise']) ?> <small class="text-muted">hl</small></td>
             </tr>
         <?php endforeach; ?>
     </tbody>
