64a8090a538d04abf05758a5d7f9e8d61f3e1445
Vincent LAURENT <vince.laurent@gmail.com>
2023-02-28 16:51:32 +0100

    Oubli d'un retour chariot

--- project/apps/nantes/modules/facturation/templates/_email.php
+++ project/apps/nantes/modules/facturation/templates/_email.php
@@ -2,6 +2,7 @@ Bonjour,
 
 Une nouvelle facture est disponible, vous pouvez la télécharger directement en cliquant sur ce lien : <?php echo ProjectConfiguration::getAppRouting()->generate('piece_public_view', array('doc_id' => $id, 'auth' => UrlSecurity::generateAuthKey($id)), true) ?>
 
+
 Vous pouvez également consulter le courrier d'information des cotisations de la récolte 2022 : https://declaration.vinsdenantes.com/docs/Cotisations_FVN_recolte_2022_vd.pdf
 
 Bien cordialement,
