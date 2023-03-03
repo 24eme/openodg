4d1210d6836982b8686d3c5ab9a826cab8a7af52
Vincent LAURENT <vince.laurent@gmail.com>
2023-03-03 09:57:25 +0100

    Bug département

--- project/plugins/acVinParcellaireAffectationAVAPlugin/lib/form/ParcellaireAffectationParcelleForm.class.php
+++ project/plugins/acVinParcellaireAffectationAVAPlugin/lib/form/ParcellaireAffectationParcelleForm.class.php
@@ -112,7 +112,10 @@ abstract class ParcellaireAffectationParcelleForm extends acCouchdbObjectForm {
         $section = preg_replace('/^0*/','',$values['section']);
         $numero_parcelle = preg_replace('/^0*/','',$values['numero_parcelle']);
         $lieu = null;
-        $dpt = $this->getObject()->departement;
+        $dpt = null;
+        if($this->getObject()->exist('departement')) {
+            $dpt = $this->getObject()->departement;
+        }
         if($config->communes->exist($commune)) {
             $dpt = $config->communes[$commune];
         }
