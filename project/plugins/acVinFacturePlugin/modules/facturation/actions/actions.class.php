cac5196af509231314a7c5ad8444ae749ff423ce
Vincent LAURENT <vince.laurent@gmail.com>
2023-03-21 08:55:27 +0100

    Cas si pas de form societe

--- project/plugins/acVinFacturePlugin/modules/facturation/actions/actions.class.php
+++ project/plugins/acVinFacturePlugin/modules/facturation/actions/actions.class.php
@@ -27,12 +27,17 @@ class facturationActions extends sfActions
 
             return sfView::SUCCESS;
         }
-         $this->formSociete->bind($request->getParameter($this->formSociete->getName()));
-         $this->formFacturationMassive->bind($request->getParameter($this->formFacturationMassive->getName()));
-         if($this->formSociete->isValid()) {
-              $soc = SocieteClient::getInstance()->find($this->formSociete->getValue('identifiant'));
-              return $this->redirect('facturation_declarant', array('id' => $soc->getMasterCompte()->_id));
-          }
+
+        if(isset($this->formSociete)) {
+            $this->formSociete->bind($request->getParameter($this->formSociete->getName()));
+            if($this->formSociete->isValid()) {
+                $soc = SocieteClient::getInstance()->find($this->formSociete->getValue('identifiant'));
+                return $this->redirect('facturation_declarant', array('id' => $soc->getMasterCompte()->_id));
+            }
+        }
+
+        $this->formFacturationMassive->bind($request->getParameter($this->formFacturationMassive->getName()));
+
           if($this->formFacturationMassive->isValid()) {
 
               $generation = $this->formFacturationMassive->save();
