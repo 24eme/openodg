a6cfc14b26ba19e04d2cd6734520326bbbc96a35
Gabriel Poma <gpoma@24eme.fr>
2023-03-30 19:23:29 +0200

    drev: si lot non conforme alors on met une alert en bg

--- project/plugins/acVinDRevPlugin/modules/drev/actions/actions.class.php
+++ project/plugins/acVinDRevPlugin/modules/drev/actions/actions.class.php
@@ -858,6 +858,11 @@ class drevActions extends sfActions {
         }
 
         $this->dr = DRClient::getInstance()->findByArgs($this->drev->identifiant, $this->drev->periode);
+        $this->mouvements = [];
+        foreach (MouvementLotHistoryView::getInstance()->getMouvementsByDeclarant($this->drev->identifiant, $this->drev->campagne)->rows as $mouvement) {
+            $this->mouvements[$mouvement->key[2].$mouvement->key[3]] = $mouvement->value;
+        }
+
         if (!$request->isMethod(sfWebRequest::POST)) {
           return sfView::SUCCESS;
         }
