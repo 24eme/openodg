37a9ac5ceed84fea8a4b95d9dc652d3fc60cecef
NicoDuclos <nicolas@24eme.fr>
2023-03-31 09:04:59 +0200

    Debut de traitement des DGC dans le potentiel de prod

--- project/plugins/acVinParcellairePlugin/lib/export/ExportParcellairePPODS.class.php
+++ project/plugins/acVinParcellairePlugin/lib/export/ExportParcellairePPODS.class.php
@@ -5,13 +5,29 @@
  */
 class ExportParcellairePPODS extends BaseExportParcellaireODS {
 
+    private $identificationParcellaire;
+    private $etablissement;
+    private $dgc;
+
     public function __construct($parcellaire) {
         parent::__construct($parcellaire, 'calculatrice_PP.ods');
+        $this->etablissement = $parcellaire->getEtablissementObject();
+        $this->identificationParcellaire = ParcellaireAffectationClient::getInstance()->getLast($this->etablissement->identifiant);
+        if (!$this->identificationParcellaire) {
+            $this->identificationParcellaire = ParcellaireIntentionAffectationClient::getInstance()->getLast($this->etablissement->identifiant);
+        }
+        if(isset($this->identificationParcellaire)) {
+            $this->dgc = $this->identificationParcellaire->getDgc();
+        }
+        else {
+            $this->dgc = 'PAS DE DGC';
+        }
+
     }
 
     protected function parseDocument() {
 
-        // Intitialise les valeurs des clés
+        // Intitialise les valeurs des clés pour les cepages CDP et DGC
         $synthese = [
             '%%GRENACHE_N' => 0.0,
             '%%SYRAH_N' => 0.0,
@@ -20,10 +36,10 @@ class ExportParcellairePPODS extends BaseExportParcellaireODS {
             '%%CINSAULT_N' => 0.0,
             '%%CARIGNAN_N' => 0.0,
             '%%CABERNET_SAUVIGNON_N' => 0.0,
-            '%%CALITOR_N' => 0.0,
-            '%%BARBAR_RS' => 0.0,
+            '%%CALITOR_NOIR_N' => 0.0,
+            '%%BARBAROUX_RS' => 0.0,
             '%%VERMENTINO_B' => 0.0,
-            '%%UGNI_B' => 0.0,
+            '%%UGNI_BLANC_B' => 0.0,
             '%%CLAIRETTE_B' => 0.0,
             '%%SEMILLON_B' => 0.0,
         ];
@@ -43,6 +59,28 @@ class ExportParcellairePPODS extends BaseExportParcellaireODS {
         }
 
         $this->parse($synthese);
+
+        // Intitialise les valeurs des clés pour les cepages DGC
+        $synthese_dgc = [
+            '%%DGC_GRENACHE_N' => 0.0,
+            '%%DGC_SYRAH_N' => 0.0,
+            '%%DGC_MOURVEDRE_N' => 0.0,
+            '%%DGC_TIBOUREN_N' => 0.0,
+            '%%DGC_CINSAULT_N' => 0.0,
+            '%%DGC_CARIGNAN_N' => 0.0,
+            '%%DGC_CABERNET_SAUVIGNON_N' => 0.0,
+            '%%DGC_VERMENTINO_B' => 0.0,
+            '%%DGC_UGNI_BLANC_B' => 0.0,
+            '%%DGC_CLAIRETTE_B' => 0.0,
+            '%%DGC_SEMILLON_B' => 0.0,
+        ];
+
+        // TODO : mettre les valeurs DGC
+
+        $this->parse($synthese_dgc);
+
+        $this->parse(['%%DGC' => $this->dgc]);
+        
     }
 
 }
