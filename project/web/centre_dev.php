9ab243f81c85eb988a9dcb33c252a10a496ee5e8
Gabriel Poma <gpoma@24eme.fr>
2023-05-03 14:49:47 +0200

    bonne app dans centre[_dev].php

--- project/web/centre_dev.php
+++ project/web/centre_dev.php
@@ -1,6 +1,6 @@
 <?php
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
-$configuration = ProjectConfiguration::getApplicationConfiguration('sancerre', 'dev', true);
+$configuration = ProjectConfiguration::getApplicationConfiguration('centre', 'dev', true);
 
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it or make something more sophisticated.
