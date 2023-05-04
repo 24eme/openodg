113b063fbb532a46453b44dd625ba20949ae873c
Vincent LAURENT <vince.laurent@gmail.com>
2023-05-04 18:38:53 +0200

    On part sur la nav de provence plutot que celle d'igp

--- project/apps/centre/templates/_header.php
+++ project/apps/centre/templates/_header.php
@@ -1,9 +1,1 @@
-<header id="header">
-    <div class="container" style="position: relative; background-color: #fff; text-align: center;">
-      <a href="<?php echo url_for('accueil') ?>">
-        <div style="margin: 15px auto;">
-          <span style="color: #555353; font-size: 20px; padding-top: 3px;">Espace des professionnels du Centre</span>
-        </div>
-      </a>
-    </div>
-</header>
+
--- project/apps/centre/templates/_nav.php
+++ project/apps/centre/templates/_nav.php
@@ -1,98 +1,85 @@
-<?php
-$route = ($sf_request->getAttribute('sf_route')) ? $sf_request->getAttribute('sf_route')->getRawValue() : NULL;
-$etablissement = null;
-$compte = null;
+<?php $route = ($sf_request->getAttribute('sf_route')) ? $sf_request->getAttribute('sf_route')->getRawValue() : NULL; ?>
+<?php $etablissement = null ?>
+<?php $compte = null; ?>
 
-if ($route instanceof EtablissementRoute) {
-    $etablissement = $route->getEtablissement();
-    $compte = $etablissement->getMasterCompte();
-}
-if ($route instanceof FacturationDeclarantRoute || $route instanceof FactureRoute || $route instanceof CompteRoute) {
-    $compte = $route->getCompte();
-    $etablissement = $compte->getEtablissement();
-}
-if ($route instanceof SocieteRoute) {
-    $etablissement = $route->getEtablissement();
-    $societe = $route->getSociete();
-    if ($societe) {
-        $compte = $societe->getMasterCompte();
-    }
-}
-if ($sf_user->isAuthenticated() && !$sf_user->hasCredential(myUser::CREDENTIAL_ADMIN) && (!$compte || !$etablissement)) {
-    $compte = $sf_user->getCompte();
-    $societe = $compte->getSociete() ;
-    if ($societe) {
-        $etablissement = $societe->getEtablissementPrincipal();
-    }
-    if (!$etablissement) {
-        $etablissement = $compte->getEtablissement();
-    }
-}
-if ($sf_user->isAuthenticated() && !$compte && !$etablissement && !$sf_user->hasCredential(myUser::CREDENTIAL_ADMIN)) {
-    throw new sfError403Exception("pas de compte");
-}
-if (($compte && ($compte->statut == CompteClient::STATUT_SUSPENDU)) && !$sf_user->hasCredential(myUser::CREDENTIAL_ADMIN)) {
-    throw new sfError403Exception("Compte inactif");
-}
-?>
+<?php if($route instanceof EtablissementRoute): ?>
+    <?php $etablissement = $route->getEtablissement(); ?>
+    <?php $campagne = $route->getCampagne(); ?>
+    <?php $compte = $etablissement->getMasterCompte(); ?>
+<?php endif; ?>
+<?php if ($route instanceof FacturationDeclarantRoute || $route instanceof FactureRoute || $route instanceof CompteRoute): ?>
+    <?php $compte = $route->getCompte(); ?>
+    <?php $societe = $compte->getSociete(); ?>
+    <?php $compte = $societe->getMasterCompte(); ?>
+    <?php $etablissement = $societe->getEtablissementPrincipal(); ?>
+<?php endif; ?>
+<?php if ($route instanceof SocieteRoute): ?>
+    <?php $societe = $route->getSociete(); ?>
+    <?php $etablissement = $route->getEtablissement(); ?>
+    <?php $compte = $route->getSociete()->getMasterCompte(); ?>
+<?php endif; ?>
+
+<?php if($sf_user->isAuthenticated() && !$sf_user->hasCredential(myUser::CREDENTIAL_ADMIN) && (!$compte || !$etablissement)): ?>
+    <?php $compte = $sf_user->getCompte(); ?>
+    <?php $societe = $compte->getSociete() ; if ($societe) $etablissement = $societe->getEtablissementPrincipal(); ?>
+    <?php if (!$etablissement) $etablissement = $compte->getEtablissement(); ?>
+<?php endif; ?>
+
+
 <nav id="menu_navigation" class="navbar navbar-default container">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed hidden-lg hidden-md" data-toggle="collapse" data-target="#menu_items_collapse" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" style="padding-top: 0;padding-right: 15px;" href="<?php echo url_for('accueil') ?>"><h1 style="color: black; font-size: 1.5em; margin-top: 10px; font-weight: bold;">Centre</h1></a>
+          <a class="navbar-brand" style="padding-top: 0;padding-right: 15px;" href="<?php echo url_for('accueil') ?>"><h1 style="color: black; font-size: 1.5em; margin-top: 10px; font-weight: bold;">Centre</h1></a>
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" style="padding-left: 0;">
             <?php if($sf_user->hasCredential(myUser::CREDENTIAL_ADMIN)): ?>
             <ul class="nav navbar-nav <?php if($compte): ?>mode-operateur<?php endif; ?>" style="border: 0;">
-                <li id="nav_item_operateur" class="<?php if(!$compte): ?>disabled<?php endif; ?>"><a <?php if($compte): ?>onclick="document.location = $(this).parents('ul.mode-operateur').find('li.active a').attr('href');"<?php endif; ?> href="#"><span class="glyphicon glyphicon-user"></span></a></li>
                 <li class="<?php if($route instanceof InterfaceDeclarationRoute): ?>active<?php endif; ?>"><a href="<?php if($etablissement && !$route instanceof InterfaceDeclarationRoute): ?><?php echo url_for('declaration_etablissement', $etablissement); ?><?php else: ?><?php echo url_for('declaration'); ?><?php endif; ?>">Déclarations</a></li>
+                <li class="<?php if($route instanceof InterfaceDegustationGeneralRoute): ?>active<?php endif; ?>"><a href="<?php if($etablissement && !$route instanceof InterfaceDegustationGeneralRoute): ?><?php echo url_for('degustation_declarant_lots_liste', array('identifiant' => $etablissement->identifiant)); ?><?php else: ?><?php echo url_for('degustation'); ?><?php endif; ?>">Dégustation</a></li>
+                <li class="<?php if($route instanceof InterfaceHabilitationRoute): ?>active<?php endif; ?>"><a href="<?php if($etablissement  && !$route instanceof InterfaceHabilitationRoute): ?><?php echo url_for('habilitation_declarant', $etablissement); ?><?php else: ?><?php echo url_for('habilitation'); ?><?php endif; ?>">Habilitations</a></li>
                 <li class="<?php if($route instanceof InterfaceDocumentsRoute): ?>active<?php endif; ?>"><a href="<?php if($etablissement  && !$route instanceof InterfaceDocumentsRoute): ?><?php echo url_for('pieces_historique', $etablissement); ?><?php else: ?><?php echo url_for('documents'); ?><?php endif; ?>">Documents</a></li>
                 <li class="<?php if($route instanceof InterfaceParcellaireRoute): ?>active<?php endif; ?>"><a href="<?php if($etablissement && !$route instanceof InterfaceParcellaireRoute): ?><?php echo url_for('parcellaire_declarant', $etablissement); ?><?php else: ?><?php echo url_for('parcellaire'); ?><?php endif; ?>">Parcellaire</a></li>
                 <li class="<?php if($route instanceof InterfaceFacturationRoute): ?>active<?php endif; ?>"><a href="<?php if($compte  && !$route instanceof InterfaceFacturationRoute): ?><?php echo url_for('facturation_declarant', $compte); ?><?php else: ?><?php echo url_for('facturation'); ?><?php endif; ?>">Facturation</a></li>
-                <li class="<?php if($route instanceof InterfaceHabilitationRoute): ?>active<?php endif; ?>"><a href="<?php if($etablissement  && !$route instanceof InterfaceHabilitationRoute): ?><?php echo url_for('habilitation_declarant', $etablissement); ?><?php else: ?><?php echo url_for('habilitation'); ?><?php endif; ?>">Habilitations</a></li>
-                <li class="<?php if($route instanceof InterfaceCompteRoute && !$route instanceof InterfaceFacturationRoute): ?>active<?php endif; ?>"><a href="<?php if($compte && !$route instanceof InterfaceCompteRoute): ?><?php echo url_for('compte_visualisation', $compte); ?><?php else: ?><?php echo url_for('compte_search'); ?><?php endif; ?>">Contacts</a></li>
+                <li class="<?php if($route instanceof InterfaceCompteRoute && !$route instanceof FacturationDeclarantRoute): ?>active<?php endif; ?>"><a href="<?php if($compte && !$route instanceof InterfaceCompteRoute || $route instanceof FacturationDeclarantRoute): ?><?php echo url_for('compte_visualisation', $compte); ?><?php else: ?><?php echo url_for('compte_search'); ?><?php endif; ?>">Contacts</a></li>
             </ul>
             <?php elseif($sf_user->isAuthenticated() && $etablissement): ?>
                 <ul class="nav navbar-nav <?php if($compte): ?>mode-operateur<?php endif; ?>" style="border: 0;">
                     <li class="<?php if($route instanceof InterfaceDeclarationRoute): ?>active<?php endif; ?>"><a href="<?php echo url_for('declaration_etablissement', $etablissement); ?>">Déclarations</a></li>
+                    <?php if ($sf_user->hasCredential(myUser::CREDENTIAL_HABILITATION)): ?>
+                       <li class="<?php if($route instanceof InterfaceDegustationGeneralRoute): ?>active<?php endif; ?>"><a href="<?php if($etablissement && !$route instanceof InterfaceDegustationGeneralRoute): ?><?php echo url_for('degustation_declarant_lots_liste', array('identifiant' => $etablissement->identifiant)); ?><?php else: ?><?php echo url_for('degustation'); ?><?php endif; ?>">Dégustation</a></li>
+                       <li class="<?php if($route instanceof InterfaceHabilitationRoute): ?>active<?php endif; ?>"><a href="<?php if($etablissement  && !$route instanceof InterfaceHabilitationRoute): ?><?php echo url_for('habilitation_declarant', $etablissement); ?><?php else: ?><?php echo url_for('habilitation'); ?><?php endif; ?>">Habilitations</a></li>
+                    <?php endif; ?>
                     <li class="<?php if($route instanceof InterfaceDocumentsRoute): ?>active<?php endif; ?>"><a href="<?php echo url_for('pieces_historique', $etablissement); ?>">Documents</a></li>
-                    <?php if(SocieteConfiguration::getInstance()->isVisualisationTeledeclaration()): ?>
-                    <li class="<?php if($route instanceof InterfaceParcellaireRoute): ?>active<?php endif; ?>"><a href="<?php echo url_for('parcellaire_declarant', $etablissement); ?>">Parcellaire</a></li>
-                    <?php endif; ?>
-                    <li class="<?php if($route instanceof InterfaceFacturationRoute): ?>active<?php endif; ?>"><a href="<?php if($compte  && !$route instanceof InterfaceFacturationRoute): ?><?php echo url_for('facturation_declarant', $compte); ?><?php endif; ?>">Facturation</a></li>
-                    <?php if(SocieteConfiguration::getInstance()->isVisualisationTeledeclaration()): ?>
-                    <li class="<?php if($route instanceof InterfaceHabilitationRoute): ?>active<?php endif; ?>"><a href="<?php echo url_for('habilitation_declarant', $etablissement); ?>">Habilitations</a></li>
-                    <li class="<?php if($route instanceof InterfaceCompteRoute &&  !$route instanceof FacturationDeclarantRoute): ?>active<?php endif; ?>"><a href="<?php echo url_for('compte_visualisation', $compte); ?>">Contacts</a></li>
-                    <?php endif; ?>
                     <li class="<?php if(preg_match('/compte/', $route->getParameters()['module'])): ?>active<?php endif; ?>"><a tabindex="-1" href="<?php echo url_for("compte_teledeclarant_modification") ?>" title="Mon compte">Mon compte</a></li>
                 </ul>
             <?php endif; ?>
             <ul class="nav navbar-nav navbar-right">
-                <?php if(NavConfiguration::getInstance()->hasStatLinks()): ?>
-                <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-stats"></span><span class="caret"></span></a>
-                  <ul class="dropdown-menu">
-                    <?php foreach(NavConfiguration::getInstance()->getStatLinks() as $i => $navItem): ?>
-                     <?php if($i > 0 && isset($navItem['title'])): ?><li role="separator" class="divider"></li><?php endif; ?>
-                     <li>
-                         <a href="<?php echo $navItem['url'] ?>">
-                             <?php if(isset($navItem['icon'])): ?><span class="glyphicon glyphicon-<?php echo $navItem['icon'] ?>"></span><?php endif; ?>
-                             <?php if(isset($navItem['title'])): ?><strong><?php endif; ?><?php echo $navItem['name'] ?><?php if(isset($navItem['title'])): ?></strong><?php endif; ?>
-                         </a>
-                    </li>
-                    <?php endforeach; ?>
-                  </ul>
-                </li>
-                <?php endif; ?>
                 <?php if($sf_user->hasCredential(myUser::CREDENTIAL_ADMIN)): ?>
+                    <?php if(sfConfig::get('app_nav_stats_'.sfConfig::get('sf_app'))): ?>
+                    <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-stats"></span><span class="caret"></span></a>
+                      <ul class="dropdown-menu">
+                        <?php foreach(sfConfig::get('app_nav_stats_'.sfConfig::get('sf_app')) as $i => $navItem):
+                                $campagne = ConfigurationClient::getInstance()->getCampagneManager(CampagneManager::FORMAT_COMPLET)->getCurrent();
+                                if($i > 0 && isset($navItem['title'])): ?><li role="separator" class="divider"></li><?php endif; ?>
+                         <li>
+                             <?php if (isset($navItem['etablissement']) && $etablissement): ?>
+                                 <a href="<?php echo str_replace('CAMPAGNE', $campagne, sprintf($navItem['url'].'?op%%25C3%%25A9rateur=%s', $etablissement->raison_sociale)) ?>">
+                             <?php else: ?>
+                                 <a href="<?php echo str_replace('CAMPAGNE', $campagne, $navItem['url']) ?>">
+                             <?php endif ?>
+                                 <?php if(isset($navItem['icon'])): ?><span class="glyphicon glyphicon-<?php echo $navItem['icon'] ?>"></span><?php endif; ?>
+                                 <?php if(isset($navItem['title'])): ?><strong><?php endif; ?><?php echo $navItem['name'] ?><?php if(isset($navItem['title'])): ?></strong><?php endif; ?>
+                             </a>
+                        </li>
+                        <?php endforeach; ?>
+                      </ul>
+                    </li>
+                <?php endif; ?>
                 <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-cog"></span><span class="caret"></span></a>
                   <ul class="dropdown-menu">
                     <li><a href="<?php echo url_for("produits") ?>">Catalogue produit</a></li>
+                    <li><a href="<?php echo url_for("facturation_template_last") ?>">Facturation</a></li>
                   </ul>
                 </li>
                 <?php elseif($sf_user->isAuthenticated()): ?>
