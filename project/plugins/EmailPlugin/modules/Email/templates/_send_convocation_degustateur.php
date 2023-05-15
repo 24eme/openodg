211401fc8410b85f950bc55d0970c563e113cdfe
Gabriel Poma <gpoma@24eme.fr>
2023-05-15 15:40:42 +0200

    mail convoc degust: précision présence / absence dans la phrase

--- project/plugins/EmailPlugin/modules/Email/templates/_send_convocation_degustateur.php
+++ project/plugins/EmailPlugin/modules/Email/templates/_send_convocation_degustateur.php
@@ -5,7 +5,7 @@ $infos = FactureConfiguration::getInstance()->getInfos();
 Nous vous invitons à participer à la prochaine commission de dégustation des vins IGP qui aura lieu le <?php echo ucfirst(format_date($degustation->date, "P", "fr_FR"))." à ".format_date($degustation->date, "HH").'h'.format_date($degustation->date, "mm"); ?> <?php echo $degustation->getLieuNom()." - ".preg_replace("/.+—[ ]*/", "", $degustation->lieu); ?>
 
 
-Pour le bon déroulé de cette dégustation nous vous remercions de bien vouloir nous confirmer votre présence en cliquant sur le lien :
+Pour le bon déroulé de cette dégustation nous vous remercions de bien vouloir nous confirmer votre présence ou votre absence en cliquant sur le lien :
 
 <?php echo url_for('degustation_convocation_presence', [
     'id' => $degustation->_id,
