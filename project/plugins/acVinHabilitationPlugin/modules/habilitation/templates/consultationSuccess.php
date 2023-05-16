e05de78e5ec93b6b07144da140c494669a315f65
Vincent LAURENT <vince.laurent@gmail.com>
2023-05-16 09:58:30 +0200

    Lien plus discret

--- project/plugins/acVinHabilitationPlugin/modules/habilitation/templates/consultationSuccess.php
+++ project/plugins/acVinHabilitationPlugin/modules/habilitation/templates/consultationSuccess.php
@@ -2,6 +2,10 @@
 
 <ol class="breadcrumb">
     <li class="active"><a href=""><span class="glyphicon glyphicon-search"></span> Consultation des habilitations</a></li>
+    <?php if($sf_user->hasCredential(myUser::CREDENTIAL_HABILITATION)): ?>
+    <li class="pull-right">
+        <a title="Consultation des habilitations" style="opacity: 0.25" href="<?php echo url_for('habilitation_declarant', $habilitation->getEtablissementObject()); ?>">Voir la version complète</a></li>
+    <?php endif; ?>
 </ol>
 
 <h3>Consulter l'habilitation d'un opérateur</h3>
@@ -9,7 +13,7 @@
 <form method="get" action="" role="form" class="form-horizontal" style="margin-top: 30px;">
     <div class="form-group">
         <div class="col-sm-4 col-sm-offset-3 col-xs-12">
-            <input name="numero" type="text" class="form-control" placeholder="Numéro CVI ou SIRET" autofocus="autofocus" <?php if(isset($numero)): ?>value="<?php echo $numero ?>"<?php endif ?>/>
+            <input name="numero" type="text" class="form-control" placeholder="Numéro CVI" autofocus="autofocus" <?php if(isset($numero)): ?>value="<?php echo $numero ?>"<?php endif ?>/>
         </div>
         <div class="col-xs-2 hidden-xs">
             <button class="btn btn-default" type="submit">Voir l'habilitation</button>
@@ -27,13 +31,11 @@
 <?php endif; ?>
 
 <div class="well">
-    <a class="btn btn-link pull-right btn-sm" onclick="navigator.clipboard.writeText(this.href); alert('Le lien a été copié dans le presse papier !'); return false;" title="Copier le lien vers cette page" href="<?php echo url_for('habilitation_consultation', ['numero' => $numero]) ?>"><small class="glyphicon glyphicon-link"></small> Lien vers cette page</a>
-    <a class="btn btn-link pull-right btn-sm" title="Export JSON" href="<?php echo url_for('habilitation_consultation', ['numero' => $numero, 'format' => 'json']) ?>"><small class="glyphicon glyphicon-transfer"></small> JSON</a>
-    <?php if($sf_user->hasCredential(myUser::CREDENTIAL_HABILITATION)): ?>
-    <a class="btn-link btn-sm pull-right" href="<?php echo url_for('habilitation_declarant', $habilitation->getEtablissementObject()); ?>"><small class="glyphicon glyphicon-eye-open"></small> Voir la version complète</a>
-    <?php endif; ?>
+    <a class="btn btn-link pull-right btn-sm" onclick="navigator.clipboard.writeText(this.href); alert('Le lien a été copié dans le presse papier !'); return false;" title="Copier le lien vers cette page" href="<?php echo url_for('habilitation_consultation', ['numero' => $numero]) ?>"><small class="glyphicon glyphicon-link"></small></a>
     <h4><span class="glyphicon glyphicon-home"></span> CVI : <?php echo $etablissement->getCvi(); ?>
     <?php if($etablissement->getSiret()): ?> - SIREN : <?php echo formatSIRET($etablissement->getSiret(), true); ?><?php endif; ?></h4>
 </div>
 
 <?php include_partial('habilitation/habilitation', array('habilitation' => $habilitation, 'public' => true)); ?>
+
+<a style="margin-top: -15px;" class="pull-right" title="Export JSON" href="<?php echo url_for('habilitation_consultation', ['numero' => $numero, 'format' => 'json']) ?>"><small>JSON</small></a>
