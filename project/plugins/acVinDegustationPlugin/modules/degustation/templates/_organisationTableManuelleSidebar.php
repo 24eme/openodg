d5cd4545d7294a16dcace8d5cbf99b55f1ec865e
Gabriel Poma <gpoma@24eme.fr>
2023-05-17 11:22:14 +0200

    préparation maj nb lots dans la table

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/_organisationTableManuelleSidebar.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/_organisationTableManuelleSidebar.php
@@ -2,9 +2,10 @@
   <div class="panel-heading">
     <h3 class="panel-title">Tables</h3>
   </div>
-  <ul class="list-group">
+  <ul class="list-group" id="liste-tables">
     <?php foreach ($degustation->getTables()->getRawValue() + [count($degustation->getTables()->getRawValue()) + 1 => []] as $table => $lots): ?>
         <a href="<?php echo url_for('degustation_organisation_table', ['id' => $degustation->_id, 'numero_table' => $table, 'tri' => $tri]) ?>"
+            data-table="<?php echo $table ?>"
             class="list-group-item<?php if ($numero_table == $table): echo " active" ; endif ?>">
             <span class="badge"><?php echo count($lots) ?></span>
             Table <?php echo DegustationClient::getNumeroTableStr($table) ?>
