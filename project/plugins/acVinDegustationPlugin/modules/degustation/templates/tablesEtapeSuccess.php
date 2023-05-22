e13294b6c7b77bb1b880b29c754b9f0b88cefc53
NicoDuclos <nicolas@24eme.fr>
2023-05-22 13:42:21 +0200

    [centre][tournees] corrige enchainement etapes

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/tablesEtapeSuccess.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/tablesEtapeSuccess.php
@@ -64,7 +64,7 @@
 </div>
 </div>
 <div class="row row-button">
-  <div class="col-xs-4"><a href="<?php echo url_for("degustation_prelevements_etape",$degustation) ?>" class="btn btn-default btn-upper"><span class="glyphicon glyphicon-chevron-left"></span> Retour</a></div>
+  <div class="col-xs-4"><a href="<?php echo url_for("degustation_tournees_etape",$degustation) ?>" class="btn btn-default btn-upper"><span class="glyphicon glyphicon-chevron-left"></span> Retour</a></div>
   <div class="col-xs-4 text-center">
   </div>
   <div class="col-xs-4 text-right"><a id="btn_suivant" <?php if (!$infosDegustation["nbLotsPrelevesSansLeurre"]):
