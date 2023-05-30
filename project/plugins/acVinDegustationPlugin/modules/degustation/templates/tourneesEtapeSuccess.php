66d9c9fbf9607351e3398fcb18ac47cd2727cdac
NicoDuclos <nicolas@24eme.fr>
2023-05-30 14:54:59 +0200

    [centre][tournees] un form pour mettre les secteurs

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/tourneesEtapeSuccess.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/tourneesEtapeSuccess.php
@@ -1,90 +1,89 @@
 <?php use_helper('Float') ?>
-<?php use_helper('Lot') ?>
+<?php use_javascript('hamza_style.js'); ?>
+<?php use_javascript('degustation.js'); ?>
 
 <?php include_partial('degustation/breadcrumb', array('degustation' => $degustation)); ?>
 <?php include_partial('degustation/step', array('degustation' => $degustation, 'active' => DegustationEtapes::ETAPE_TOURNEES)); ?>
 
+<form action="<?php echo url_for("degustation_tournees_etape", $degustation) ?>" method="post" class="ajaxForm form-horizontal degustation tournees">
+<?php
+echo $form->renderHiddenFields();
+echo $form->renderGlobalErrors();
+?>
 <div class="page-header no-border">
-  <h2>Tournées</h2>
-</div>
-<div class="row">
-  <div class="col-xs-offset-3 col-xs-6">
-    <h3>Tournée secteur 0</h3>
-  </div>
-  <div class="col-xs-3 text-right">
-    <div class="btn-group">
-        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">
-            Export PDF <span class="caret"></span>
-        </button>
-        <ul class="dropdown-menu">
-            <li><a href="#tournees">Tournées</a></li>
-            <li><a href="#etiquettes">Etiquettes</a></li>
-        </ul>
+    <h2>Tournées</h2>
     </div>
-  </div>
-</div>
-<div class="row">
-  <div class="col-xs-3">
-    <div class="panel panel-default" style="min-height: 160px">
-      <div class="panel-heading">
-        <h2 class="panel-title">
-          Secteurs des tournées
-        </h2>
-      </div>
-      <div class="list-group">
-        <?php foreach ($secteurs as $secteur): ?>
-          <a href="#" class="list-group-item">
-            <?php echo $secteur; ?>
-          </a>
-        <?php endforeach; ?>
-      </div>
+    <div class="row">
+    <div class="col-xs-offset-3 col-xs-6">
+        <h3>Tournée secteur 0</h3>
     </div>
-  </div>
-  <div class="col-xs-9">
-    <table class="table table-bordered table-condensed">
-      <thead>
-        <tr>
-          <th class="col-xs-3 text-left">Opérateur</th>
-          <th class="col-xs-4 text-left">Adresse du logement</th>
-          <th class="col-xs-2 text-left">Commune du logement</th>
-          <th class="col-xs-1 text-left">Nombre de lots</th>
-          <th class="col-xs-2 text-left">Secteur</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php foreach ($degustation->getLotsByOperateurs() as $lots):?>
-          <?php $operateur_infos = splitLogementAdresse($lots[0]->getAdresseLogement()); ?>
-          <tr class="vertical-center">
-            <td class="text-left"><?php echo $operateur_infos['nom']; ?></td>
-            <td class="text-left"><?php echo $operateur_infos['adresse']; ?></td>
-            <td class="text-left"><?php echo $operateur_infos['commune']; ?> (<?php echo $operateur_infos['code_postal']; ?>)</td>
-            <td class="text-center"><?php echo count($lots); ?></td>
-            <td class="text-center">
-              <div class="btn-group">
-                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">
-                    Secteur 0 <span class="caret"></span>
-                </button>
-                <ul class="dropdown-menu">
-                  <?php foreach ($secteurs as $anchor => $secteur): ?>
-                    <li><a href="#<?php echo $anchor;?>"><?php echo $secteur; ?></a></li>
-                  <?php endforeach; ?>
-                </ul>
-              </div>
-            </td>
-          </tr>
-        <?php endforeach; ?>
-      </tbody>
-    </table>
-  </div>
-</div>
+    <div class="col-xs-3 text-right">
+        <div class="btn-group">
+            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">
+                Export PDF <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu">
+                <li><a href="#tournees">Tournées</a></li>
+                <li><a href="#etiquettes">Etiquettes</a></li>
+            </ul>
+        </div>
+    </div>
+    </div>
+    <div class="row">
+    <div class="col-xs-3">
+        <div class="panel panel-default" style="min-height: 160px">
+        <div class="panel-heading">
+            <h2 class="panel-title">
+            Secteurs des tournées
+            </h2>
+        </div>
+        <div class="list-group">
+            <?php foreach ($form->getRegions() as $secteur): ?>
+            <a href="#" class="list-group-item">
+                <?php echo $secteur; ?>
+            </a>
+            <?php endforeach; ?>
+        </div>
+        </div>
+    </div>
+    <div class="col-xs-9">
+        <table class="table table-bordered table-condensed">
+        <thead>
+            <tr>
+            <th class="col-xs-3 text-left">Opérateur</th>
+            <th class="col-xs-4 text-left">Adresse du logement</th>
+            <th class="col-xs-2 text-left">Commune du logement</th>
+            <th class="col-xs-1 text-left">Nombre de lots</th>
+            <th class="col-xs-2 text-left">Secteur</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($operators as $key => $operator):?>
+            <tr class="vertical-center">
+                <td class="text-left"><?php echo $operator['nom']; ?></td>
+                <td class="text-left"><?php echo $operator['adresse']; ?></td>
+                <td class="text-left"><?php echo $operator['commune']; ?> (<?php echo $operator['code_postal']; ?>)</td>
+                <td class="text-center"><?php echo $operator['nbLots']; ?></td>
+                <td class="text-center"><?php echo $operator['select']->render(['class' => 'form-control', 'value' => 'SECTEUR_1']); ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+        </table>
+    </div>
+    </div>
 
-<div class="row row-button">
-  <div class="col-xs-4"><a href="<?php echo url_for("degustation_prelevements_etape",$degustation) ?>" class="btn btn-default btn-upper"><span class="glyphicon glyphicon-chevron-left"></span> Retour</a></div>
-  <div class="col-xs-4 text-center">
-  </div>
-  <div class="col-xs-4 text-right"><a id="btn_suivant" <?php if (!$infosDegustation["nbLotsPrelevesSansLeurre"]):
-    echo 'disabled="disabled"';
-  endif; ?> class="btn btn-primary btn-upper"
-      href="<?php echo ($infosDegustation["nbLotsPrelevesSansLeurre"]) ? url_for('degustation_tables_etape', $degustation) : "#"; ?>">Valider&nbsp;<span
-        class="glyphicon glyphicon-chevron-right"></span></a></div>
-</div>
+    <div class="row row-button">
+        <div class="col-xs-4"><a href="<?php echo url_for("degustation_prelevements_etape",$degustation) ?>" class="btn btn-default btn-upper"><span class="glyphicon glyphicon-chevron-left"></span> Retour</a></div>
+    <div class="col-xs-4 text-center"></div>
+    <div class="col-xs-4 text-right">
+        <!-- <a id="btn_suivant"
+        class="btn btn-primary btn-upper"
+        href="<?php echo ($infosDegustation["nbLotsPrelevesSansLeurre"]) ? url_for('degustation_tables_etape', $degustation) : "#"; ?>"
+        <?php if (!$infosDegustation["nbLotsPrelevesSansLeurre"]): echo 'disabled="disabled"'; endif; ?>
+        >
+        Valider&nbsp;<span class="glyphicon glyphicon-chevron-right"></span>
+        </a> -->
+        <button type="submit" class="btn btn-primary btn-upper">Valider</button>
+    </div>
+    </div>
+</form>
