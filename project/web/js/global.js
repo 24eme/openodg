158e416375adea0b0ee94a2d58ce9742bf6fb683
Tangui Morlier <tangui@tangui.eu.org>
2023-06-21 16:29:00 +0200

    Évite la confusion potentielle sur l'édition vs l'ajout de produit

--- project/web/js/global.js
+++ project/web/js/global.js
@@ -946,6 +946,7 @@
            table.find('.invisible').removeClass('invisible').addClass('visible');
           $(this).children('span').each(function(){ $(this).removeClass('glyphicon-eye-open');/*.addClass('glyphicon-eye-close');*/ });
           $(this).css("opacity","0.6");
+          $('#editHabilitation').hide();
         }else{
           table.find('tr[data-hide="1"]').each(function(){
             $(this).hide();
@@ -956,6 +957,7 @@
           table.find('.visible').removeClass('visible').addClass('invisible');
           $(this).children('span').each(function(){ $(this).removeClass('glyphicon-eye-close').addClass('glyphicon-eye-open'); });
           $(this).css("opacity","1");
+          $('#editHabilitation').show();
         }
       });
 
