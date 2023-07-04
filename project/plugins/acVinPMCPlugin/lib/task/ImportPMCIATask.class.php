124e3dc1c3b56cfd38ad41e4dc6a77a4198a000a
Vincent LAURENT <vince.laurent@gmail.com>
2023-07-05 01:02:52 +0200

    Date commission lot

--- project/plugins/acVinPMCPlugin/lib/task/ImportPMCIATask.class.php
+++ project/plugins/acVinPMCPlugin/lib/task/ImportPMCIATask.class.php
@@ -97,6 +97,7 @@ EOF;
             $lot->volume = $volume;
             $lot->numero_logement_operateur = trim($logement.' '.$numeroLot);
             $lot->date_degustation_voulue = $datePresentation;
+            $lot->date_commission = $dateCommission;
             $lot->affectable = true;
 
             $pmc->add('date_commission', $dateCommission);
