9c6135c8cbb572ebad99cba07c4309468c002611
Vincent LAURENT <vince.laurent@gmail.com>
2023-07-31 18:23:35 +0200

    Mise à jour graphique de l'erreur 500 de rhône

--- project/apps/rhone/config/error/error.html.php
+++ project/apps/rhone/config/error/error.html.php
@@ -8,27 +8,24 @@
 <!-- ####### PLEASE KEEP ####### -->
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta http-equiv="X-Ua-Compatible" content="IE=edge" />
+        <meta http-equiv="X-Ua-Compatible" content="IE=edge" />
         <meta name="title" content="Plateforme de télédéclaration du syndicat Côtes-du-Rhône" />
-<meta name="charset" content="utf-8" />
-<meta name="description" content="Plateforme de télédéclaration du syndicat Côtes-du-Rhône" />
-<meta name="robots" content="index,follow" />
-<meta name="content-language" content="fr-FR" />
-<meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="charset" content="utf-8" />
+        <meta name="description" content="Plateforme de télédéclaration du syndicat Côtes-du-Rhône" />
+        <meta name="robots" content="noindex" />
+        <meta name="content-language" content="fr-FR" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Plateforme de télédéclaration du syndicat Côtes-du-Rhône</title>
 
         <link rel="shortcut icon" type="image/x-icon" href="/favico.ico" />
         <link rel="icon" type="image/x-icon" href="/favico.ico" />
         <link rel="icon" type="image/png" href="/favico.png" />
-
         <link rel="stylesheet" type="text/css" media="all" href="/css/compile_rhone.css?201706281900" />
-<link rel="stylesheet" type="text/css" media="print" href="/css/print.css" />
-<link rel="stylesheet" type="text/css" media="all" href="/css/../components/vins/vins.css" />
-<link rel="stylesheet" type="text/css" media="screen" href="/js/lib/leaflet/leaflet.css" />
-
+        <link rel="stylesheet" type="text/css" media="print" href="/css/print.css" />
+        <link rel="stylesheet" type="text/css" media="all" href="/css/../components/vins/vins.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="/js/lib/leaflet/leaflet.css" />
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,600" rel="stylesheet" type="text/css">
         <link href="/css/style_rhone.css" rel="stylesheet" type="text/css">
-
         <script type="text/javascript" src="/js/lib/modernizr-2.8.2.js"></script>
         <script type="text/javascript" src="/js/lib/device.min.js"></script>
 
@@ -49,21 +46,19 @@
         <!-- ####### PLEASE KEEP ####### -->
 
 
-                        <header id="header">
-    <div class="container" style="position: relative; background-color: #002b6f; text-align: center;">
-    	<a href="/">
-        	<img style="height: 80px; border-left: 2px solid #fff;" class="pull-right" src="/images/logo_odgrhone.png" />
-        	<img style="height: 80px;" class="pull-left" src="/images/bg/bg_rhone_site.jpg" />
-        	<span style="color: #fff; font-size: 20px; padding-right: 130px; padding-top: 7px; display: inline-block;"><span style="color: #fff; font-size: 17px;">Espace professionnel<br /></span><span style="color: #fff">syndicat-cotesdurhone<strong style="color: #9f0038;">.</strong><span style="color: #9b9b9b">com</span></span></span>
-    	</a>
-    </div>
-</header>
+        <header id="header">
+            <div class="container" style="position: relative; background-color: #002b6f; text-align: center;">
+            	<a href="/">
+                	<img style="height: 80px; border-left: 2px solid #fff;" class="pull-right" src="/images/logo_odgrhone.png" />
+                	<img style="height: 80px;" class="pull-left" src="/images/bg/bg_rhone_site.jpg" />
+                	<span style="color: #fff; font-size: 20px; padding-right: 130px; padding-top: 7px; display: inline-block;"><span style="color: #fff; font-size: 17px;">Espace professionnel<br /></span><span style="color: #fff">syndicat-cotesdurhone<strong style="color: #9f0038;">.</strong><span style="color: #9b9b9b">com</span></span></span>
+            	</a>
+            </div>
+        </header>
 
 
- <section id="content" class="container">
- 
-
-                <div class="page-header no-border">
+        <section id="content" class="container">
+            <div class="page-header no-border">
 
                 <h2><span class="glyphicon glyphicon-warning-sign"></span>&nbsp;&nbsp;Une erreur est survenue</h2>
             </div>
@@ -74,7 +69,7 @@
                     Nous sommes désolés et vous prions de nous contacter afin de nous signaler et détailler le probleme.
                     </p>
                     <p>
-                    Nous tâcherons alors de le résoudre dans les plus brefs délais. 
+                    Nous tâcherons alors de le résoudre dans les plus brefs délais.
                     </p>
                 </div>
             </div>
@@ -85,40 +80,18 @@
                     <a style="margin-top: 10px;" href="javascript:history.back()" class="btn btn-default btn-upper"><span class="eleganticon arrow_carrot-left"></span>&nbsp;&nbsp;Retourner à la page précédente</a>
                 </div>
                 <div class="col-xs-4">
-                    
                 </div>
             </div>
- 
- </section>
+        </section>
 
+        <footer id="footer" class="container hidden-xs hidden-sm text-center" role="contentinfo" style="margin-top: 40px;">
+            <nav role="navigation">
+                <ul class="list-inline" style="font-size: 13px;">
+                    <li><a href="/contact">Contact</a></li>
+                    <li><a href="/mentions-legales">Mentions légales</a></li>
+                </ul>
+            </nav>
+        </footer>
 
-
-            <footer id="footer" class="container hidden-xs hidden-sm text-center" role="contentinfo" style="margin-top: 40px;">
-                <nav role="navigation">
-                    <ul class="list-inline" style="font-size: 13px;">
-                        <li><a href="/contact">Contact</a></li>
-                        <li><a href="/mentions-legales">Mentions légales</a></li>
-                    </ul>
-                </nav>
-            </footer>
-            <!-- end #footer -->
-
-        <!-- end #page -->
-
-        <div class="alert hidden alert-danger notification" id="ajax_form_error_notification">Une erreur est survenue</div>
-        <div class="alert hidden alert-success notification" id="ajax_form_progress_notification">Enregistrement en cours ...</div>
-        <script type="text/javascript" src="/js/lib/jquery/jquery.min.js"></script>
-<script type="text/javascript" src="/js/lib/jquery.plugins.min.js?201709221633"></script>
-<script type="text/javascript" src="/js/lib/bootstrap/bootstrap.min.js"></script>
-<script type="text/javascript" src="/js/lib/bootstrap3-typeahead.min.js"></script>
-<script type="text/javascript" src="/js/lib/bootstrap-switch.min.js"></script>
-<script type="text/javascript" src="/js/lib/leaflet/leaflet.js"></script>
-<script type="text/javascript" src="/js/global.js?201711052339"></script>
-<script type="text/javascript" src="/js/compte.js"></script>
-<script type="text/javascript" src="/js/drev.js?202105041230"></script>
-<script type="text/javascript" src="/js/drevmarc.js"></script>
-<script type="text/javascript" src="/js/form.js"></script>
-<script type="text/javascript" src="/js/parcellaire.js"></script>
-<script type="text/javascript" src="/js/ava.js"></script>
     </body>
 </html>
