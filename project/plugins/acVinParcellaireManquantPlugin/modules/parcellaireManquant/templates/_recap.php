a7f93ec9e2c0c0c5f6272f646c58def6b4d92c36
Gabriel Poma <gpoma@24eme.fr>
2023-08-09 19:12:34 +0200

    centre: manquant: 20% premiere etape

--- project/plugins/acVinParcellaireManquantPlugin/modules/parcellaireManquant/templates/_recap.php
+++ project/plugins/acVinParcellaireManquantPlugin/modules/parcellaireManquant/templates/_recap.php
@@ -9,7 +9,7 @@
 		        <th class="col-xs-1">Année plantat°</th>
 		        <th class="col-xs-1" style="text-align: right;">Surf. <span class="text-muted small">(ha)</span></th>
 		        <th class="col-xs-1">Densité</th>
-		        <th class="col-xs-1">% de pieds manquants <span class="text-muted">(si&nbsp;+&nbsp;de&nbsp;20%)</span></th>
+		        <th class="col-xs-1">% de pieds manquants</th>
 
             </tr>
 		</thead>
