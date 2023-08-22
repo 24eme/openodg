f975f20724ab67615f89a12591817e00b1aee18a
Tangui Morlier <tangui@tangui.eu.org>
2023-08-22 14:58:36 +0200

    Libellé des totaux sans XXX

--- project/plugins/acVinDRevPlugin/lib/controle/DRevValidation.class.php
+++ project/plugins/acVinDRevPlugin/lib/controle/DRevValidation.class.php
@@ -396,9 +396,9 @@ class DRevValidation extends DeclarationLotsValidation
             }
             if ($this->document->hasDR() && isset($synthese[$couleur]['volume_restant_max']) && round($synthese[$couleur]['volume_restant_max'], 4) < -0.0001) {
                 if ($this->document->exist('achat_tolerance') && $this->document->get('achat_tolerance')) {
-                    $this->addPoint(self::TYPE_WARNING, 'lot_volume_total_depasse_warn', $couleur, $this->generateUrl('drev_lots', array('id' => $this->document->_id)));
+                    $this->addPoint(self::TYPE_WARNING, 'lot_volume_total_depasse_warn', $synthese[$couleur]['libelle'], $this->generateUrl('drev_lots', array('id' => $this->document->_id)));
                 }else{
-                    $this->addPoint(self::TYPE_ERROR, 'lot_volume_total_depasse', $couleur, $this->generateUrl('drev_lots', array('id' => $this->document->_id)));
+                    $this->addPoint(self::TYPE_ERROR, 'lot_volume_total_depasse', $synthese[$couleur]['libelle'], $this->generateUrl('drev_lots', array('id' => $this->document->_id)));
                 }
             }
         }
