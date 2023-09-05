0cced1d3bf9488a6d9b5ff8d6f472f00fe4001a1
Tangui Morlier <tangui@tangui.eu.org>
2023-09-05 16:48:54 +0200

    export des données dans un seul csv

--- project/bin/export_fede.sh
+++ project/bin/export_fede.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 campagne="$1"
-if !test "$campagne"; then
+if ! test "$campagne"; then
     campagne=$(date '+%Y')
 fi
 
@@ -8,10 +8,10 @@ cd ~/prodouane_scrapy
 
 bash bin/download_all.sh
 
-mkdir documents/fede
+mkdir -p documents/fede
 for dep in  37 41 44 49 79 86; do
-    cp "documents/dr-"$campagne"-""$dep"*  documents/fede/
-    cp "documents/production-"$campagne"-""$dep"*  documents/fede/
+    rsync "documents/dr-"$campagne"-""$dep"*  documents/fede/
+    rsync "documents/production-"$campagne"-""$dep"*  documents/fede/
 done
 
 cd -
@@ -30,5 +30,7 @@ done
 rename 's/production-/sv-/' ~/prodouane_scrapy/documents/fede/production-$campagne-*.pdf
 
 cd ~/prodouane_scrapy/documents/fede
-mkdir final
-cp $(grep -lE ';ANJCDL;|;BON;|;SAVCDS;|;SAV;|;SAVRAM;|;CAJ;|;ANJ;|;AJV;|;RLO;|;SAU;|;CLO;|;SAUCHA;|;COB;|;COL;|;COS;|;QDC;|;RAJ;|;AJVBRI;' dr*csv sv*csv  | sed 's/.csv/*/') final/
+mkdir -p final/documents
+cp $(grep -lE ';ANJCDL;|;BON;|;SAVCDS;|;SAV;|;SAVRAM;|;CAJ;|;ANJ;|;AJV;|;RLO;|;SAU;|;CLO;|;SAUCHA;|;COB;|;COL;|;COS;|;QDC;|;RAJ;|;AJVBRI;' "dr-"$campagne"-"*csv "sv-"$campagne"-"*csv  | sed 's/.csv/*/') final/documents/
+echo '#Type;Campagne;Identifiant;CVI;Raison Sociale;Code Commune;Commune;Bailleur Nom;Bailleur PPM;Certification;Genre;Appellation;Mention;Lieu;Couleur;Cepage;INAO;Produit;Complement;Code;Categorie;Valeur;CVI Tiers;Valeur Motif / Raison Sociale Tiers;Code Commune Tiers;Commune Tiers;Id Colonne;Organisme;Hash produit;Last DRev id if exist;Last DRev id with produit filter if exist;Doc Id;Famille calculee;Millesime;Famille ligne calculee;label calculee' > "final/dr-"$campagne".csv"
+cat final/documents/*csv >> "final/dr-"$campagne".csv"
