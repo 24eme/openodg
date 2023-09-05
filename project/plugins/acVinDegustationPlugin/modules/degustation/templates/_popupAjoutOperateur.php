8f8ff1796decbaa2ff8b2ac684e4fcc126bfc90f
Vincent LAURENT <vince.laurent@gmail.com>
2023-09-05 11:48:19 +0200

    Mise en forme de la modal d'ajout

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/_popupAjoutOperateur.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/_popupAjoutOperateur.php
@@ -3,22 +3,36 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4>Sélectionnez un opérateur à prélever pour la dégustation en aléatoire ou aléatoire renforcé</h4>
+                <h4>Sélectionnez un opérateur à prélever</h4>
             </div>
             <div class="modal-body">
-                <form action="<?php echo url_for("degustation_selection_operateurs", $degustation) ?>" method="post" class="form-horizontal degustation prelevements selectionlots">
+                <form id="form_degustation_ajout_operateur" action="<?php echo url_for("degustation_selection_operateurs", $degustation) ?>" method="post" class="form-horizontal">
                     <?php echo $form->renderHiddenFields(); ?>
 
                     <div class="bg-danger">
                         <?php echo $form->renderGlobalErrors(); ?>
                     </div>
 
-                    <?php echo $form['identifiant']->render() ?>
-                    <?php echo $form['initial_type']->render() ?>
+                    <div class="form-group">
+                        <?php echo $form['initial_type']->renderLabel(null, ['class' => 'col-sm-3 control-label']) ?>
+                        <div class="col-sm-9">
+                            <?php echo $form['initial_type']->render() ?>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <?php echo $form['identifiant']->renderLabel(null, ['class' => 'col-sm-3 control-label']) ?>
+                        <div class="col-sm-9">
+                            <?php echo $form['identifiant']->render() ?>
+                        </div>
+                    </div>
+
                 </form>
             </div>
             <div class="modal-footer">
                 <a class="btn btn-default btn pull-left" data-dismiss="modal">Annuler</a>
+
+                <button form="form_degustation_ajout_operateur" class="btn btn-primary" type="submit">Ajouter</a>
             </div>
         </div>
     </div>
