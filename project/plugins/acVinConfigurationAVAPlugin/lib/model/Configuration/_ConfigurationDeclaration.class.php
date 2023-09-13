5f39a64d3bea407dddaaf45b0088b35e00913b29
Tangui Morlier <tangui@tangui.eu.org>
2023-09-13 12:24:12 +0200

    Fonctionn pour libelle_long à true

--- project/plugins/acVinConfigurationAVAPlugin/lib/model/Configuration/_ConfigurationDeclaration.class.php
+++ project/plugins/acVinConfigurationAVAPlugin/lib/model/Configuration/_ConfigurationDeclaration.class.php
@@ -381,17 +381,14 @@ abstract class _ConfigurationDeclaration extends acCouchdbDocumentTree {
 
     public function getLibelleComplet($libelle_long = false)
     {
-        if($libelle_long) {
+        if(is_null($this->libelleCompletCache)) {
+            $this->libelleCompletCache = $this->getParent()->getLibelleComplet();
+        }
+    	$libelle = $this->libelleCompletCache;
 
+        if($libelle_long) {
             return trim(trim($libelle).' '.$this->getLibelleLong());
         }
-
-        if(!is_null($this->libelleCompletCache)) {
-
-            return $this->libelleCompletCache;
-        }
-
-    	$libelle = $this->getParent()->getLibelleComplet();
 
         $this->libelleCompletCache = trim(trim($libelle).' '.$this->libelle);
 
