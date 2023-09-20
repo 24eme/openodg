5bf1bdba6b8dcad9d2d9898e83469d384037a2fb
Tangui Morlier <tangui@tangui.eu.org>
2023-09-20 13:48:06 +0200

    Oubli de l'organisme dans les détails

--- project/plugins/acVinFacturePlugin/lib/export/ExportFactureCSV.class.php
+++ project/plugins/acVinFacturePlugin/lib/export/ExportFactureCSV.class.php
@@ -77,6 +77,7 @@ class ExportFactureCSV implements InterfaceDeclarationExportCsv {
                 $csv .= $this->floatHelper->formatFr($detail->montant_tva).";";
                 $csv .= $this->floatHelper->formatFr($detail->montant_ht + $detail->montant_tva).";";
                 $csv .= ";;";
+                $csv .= Organisme::getCurrentOrganisme().";";
                 $csv .= $this->facture->_id.";";
                 $csv .= $campagne.";";
                 $csv .= $this->facture->getNumeroOdg().";";
