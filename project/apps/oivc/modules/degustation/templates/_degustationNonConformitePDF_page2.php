9be64ef156cdac20a3d2bf65743b0f744e21a0e0
Gabriel Poma <gpoma@24eme.fr>
2023-09-21 16:57:54 +0200

    centre: oi: fiche de manquement controle produit

---
+++ project/apps/oivc/modules/degustation/templates/_degustationNonConformitePDF_page2.php
@@ -0,0 +1,80 @@
+<?php use_helper("Date"); ?>
+<?php use_helper('Lot'); ?>
+<?php use_helper('TemplatingPDF'); ?>
+
+<style>
+    <?php echo style(); ?>
+    table {
+        font-size: 12px;
+    }
+
+    th {
+        font-weight: bold;
+    }
+</style>
+<br/>
+<br/>
+
+<table>
+    <tr>
+        <td style="width: 70%">Date de rédaction : <?php echo format_date($degustation->date, "P", "fr_FR"); ?></td>
+        <td style="width: 30%">N° : <?php echo $lot->unique_id; ?></td>
+    </tr>
+</table>
+
+<br/><br/>
+
+<table style="text-align: center"><tr><td><strong>Fiche De Manquement Contrôle Produit</strong></td></tr></table>
+
+<br/><br/>
+
+<table><tr><td>Nom opérateur : <?php echo $etablissement->raison_sociale ?></td></tr></table>
+
+<br/><br/>
+
+<table><tr><td><strong>N° de rapport d'inspection correspondant : </strong></td></tr></table>
+
+<br/>
+<br/>
+
+<table border="1">
+    <tr><td>À REMPLIR PAR L'OPÉRATEUR</td></tr>
+    <tr><td>Éventuelles observations :<br/><br/><br/><br/><br/><br/><br/></td></tr>
+    <tr><td>Mesure(s) de correction proposée(s) :<br/><br/><br/><br/><br/><br/><br/></td></tr>
+    <tr><td>Je souhaite que le lot soit reprélevé au mois de :</td></tr>
+    <tr><td>Je souhaite exercer un recours : OUI <?php echo echoCheck(null, false) ?></td></tr>
+    <tr><td>Nom du responsable de l'entreprise :<br/><br/>Date et signature :<br/><br/></td></tr>
+</table>
+
+<br/>
+<br/>
+
+<table border="1">
+    <tr><td colspan="6">PARTIE RÉSERVÉE À L'OIVC</td></tr>
+    <tr><td colspan="6">RÉSULTAT DU CONTRÔLE DE LA MESURE DE CORRECTION APRÈS SON ÉXECUTION</td></tr>
+    <tr style="text-align: center">
+        <td></td> <td>Date</td> <td>Conforme</td> <td>Non conforme</td> <td>Libellé manquement</td> <td>Code manquement</td>
+    </tr>
+    <tr>
+        <td>Examen analytique</td> <td></td> <td></td> <td></td> <td></td> <td></td>
+    </tr>
+    <tr>
+        <td>Examen organoleptique</td> <td></td> <td></td> <td></td> <td></td> <td></td>
+    </tr>
+    <tr><td colspan="6">Date transmission INAO :</td></tr>
+</table>
+
+<br/><br/>
+
+<table>
+<tr>
+  <td style="width: 50%"><strong>Nom du responsable d'inspection :</strong></td>
+  <td style="width: 25%"><strong>Date :</strong></td>
+  <td style="width: 25%"><strong>Signature :</strong></td>
+</tr>
+</table>
+
+<br/><br/>
+<br/><br/>
+
+<table><tr><td>Fiche à retourner à l'OIVC par courrier au <?php echo Organisme::getInstance()->getAdresse() ?></td></tr></table>
