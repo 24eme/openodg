99c601f01dadc45ddf16f179e816f26e4d28b1e2
Tangui Morlier <tangui@tangui.eu.org>
2023-09-21 14:43:45 +0200

    Déporte la conversion du CSV DR Civa vers la DREV dans la classe de lecture de la DR (pour préparer l'arrivée de la SV)

---
+++ project/lib/import/CIVACsvFile.class.php
@@ -0,0 +1,9 @@
+<?php
+
+abstract class CIVACsvFile {
+
+    abstract public function updateDRevCepage(DRev $drev);
+    abstract public function updateDRevProduitDetail(DRev $drev);
+
+
+}
--- project/lib/import/DRCIVACsvFile.class.php
+++ project/lib/import/DRCIVACsvFile.class.php
@@ -1,6 +1,6 @@
 <?php
 
-class DRCIVACsvFile
+class DRCIVACsvFile extends CIVACsvFile
 {
   const CSV_ACHETEUR_CVI = 0;
   const CSV_ACHETEUR_LIBELLE = 1;
@@ -98,4 +98,124 @@ class DRCIVACsvFile
       array_shift($this->csvdata);
     return $this->csvdata;
   }
+
+  public function updateDRevProduitDetail(DRev $drev) {
+      $drev->resetProduitDetail();
+      foreach ($this->getCsvAcheteur($drev->identifiant) as $line) {
+          $hashProduit = DRCIVACsvFile::getHashProduitByLine($line);
+
+          if (!preg_match("/^TOTAL/", $line[DRCIVACsvFile::CSV_LIEU]) && !preg_match("/^TOTAL/", $line[DRCIVACsvFile::CSV_CEPAGE])) {
+
+              continue;
+          }
+
+          $hashProduit = preg_replace("/(mentionVT|mentionSGN)/", "mention", $hashProduit);
+          $hashProduit = preg_replace('|/recolte.|', '/declaration/', $hashProduit);
+
+          if (!$drev->getConfiguration()->exist($hashProduit)) {
+              continue;
+          }
+
+          $config = $drev->getConfiguration()->get($hashProduit)->getNodeRelation('revendication');
+
+          if ($config instanceof ConfigurationCepage) {
+              continue;
+          }
+
+          if ($config instanceof ConfigurationLieu) {
+              continue;
+          }
+
+          if ($config->getLieu()->hasManyCouleur() && !$config instanceof ConfigurationCouleur) {
+              continue;
+          }
+
+          if ($config instanceof ConfigurationAppellation) {
+              $config = $config->mention->lieu->couleur;
+          }
+
+          if ($config instanceof ConfigurationMention) {
+              $config = $config->lieu->couleur;
+          }
+
+          if (!$config instanceof ConfigurationCouleur) {
+              continue;
+          }
+
+          $produit = $drev->addProduit($config->getHash());
+          $produitDetail = $produit->detail;
+          if($line[DRCIVACsvFile::CSV_VTSGN]) {
+              $produitDetail = $produit->detail_vtsgn;
+          }
+
+          $produitDetail->volume_total += (float) $line[DRCIVACsvFile::CSV_VOLUME_TOTAL];
+          $produitDetail->usages_industriels_total += (float) $line[DRCIVACsvFile::CSV_USAGES_INDUSTRIELS_TOTAL];
+          if(preg_match("/^[0-9\.,]+$/", $line[DRCIVACsvFile::CSV_VCI_TOTAL]) && ((float) $line[DRCIVACsvFile::CSV_VCI_TOTAL]) > 0) {
+              $produitDetail->vci_total += (float) $line[DRCIVACsvFile::CSV_VCI_TOTAL];
+          }
+          $produitDetail->superficie_total += (float) $line[DRCIVACsvFile::CSV_SUPERFICIE_TOTALE];
+          $produitDetail->volume_sur_place += (float) $line[DRCIVACsvFile::CSV_VOLUME];
+
+          if (!$produitDetail->exist('superficie_vinifiee')) {
+              $produitDetail->add('superficie_vinifiee');
+          }
+          if($line[DRCIVACsvFile::CSV_SUPERFICIE] != "") {
+              $produitDetail->superficie_vinifiee += (float) $line[DRCIVACsvFile::CSV_SUPERFICIE];
+          } else {
+              $produitDetail->superficie_vinifiee = null;
+          }
+          if ($line[DRCIVACsvFile::CSV_USAGES_INDUSTRIELS] == "") {
+              $produitDetail->usages_industriels_sur_place = -1;
+          } elseif ($produitDetail->usages_industriels_sur_place != -1) {
+              $produitDetail->usages_industriels_sur_place += (float) $line[DRCIVACsvFile::CSV_USAGES_INDUSTRIELS];
+          }
+
+          if(preg_match("/^[0-9\.,]+$/", $line[DRCIVACsvFile::CSV_VCI]) && ((float) $line[DRCIVACsvFile::CSV_VCI]) > 0) {
+              $produitDetail->vci_sur_place += (float) $line[DRCIVACsvFile::CSV_VCI];
+          }
+      }
+
+      $drev->updateProduitDetail();
+  }
+
+  public function updateDRevCepage(DRev $drev) {
+      $drev->resetCepage();
+
+      foreach ($this->getCsvAcheteur($drev->identifiant) as $line) {
+          if (
+                  preg_match("/^TOTAL/", $line[DRCIVACsvFile::CSV_APPELLATION]) ||
+                  preg_match("/^TOTAL/", $line[DRCIVACsvFile::CSV_LIEU]) ||
+                  preg_match("/^TOTAL/", $line[DRCIVACsvFile::CSV_CEPAGE])
+          ) {
+
+              continue;
+          }
+
+          $hashProduit = DRCIVACsvFile::getHashProduitByLine($line);
+
+          $hash = preg_replace("|/detail/.+$|", "", preg_replace('|/recolte.|', '/declaration/', preg_replace("|/detail/[0-9]+$|", "", $hashProduit)));
+          $hash = preg_replace("/(mentionVT|mentionSGN)/", "mention", $hash);
+
+          if (!$drev->getConfiguration()->exist($hash)) {
+              continue;
+          }
+
+          $config = $drev->getConfiguration()->get($hash);
+          $detail = $drev->getOrAdd($config->getHash())->addDetailNode($line[DRCIVACsvFile::CSV_LIEU]);
+          if ($line[DRCIVACsvFile::CSV_VTSGN] == "VT") {
+              $detail->volume_revendique_vt += (float) $line[DRCIVACsvFile::CSV_VOLUME];
+              $detail->superficie_revendique_vt += (float) $line[DRCIVACsvFile::CSV_SUPERFICIE_TOTALE];
+          } elseif ($line[DRCIVACsvFile::CSV_VTSGN] == "SGN") {
+              $detail->volume_revendique_sgn += (float) $line[DRCIVACsvFile::CSV_VOLUME];
+              $detail->superficie_revendique_sgn += (float) $line[DRCIVACsvFile::CSV_SUPERFICIE_TOTALE];
+          } else {
+              $detail->volume_revendique += (float) $line[DRCIVACsvFile::CSV_VOLUME];
+              $detail->superficie_revendique += (float) $line[DRCIVACsvFile::CSV_SUPERFICIE_TOTALE];
+          }
+
+          $detail->updateTotal();
+          $detail->getLibelle();
+      }
+  }
+
 }
