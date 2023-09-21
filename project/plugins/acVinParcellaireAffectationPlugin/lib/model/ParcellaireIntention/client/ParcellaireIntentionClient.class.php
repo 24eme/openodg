102bf978285281c307c768efeb4d58e86021aa2e
Tangui Morlier <tangui@tangui.eu.org>
2023-09-21 16:50:45 +0200

    Début de la pseudo intention

---
+++ project/plugins/acVinParcellaireAffectationPlugin/lib/model/ParcellaireIntention/client/ParcellaireIntentionClient.class.php
@@ -0,0 +1,61 @@
+<?php
+
+class ParcellaireIntentionClient {
+
+      private static $instance;
+
+      public static function getInstance() {
+          if (!self::$instance) {
+              self::$instance = new ParcellaireIntentionClient();
+          }
+          return self::$instance;
+      }
+
+      public function createDoc($identifiant, $periode, $papier = false, $date = null, $type = ParcellaireIntentionAffectationClient::TYPE_COUCHDB) {
+      	if (!$date) {
+          $date = date('Y-m-d');
+        }
+        return $this->createOrGetDocFromIdentifiantAndDate($identifiant, $periode, $papier, $date, $type);
+      }
+
+      public function createOrGetDocFromIdentifiantAndDate($identifiant, $periode, $papier = false, $date = null, $type = ParcellaireIntentionAffectationClient::TYPE_COUCHDB) {
+          if (ParcellaireConfiguration::getInstance()->affectationNeedsIntention()) {
+              return ParcellaireIntentionAffectationClient::getInstance()->createOrGetDocFromIdentifiantAndDate($identifiant, $periode, $papier, $date, $type);
+          }
+          return new ParcellaireIntentionAuto();
+      }
+
+      public function getLast($identifiant, $max_annee = '9999', $hydrate = acCouchdbClient::HYDRATE_DOCUMENT){
+          if (ParcellaireConfiguration::getInstance()->affectationNeedsIntention()) {
+              return ParcellaireIntentionClient::getInstance()->getLast($identifiant, $max_annee, $hydrate);
+          }
+          return new ParcellaireIntentionAuto();
+      }
+
+      public function getDateOuverture($type = self::TYPE_COUCHDB) {
+          if ($type == self::TYPE_COUCHDB) {
+              $dates = sfConfig::get('app_dates_ouverture_parcellaire_irrigue');
+          }
+          if (!is_array($dates) || !isset($dates['debut']) || !isset($dates['fin'])) {
+              return array('debut'=>'1900-01-01', 'fin' => '9999-12-31');
+          }
+          return $dates;
+      }
+
+      public function getDateOuvertureDebut($type = self::TYPE_COUCHDB) {
+          $dates = $this->getDateOuverture($type);
+          return $dates['debut'];
+      }
+
+      public function getDateOuvertureFin($type = self::TYPE_COUCHDB) {
+          $dates = $this->getDateOuverture($type);
+          return $dates['fin'];
+      }
+
+      public function isOpen($type = self::TYPE_COUCHDB, $date = null) {
+          if (is_null($date)) {
+              $date = date('Y-m-d');
+          }
+          return $date >= $this->getDateOuvertureDebut($type) && $date <= $this->getDateOuvertureFin($type);
+      }
+}
