cd8e273a5f5d5a9800d5b125ccbfcf1e29c30c03
Jb Lm <lemetayer.jb@gmail.com>
2023-09-22 17:06:42 +0200

    hash des lots revendicable par lot en conf et IGP par defaut

--- project/plugins/acVinConfigurationPlugin/lib/model/Configuration/ConfigurationCepage.class.php
+++ project/plugins/acVinConfigurationPlugin/lib/model/Configuration/ConfigurationCepage.class.php
@@ -213,8 +213,8 @@ class ConfigurationCepage extends BaseConfigurationCepage {
     /* FIN DR */
 
     public function isRevendicationParLots() {
-
-        return preg_match("/IGP/", $this->getHash());
+        $hashRevendicableParLots = sfConfig::get('app_hash_revendicable_par_lots', 'IGP');
+        return preg_match("/$hashRevendicableParLots/", $this->getHash());
     }
 
     public function isRevendicationAOC() {
