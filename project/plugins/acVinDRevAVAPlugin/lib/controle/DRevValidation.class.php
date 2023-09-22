29880de426e5f3980a48fff9f707fbe0067a011f
Tangui Morlier <tangui@tangui.eu.org>
2023-09-22 17:03:17 +0200

    Clarification du message sur le manque de lots déclarés

--- project/plugins/acVinDRevAVAPlugin/lib/controle/DRevValidation.class.php
+++ project/plugins/acVinDRevAVAPlugin/lib/controle/DRevValidation.class.php
@@ -24,7 +24,7 @@ class DRevValidation extends DocumentValidation {
 
         //$this->addControle(self::TYPE_WARNING, 'lot_vtsgn_sans_prelevement', 'Vous avez déclaré des lots VT/SGN sans spécifier de période de prélèvement.');
         $this->addControle(self::TYPE_WARNING, 'declaration_lots', 'Vous devez déclarer vos lots.');
-        $this->addControle(self::TYPE_ERROR, 'declaration_lots_inferieur', 'Vous avez revendiqué des cepages  qui n\'ont pas de lots.');
+        $this->addControle(self::TYPE_ERROR, 'declaration_lots_inferieur', 'Vous devez déclarer au moins autant de lots de cepages revendiqués.');
 
         $this->addControle(self::TYPE_WARNING, 'revendication_cepage_sans_lot', 'Vous ne déclarez aucun lot pour un cépage que vous avez revendiqué. Si c\'est un lot qui a été replié en assemblage, ne tenez pas compte de ce point de vigilance.');
 
@@ -290,8 +290,9 @@ class DRevValidation extends DocumentValidation {
             }
         }
         foreach ($nb_total_lots_cepages as $key => $value) {
-          if($value < $this->document->prelevements->get($key)->getNbLotsMinimum()) {
-            $this->addPoint(self::TYPE_ERROR, 'declaration_lots_inferieur', "Dégustation conseil - ".$this->document->prelevements->get($key)->libelle_produit, $this->generateUrl('drev_lots', $this->document->prelevements->get($key)));
+          $nbmin = $this->document->prelevements->get($key)->getNbLotsMinimum();
+          if($value < $nbmin) {
+            $this->addPoint(self::TYPE_ERROR, 'declaration_lots_inferieur', "Dégustation conseil - vos $value lot(s) / $nbmin de ".$this->document->prelevements->get($key)->libelle_produit, $this->generateUrl('drev_lots', $this->document->prelevements->get($key)));
             break;
           }
         }
