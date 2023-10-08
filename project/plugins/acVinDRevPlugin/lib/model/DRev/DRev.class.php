b99a4dcdc6be70e6535f78347237b663b4c09fbb
Vincent LAURENT <vince.laurent@gmail.com>
2023-10-08 23:50:35 +0200

    Bug

--- project/plugins/acVinDRevPlugin/lib/model/DRev/DRev.class.php
+++ project/plugins/acVinDRevPlugin/lib/model/DRev/DRev.class.php
@@ -1727,7 +1727,7 @@ class DRev extends BaseDRev implements InterfaceProduitsDocument, InterfaceVersi
 
     public function getQuantiteVolumeVendue($produitFilter = null) {
         $docDouanier = $this->getDocumentDouanier(null, $this->getPeriode());
-        if ($docDouanier->type != DRCsvFile::CSV_TYPE_DR) {
+        if (!$docDouanier || $docDouanier->type != DRCsvFile::CSV_TYPE_DR) {
             return;
 
         }
