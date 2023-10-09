edb44fb7934fa07fe2f0c57666617ff327e8865b
tale-fau <lefaucheurtanguy@gmail.com>
2023-10-09 18:51:17 +0200

    fix le bug de vincent

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/prelevementsEtapeSuccess.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/prelevementsEtapeSuccess.php
@@ -68,7 +68,6 @@
                 </tr>
                 <tr>
                     <td class="col-xs-1 text-right"><?php echo count($degustation->getLotsPreleves()) ?></td>
-                    <td>lots prélevés<?php if ($nbDifferes = count($degustation->getLotsDifferes())): ?> (dont <?php echo $nbDifferes ?> différé<?php if($nbDifferes>1): ?>s<?php endif; ?>)<?php endif; ?></td>
                 </tr>
                 <tr>
                     <td class="col-xs-1 text-right"><?php echo count($degustation->getLotsSansVolume()); ?></td>
