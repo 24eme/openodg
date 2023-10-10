f10e2923206f680189ba00a60188c08704e46d15
Jb Lm <lemetayer.jb@gmail.com>
2023-10-10 10:28:28 +0200

    alignement du bouton devalider a droite

--- project/plugins/acVinParcellaireManquantPlugin/modules/parcellaireManquant/templates/visualisationSuccess.php
+++ project/plugins/acVinParcellaireManquantPlugin/modules/parcellaireManquant/templates/visualisationSuccess.php
@@ -53,16 +53,14 @@
             </a>
     </div>
 
-    <div class="col-xs-2 text-right">
-        <?php if ($parcellaireManquant->validation && ParcellaireSecurity::getInstance($sf_user, $parcellaireManquant->getRawValue())->isAuthorized(ParcellaireSecurity::DEVALIDATION)): ?>
-                    <a class="btn btn-xs btn-default pull-right" href="<?php echo url_for('parcellairemanquant_devalidation', $parcellaireManquant) ?>" onclick="return confirm('Êtes-vous sûr de vouloir dévalider votre declaration de pieds manquants ?');"><span class="glyphicon glyphicon-remove-sign"></span>&nbsp;&nbsp;Dévalider</a>
-        <?php endif; ?>
-    </div>
     <div class="col-xs-3 text-right">
         <?php if(!$parcellaireManquant->validation): ?>
                 <a href="<?php echo url_for("parcellairemanquant_edit", $parcellaireManquant) ?>" class="btn btn-primary"><span class="glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;Continuer la saisie</a>
         <?php elseif(!$parcellaireManquant->validation_odg && $sf_user->isAdmin()): ?>
                 <a onclick='return confirm("Êtes vous sûr de vouloir approuver cette déclaration ?");' href="<?php echo url_for("parcellairemanquant_validation_admin", array("sf_subject" => $parcellaireManquant, "service" => null)) ?>" class="btn btn-default btn-upper"><span class="glyphicon glyphicon-ok-sign"></span>&nbsp;&nbsp;Approuver</a>
+        <?php endif; ?>
+        <?php if ($parcellaireManquant->validation && ParcellaireSecurity::getInstance($sf_user, $parcellaireManquant->getRawValue())->isAuthorized(ParcellaireSecurity::DEVALIDATION)): ?>
+                    <a class="btn btn-xs btn-default pull-right" href="<?php echo url_for('parcellairemanquant_devalidation', $parcellaireManquant) ?>" onclick="return confirm('Êtes-vous sûr de vouloir dévalider votre declaration de pieds manquants ?');"><span class="glyphicon glyphicon-remove-sign"></span>&nbsp;&nbsp;Dévalider</a>
         <?php endif; ?>
     </div>
 </div>
