858a2089fc3b28959844314a13f188367582b8c3
Gabriel Poma <gpoma@24eme.fr>
2023-10-10 10:50:27 +0200

    centre: point vigilance pourcentage < 20%

--- project/plugins/acVinParcellaireManquantPlugin/lib/controle/ParcellaireManquantValidation.class.php
+++ project/plugins/acVinParcellaireManquantPlugin/lib/controle/ParcellaireManquantValidation.class.php
@@ -9,6 +9,7 @@ class ParcellaireManquantValidation extends DocumentValidation {
 
     public function configure()
     {
+        $this->addControle(self::TYPE_WARNING, 'pourcentage_inf_20', "Le pourcentage de pied manquant est inférieur à 20%");
         $this->addControle(self::TYPE_ERROR, 'pourcentage_nul', "Le pourcentage de pied manquant ne peut pas être nul ou à zéro");
     }
 
@@ -18,6 +19,10 @@ class ParcellaireManquantValidation extends DocumentValidation {
             if(!$parcelle->pourcentage || $parcelle->pourcentage === 0){
                 $this->addPoint(self::TYPE_ERROR, 'pourcentage_nul', "Parcelle n° {$parcelle->section} {$parcelle->numero_parcelle} - Le pourcentage de pied mort n'est pas conforme", $this->generateUrl('parcellairemanquant_manquants', $this->document));
                 continue;
+            }
+
+            if ($parcelle->pourcentage < 20) {
+                $this->addPoint(self::TYPE_WARNING, 'pourcentage_inf_20', "Parcelle n° {$parcelle->section} {$parcelle->numero_parcelle} - Le pourcentage de pied mort est de {$parcelle->pourcentage}%", $this->generateUrl('parcellairemanquant_manquants', $this->document));
             }
         }
     }
