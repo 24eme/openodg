544aa197cd3aa26fa92b97bdfb028fb47c40c40d
Tangui Morlier <tangui@tangui.eu.org>
2023-10-10 09:13:46 +0200

    Region des différents index

--- project/web/pouilly_dev.php
+++ project/web/pouilly_dev.php
@@ -1,7 +1,8 @@
 <?php
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 $configuration = ProjectConfiguration::getApplicationConfiguration('centre', 'dev', true);
-sfConfig::set('app_region', 'QUINCY');
+sfConfig::set('app_region', 'POUILLY');
+session_name('POUILLY');
 
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it or make something more sophisticated.
