17525ba688d5590723aba7412855602eaf88d622
Gabriel Poma <gpoma@24eme.fr>
2023-10-10 10:29:50 +0200

    centre: manquant, check si pourcentage nul ou a 0

--- project/plugins/acVinParcellaireManquantPlugin/lib/controle/ParcellaireManquantValidation.class.php
+++ project/plugins/acVinParcellaireManquantPlugin/lib/controle/ParcellaireManquantValidation.class.php
@@ -7,11 +7,18 @@ class ParcellaireManquantValidation extends DocumentValidation {
         parent::__construct($document, $options);
     }
 
-    public function configure() {
-
+    public function configure()
+    {
+        $this->addControle(self::TYPE_ERROR, 'pourcentage_nul', "Le pourcentage de pied manquant ne peut pas être nul ou à zéro");
     }
 
-    public function controle() {
-
+    public function controle()
+    {
+        foreach ($this->document->getParcelles() as $parcelle) {
+            if(!$parcelle->pourcentage || $parcelle->pourcentage === 0){
+                $this->addPoint(self::TYPE_ERROR, 'pourcentage_nul', "Parcelle n° {$parcelle->section} {$parcelle->numero_parcelle} - Le pourcentage de pied mort n'est pas conforme", $this->generateUrl('parcellairemanquant_manquants', $this->document));
+                continue;
+            }
+        }
     }
 }
