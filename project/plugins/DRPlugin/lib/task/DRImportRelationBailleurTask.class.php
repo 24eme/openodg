deb9ba2d608a9b3128a415122d8854c751a36725
tale-fau <lefaucheurtanguy@gmail.com>
2023-10-17 18:37:19 +0200

    Ajout relations cooperative et negociant vinificateur

--- project/plugins/DRPlugin/lib/task/DRImportRelationBailleurTask.class.php
+++ project/plugins/DRPlugin/lib/task/DRImportRelationBailleurTask.class.php
@@ -27,10 +27,38 @@ EOF;
         $databaseManager = new sfDatabaseManager($this->configuration);
         $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
 
-        $dr = DRClient::getInstance()->find($arguments['doc_id']);
-        $etablissement_source = $dr->getEtablissementObject();
-        $etablissement_dst = $dr->getBailleurs();
-        foreach ($etablissement_dst as $etablissement) {
+        $declaration_production = DRClient::getInstance()->find($arguments['doc_id']);
+        $etablissement_source = $declaration_production->getEtablissementObject();
+        $etablissements_bailleur = $declaration_production->getBailleurs();
+        $tiers = array();
+        if ($etablissements_bailleur) {
+            $this->getBailleurRelation($etablissement_source, $etablissements_bailleur);
+        }
+        if ($declaration_production->isApporteur()) {
+            $tiers = $declaration_production->getTiers(false, "NEGOCIANT_VINIFICATEUR");
+            $this->setTiersRelation($etablissement_source, $tiers, "NEGOCIANT_VINIFICATEUR");
+        }
+        if ($declaration_production->hasApporteurs()) {
+            $tiers = $declaration_production->getApporteurs(true);
+            if ($declaration_production["type"] === "SV11") {
+                $this->setTiersRelation($etablissement_source, $tiers, "COOPERATEUR");
+            }
+            if ($declaration_production["type"] === "SV12") {
+                $this->setTiersRelation($etablissement_source, $tiers, "APPORTEUR_RAISIN");
+            }
+        }
+    }
+
+    public function setTiersRelation($etablissement_source, $tiers, $relation) {
+        foreach ($tiers as $etablissement) {
+            $etablissement_source->addLiaison($relation, EtablissementClient::getInstance()->find($etablissement['etablissement']->_id), true);
+        }
+        $etablissement_source->save();
+    }
+
+
+    public function getBailleurRelation($etablissement_source, $etablissements_bailleur) {
+        foreach ($etablissements_bailleur as $etablissement) {
             $etablissement_id = $etablissement['etablissement_id'];
             if ($etablissement_id == null) {
                 $etab = EtablissementClient::getInstance()->find(EtablissementClient::getInstance()->findByRaisonSociale($etablissement['raison_sociale']));
