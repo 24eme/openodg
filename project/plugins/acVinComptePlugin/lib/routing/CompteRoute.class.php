28d811ff93f22b9bfd2b0007984a8960295e0d86
Tangui Morlier <tangui@tangui.eu.org>
2023-10-18 12:09:57 +0200

    Accès aux factures que pour les opérateurs habilités, accès à la fiche établissement pour tous, restriction pour les PDF de dégustation

--- project/plugins/acVinComptePlugin/lib/routing/CompteRoute.class.php
+++ project/plugins/acVinComptePlugin/lib/routing/CompteRoute.class.php
@@ -3,13 +3,26 @@
 class CompteRoute extends sfObjectRoute implements InterfaceCompteRoute {
 
     protected $compte = null;
+    protected $accesses = array();
 
     protected function getObjectForParameters($parameters = null) {
       $this->compte = CompteClient::getInstance()->find(CompteClient::getInstance()->getId($parameters['identifiant']));
 
       $myUser = sfContext::getInstance()->getUser();
-      if ($myUser->isAdmin()) {
+      if ($myUser->isAdmin() || (isset($this->accesses['allow_admin_odg']) && $this->accesses['allow_admin_odg'] && $myUser->isAdminODG())) {
           return $this->compte;
+      }
+      if ($myUser->isAdminODG() && $this->getSociete()) {
+          $compteUser = $myUser->getCompte();
+          $region = $compteUser->region;
+          if ($region) {
+              foreach($this->getSociete()->getEtablissementsObj() as $e) {
+                  if (HabilitationClient::getInstance()->isRegionInHabilitation($e->identifiant, $region)) {
+                      return $this->compte;
+                  }
+              }
+          }
+          throw new sfError403Exception("Vous n'avez pas le droit d'accéder à cette page (region)");
       }
       if ($myUser->hasTeledeclaration() && !$myUser->hasDrevAdmin()
             && $myUser->getCompte()->identifiant != $this->getCompte()->getSociete()->getMasterCompte()->identifiant)
@@ -36,7 +49,10 @@ class CompteRoute extends sfObjectRoute implements InterfaceCompteRoute {
       return $this->societe;
     }
 
-    public function getCompte() {
+    public function getCompte($parameters = null) {
+      if (isset($parameters['allow_admin_odg'])){
+          $this->accesses['allow_admin_odg'] = $parameters['allow_admin_odg'];
+      }
       if (!$this->compte) {
            $this->compte = $this->getObject();
       }
