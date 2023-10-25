3b36e57db7cd2a686160559f95bfa4308f868798
Vincent LAURENT <vince.laurent@gmail.com>
2023-10-25 12:54:52 +0200

    pas reuilly mais oivc

--- project/web/oivc.php
+++ project/web/oivc.php
@@ -4,6 +4,6 @@ require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('oivc', 'prod', false);
 session_name('OIVC');
-sfConfig::set('app_region', 'REUILLY');
+sfConfig::set('app_region', 'OIVC');
 
 sfContext::createInstance($configuration)->dispatch();
