4bc295b9109196d64e5190029d62840b4b09ae09
Tangui Morlier <tangui@tangui.eu.org>
2023-11-02 15:07:15 +0100

    Refactor multidomains

--- project/plugins/acCASPlugin/lib/acCAS.class.php
+++ project/plugins/acCASPlugin/lib/acCAS.class.php
@@ -13,28 +13,33 @@ class acCAS extends phpCAS {
     }
 
     private static function initCasInfo() {
-      if (isset($_SESSION['app_cas_domain'])) {
-        return ;
+      $postfix = '';
+      if (isset($_GET['cas_postfix'])) {
+        $postfix = $_GET['cas_postfix']);
       }
-      $_SESSION['app_cas_domain'] = sfConfig::get('app_cas_domain');
-      $_SESSION['app_cas_port'] = sfConfig::get('app_cas_port');
-      $_SESSION['app_cas_path'] = sfConfig::get('app_cas_path');
-      $_SESSION['app_cas_url'] = sfConfig::get('app_cas_url');
+      if (isset($_GET['ticket'])) {
+        $postfix = preg_replace('/.*-/', '', $_GET['ticket']);
+      }
+      if (!$postfix && isset($_SESSION['app_cas_origin'])) {
+          $postfix = $_SESSION['app_cas_origin'];
+      }
+      $multidomains = sfConfig::get('app_cas_multidomains', array());
+      if ($postfix && count($multidomains) && isset($multidomains[$postfix])) {
+        $_SESSION['app_cas_domain'] = $multidomains[$postfix]['domain'];
+        $_SESSION['app_cas_port'] = $multidomains[$postfix]['port'];
+        $_SESSION['app_cas_path'] = $multidomains[$postfix]['path'];
+        $_SESSION['app_cas_url'] = $multidomains[$postfix]['url'];
+        $_SESSION['app_cas_origin'] = $postfix;
+      }else{
+        $_SESSION['app_cas_domain'] = sfConfig::get('app_cas_domain');
+        $_SESSION['app_cas_port'] = sfConfig::get('app_cas_port');
+        $_SESSION['app_cas_path'] = sfConfig::get('app_cas_path');
+        $_SESSION['app_cas_url'] = sfConfig::get('app_cas_url');
+      }
     }
 
     public static function processAuth() {
         self::initCasInfo();
-        $multidomains = sfConfig::get('app_cas_multidomains', array());
-        if (!isset($multidomains['viticonnect'])) {
-            $multidomains['viticonnect'] = array( 'domain' => 'viticonnect.net', 'port' => '443', 'path' => 'cas', 'url' => 'https://viticonnect.net/cas' );
-        }
-        if (isset($_GET['ticket']) && count($multidomains) && ($postfix = preg_replace('/.*-/', '', $_GET['ticket'])) && isset($multidomains[$postfix])) {
-          $_SESSION['app_cas_domain'] = $multidomains[$postfix]['domain'];
-          $_SESSION['app_cas_port'] = $multidomains[$postfix]['port'];
-          $_SESSION['app_cas_path'] = $multidomains[$postfix]['path'];
-          $_SESSION['app_cas_url'] = $multidomains[$postfix]['url'];
-          $_SESSION['app_cas_origin'] = $postfix;
-        }
         //phpCAS::setDebug('/tmp/cas.log');
         @acCAS::client(CAS_VERSION_2_0, $_SESSION['app_cas_domain'], $_SESSION['app_cas_port'], $_SESSION['app_cas_path'], false);
         @acCAS::setNoCasServerValidation();
