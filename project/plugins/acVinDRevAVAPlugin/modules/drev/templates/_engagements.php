ffdf2b4efb7df35efcce460677c5511c4b9102f2
Vincent LAURENT <vince.laurent@gmail.com>
2023-11-06 13:09:07 +0100

    Affichage de la déclaration de production dans les documents

--- project/plugins/acVinDRevAVAPlugin/modules/drev/templates/_engagements.php
+++ project/plugins/acVinDRevAVAPlugin/modules/drev/templates/_engagements.php
@@ -2,19 +2,19 @@
 
 <div class="alert" role="alert" id="engagements">
     <div class="form-group">
-   
+
         <div class="alert alert-danger <?php if(!$form->hasErrors()): ?>hidden<?php endif; ?>" role="alert">
     	    <ul class="error_list">
     			<li class="text-left">Vous devez cocher pour valider votre déclaration.</li>
     		</ul>
     	</div>
-        
+
         <?php foreach ($validation->getEngagements() as $engagement): ?>
         <div class="checkbox-container <?php if ($form['engagement_' . $engagement->getCode()]->hasError()): ?>has-error<?php endif; ?>">
-            <div class="checkbox<?php if($engagement->getCode() == DRevDocuments::DOC_DR && $drev->hasDR()): ?> disabled<?php endif; ?>">
+            <div class="checkbox<?php if(in_array($engagement->getCode(), [DRevDocuments::DOC_DR, DRevDocuments::DOC_SV]) && $drev->hasDROrSV()): ?> disabled<?php endif; ?>">
                 <label>
-                	<?php 
-                		if ($engagement->getCode() == DRevDocuments::DOC_DR && $drev->hasDR()) {
+                	<?php
+                		if (in_array($engagement->getCode(), [DRevDocuments::DOC_DR, DRevDocuments::DOC_SV]) && $drev->hasDROrSV()) {
                 			echo $form['engagement_' . $engagement->getCode()]->render(array('checked' => 'checked'));
                 		} else {
                 			echo $form['engagement_' . $engagement->getCode()]->render();
@@ -22,6 +22,7 @@
                 	?>
                     <?php echo $engagement->getRawValue()->getMessage() ?>
                     <?php if ($engagement->getCode() == DRevDocuments::DOC_DR && $drev->hasDR()): ?>- <a href="<?php echo $drev->getAttachmentUri('DR.pdf'); ?>" target="_blank"><small>Télécharger ma DR</small></a><?php endif; ?>
+                    <?php if ($engagement->getCode() == DRevDocuments::DOC_SV && $drev->hasSV()): ?>- <a href="<?php echo $drev->getAttachmentUri('SV.pdf'); ?>" target="_blank"><small>Télécharger mon document de production</small></a><?php endif; ?>
                 </label>
             </div>
             </div>
