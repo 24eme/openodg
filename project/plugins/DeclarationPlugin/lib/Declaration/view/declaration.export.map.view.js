f598077d900a7347d006c77e4f7e29779c6b3335
tale-fau <tale-fau@24eme.fr>
2023-11-17 14:53:15 +0100

    Ajout de l'export de PMC et de PMCNC

--- project/plugins/DeclarationPlugin/lib/Declaration/view/declaration.export.map.view.js
+++ project/plugins/DeclarationPlugin/lib/Declaration/view/declaration.export.map.view.js
@@ -1,5 +1,5 @@
 function(doc) {
-       if(doc.type != "DRev" && doc.type != "RegistreVCI" && doc.type != "DRevMarc" && doc.type != "ParcellaireAffectation" && doc.type != "ParcellaireIntentionAffectation" && doc.type != "Tirage" && doc.type != "TravauxMarc" && doc.type != "ParcellaireIrrigable"  && doc.type != "ParcellaireIrrigue" && doc.type != "Habilitation" && doc.type != "DR" && doc.type != "SV12" && doc.type != "SV11" && doc.type != "Facture" && doc.type != "RegistreVCI" && doc.type != "Degustation" && doc.type != "Conditionnement" && doc.type != "Transaction" && doc.type != "ChgtDenom" && doc.type != "Constats") {
+       if(doc.type != "DRev" && doc.type != "RegistreVCI" && doc.type != "DRevMarc" && doc.type != "ParcellaireAffectation" && doc.type != "ParcellaireIntentionAffectation" && doc.type != "Tirage" && doc.type != "TravauxMarc" && doc.type != "ParcellaireIrrigable"  && doc.type != "ParcellaireIrrigue" && doc.type != "Habilitation" && doc.type != "DR" && doc.type != "SV12" && doc.type != "SV11" && doc.type != "Facture" && doc.type != "RegistreVCI" && doc.type != "Degustation" && doc.type != "Conditionnement" && doc.type != "Transaction" && doc.type != "ChgtDenom" && doc.type != "Constats" && doc.type != "PMC" && doc.type != "PMCNC") {
            return;
        }
        if(doc.lecture_seule) {
