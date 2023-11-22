00716a0194f70222c0bacbf87d99e7d0bd03a46e
Vincent LAURENT <vince.laurent@gmail.com>
2023-11-22 15:17:14 +0100

    Plus d'espace

--- project/apps/oivc/modules/degustation/templates/_footerCourrier.php
+++ project/apps/oivc/modules/degustation/templates/_footerCourrier.php
@@ -3,4 +3,4 @@
 <p></p>
 <p></p>
 
-<table style="width:1100px; padding-left:400px;" ><tr><td><?php echo nl2br(str_replace(",", "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", Organisme::getInstance(null, 'degustation')->getResponsable())) ?></td></tr></table>
+<table style="width:1100px; padding-left:400px;" ><tr><td><?php echo nl2br(str_replace(",", "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", Organisme::getInstance(null, 'degustation')->getResponsable())) ?></td></tr></table>
