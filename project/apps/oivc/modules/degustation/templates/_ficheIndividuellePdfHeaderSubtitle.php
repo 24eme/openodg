977afd09a3875741aaab0a0a35926e38d408c460
Jb Lm <lemetayer.jb@gmail.com>
2023-11-28 15:12:50 +0100

    update header subtitle synthese organoleptique

--- project/apps/oivc/modules/degustation/templates/_ficheIndividuellePdfHeaderSubtitle.php
+++ project/apps/oivc/modules/degustation/templates/_ficheIndividuellePdfHeaderSubtitle.php
@@ -1,3 +1,3 @@
-Annexe PI01.1 Fiche de contrôle organoleptique / 0717 / REV D
+Annexe PI01.1 Fiche de contrôle organoleptique / 1123 / REV E
 
 Contrôle organoleptique du <?php echo $degustation->getDateFormat('d/m/Y')."\n".$degustation->lieu ?>
