0921ec04138c3698ed1438a4cbf66fbab3de34ed
Jb Lm <lemetayer.jb@gmail.com>
2023-11-28 15:08:42 +0100

    update header subtitle des listes echantillons ventiles

--- project/apps/oivc/modules/degustation/templates/_ficheTablesEchantillonsParDossierPdfHeaderSubtitle.php
+++ project/apps/oivc/modules/degustation/templates/_ficheTablesEchantillonsParDossierPdfHeaderSubtitle.php
@@ -1,3 +1,3 @@
-Annexe PI01.6 Fiche des échantillons ventilés / 1216 / REV A
+Annexe PI01.6 Fiche des échantillons ventilés / 1123 / REV B
 
 Contrôle organoleptique du <?php echo $degustation->getDateFormat('d/m/Y')."\n".$degustation->lieu ?>
