e14f2a41b96a84cdd6743945e18db4a105a339de
Jb Lm <lemetayer.jb@gmail.com>
2023-12-04 15:19:26 +0100

    sauvegarde du formulaire d'affectation des heures de prelevement

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/organisationEtapeSuccess.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/organisationEtapeSuccess.php
@@ -71,7 +71,26 @@ echo $form->renderGlobalErrors();
     </div>
     </div>
 
+    <div class="row row-margin row-button mb-4">
+        <div class="col-xs-4"></div>
+        <div class="col-xs-4 text-center"></div>
+        <div class="col-xs-4 text-right">
+            <button type="submit" class="btn btn-xs btn-primary">
+                Enregistrer
+            </button>
+        </div>
+    </div>
 
-    <?php include_partial('degustation/pagination', array('degustation' => $degustation, 'active' => DegustationEtapes::ETAPE_ORGANISATION, 'is_enabled' => true)); ?>
+    <div class="row row-margin row-button">
+        <div class="col-xs-4"><a href="<?php echo url_for("degustation_selection_operateurs", $degustation) ?>" class="btn btn-default btn-upper"><span class="glyphicon glyphicon-chevron-left"></span> Retour</a></div>
+        <div class="col-xs-4 text-center">
+
+        </div>
+        <div class="col-xs-4 text-right">
+            <a id="btn_suivant" class="btn btn-primary btn-upper pull-right" href="<?php echo url_for('degustation_tournees_etape', $degustation); ?>">
+                Étape suivante&nbsp;<span class="glyphicon glyphicon-chevron-right"></span>
+            </a>
+        </div>
+    </div>
 
 </form>
