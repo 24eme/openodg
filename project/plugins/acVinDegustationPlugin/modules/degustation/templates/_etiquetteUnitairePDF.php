a72e783278c3048a0785c078c3eff18a7288b0ca
Vincent LAURENT <vince.laurent@gmail.com>
2023-12-13 17:33:09 +0100

    Pas de certification

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/_etiquetteUnitairePDF.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/_etiquetteUnitairePDF.php
@@ -16,7 +16,7 @@
       </tr>
       <tr>
         <td colspan="2" style="overflow-wrap:break-word;text-align: center; height: 28.5px; line-height: 14px; overflow: hidden; vertical-align: middle;"><strong>&nbsp;&nbsp;<?php echo truncate_text(strtoupper(
-            KeyInflector::unaccent(($lotInfo->lot->getConfig() ? $lotInfo->lot->getConfig()->getCertification()->libelle : $lotInfo->lot->details)." ".$lotInfo->lot->produit_libelle)
+            KeyInflector::unaccent(($lotInfo->lot->getConfig() ? null : $lotInfo->lot->details)." ".$lotInfo->lot->produit_libelle)
         ), 50, '…', 'middle') .' '.  $lotInfo->lot->millesime;  ?></strong>
         </td>
       </tr>
