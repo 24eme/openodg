701eae4d80c1eed4eec35a932b84f40577c82544
Tangui Morlier <tangui@tangui.eu.org>
2023-12-21 15:08:40 +0100

    Erreur d'arguments

--- project/bin/generate_export_dr_from_douane.sh
+++ project/bin/generate_export_dr_from_douane.sh
@@ -3,14 +3,14 @@
 cd $(dirname $0)/.. > /dev/null 2>&1
 
 # Mode multi app
-if ! test -f $(echo $0 | sed 's/[^\/]*$//')config.inc && ! test $1 ; then
+if ! test -f $(echo $0 | sed 's/[^\/]*$//')config.inc && ! test "$2" ; then
     ls . $(echo $0 | sed 's/[^\/]*$//') | grep "config_" | grep ".inc$" | sed 's/config_//' | sed 's/\.inc//' | while read app; do
         bash $(echo $0 | sed 's/[^\/]*$//')/generate_export_dr_from_douane.sh $1 $app;
     done
     exit 0;
 fi
 
-if ! test $2 ; then
+if ! test "$2" ; then
     . $(echo $0 | sed 's/[^\/]*$//')config.inc
     confname='config.inc'
 else
