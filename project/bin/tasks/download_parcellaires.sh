0ca058a1b1913a7f4e396af58248125d5ff46699
Gabriel Poma <gpoma@24eme.fr>
2024-01-11 17:26:35 +0100

    shell: maj script téléchargement parcellaire

--- project/bin/tasks/download_parcellaires.sh
+++ project/bin/tasks/download_parcellaires.sh
@@ -3,4 +3,13 @@
 # Title: Téléchargement des parcellaires
 # Description: Télécharge l'ensemble des parcellaires
 
-echo "Test"
+source "$(dirname $0)/../config.inc"
+
+bash ~/Code/prodouane_scrapy/bin/download_parcellaires.sh
+
+for csv in ~/Code/prodouane_scrapy/documents/parcellaire-*.csv; do
+    [ -e "$csv" ] || continue
+    cvi=$(basename "$csv" ".csv" | cut -d'-' -f2)
+    echo "Import du parcellaire du cvi $cvi"
+    php "$WORKINGDIR/symfony" $SYMFONYTASKOPTIONS import:parcellaire-douanier "$cvi"
+done
