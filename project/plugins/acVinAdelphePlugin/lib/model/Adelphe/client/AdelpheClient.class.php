5ed8257b86e80f9bbed9805a6919a5b3d1adfa80
Jb Lm <lemetayer.jb@gmail.com>
2024-01-16 17:57:47 +0100

    fct pour recuperation d'historique adelphe

--- project/plugins/acVinAdelphePlugin/lib/model/Adelphe/client/AdelpheClient.class.php
+++ project/plugins/acVinAdelphePlugin/lib/model/Adelphe/client/AdelpheClient.class.php
@@ -10,21 +10,32 @@ class AdelpheClient extends acCouchdbClient {
   }
 
   public function find($id, $hydrate = self::HYDRATE_DOCUMENT, $force_return_ls = false) {
-      $doc = parent::find($id, $hydrate, $force_return_ls);
-      if($doc && $doc->type != self::TYPE_MODEL) {
-          throw new sfException(sprintf("Document \"%s\" is not type of \"%s\"", $id, self::TYPE_MODEL));
-      }
-      return $doc;
+    $doc = parent::find($id, $hydrate, $force_return_ls);
+    if($doc && $doc->type != self::TYPE_MODEL) {
+      throw new sfException(sprintf("Document \"%s\" is not type of \"%s\"", $id, self::TYPE_MODEL));
     }
+    return $doc;
+  }
 
-    public function createDoc($identifiant, $periode, $papier = false)
-    {
-        $adelphe = new Adelphe();
-        $adelphe->initDoc($identifiant, $periode);
-        $adelphe->storeDeclarant();
-        if($papier) {
-            $adelphe->add('papier', 1);
-        }
-        return $adelphe;
+  public function createDoc($identifiant, $periode, $papier = false) {
+    $adelphe = new Adelphe();
+    $adelphe->initDoc($identifiant, $periode);
+    $adelphe->storeDeclarant();
+    if($papier) {
+        $adelphe->add('papier', 1);
     }
+    return $adelphe;
+  }
+
+  public function findMasterByIdentifiantAndPeriode($identifiant, $periode, $hydrate = acCouchdbClient::HYDRATE_DOCUMENT) {
+    return $this->findMasterByIdentifiantAndCampagne($identifiant, $periode.'-'.($periode + 1), $hydrate);
+  }
+
+  public function findMasterByIdentifiantAndCampagne($identifiant, $campagne, $hydrate = acCouchdbClient::HYDRATE_DOCUMENT) {
+    $docs = DeclarationClient::getInstance()->viewByIdentifiantCampagneAndType($identifiant, $campagne, self::TYPE_MODEL);
+    foreach ($docs as $id => $doc) {
+      return $this->find($id, $hydrate);
+    }
+    return null;
+  }
 }
