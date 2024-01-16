712cc89aaed8e029c1c4554ad8ced145d543977b
Jb Lm <lemetayer.jb@gmail.com>
2024-01-16 21:23:00 +0100

    ajout des declarations adelphe dans la vue

--- project/plugins/DeclarationPlugin/lib/Declaration/view/declaration.identifiant.map.view.js
+++ project/plugins/DeclarationPlugin/lib/Declaration/view/declaration.identifiant.map.view.js
@@ -1,6 +1,6 @@
 function(doc) {
 
-     if(doc.type != "DRev" && doc.type != "Conditionnement" && doc.type != "Transaction" && doc.type != "RegistreVCI" && doc.type != "DRevMarc" && doc.type != "ParcellaireAffectation" && doc.type != "Tirage" && doc.type != "TravauxMarc" && doc.type != "ParcellaireIrrigable") {
+     if(doc.type != "DRev" && doc.type != "Conditionnement" && doc.type != "Transaction" && doc.type != "RegistreVCI" && doc.type != "DRevMarc" && doc.type != "ParcellaireAffectation" && doc.type != "Tirage" && doc.type != "TravauxMarc" && doc.type != "ParcellaireIrrigable" && doc.type != "Adelphe") {
 
          return;
      }
