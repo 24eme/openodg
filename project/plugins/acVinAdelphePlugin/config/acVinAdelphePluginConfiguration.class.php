91e5fbc60fa158c1c6ce644543943208b9430390
Jb Lm <lemetayer.jb@gmail.com>
2024-01-19 16:29:03 +0100

    ajout d'une configuration pour adelphe

--- project/plugins/acVinAdelphePlugin/config/acVinAdelphePluginConfiguration.class.php
+++ project/plugins/acVinAdelphePlugin/config/acVinAdelphePluginConfiguration.class.php
@@ -2,7 +2,18 @@
 
 class acVinAdelphePluginConfiguration extends sfPluginConfiguration
 {
-  public function initialize() {
+    public function setup() {
+        if ($this->configuration instanceof sfApplicationConfiguration) {
+            $configCache = $this->configuration->getConfigCache();
+            $configCache->registerConfigHandler('config/adelphe.yml', 'sfDefineEnvironmentConfigHandler', array('prefix' => 'adelphe_'));
+            $configCache->checkConfig('config/adelphe.yml');
+        }
+    }
 
-  }
+    public function initialize() {
+        if ($this->configuration instanceof sfApplicationConfiguration) {
+            $configCache = $this->configuration->getConfigCache();
+            include($configCache->checkConfig('config/adelphe.yml'));
+        }
+    }
 }
