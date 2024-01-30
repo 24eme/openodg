93a813dbc67ad691edf7f56022daf6f2714f8ee3
Jb Lm <lemetayer.jb@gmail.com>
2024-01-30 14:33:37 +0100

    email de notification declaration adelphe

---
+++ project/plugins/EmailPlugin/modules/Email/templates/_send_adelphe_validation.php
@@ -0,0 +1,9 @@
+Bonjour,
+
+Votre déclaration Adelphe <?php echo $adelphe->getPeriode(); ?> a bien été transmise au syndicat via le site votre syndicat.
+
+Pour toute question, n'hésitez pas à contacter votre syndicat.
+
+Bonne journée.
+
+<?php echo include_partial('Email/footerMail'); ?>
