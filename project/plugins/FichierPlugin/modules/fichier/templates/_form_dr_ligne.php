91a8b08f8457c79de04ee500e13e3f9102e6f3c2
Tangui Morlier <tangui@tangui.eu.org>
2024-02-09 12:39:07 +0100

    Tiers plus large pour aider à la saisie

--- project/plugins/FichierPlugin/modules/fichier/templates/_form_dr_ligne.php
+++ project/plugins/FichierPlugin/modules/fichier/templates/_form_dr_ligne.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 	$options = array('required' => 'required', 'class' => 'form-control select2autocompleteAjax input-md', "autofocus" => "autofocus");
 ?>
 <div class="form-group drLigne">
 	<div class="col-xs-3">
 		<?php echo $form['produit']->render(array('class' => 'form-control input select2 select2-offscreen select2autocomplete liste-produits', 'placeholder' => "Produit")) ?>
 	</div>
-	<div class="col-xs-2">
+	<div class="col-xs-1">
 		<?php echo $form['complement']->render(array('class' => 'form-control input complement-produit', 'placeholder' => "Complément")) ?>
 	</div>
 	<div class="col-xs-2">
@@ -17,7 +17,7 @@
 	<div class="col-xs-1">
 		<?php echo $form['valeur']->render(array('class' => 'form-control input text-right', 'placeholder' => "Volume / Valeur")) ?>
 	</div>
-	<div class="col-xs-2">
+	<div class="col-xs-3">
 		<div class="col-xs-10" style="padding-right: 0px !important; padding-left: 0px !important;">
 			<?php echo $form['tiers']->render(array_merge($options, array('placeholder' => "Tiers"))) ?>
 		</div>
