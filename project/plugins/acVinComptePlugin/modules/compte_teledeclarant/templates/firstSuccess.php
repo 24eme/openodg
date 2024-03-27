9abece3cd6edf96929bbee8ac3cc8198ee4066c1
Vincent LAURENT <vince.laurent@gmail.com>
2024-03-27 12:04:43 +0100

    Mise en forme du processus de création de compte

--- project/plugins/acVinComptePlugin/modules/compte_teledeclarant/templates/firstSuccess.php
+++ project/plugins/acVinComptePlugin/modules/compte_teledeclarant/templates/firstSuccess.php
@@ -1,37 +1,35 @@
-<div id="principal" class="clearfix" style="margin-top: 20px;">
-    <form action="" method="post" class="form-horizontal" name ="firstConnection">
-        <div class="row">
-            <div class="col-xs-8 col-xs-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h2 style="margin-top: 10px;">Première connexion</h2>
-                    </div>
-                    <div class="panel-body">
-                        <p>Afin de créer de votre compte pour la <strong>plateforme ODG</strong>, veuillez remplir les champs suivants :</p>
-                        <div id="nouvelle_declaration" style="margin-top: 30px;">
-                            <?php echo $form->renderHiddenFields(); ?>
-                            <?php echo $form->renderGlobalErrors(); ?>
-                            <div class="form-group">
-                                <?php echo $form['login']->renderError() ?>
-                                <?php echo $form['login']->renderLabel(null, ['class' => 'control-label col-sm-5']) ?>
-                                <div class="col-sm-4">
-                                    <?php echo $form['login']->render() ?>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <?php echo $form['mdp']->renderError() ?>
-                                <?php echo $form['mdp']->renderLabel(null, ['class' => 'control-label col-sm-5']) ?>
-                                <div class="col-sm-4">
-                                    <?php echo $form['mdp']->render() ?>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="text-right">
-                            <button class="btn btn-success" type="submit">Valider</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<form action="" method="post" class="form-horizontal" name="firstConnection">
+  <div class="row">
+    <div class="col-xs-10 col-xs-offset-1">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h4 class="titre_principal">Premiere connexion</h4>
         </div>
-    </form>
-</div>
+        <div class="panel-body">
+          <p style="margin-bottom: 30px;">Afin d'accèder à la plateforme de télédéclaration, veuillez remplir les champs suivants :</p>
+
+          <?php echo $form->renderHiddenFields(); ?>
+          <?php echo $form->renderGlobalErrors(); ?>
+
+          <?php echo $form['login']->renderError() ?>
+          <div class="form-group">
+            <?php echo $form['login']->renderLabel(null, ['class' => 'col-sm-4 control-label']) ?>
+            <div class="col-sm-4">
+              <?php echo $form['login']->render(['autofocus' => 'autofocus']) ?>
+            </div>
+          </div>
+          <?php echo $form['mdp']->renderError() ?>
+          <div class="form-group">
+            <?php echo $form['mdp']->renderLabel(null, ['class' => 'col-sm-4 control-label']) ?>
+            <div class="col-sm-4">
+              <?php echo $form['mdp']->render() ?>
+            </div>
+          </div>
+          <div class="text-right" style="margin-top: 30px;">
+            <button class="btn btn-primary" type="submit">Valider</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</form>
