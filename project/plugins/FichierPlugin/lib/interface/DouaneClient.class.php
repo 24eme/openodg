db8a62148a77dbf156e7b6d9c6d5c6db1459f9f7
Tangui Morlier <tangui@tangui.eu.org>
2024-03-27 15:00:32 +0100

    getDocumentsDouaniers doit au moins renvoyer la DR de la personne (cc @tale-fau)

--- project/plugins/FichierPlugin/lib/interface/DouaneClient.class.php
+++ project/plugins/FichierPlugin/lib/interface/DouaneClient.class.php
@@ -9,12 +9,8 @@ class DouaneClient extends acCouchdbClient
 
     public function getDocumentsDouaniers($identifiant, $periode, $hydrate = acCouchdbClient::HYDRATE_DOCUMENT) {
         $etablissements = EtablissementClient::getInstance()->findByIdentifiant($identifiant)->getMeAndLiaisonOfType(EtablissementClient::TYPE_LIAISON_METAYER);
-
         $fichiers = array();
         foreach($etablissements as $e) {
-            if ($e->identifiant == $identifiant) {
-                continue;
-            }
             $f = new DRev();
             $f = $f->getDocumentDouanierEtablissement(null, $periode, $e->identifiant, $hydrate);
             if ($f) {
