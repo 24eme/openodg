38f111c91f62a7f93a774c8ba079f25447de9fa6
Tangui Morlier <tangui@tangui.eu.org>
2024-04-19 13:23:43 +0200

    Document d'ordre du courrier est calculé

--- project/plugins/acVinCourrierPlugin/lib/model/Courrier/CourrierLot.class.php
+++ project/plugins/acVinCourrierPlugin/lib/model/Courrier/CourrierLot.class.php
@@ -8,14 +8,6 @@ class CourrierLot extends BaseCourrierLot
         return CourrierMouvementLots::freeInstance($this->getDocument());
     }
 
-    public function getDocumentOrdre() {
-        if ($this->id_document_provenance === null) {
-            throw new sfException('Doit avoir un document provenance');
-        }
-
-        return $this->_get('document_ordre');
-    }
-
     public function getDocumentType() {
 
         return CourrierClient::TYPE_MODEL;
