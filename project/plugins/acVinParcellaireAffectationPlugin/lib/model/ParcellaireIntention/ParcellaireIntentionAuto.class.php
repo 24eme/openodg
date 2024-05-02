d5edda3bf278cd1dc4878c094e8d32d5778bd5fd
Vincent LAURENT <vince.laurent@gmail.com>
2024-05-02 16:01:28 +0200

    Parcellaire qui fonctionne avec le Ventoux en mode Ventoux

--- project/plugins/acVinParcellaireAffectationPlugin/lib/model/ParcellaireIntention/ParcellaireIntentionAuto.class.php
+++ project/plugins/acVinParcellaireAffectationPlugin/lib/model/ParcellaireIntention/ParcellaireIntentionAuto.class.php
@@ -1,9 +1,16 @@
 <?php
 
 class ParcellaireIntentionAuto extends ParcellaireIntentionAffectation {
-
     public function save() {
         throw new sfException('Cannont save ParcellaireIntentionAuto');
+    }
+
+    public function updateParcelles() {
+        $this->addParcellesFromParcellaire(["DEFAUT"]);
+    }
+
+    public function getDenominationAire() {
+        return "Ventoux";
     }
 
 }
