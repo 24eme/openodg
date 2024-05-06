8b19e8320aefc7c2d5a302457c9d5f8fac1a2d4e
Vincent LAURENT <vince.laurent@gmail.com>
2024-05-06 09:59:04 +0200

    Pareille pour la date de demande

--- project/lib/task/importVentoux/importOperateursHabilitationsVentouxCsvTask.class.php
+++ project/lib/task/importVentoux/importOperateursHabilitationsVentouxCsvTask.class.php
@@ -163,7 +163,7 @@ EOF;
     private function importHabilitation($etablissement, $data)
     {
         $identifiant = $etablissement->identifiant;
-        $date_demande  = ($data[self::CSV_DATE_SAISIE_IDENTIFICATION]) ? DateTime::createFromFormat('d/m/Y', $data[self::CSV_DATE_SAISIE_IDENTIFICATION])->format('Y-m-d') : null;
+        $date_demande  = ($data[self::CSV_DATE_SAISIE_IDENTIFICATION]) ? DateTime::createFromFormat('d/m/Y', explode(" ", $data[self::CSV_DATE_SAISIE_IDENTIFICATION])[0])->format('Y-m-d') : null;
         $date_decision = ($data[self::CSV_DATE_HABILITATION]) ? DateTime::createFromFormat('d/m/Y', explode(" ", $data[self::CSV_DATE_HABILITATION])[0])->format('Y-m-d') : null;
 
         $habilitation = HabilitationClient::getInstance()->createOrGetDocFromIdentifiantAndDate($identifiant, $date_demande);
