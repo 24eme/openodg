b444a84620edfaba2b13fa543279d9bce5e6b217
Vincent LAURENT <vince.laurent@gmail.com>
2024-05-21 10:43:32 +0200

    Campagne 2023 pour l'import des documents de production

--- project/bin/import_ventoux.sh
+++ project/bin/import_ventoux.sh
@@ -107,7 +107,7 @@ php symfony import:parcellaireaffectation-ventoux --application="$ODG" $DATA_DIR
 
 echo "Mise a jour des relations en fonction des documents de production"
 
-curl -s http://$COUCHHOST:$COUCHPORT/$COUCHBASE/_design/declaration/_view/tous\?reduce\=false | cut -d '"' -f 4 | grep 'DR-\|SV11-\|SV12-' | grep '\-2022' | while read id; do php symfony production:import-relation $id --application=centre; done
+curl -s http://$COUCHHOST:$COUCHPORT/$COUCHBASE/_design/declaration/_view/tous\?reduce\=false | cut -d '"' -f 4 | grep 'DR-\|SV11-\|SV12-' | grep '\-2023' | while read id; do php symfony production:import-relation $id --application="$ODG"; done
 
 echo "Mise à jour des tags de compte"
 
