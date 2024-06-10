6a1c81e046258562402b4f1da0340f6797549cc2
Vincent LAURENT <vince.laurent@gmail.com>
2024-06-10 11:40:54 +0200

    Mise à jour des apporteurs sur le 1er onglet de l'affectation coop

--- project/plugins/acVinParcellaireAffectationPlugin/lib/model/ParcellaireAffectationCoop/client/ParcellaireAffectationCoopClient.class.php
+++ project/plugins/acVinParcellaireAffectationPlugin/lib/model/ParcellaireAffectationCoop/client/ParcellaireAffectationCoopClient.class.php
@@ -31,8 +31,11 @@ class ParcellaireAffectationCoopClient extends acCouchdbClient {
         $parcellaireAffectationCoop->initDoc($identifiant, $periode, $type);
 
         $sv11 = SV11Client::getInstance()->find("SV11-".$identifiant."-".($periode - 1));
+        if (!$sv11) {
+            throw new sfException('SV11 misssing '."SV11-".$identifiant."-".($periode - 1));
+        }
 
-        $parcellaireAffectationCoop->buildApporteurs($sv11);
+        $parcellaireAffectationCoop->buildApporteurs();
 
         return $parcellaireAffectationCoop;
     }
