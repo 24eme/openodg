175da1fcee7e758ef6d661d2482e7b3d56fd141f
Vincent LAURENT <vince.laurent@gmail.com>
2024-06-18 11:13:29 +0200

    On lance le script depuis le working dir

--- project/bin/tasks/reconduction_affectable.sh
+++ project/bin/tasks/reconduction_affectable.sh
@@ -3,6 +3,8 @@
 # Title: Reconduction des affectations
 # Description: Reconduit les affectations parcellaires depuis la déclaration de l'année précédente
 
-CAMPAGNE="$(date +%Y -d'1 year ago')"
+source "$(dirname $0)/../config.inc"
 
+CAMPAGNE="$(date +%Y -d'1 year ago')"
+cd $WORKINGDIR
 bash bin/copy_declarations_to_next_campagne.sh PARCELLAIREAFFECTATION "$CAMPAGNE"
