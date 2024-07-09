cb925666b3b80c4dc32e02080b8fe58ea0a92ebc
chachathecool <charlene.connorshg@gmail.com>
2024-07-09 15:37:04 +0200

    Pour coop -> déclaration des apporteurs : modification libellé "irrigation" par "irrigable"

--- project/plugins/acVinParcellaireAffectationPlugin/modules/parcellaireAffectationCoop/templates/listeSuccess.php
+++ project/plugins/acVinParcellaireAffectationPlugin/modules/parcellaireAffectationCoop/templates/listeSuccess.php
@@ -17,7 +17,7 @@
             <th>Nom</th>
             <th class="col-xs-2 text-center">Affectation</th>
             <th class="col-xs-2 text-center">Manquant</th>
-            <th class="col-xs-2 text-center">Irrigation</th>
+            <th class="col-xs-2 text-center">Irrigable</th>
         </tr>
     <?php foreach ($parcellaireAffectationCoop->getApporteursChoisis() as $apporteur): ?>
         <tr class="hamzastyle-item <?php if($apporteur->getDeclarationStatut("ParcellaireAffectation") == ParcellaireAffectationCoopApporteur::STATUT_NON_IDENTIFIEE): ?>text-muted<?php endif; ?>" data-words='<?php echo json_encode(array($apporteur->nom, $apporteur->cvi), JSON_HEX_APOS | JSON_HEX_QUOT | JSON_UNESCAPED_UNICODE); ?>' >
