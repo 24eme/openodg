1b5ac70defbdc986f1e843f646aa2fa02e5e3dd6
Tangui Morlier <tangui@tangui.eu.org>
2024-07-10 16:36:23 +0200

    source du libelle

--- project/plugins/acVinParcellairePlugin/lib/model/Parcellaire/client/ParcellaireClient.class.php
+++ project/plugins/acVinParcellairePlugin/lib/model/Parcellaire/client/ParcellaireClient.class.php
@@ -419,8 +419,8 @@ class ParcellaireClient extends acCouchdbClient {
                 $p1->produit_hash = $p2->getParent()->getParent()->getHash();
             }
         }
-        if ($p1->exist('source_produit_libelle') && $p2->exist('source_produit_libelle') && $p2->source_produit_libelle) {
-            $p1->source_produit_libelle = $p2->source_produit_libelle;
+        if ($p2->exist('source_produit_libelle') && $p2->source_produit_libelle) {
+            $p1->add('source_produit_libelle', $p2->source_produit_libelle);
         }
         return $p1;
 
