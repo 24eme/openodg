748ed29895ffd22ada2c5fe3e9b7bfb3596c17b7
Gabriel Poma <gpoma@24eme.fr>
2024-07-16 15:18:59 +0200

    margin top sur la div des flashs

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/preleveSuccess.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/preleveSuccess.php
@@ -12,8 +12,6 @@
   <h3><?php echo ucfirst(format_date($degustation->date, "P", "fr_FR"))." à ".format_date($degustation->date, "H")."h".format_date($degustation->date, "mm") ?> <small><?php echo $degustation->getLieuNom(); ?></small></h3>
 </div>
 
-<?php include_partial('global/flash'); ?>
-
 <?php include_partial('degustation/synthese', array('degustation' => $degustation, 'infosDegustation' => $infosDegustation)); ?>
 
 <div class="row">
