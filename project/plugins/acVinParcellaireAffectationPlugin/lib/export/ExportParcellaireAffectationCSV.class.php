80b9c0d9f4c11b1bedd965d88c75013f057f3da6
Gabriel Poma <gpoma@24eme.fr>
2024-08-29 15:59:17 +0200

    on rajoute la destination dans l'export ParcellaireAffectation

--- project/plugins/acVinParcellaireAffectationPlugin/lib/export/ExportParcellaireAffectationCSV.class.php
+++ project/plugins/acVinParcellaireAffectationPlugin/lib/export/ExportParcellaireAffectationCSV.class.php
@@ -8,7 +8,7 @@ class ExportParcellaireAffectationCSV implements InterfaceDeclarationExportCsv {
 
     public static function getHeaderCsv() {
 
-        return "Campagne;Identifiant Société;Identifiant Opérateur;CVI Opérateur;Siret Opérateur;Nom Opérateur;Adresse Opérateur;Code postal Opérateur;Commune Opérateur;Email;Type de déclaration;Certification;Genre;Appellation;Mention;Lieu;Produit;IDU;Code commune;Commune;Lieu-dit;Section;Numéro parcelle;Cépage;Année de plantation;Surface;Dénomination complémentaire;Surface identifiée;Signataire;Date de validation;Type de declaration;Pseudo production id;doc id\n";
+        return "Campagne;Identifiant Société;Identifiant Opérateur;CVI Opérateur;Siret Opérateur;Nom Opérateur;Adresse Opérateur;Code postal Opérateur;Commune Opérateur;Email;Type de déclaration;Certification;Genre;Appellation;Mention;Lieu;Produit;IDU;Code commune;Commune;Lieu-dit;Section;Numéro parcelle;Cépage;Année de plantation;Surface;Dénomination complémentaire;Surface identifiée;Destination identifiant;Destination nom;Destination CVI;Destination superficie;Signataire;Date de validation;Type de declaration;Pseudo production id;doc id\n";
     }
 
     public function __construct($doc, $header = true, $region = null) {
@@ -43,9 +43,22 @@ class ExportParcellaireAffectationCSV implements InterfaceDeclarationExportCsv {
         }
         $ligne_base = sprintf("%s;\"%s\";\"%s\";\"%s\";\"%s\";\"%s\";\"%s\";\"%s\";\"%s\";\"%s\"", $this->doc->campagne, $this->doc->getEtablissementObject()->getSociete()->identifiant, $this->doc->identifiant, $this->doc->declarant->cvi, $this->doc->declarant->siret, $this->protectStr($this->doc->declarant->raison_sociale), $this->protectStr($this->doc->declarant->adresse), $this->doc->declarant->code_postal, $this->protectStr($this->doc->declarant->commune), $this->doc->declarant->email);
         foreach ($this->doc->declaration->getParcelles() as $parcelle) {
-                if (!$parcelle->affectee) {
-                    continue;
-                }
+            if (!$parcelle->affectee) {
+                continue;
+            }
+
+            if (! $parcelle->exist('destinations')) {
+                $destination = new stdclass();
+                $destination->identifiant = null;
+                $destination->nom = null;
+                $destination->cvi = null;
+                $destination->superficie = null;
+                $destinations = [$destination];
+            } else {
+                $destinations = $parcelle->destinations;
+            }
+
+            foreach ($destinations as $destination) {
             	$configProduit = $parcelle->getProduit()->getConfig();
 
             	$certification = $configProduit->getCertification()->getKey();
@@ -55,7 +68,7 @@ class ExportParcellaireAffectationCSV implements InterfaceDeclarationExportCsv {
             	$lieu = $configProduit->getLieu()->getKey();
 
             	$libelle_complet = $this->protectStr(trim($parcelle->getProduit()->getLibelle()));
-            	$csv .= sprintf("%s;Parcellaire Affectation;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", $ligne_base,
+                $csv .= sprintf("%s;Parcellaire Affectation;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", $ligne_base,
             	$certification,$genre,$appellation,$mention,$lieu,$libelle_complet,
             	$this->protectStr($parcelle->idu),
             	$parcelle->code_commune,
@@ -68,12 +81,17 @@ class ExportParcellaireAffectationCSV implements InterfaceDeclarationExportCsv {
             	$this->formatFloat($parcelle->getSuperficieParcellaire()),
                 $this->protectStr(strtoupper($parcelle->getDgc())),
             	$this->formatFloat($parcelle->superficie),
+                $destination->identifiant,
+                $this->protectStr($destination->nom),
+                $destination->cvi,
+                $this->formatFloat($destination->superficie),
                 $this->doc->exist('signataire') ? $this->protectStr($this->doc->signataire) : null,
             	$this->doc->validation,
             	$mode,
                 str_replace(explode('-', $this->doc->_id)[0].'-', '', $this->doc->_id),
                 $this->doc->_id
                 );
+            }
         }
 
         return $csv;
