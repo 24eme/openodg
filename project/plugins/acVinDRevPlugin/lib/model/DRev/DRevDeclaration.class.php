a4a744bee3580e0cf93db981ea0ba572d26a08d5
Vincent LAURENT <vince.laurent@gmail.com>
2024-11-25 17:52:05 +0100

    Revert "Si la DRev n'a pas de region et n'est pas validé alors on n'active pas"
    This reverts commit 31eddc26e488d63b60c8633701e5af3d5a2cfdcf.

--- project/plugins/acVinDRevPlugin/lib/model/DRev/DRevDeclaration.class.php
+++ project/plugins/acVinDRevPlugin/lib/model/DRev/DRevDeclaration.class.php
@@ -87,9 +87,6 @@ class DRevDeclaration extends BaseDRevDeclaration
 	}
 
 	public function getSyndicats() {
-        if(!$this->getDocument()->exist('regions')) {
-            return [];
-        }
 		$syndicats = array();
 		foreach (RegionConfiguration::getInstance()->getOdgRegions() as $region) {
 			if(!count($this->getProduitsByRegion($region))) {
