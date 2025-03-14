052f09245bc8af27fec22687f3d36f4e0fd1716a
Vincent LAURENT <vince.laurent@gmail.com>
2025-03-14 15:09:08 +0100

    Modif de texte pour l'ava

--- project/apps/ava/modules/facturation/templates/_email.php
+++ project/apps/ava/modules/facturation/templates/_email.php
@@ -1,8 +1,6 @@
 Bonjour,
 
-L'AVA a développé un espace "Facture" sur son portail de dématérialisation afin de vous permettre de télécharger chacune d'entre elles.
-
-Vous trouverez ainsi sur cet espace la facture suivante : <?php echo ProjectConfiguration::getAppRouting()->generate('piece_public_view', array('doc_id' => $id, 'auth' => UrlSecurity::generateAuthKey($id)), true) ?>
+Une facture est disponible sur notre portail de dématérialisation, vous pouvez la télécharger directement en cliquant sur ce lien : <?php echo ProjectConfiguration::getAppRouting()->generate('piece_public_view', array('doc_id' => $id, 'auth' => UrlSecurity::generateAuthKey($id)), true) ?>
 
 
 La note explicative des cotisations est également disponible pour téléchargement : https://declaration.ava-aoc.fr/docs/explications_cotisations.pdf
