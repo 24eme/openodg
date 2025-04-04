698ab2b2dc290e0caa4866ed76ed52e2be250dd0
chachathecool <char.var@posteo.com>
2025-04-04 18:19:12 +0200

    Feat: adapter les plugins irrigation à l'AVA

--- project/plugins/acVinParcellaireIrriguePlugin/modules/parcellaireIrrigue/actions/components.class.php
+++ project/plugins/acVinParcellaireIrriguePlugin/modules/parcellaireIrrigue/actions/components.class.php
@@ -3,9 +3,11 @@
 class parcellaireIrrigueComponents extends sfComponents {
 
     public function executeMonEspace(sfWebRequest $request) {
-        if(strpos($this->etablissement->famille, 'PRODUCTEUR') === false) {
+        if (! ($this->etablissement->hasFamille(EtablissementFamilles::FAMILLE_PRODUCTEUR) || $this->etablissement->hasFamille(EtablissementFamilles::FAMILLE_PRODUCTEUR_VINIFICATEUR)))
+        {
             return;
         }
+
         $this->parcellaire = ParcellaireClient::getInstance()->getLast($this->etablissement->identifiant, acCouchdbClient::HYDRATE_JSON);
         $this->parcellaireIrrigable = ParcellaireIrrigableClient::getInstance()->findOrCreate($this->etablissement->identifiant, $this->periode);
         if(!$this->parcellaireIrrigable->_rev) {
