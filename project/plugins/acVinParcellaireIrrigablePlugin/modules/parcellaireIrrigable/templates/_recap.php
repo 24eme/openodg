93e7f6e27539ef96e9e0ae0c21c110f9bcd42949
Gabriel Poma <gpoma@24eme.fr>
2025-04-15 17:29:51 +0200

    ava: ares / ha partout dans Irrig{able,ue}

--- project/plugins/acVinParcellaireIrrigablePlugin/modules/parcellaireIrrigable/templates/_recap.php
+++ project/plugins/acVinParcellaireIrrigablePlugin/modules/parcellaireIrrigable/templates/_recap.php
@@ -7,7 +7,7 @@
 		        <th class="col-xs-1 text-center">Section / N° parcelle</th>
 		        <th class="col-xs-2">Cépage</th>
 		        <th class="col-xs-1 text-center">Année plantat°</th>
-		        <th class="col-xs-1 text-right">Surf. <span class="text-muted small">(ha)</span></th>
+		        <th class="col-xs-1 text-right">Surf. <span class="text-muted small">(<?php echo ParcellaireConfiguration::getInstance()->isAres() ? 'ares' : 'ha' ?>)</span></th>
 		        <th class="col-xs-2 text-center">Type de matériel</th>
 		        <th class="col-xs-2 text-center">Type de ressource</th>
 
@@ -25,7 +25,12 @@ foreach ($parcellaireIrrigable->declaration->getParcellesByCommune() as $commune
 				<td class="text-center"><?php echo $parcelle->section; ?> <?php echo $parcelle->numero_parcelle; ?></td>
 				<td><span class="text-muted"><?php echo $parcelle->getProduitLibelle(); ?></span> <?php echo $parcelle->cepage; ?></td>
 				<td class="text-center"><?php echo $parcelle->campagne_plantation; ?></td>
-				<td class="text-right"><?php echoFloatFr($parcelle->superficie, 4); $somme_superficie += $parcelle->superficie; ?></td>
+                <?php if (ParcellaireConfiguration::getInstance()->isAres()): ?>
+                    <td class="text-right"><?php echoFloatFr($parcelle->getSuperficie(ParcellaireClient::PARCELLAIRE_SUPERFICIE_UNIT_ARE)); ?></td>
+                <?php else: ?>
+                    <td class="text-right"><?php echoFloatFr($parcelle->getSuperficie()); ?></td>
+                <?php endif ?>
+                <?php $somme_superficie += $parcelle->superficie; ?>
             	<td class="text-center"><?php echo $parcelle->materiel; ?></td>
             	<td class="text-center"><?php echo $parcelle->ressource; ?></td>
             </tr>
@@ -35,7 +40,11 @@ foreach ($parcellaireIrrigable->declaration->getParcellesByCommune() as $commune
 <tfooter>
     <tr>
         <th colspan="5">Total superficie</th>
-        <th class="text-right"><?php echoFloatFr($somme_superficie, 4); ?></th>
+        <?php if (ParcellaireConfiguration::getInstance()->isAres()): ?>
+            <th class="text-right"><?php echoFloatFr($somme_superficie * 100, 2); ?></th>
+        <?php else: ?>
+            <th class="text-right"><?php echoFloatFr($somme_superficie, 4); ?></th>
+        <?php endif ?>
         <th colspan="2">&nbsp;</th>
     <tr>
 <tfooter>
