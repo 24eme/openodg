7aa9182fa7a152ed10ff743ec15f557c04ce51cf
Tangui Morlier <tangui@tangui.eu.org>
2025-05-15 11:32:18 +0200

    [Provence] changé pour irrigable

--- project/plugins/acVinParcellaireAffectationPlugin/modules/parcellaireAffectationCoop/templates/_monEspace.php
+++ project/plugins/acVinParcellaireAffectationPlugin/modules/parcellaireAffectationCoop/templates/_monEspace.php
@@ -7,7 +7,7 @@
             <h3 class="panel-title">Déclarations  <?php echo $periode ?> de vos apporteurs</h3>
         </div>
         <div class="panel-body">
-            <p class="explications">Vous pouvez déclarer les affectations parcellaires, les déclaraion de manquants et d'irrigablilité de vos apporteurs.</p>
+            <p class="explications">Vous pouvez déclarer les affectations parcellaires, les déclaraion de manquants et de parcelles irrigables de vos apporteurs.</p>
             <div class="actions">
                 <a id="btn_affection_parcellaire_coop" class="btn btn-block <?php if($parcellaireAffectationCoop): ?>btn-primary<?php else: ?>btn-default<?php endif; ?>" href="<?php echo url_for('parcellaireaffectationcoop_edit', array('sf_subject' => $etablissement, 'periode' => $periode)) ?>"><?php if($parcellaireAffectationCoop): ?>Continuer à déclarer pour vos apporteurs<?php else: ?>Déclarer pour vos apporteurs<?php endif; ?></a>
             </div>
