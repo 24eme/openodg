7fc1de5cfad7303e9e67e25eba404058c001050d
JB LM <lemetayer.jb@gmail.com>
2025-05-19 18:02:43 +0200

    ajout des parcelles en mode multi produit dans l'intention auto

--- project/plugins/acVinParcellaireAffectationPlugin/lib/model/ParcellaireIntention/ParcellaireIntentionAuto.class.php
+++ project/plugins/acVinParcellaireAffectationPlugin/lib/model/ParcellaireIntention/ParcellaireIntentionAuto.class.php
@@ -18,17 +18,28 @@ class ParcellaireIntentionAuto extends ParcellaireIntentionAffectation {
         $this->remove('declaration');
         $this->add('declaration');
         foreach($parcelles as $pid => $parcelle) {
-            if ( !in_array($this->getDenominationAire(),  array_keys($parcelle->getIsInAires())) &&
-                 !in_array(AireClient::PARCELLAIRE_AIRE_GENERIC_AIRE,  array_keys($parcelle->getIsInAires())) )
-            {
+            if ( !in_array($this->getDenominationAire($parcelle->getProduitLibelle()),  array_keys($parcelle->getIsInAires())) &&
+                 !in_array(AireClient::PARCELLAIRE_AIRE_GENERIC_AIRE,  array_keys($parcelle->getIsInAires()))) {
                 continue;
             }
-            $node = $this->declaration->add($this->getDenominationAireHash());
-            $node->libelle = $this->getDenominationAire();
-            $node = $node->detail->add($pid);
-            ParcellaireClient::CopyParcelle($node, $parcelle, true);
-            $parcelle->produit_hash = $this->getDenominationAireHash();
-            $node->affectation = 1;
+            $hashes = $this->getDenominationAireHash();
+            $nbHashes = count($hashes);
+            foreach ($hashes as $hash) {
+                if ($nbHashes > 1) {
+                    $tmp = explode('/', $hash);
+                    $lastHashData = end($tmp);
+                    $newPid = strtoupper($lastHashData).'-'.$pid;
+                } else {
+                    $newPid = $pid;
+                }
+                $node = $this->declaration->add($hash);
+                $node->libelle = $this->getDenominationAire();
+                $node = $node->detail->add($newPid);
+                ParcellaireClient::CopyParcelle($node, $parcelle, true);
+                $node->produit_hash = $hash;
+                $node->parcelle_id = $newPid;
+                $node->affectation = 1;
+            }
         }
     }
 
