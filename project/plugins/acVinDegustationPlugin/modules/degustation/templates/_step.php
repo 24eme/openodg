06da28bb165973d03cb606dfd1ae3960192d6412
Vincent LAURENT <vince.laurent@gmail.com>
2025-06-23 12:51:44 +0200

    igpsudouest: mode tournée avec l'organisation par table

--- project/plugins/acVinDegustationPlugin/modules/degustation/templates/_step.php
+++ project/plugins/acVinDegustationPlugin/modules/degustation/templates/_step.php
@@ -3,7 +3,7 @@ $etapes =  DegustationEtapes::getInstance();
 $active = ($active) ? $active : $etapes->getFirst();
 ?>
 
-<div id="etapes_degustation">
+<div id="etapes_degustation" class="<?php if(DegustationConfiguration::getInstance()->isTourneesParSecteur()): ?>many-step<?php endif; ?>">
 <?php include_partial('global/step', array('object' => $degustation, 'etapes' => $etapes, 'step' => $active)); ?>
 </div>
 
