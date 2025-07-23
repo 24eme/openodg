c5f5d215a73f7e41963d52e4a53a41bda4db6a37
Vincent LAURENT <vince.laurent@gmail.com>
2025-07-23 19:11:24 +0200

    gaillac: Test pour simuler la saisie de volume AOC sans import de DR puis un import de DR dans une modificatrice suivante

--- project/test/unit/25DRevIGPxAOPFormTest.php
+++ project/test/unit/25DRevIGPxAOPFormTest.php
@@ -52,26 +52,26 @@ foreach(ArchivageAllView::getInstance()->getDocsByTypeAndCampagne('Revendication
 }
 
 $config = ConfigurationClient::getConfigurationByCampagne($campagne);
-$produitconfig_aoc = null;
+$produitconfig_aop = null;
 $produitconfig_igp = null;
 foreach($config->getProduits() as $produitconfig) {
     if(!$produitconfig->getRendement()) {
         continue;
     }
-    if(strpos($produitconfig->getCertification()->getKey(), 'AO') === 0 && !$produitconfig_aoc) {
-        $produitconfig_aoc = $produitconfig->getCepage();
+    if(strpos($produitconfig->getCertification()->getKey(), 'AO') === 0 && !$produitconfig_aop) {
+        $produitconfig_aop = $produitconfig->getCepage();
         continue;
     } elseif(strpos($produitconfig->getCertification()->getKey(), 'IGP') === 0 && !$produitconfig_igp) {
         $produitconfig_igp = $produitconfig->getCepage();
         continue;
     }
-    if (!$produitconfig_aoc || !$produitconfig_igp) {
+    if (!$produitconfig_aop || !$produitconfig_igp) {
         continue;
     }
 
     break;
 }
-if ( ! $produitconfig_aoc || ! $produitconfig_igp ) {
+if ( ! $produitconfig_aop || ! $produitconfig_igp ) {
     $t = new lime_test(1);
     $t->comment($config->_id);
     $t->pass("no DREV AOC and IGP for ".$application);
@@ -85,21 +85,20 @@ $t->comment($config->_id);
 $csvContentTemplate = file_get_contents(dirname(__FILE__).'/../data/dr_douane.csv');
 
 $csvTmpFile = tempnam(sys_get_temp_dir(), 'openodg').".csv";
-file_put_contents($csvTmpFile, str_replace(array("%cvi%", "%code_inao_1%", "%libelle_produit_1%","%code_inao_2%", "%libelle_produit_2%"), array($viti->cvi, $produitconfig_aoc->getCodeDouane(), $produitconfig_aoc->getLibelleComplet(), $produitconfig_igp->getCodeDouane(), $produitconfig_igp->getLibelleComplet()), $csvContentTemplate));
+file_put_contents($csvTmpFile, str_replace(array("%cvi%", "%code_inao_1%", "%libelle_produit_1%","%code_inao_2%", "%libelle_produit_2%"), array($viti->cvi, $produitconfig_aop->getCodeDouane(), $produitconfig_aop->getLibelleComplet(), $produitconfig_igp->getCodeDouane(), $produitconfig_igp->getLibelleComplet()), $csvContentTemplate));
 $t->comment("utilise le fichier test/data/dr_douane.csv");
-$t->comment("%libelle_produit_1% = ".$produitconfig_aoc->getLibelleComplet());
+$t->comment("%libelle_produit_1% = ".$produitconfig_aop->getLibelleComplet());
 $t->comment("%libelle_produit_2% = ".$produitconfig_igp->getLibelleComplet());
 
 $drev = DRevClient::getInstance()->createDoc($viti->identifiant, $periode);
 $t->comment('Etape superficie');
 $t->ok(!DrevEtapes::getInstance()->isEtapeDisabled(DrevEtapes::ETAPE_REVENDICATION_SUPERFICIE, $drev), "On a accès à l'étape superficie");
+
 $drev->addProduit($produitconfig_igp->getHash());
-if($drev->storeEtape(DrevEtapes::ETAPE_REVENDICATION_SUPERFICIE)) {
-    $drev->save();
-}
+
 $t->comment("Étape lots");
 $t->ok(!DrevEtapes::getInstance()->isEtapeDisabled(DrevEtapes::ETAPE_LOTS, $drev), "On a accès à l'étape lots");
-$t->ok(DrevEtapes::getInstance()->isEtapeDisabled(DrevEtapes::ETAPE_REVENDICATION, $drev), "On a accès à l'étape AOC revendication");
+$t->ok(DrevEtapes::getInstance()->isEtapeDisabled(DrevEtapes::ETAPE_REVENDICATION, $drev), "On a pas accès à l'étape AOC revendication");
 
 $drevConfig = DRevConfiguration::getInstance();
 $t->ok(count($drevConfig->getSpecificites()), "La configuration retourne bien des spécificités");
@@ -137,8 +136,47 @@ $t->is(count($drev->lots), 1, "Seulement le lot non vide est conservé");
 
 $t->is(count($drev->mouvements_lots), 1, 'Il y a bien des mouvements de lots');
 
-$drev_m01  = $drev->generateModificative();
+$t->comment("Nouvelle drev pour déclarer de l'AOC sans DR");
+
+$drev_m01 = $drev->generateModificative();
 $drev_m01->save();
+
+$t->comment($drev_m01->_id);
+
+$t->ok(DrevEtapes::getInstance()->isEtapeDisabled(DrevEtapes::ETAPE_REVENDICATION, $drev_m01), "On a pas accès à l'étape AOC revendication");
+
+$t->comment("Etape superficie");
+
+$produitAop = $drev_m01->addProduit($produitconfig_aop->getHash());
+
+$form = new DRevSuperficieForm($drev_m01, array('disabled_dr' => true));
+$valuesRev = [
+    '_revision' => $drev_m01->_rev,
+    'produits' => [$produitAop->getHash() => [ "superficie_revendique" => 1]]
+];
+$form->bind($valuesRev);
+$t->ok($form->isValid(), "Le formulaire est valide");
+$form->save();
+
+$t->comment("Etape volume");
+
+$t->ok(!DrevEtapes::getInstance()->isEtapeDisabled(DrevEtapes::ETAPE_REVENDICATION, $drev_m01), "On a accès à l'étape AOC revendication");
+
+$form = new DRevRevendicationForm($drev_m01, array('disabled_dr' => true));
+$valuesRev = [
+    '_revision' => $drev_m01->_rev,
+    'produits' => [$produitAop->getHash() => [ "volume_revendique_issu_recolte" => 40]]
+];
+$form->bind($valuesRev);
+$t->ok($form->isValid(), "Le formulaire est valide");
+$form->save();
+
+$drev_m01->validate();
+$drev_m01->validateOdg();
+$drev_m01->save();
+
+$drev_m02 = $drev_m01->generateModificative();
+$drev_m02->save();
 
 $t->comment("Récupération des données à partir de la DR");
 
@@ -151,21 +189,21 @@ $dr->storeFichier($csvTmpFile);
 $dr->save();
 unlink($csvTmpFile);
 
-$drev_m01->resetAndImportFromDocumentDouanier();
-$drev_m01->save();
-$t->comment($drev_m01->_id);
-$t->is(count($drev_m01->declaration), 2, "Après la DR, on retrouve les deux produits dans déclaration");
-$t->ok($drev_m01->exist($produitconfig_aoc->getHash()), "La DR a permis d'avoir le produit AOC");
-$t->ok($drev_m01->exist($produitconfig_igp->getHash()), "Le produit IGP existe toujours");
+$drev_m02->resetAndImportFromDocumentDouanier();
+$drev_m02->save();
+$t->comment($drev_m02->_id);
+$t->is(count($drev_m02->declaration), 2, "Après la DR, on retrouve les deux produits dans déclaration");
+$t->ok($drev_m02->exist($produitconfig_aop->getHash()), "La DR a permis d'avoir le produit AOC");
+$t->ok($drev_m02->exist($produitconfig_igp->getHash()), "Le produit IGP existe toujours");
 
-$t->ok(!DrevEtapes::getInstance()->isEtapeDisabled(DrevEtapes::ETAPE_LOTS, $drev_m01), "Après l'import de la DR, on a accès à l'étape lots");
-$t->ok(!DrevEtapes::getInstance()->isEtapeDisabled(DrevEtapes::ETAPE_REVENDICATION, $drev_m01), "Après l'import de la DR, on a accès à l'étape AOC revendication");
+$t->ok(!DrevEtapes::getInstance()->isEtapeDisabled(DrevEtapes::ETAPE_LOTS, $drev_m02), "Après l'import de la DR, on a accès à l'étape lots");
+$t->ok(!DrevEtapes::getInstance()->isEtapeDisabled(DrevEtapes::ETAPE_REVENDICATION, $drev_m02), "Après l'import de la DR, on a accès à l'étape AOC revendication");
 
-$produits = $drev_m01->getProduits();
-$produit1 = current($drev_m01->get($produitconfig_aoc->getCepage()->getHash())->getProduits());
+$produits = $drev_m02->getProduits();
+$produit1 = current($drev_m02->get($produitconfig_aop->getCepage()->getHash())->getProduits());
 $produit_hash1 = $produit1->getCepage()->getHash();
 next($produits);
-$produit2 = end($drev_m01->get($produitconfig_igp->getCepage()->getHash())->getProduits());
+$produit2 = end($drev_m02->get($produitconfig_igp->getCepage()->getHash())->getProduits());
 $produit_hash2 = $produit2->getCepage()->getHash();
-$t->is($produit_hash1, $produitconfig_aoc->getHash(), "Le premier produit est le produit AOC");
+$t->is($produit_hash1, $produitconfig_aop->getHash(), "Le premier produit est le produit AOC");
 $t->is($produit_hash2, $produitconfig_igp->getHash(), "Le premier produit est le produit IGP");
