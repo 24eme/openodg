69c885a4cec42717e3049eb791964d10a4eaa994
tale-fau <tale-fau@24eme.fr>
2025-07-24 15:50:23 +0200

    [IGPSO] Modification des courriers

--- project/apps/igpsudouest/modules/degustation/templates/_degustationNonConformitePDF_page1.php
+++ project/apps/igpsudouest/modules/degustation/templates/_degustationNonConformitePDF_page1.php
@@ -2,7 +2,10 @@
 <?php use_helper('Lot'); ?>
 
 <?php
-    if ($lot->conformite == Lot::CONFORMITE_NONCONFORME_MAJEUR_CONDITIONNÉ) {
+    if ($lot->conformite == Lot::CONFORMITE_NONCONFORME_MINEUR) {
+        echo include_partial('degustationNonConformiteMinPDF_page1', array('degustation' => $degustation, 'etablissement' => $etablissement, "lot" => $lot));
+    }
+    else if ($lot->conformite == Lot::CONFORMITE_NONCONFORME_MAJEUR_CONDITIONNÉ) {
         echo include_partial('degustationNonConformiteConditionnePDF_page1', array('degustation' => $degustation, 'etablissement' => $etablissement, "lot" => $lot));
     } else {
         echo include_partial('degustationNonConformiteNonConditionnePDF_page1', array('degustation' => $degustation, 'etablissement' => $etablissement, "lot" => $lot));
--- project/apps/igpsudouest/modules/degustation/templates/_degustationNonConformitePDF_page2.php
+++ project/apps/igpsudouest/modules/degustation/templates/_degustationNonConformitePDF_page2.php
@@ -3,7 +3,10 @@
 <?php use_helper('TemplatingPDF'); ?>
 
 <?php
-if ($lot->conformite == Lot::CONFORMITE_NONCONFORME_MAJEUR_CONDITIONNÉ) {
+if ($lot->conformite == Lot::CONFORMITE_NONCONFORME_MINEUR) {
+    echo include_partial('degustationNonConformiteMinPDF_page2', array('degustation' => $degustation, 'etablissement' => $etablissement, "lot" => $lot));
+}
+else if ($lot->conformite == Lot::CONFORMITE_NONCONFORME_MAJEUR_CONDITIONNÉ) {
     echo include_partial('degustationNonConformiteConditionnePDF_page2', array('degustation' => $degustation, 'etablissement' => $etablissement, "lot" => $lot));
 } else {
     echo include_partial('degustationNonConformiteNonConditionnePDF_page2', array('degustation' => $degustation, 'etablissement' => $etablissement, "lot" => $lot));
