5c0211f45355451468fc03de5c46a8bbc6321fcb
Vincent LAURENT <vince.laurent@gmail.com>
2025-07-29 12:09:51 +0200

    gaillac: Etape produit au lieu de superficie et pas de superficie saisissable

--- project/plugins/acVinDRevPlugin/lib/DrevConfiguration.class.php
+++ project/plugins/acVinDRevPlugin/lib/DrevConfiguration.class.php
@@ -193,4 +193,9 @@ class DRevConfiguration extends DeclarationConfiguration {
         return isset($this->configuration['send_to_innovagro']) && boolval($this->configuration['send_to_innovagro']);
     }
 
+    public function isSaisieSuperficieRevendique() {
+
+        return isset($this->configuration['saisie_superficie_revendique']) && boolval($this->configuration['saisie_superficie_revendique']);
+    }
+
 }
