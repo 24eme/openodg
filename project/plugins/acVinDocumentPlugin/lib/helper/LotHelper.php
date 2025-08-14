e6fef9dae0ae5992f871730834023211c1baa27f
tale-fau <tale-fau@24eme.fr>
2025-08-14 12:05:05 +0200

    [IGPGASCOGNE] TMA - Changement de condition pour l'affichage des bons glyphicon

--- project/plugins/acVinDocumentPlugin/lib/helper/LotHelper.php
+++ project/plugins/acVinDocumentPlugin/lib/helper/LotHelper.php
@@ -114,7 +114,7 @@ function pictoDegustable($lot) {
         throw new Exception("Lot ".$lot->getDocument()->_id.":".$lot->getHash()." non trouvé");
     }
 
-    if($lotOrigine->id_document_affectation && strpos($lotOrigine->id_document_affectation, 'DEGUSTATION')) {
+    if($lotOrigine->id_document_affectation && $lotOrigine->date_commission <= date("Y-m-d")) {
         return '<span title="Dégusté" class="glyphicon glyphicon-ok-circle text-success"></span>';
     }
 
