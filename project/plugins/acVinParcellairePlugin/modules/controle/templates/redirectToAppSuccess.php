59ce4d6077a1c60bfcd56e8509d89228f045c1d8
JB LM <lemetayer.jb@gmail.com>
2025-09-30 22:38:11 +0200

    redirection vers l'app du controle terrain en sauvegardant les parcelles a controler dans le localstorage

---
+++ project/plugins/acVinParcellairePlugin/modules/controle/templates/redirectToAppSuccess.php
@@ -0,0 +1,9 @@
+<script id="dataJson" type="application/json">
+<?php echo $sf_data->getRaw('json') ?>
+</script>
+<script>
+    let controles = (localStorage.getItem("controles")) ? JSON.parse(localStorage.getItem("controles")) : {};
+    controles["<?php echo $controle->_id ?>"] = JSON.parse(document.getElementById("dataJson").textContent);
+    localStorage.setItem("controles", JSON.stringify(controles));
+    window.location.replace("<?php echo url_for('controle_appterrain', $controle) ?>");
+</script>
