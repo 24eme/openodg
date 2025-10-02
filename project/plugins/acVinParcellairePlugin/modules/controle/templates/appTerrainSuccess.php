c357b801b79e6c6b1a17adab6c45cacb49e971ff
JB LM <lemetayer.jb@gmail.com>
2025-10-02 18:16:43 +0200

    revue global de l'app controle terrain

--- project/plugins/acVinParcellairePlugin/modules/controle/templates/appTerrainSuccess.php
+++ project/plugins/acVinParcellairePlugin/modules/controle/templates/appTerrainSuccess.php
@@ -1,131 +1,134 @@
 <div id="app">
-    <h1>Parcelles à contrôler {{ controle.identifiant }}</h1>
-
-    <div v-if="page === 'parcelles'">
-
+    <div v-if="!controleCourant">
+        <h2>Opérateurs à contrôler</h2>
         <table class="table table-bordered table-condensed table-striped tableParcellaire">
             <thead>
                 <tr>
-                    <th class="col-xs-2">Commune</th>
-                    <th class="col-xs-2">Lieu-dit</th>
-                    <th class="col-xs-1" style="text-align: right;">Section</th>
-                    <th class="col-xs-1">N° parcelle</th>
-                    <th class="col-xs-3">Cépage</th>
-                    <th class="col-xs-1">Année plantat°</th>
-                    <th class="col-xs-1" style="text-align: right;">Surface <span class="text-muted small"><?php echo ParcellaireConfiguration::getInstance()->isAres() ? 'ares' : 'ha' ?></span></th>
+                    <th class="col-xs-5">Opérateur</th>
+                    <th class="col-xs-5">Infos</th>
+                    <th class="col-xs-1 text-center">Parcelles</th>
                     <th class="col-xs-1 text-center">Statut</th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="(parcelle, key) in controle.parcelles" class="parcellerow switch-to-higlight" >
-                    <td>{{ parcelle.commune }}</td>
-                    <td>{{ parcelle.lieu }}</td>
-                    <td style="text-align: right;">{{ parcelle.section }}</td>
-                    <td>{{ parcelle.numero_parcelle }}</td>
-                    <td><span class="text-muted">{{ parcelle.source_produit_libelle }}</span> {{ parcelle.cepage }}</td>
-                    <td class="text-center">{{ parcelle.campagne_plantation }}</td>
-                    <td class="text-right">{{ parcelle.superficie }}</td>
+                <tr v-for="(controle, key) in controles">
+                    <td>
+                        <strong>{{ controle.declarant.nom }}</strong> <span class="small">{{ controle.identifiant }}</span><br />
+                        <span class="text-muted">CVI {{ controle.declarant.cvi }} - SIRET {{ controle.declarant.siret }}</span>
+                    </td>
+                    <td>
+                        {{ controle.declarant.adresse }} {{ controle.declarant.code_postal }} {{ controle.declarant.commune }}<br />
+                        <a href="mailto:{{ controle.declarant.email }}">{{ controle.declarant.email }}</a> / <a href="callto:{{ controle.declarant.telephone_bureau }}">{{ controle.declarant.telephone_bureau }}</a> / <a href="callto:{{ controle.declarant.telephone_mobile }}">{{ controle.declarant.telephone_mobile }}</a>
+                    </td>
                     <td class="text-center">
-                        <select v-model="parcelle.controle.statut">
-                            <option value=""></option>
-                            <option value="ATTENTE">En attente / Controlé</option>
-                            <option value="CONFORME">Conforme</option>
-                            <option value="NONCONFORME">Non conforme</option>
-                        </select>
+                        {{ Object.keys(controle.parcelles).length }}
+                    </td>
+                    <td class="text-center">
+                        <a href="#" @click.prevent="setControleCourant(key)">A contrôler</a>
                     </td>
                 </tr>
             </tbody>
         </table>
-        <div class="row row-margin row-button">
-            <div class="col-xs-8"></div>
-            <div class="col-xs-4 text-right"><button @click="page = 'pointsControle'" class="btn btn-primary btn-upper">Continuer <span class="glyphicon glyphicon-chevron-right"></span></button></div>
-        </div>
-    </div>
-    <div v-else-if="page === 'pointsControle'">
-
-        <ul class="nav nav-tabs">
-            <li role="presentation" v-for="i in parcellesNonConformes" :i="i" @click="currentKey = i" :class="{active: currentKey === i}"><a href="#">{{ i }}</a></li>
-        </ul>
-        <form class="form-horizontal" v-if="currentKey">
-            <div class="form-group">
-                <label class="col-sm-2 control-label">Point résultat</label>
-                <div class="col-sm-10">
-                    <label class="radio-inline">
-                      <input type="radio" value="C" v-model="parcelleCourante.controle.pt_resultat"> (C)
-                    </label>
-                    <label class="radio-inline">
-                      <input type="radio" value="NC" v-model="parcelleCourante.controle.pt_resultat"> (NC)
-                    </label>
-                </div>
-            </div>
-            <div class="form-group">
-                <label class="col-sm-2 control-label">Observation</label>
-                <div class="col-sm-10">
-                    <textarea v-model="parcelleCourante.controle.observation" rows="4" cols="40"></textarea>
-                </div>
-            </div>
-        </form>
-
-        <div class="row row-margin row-button">
-            <div class="col-xs-4 text-left"><button @click="page = 'parcelles'" class="btn btn-secondary btn-upper"> <span class="glyphicon glyphicon-chevron-left"></span> Retour</button></div>
-            <div class="col-xs-8"></div>
-        </div>
     </div>
     <div v-else>
-        <p>Aucun résultat</p>
+        <div class="well">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="row">
+                        <div style="margin-bottom: 5px;" class="col-xs-3 text-muted">&nbsp;</div>
+                        <div style="margin-bottom: 5px" class="col-xs-9">
+                            <h4 class="strong">{{ controleCourant.declarant.nom }}</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12">
+                    <div class="row">
+                        <div style="margin-bottom: 5px;" class="col-xs-3 text-muted">
+                            Ids&nbsp;:
+                        </div>
+                        <div style="margin-bottom: 5px" class="col-xs-9">
+                            <span class="text-muted">{{ controleCourant.identifiant }} - CVI : {{ controleCourant.declarant.cvi }} - SIRET : {{ controleCourant.declarant.siret }}</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12">
+                    <div class="row">
+                        <div style="margin-bottom: 5px;" class="col-xs-3 text-muted">
+                            Adresse&nbsp;:
+                        </div>
+                        <div style="margin-bottom: 5px" class="col-xs-9">
+                            <address style="margin-bottom: 0;">
+                                {{ controleCourant.declarant.adresse }} {{ controleCourant.declarant.code_postal }} {{ controleCourant.declarant.commune }}
+                            </address>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12">
+                    <div class="row">
+                        <div style="margin-bottom: 5px;" class="col-xs-3 text-muted">
+                            Contact&nbsp;:
+                        </div>
+                        <div style="margin-bottom: 5px" class="col-xs-9">
+                            <a href="mailto:{{ controleCourant.declarant.email }}">{{ controleCourant.declarant.email }}</a> / <a href="callto:{{ controleCourant.declarant.telephone_bureau }}">{{ controleCourant.declarant.telephone_bureau }}</a> / <a href="callto:{{ controleCourant.declarant.telephone_mobile }}">{{ controleCourant.declarant.telephone_mobile }}</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h2>Parcelles ({{ Object.keys(controleCourant.parcelles).length }})</h2>
+        <table class="table table-bordered table-condensed table-striped tableParcellaire">
+            <thead>
+                <tr>
+                    <th class="col-xs-2">Commune</th>
+                    <th class="col-xs-1">Lieu-dit</th>
+                    <th class="col-xs-1 text-center">Section / N° parcelle</th>
+                    <th class="col-xs-4">Cépage</th>
+                    <th class="col-xs-1 text-center">Année plantat°</th>
+                    <th class="col-xs-1 text-right">Superficie <span class="text-muted small">(ha)</span></th>
+                    <th class="col-xs-1 text-center">Écart Pieds/Rang</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(parcelle, key) in controleCourant.parcelles">
+                    <td>{{ parcelle.commune }}</td>
+                    <td>{{ parcelle.lieu }}</td>
+                    <td class="text-center">{{ parcelle.section }} {{ parcelle.numero_parcelle }}<br /><span class="text-muted">{{ parcelle.parcelle_id }}</span></td>
+                    <td><span class="text-muted">{{ parcelle.source_produit_libelle }}</span> {{ parcelle.cepage }}</td>
+                    <td class="text-center">{{ parcelle.campagne_plantation }}</td>
+                    <td class="text-right">{{ echoFloat(parcelle.superficie) }}</td>
+                    <td class="text-center">{{ parcelle.ecart_pieds }} / {{ parcelle.ecart_rang }}</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <button class="btn btn-default" @click="controleCourant = null"><span class="glyphicon glyphicon-chevron-left"></span> Retour</button>
     </div>
-  </div>
+
+</div>
 
 <script>
 
-const CONTROLE_ID = "<?php echo $controle->_id ?>";
 
 const { createApp } = Vue;
 
 createApp({
   data() {
     return {
-      page: "parcelles",
-      controle: {},
-      currentKey: null
+      controles: {},
+      controleCourant: null
     };
   },
   mounted() {
-    const controles = JSON.parse(localStorage.getItem("controles")) || {};
-    this.controle = controles[CONTROLE_ID] || [];
-    for (let key in this.controle.parcelles) {
-        if (!("controle" in this.controle.parcelles[key])) {
-            this.controle.parcelles[key].controle = {
-                'statut': null,
-                'observation': null,
-                'pt_resultat': null
-            };
-        } else {
-            if (!this.currentKey && this.controle.parcelles[key].controle.statut === "NONCONFORME") {
-                this.currentKey = key;
-            }
-        }
-    }
+    this.controles = JSON.parse(localStorage.getItem("controles")) || {};
   },
-  watch: {
-    controle: {
-      handler(newVal) {
-        const controles = JSON.parse(localStorage.getItem("controles")) || {};
-        controles[CONTROLE_ID] = newVal;
-        localStorage.setItem("controles", JSON.stringify(controles));
-        console.log("Données sauvegardées");
-      },
-      deep: true
-    }
+  methods: {
+    setControleCourant(id) {
+      this.controleCourant = this.controles[id];
+    },
+    echoFloat(val, nbDecimal = 5) {
+        return val ? Number(val).toFixed(nbDecimal) : '';
+    },
   },
-  computed: {
-     parcellesNonConformes() {
-       return Object.keys(this.controle.parcelles || {}).filter(k => this.controle.parcelles[k].controle.statut === "NONCONFORME");
-     },
-     parcelleCourante() {
-       return this.controle.parcelles[this.currentKey];
-     }
-   },
 }).mount("#app");
 
 </script>
