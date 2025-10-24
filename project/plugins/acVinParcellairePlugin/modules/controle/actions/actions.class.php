bfef325d0091c6c75e806c4597a0597377f9f5ca
Tangui Morlier <tangui@tangui.eu.org>
2025-10-24 18:49:52 +0200

    MaJ du localstorage en fonction des jours choisis

--- project/plugins/acVinParcellairePlugin/modules/controle/actions/actions.class.php
+++ project/plugins/acVinParcellairePlugin/modules/controle/actions/actions.class.php
@@ -4,6 +4,7 @@ class controleActions extends sfActions
 
     private function getControlesPlanifies($date = null) {
         $stats = [];
+        $this->controles = ControleClient::getInstance()->findAllByStatus();
         foreach($this->controles[ControleClient::CONTROLE_STATUT_PLANIFIE] as $c) {
             if ($date && $date != $c->date_tournee) {
                 continue;
@@ -13,14 +14,13 @@ class controleActions extends sfActions
             }
             $stats[$c->date_tournee]['nb_parcelles'] += count($c->parcelles);
             $stats[$c->date_tournee]['operateurs'][] = $c->declarant->nom;
-            $stats[$c->date_tournee]['controles'][] = $c;
+            $stats[$c->date_tournee]['controles'][] = $c->getData();
         }
         return $stats;
     }
 
     public function executeIndex(sfWebRequest $request)
     {
-        $this->controles = ControleClient::getInstance()->findAllByStatus();
         $this->stats = $this->getControlesPlanifies();
     }
 
@@ -51,20 +51,16 @@ class controleActions extends sfActions
         if ($request->isMethod(sfWebRequest::POST)) {
             $this->controle->updateParcelles($request->getPostParameter('parcelles', []));
             $this->controle->save();
-            return $this->redirect('controle_saveinlocalstorage', array('id' => $this->controle->_id));
+            return $this->redirect('controle_index');
         }
-    }
-
-    public function executeSaveInLocalStorage(sfWebRequest $request)
-    {
-        $this->controle = $this->getRoute()->getControle();
-        $this->json = json_encode($this->controle->getData(), JSON_HEX_TAG|JSON_HEX_AMP|JSON_HEX_APOS|JSON_HEX_QUOT);
     }
 
     public function executeAppTerrain(sfWebRequest $request)
     {
         $this->date_tournee = $request->getParameter('date');
         $this->controles = $this->getControlesPlanifies($this->date_tournee);
+        $this->json = json_encode($this->controles[$this->date_tournee]['controles'], JSON_HEX_TAG|JSON_HEX_AMP|JSON_HEX_APOS|JSON_HEX_QUOT);
+
         $this->setLayout('appLayout');
     }
 
