6613e05b3f237405fe6e9ea9105832672b4af031
Tangui Morlier <tangui@tangui.eu.org>
2025-11-12 15:53:20 +0100

    Embed GeoJson in parcelle

--- project/plugins/acVinParcellairePlugin/lib/model/Controle/ControleParcelle.class.php
+++ project/plugins/acVinParcellairePlugin/lib/model/Controle/ControleParcelle.class.php
@@ -1,4 +1,9 @@
 <?php
 class ControleParcelle extends BaseControleParcelle
 {
+    public function  getData() {
+        $data = parent::getData();
+        $data->geojson = $this->getGeoJson();
+        return $data;
+    }
 }
