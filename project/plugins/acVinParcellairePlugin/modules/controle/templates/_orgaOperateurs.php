34d310d2652d3fa58bbcdf4b7023d192189462f8
Vincent LAURENT <vince.laurent@gmail.com>
2025-12-16 02:32:05 +0100

    Tableau des opérateurs

--- project/plugins/acVinParcellairePlugin/modules/controle/templates/_orgaOperateurs.php
+++ project/plugins/acVinParcellairePlugin/modules/controle/templates/_orgaOperateurs.php
@@ -2,3 +2,16 @@
 <hr class="mt-2 mb-4" />
 <div id="map" style="height: 70vh;"></div>
 <hr />
+
+<table class="table table-bordered table-stripped">
+    <thead>
+        <tr>
+            <th>Opérateur</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr v-for="controle in controles">
+            <td>{{ controle.declarant.nom }}<td>
+        </tr>
+    </tbody>
+</table>
