2a4bad3f14bbf140002bcc10a28309454d7ca79d
Vincent LAURENT <vince.laurent@gmail.com>
2025-12-16 13:59:17 +0100

    Navigation

--- project/plugins/acVinParcellairePlugin/modules/controle/templates/_terrainBlocDeclarant.php
+++ project/plugins/acVinParcellairePlugin/modules/controle/templates/_terrainBlocDeclarant.php
@@ -4,12 +4,13 @@
 <a href="mailto:{{ controleCourant.declarant.email }}">{{ controleCourant.declarant.email }}</a><br />
 <a href="callto:{{ controleCourant.declarant.telephone_bureau }}">{{ controleCourant.declarant.telephone_bureau }}</a><br /><a href="callto:{{ controleCourant.declarant.telephone_mobile }}">{{ controleCourant.declarant.telephone_mobile }}</a>
 </p>
-<br/>
 <div v-if="controleCourant.liaisons_operateurs[0]">
-    <h4>Cave(s) coopérative affectée(s) à cet opérateur :</h4>
-    <li v-for="liaison in controleCourant.liaisons_operateurs" style="list-style-type: none">
-        {{ liaison.libelle_etablissement }} - <span class="text-muted"> CVI: {{ liaison.cvi }}</span>
-    </li>
+    <p>Cave(s) coopérative affectée(s) à cet opérateur :</p>
+    <ul>
+        <li v-for="liaison in controleCourant.liaisons_operateurs" style="list-style-type: none">
+            {{ liaison.libelle_etablissement }} - <span class="text-muted"> CVI: {{ liaison.cvi }}</span>
+        </li>
+    </ul>
 </div>
 <div v-else>
     <p>Cet opérateur n'apporte pas à une cave coopérative</p>
