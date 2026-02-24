96ef1f906778888179fa2b182e96b58912baa4ab
Tangui Morlier <tangui@tangui.eu.org>
2026-02-24 16:22:15 +0100

    Est executé dans l'export.sh

--- project/bin/export_extra_nantes.sh
+++ project/bin/export_extra_nantes.sh
@@ -2,15 +2,4 @@
 
 . bin/config.inc
 
-if ! test "$SYMFONYTASKOPTIONS" ; then
-    exit;
-fi
-
 sed -i 's/ 20[23][0-9] / /' $EXPORTDIR/factures.csv
-
-cd bin/notebook/ ;
-if test -d pyenv ; then
-    source pyenv/bin/activate
-fi
-python3 nantes_factures_synthese.py ;
-cd -
