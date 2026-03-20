d5c47b49d38c7a6fbfc553fd879f51f25154415e
tale-fau <tale-fau@24eme.fr>
2026-03-20 17:08:41 +0100

    [igp] ajout de la classe d'etapes prise de mousse

--- project/plugins/acVinChgtDenomPlugin/modules/prisedemousse/templates/_step.php
+++ project/plugins/acVinChgtDenomPlugin/modules/prisedemousse/templates/_step.php
@@ -1,3 +1,3 @@
 <?php use_helper('Lot') ?>
 <?php
-include_partial('global/step', array('object' => $prisedemousse, 'etapes' => ChgtDenomEtapes::getInstance(), 'step' => $step, 'ajax' => isset($ajax) && $ajax));
+include_partial('global/step', array('object' => $prisedemousse, 'etapes' => PriseDeMousseEtapes::getInstance(), 'step' => $step, 'ajax' => isset($ajax) && $ajax));
