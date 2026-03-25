c31a2d01048c67d0bf6c669b4fe9ba4255e6cb07
tale-fau <tale-fau@24eme.fr>
2026-03-25 16:37:05 +0100

    [provence] mise en place des pièces

--- project/plugins/acVinParcellairePlugin/lib/model/Controle/base/BaseControleManquement.class.php
+++ project/plugins/acVinParcellairePlugin/lib/model/Controle/base/BaseControleManquement.class.php
@@ -4,13 +4,34 @@
  * 
  * Base model for ControleManquement
 
- * @property string $libelle
+ * @property string $constat_date
+ * @property string $notification_date
+ * @property string $cloture_date
+ * @property string $actif
+ * @property acCouchdbJson $parcelles_id
+ * @property string $libelle_point_de_controle
+ * @property string $libelle_manquement
  * @property string $observations
+ * @property string $delais
 
- * @method string getLibelle()
- * @method string setLibelle()
+ * @method string getConstatDate()
+ * @method string setConstatDate()
+ * @method string getNotificationDate()
+ * @method string setNotificationDate()
+ * @method string getClotureDate()
+ * @method string setClotureDate()
+ * @method string getActif()
+ * @method string setActif()
+ * @method acCouchdbJson getParcellesId()
+ * @method acCouchdbJson setParcellesId()
+ * @method string getLibellePointDeControle()
+ * @method string setLibellePointDeControle()
+ * @method string getLibelleManquement()
+ * @method string setLibelleManquement()
  * @method string getObservations()
  * @method string setObservations()
+ * @method string getDelais()
+ * @method string setDelais()
  
  */
 
