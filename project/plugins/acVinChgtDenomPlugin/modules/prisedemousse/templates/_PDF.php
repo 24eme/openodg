c1fa82fc2fb33829fc71fd28cbc56d9b673b9409
chachathecool <char.var@posteo.com>
2026-04-03 16:51:54 +0200

    Refacto PDM : modification du texte du pdf

--- project/plugins/acVinChgtDenomPlugin/modules/prisedemousse/templates/_PDF.php
+++ project/plugins/acVinChgtDenomPlugin/modules/prisedemousse/templates/_PDF.php
@@ -25,14 +25,14 @@
 <br/>
 <table><tr><td style="width: 324px;"><?php echo 'Le ' . format_date(date('Y-m-d'), "P", "fr_FR"); ?></td></tr></table>
 <br/><br/>
-    <table><tr><td><strong>Objet :</strong> Prise de mousse d'un lot</td></tr></table>
+    <table><tr><td><strong>Objet :</strong>Déclaration de prise de mousse</td></tr></table>
 <br/><br/>
 
 <table><tr><td>Madame, Monsieur,</td></tr></table>
 
 <?php $lotOrigine = $prisedemousse->getLotOrigine(); ?>
 
-<table><tr><td>Nous vous prions de bien vouloir trouver ci-dessous la <?php if (!$prisedemousse->getDegustationVMQ()->isAffecte() && !$prisedemousse->getDegustationVMQ()->isAffectable() && $prisedemousse->getDegustationVMQ()->isConforme()): ?> confirmation <?php else: ?> demande <?php endif ?> de prise de mousse de votre lot :</td></tr></table>
+<table><tr><td>Nous vous prions de bien vouloir trouver ci-dessous la <?php if (!$prisedemousse->getDegustationVMQ()->isAffecte() && !$prisedemousse->getDegustationVMQ()->isAffectable() && $prisedemousse->getDegustationVMQ()->isConforme()): ?> confirmation <?php else: ?> demande <?php endif ?> de prise de mousse de votre lot originalement déclaré en vin de base :</td></tr></table>
 
 <br/><br/>
 
@@ -75,7 +75,7 @@
         <?php if (!$prisedemousse->getDegustationVMQ()->isAffecte() && !$prisedemousse->getDegustationVMQ()->isAffectable() && $prisedemousse->getDegustationVMQ()->isConforme()): ?>
             <td style="text-align:center">qui devient le lot commercialisable suivant :  </td>
         <?php else: ?>
-            <td style="text-align:left">qui après le contrôle organoleptique (en attente) et conformément au cahier des charges/plan de contrôle deviendra le lot ci-dessous : </td>
+            <td style="text-align:left">qui après le contrôle organisé conformément au cahier des charges/plan de contrôle deviendra le lot ci-dessous : </td>
         <?php endif;?>
     </tr>
 </table>
