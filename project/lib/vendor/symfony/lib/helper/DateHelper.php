14eec9e9e4201f1223d46df009758927067104ac
Tangui Morlier <tangui@tangui.eu.org>
2026-04-21 11:24:26 +0200

    Gestion des dates qui ne sont en iso avec des -

--- project/lib/vendor/symfony/lib/helper/DateHelper.php
+++ project/lib/vendor/symfony/lib/helper/DateHelper.php
@@ -33,12 +33,12 @@ function format_date($date, $format = 'd', $culture = null, $charset = null)
 {
     static $dateFormats = [];
 
-    if (strpos($date, '-') === false) {
-        $date = substr($date, 0, 4).'-'.substr($date, 4, 2).'-'.substr($date, 6, 2);
+    if (! $date) {
+        return null;
     }
 
-    if (null === $date) {
-        return null;
+    if (strpos($date, '-') === false) {
+        $date = substr($date, 0, 4).'-'.substr($date, 4, 2).'-'.substr($date, 6, 2);
     }
 
     if (!$culture) {
