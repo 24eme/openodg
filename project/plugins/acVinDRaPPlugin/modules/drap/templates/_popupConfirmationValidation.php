91171e797d2aed26de360bc826ca76f9e72ff281
chachathecool <char.var@posteo.com>
2026-04-26 14:25:31 +0200

    fix rhone DRaP : mettre l'id drap pour afficher la pop-up de validation

--- project/plugins/acVinDRaPPlugin/modules/drap/templates/_popupConfirmationValidation.php
+++ project/plugins/acVinDRaPPlugin/modules/drap/templates/_popupConfirmationValidation.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="parcellaireirrigable-confirmation-validation" role="dialog" aria-labelledby="Confirmation de validation" aria-hidden="true">
+<div class="modal fade" id="drap-confirmation-validation" role="dialog" aria-labelledby="Confirmation de validation" aria-hidden="true">
 	<div class="modal-dialog">
 		<div class="modal-content">
 				<div class="modal-header">
