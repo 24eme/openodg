6bbf55f5a6c7d1f29476de286012afe461c84fde
tale-fau <tale-fau@24eme.fr>
2026-04-27 14:10:27 +0200

    [odgrhone] Mise en place PDF DRaP

--- project/plugins/acVinDRaPPlugin/modules/drap/templates/validationSuccess.php
+++ project/plugins/acVinDRaPPlugin/modules/drap/templates/validationSuccess.php
@@ -77,8 +77,7 @@
    </div>
     <div style="padding-top: 10px;" class="row row-margin row-button">
         <div class="col-xs-4">
-            <a href=
-                "<?php echo url_for("drap_destinations", $drap) ?>"
+            <a href="<?php echo url_for("drap_destinations", $drap) ?>" class="btn btn-default btn-upper"><span class="glyphicon glyphicon-chevron-left"></span> Retourner à l'étape précédente</a>
         </div>
         <div class="col-xs-4 text-center">
             <a href="<?php echo url_for('drap_export_pdf', $drap) ?>" class="btn btn-primary">
