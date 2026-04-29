f61c1ad32c6be06c12b9b11cc3aa98d39c6111c5
tale-fau <tale-fau@24eme.fr>
2026-04-29 11:55:55 +0200

    [odgrhone] ajout adresse mail

--- project/plugins/acVinParcellairePlugin/modules/controle/actions/components.class.php
+++ project/plugins/acVinParcellairePlugin/modules/controle/actions/components.class.php
@@ -7,8 +7,8 @@ class controleComponents extends sfComponents {
     {
         $this->controle = ControleClient::getInstance()->find($request->getParameter('id_controle'));
 
-        $this->subject = sprintf("%s - Suite contrôle interne ODG %s",Organisme::getInstance(null, 'controle')->getNom(), $this->controle->getDateFormat('Y'));
+        $this->subject = sprintf("%s - Suite contrôle interne ODG %s",Organisme::getInstance()->getNom(), $this->controle->getDateFormat('Y'));
         $this->email = EtablissementClient::getInstance()->find($this->controle->identifiant)->getEmail();
-        $this->cc = Organisme::getInstance(null, 'controle')->getEmail();
+        $this->cc = Organisme::getInstance()->getEmail();
     }
 }
