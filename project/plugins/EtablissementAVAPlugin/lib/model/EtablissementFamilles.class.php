026b7c85fd74b1e761710e0487206c55177b4d7e
Gabriel Poma <gpoma@24eme.fr>
2026-05-22 11:09:38 +0200

    ava: fix constante etablissement famille qui n'existe pas

--- project/plugins/EtablissementAVAPlugin/lib/model/EtablissementFamilles.class.php
+++ project/plugins/EtablissementAVAPlugin/lib/model/EtablissementFamilles.class.php
@@ -6,4 +6,5 @@ class EtablissementFamilles
 {
     const FAMILLE_PRODUCTEUR = EtablissementClient::FAMILLE_PRODUCTEUR;
     const FAMILLE_PRODUCTEUR_VINIFICATEUR = EtablissementClient::FAMILLE_VINIFICATEUR;
+    const FAMILLE_COOPERATIVE = EtablissementClient::FAMILLE_CAVE_COOPERATIVE;
 }
