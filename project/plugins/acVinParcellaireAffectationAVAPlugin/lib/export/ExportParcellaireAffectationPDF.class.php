755fccd185d869a17c3b68f7a1067231024d6d80
chachathecool <char.var@posteo.com>
2026-06-09 14:46:45 +0200

    Revert "Feat [AVA] : ajouter tableaux de synthèse dans le pdf"
    This reverts commit 6a5daf4bdfa679dc8c673c1a75529b637831ca74.

--- project/plugins/acVinParcellaireAffectationAVAPlugin/lib/export/ExportParcellaireAffectationPDF.class.php
+++ project/plugins/acVinParcellaireAffectationAVAPlugin/lib/export/ExportParcellaireAffectationPDF.class.php
@@ -48,17 +48,12 @@ class ExportParcellaireAffectationPDF extends ExportPDF {
         }
 
         foreach ($this->parcellesForDetails as $pageid => $parcellesForDetail) {
-            $this->printable_document->addPage($this->getPartial('parcellaireAffectation/pdf', array('parcellaire' => $this->parcellaire, 'parcellesForDetail' => $parcellesForDetail, 'cviFilter' => $this->cviFilter, 'lastPage' => false)));
+            $this->printable_document->addPage($this->getPartial('parcellaireAffectation/pdf', array('parcellaire' => $this->parcellaire, 'parcellesForDetail' => $parcellesForDetail, 'cviFilter' => $this->cviFilter)));
         }
         if ((count($this->parcellesForDetails) == 1) && (count($this->parcellesForDetails[$pageid]->parcelles) < count($this->parcellesForRecap))) {
-            $this->printable_document->addPage($this->getPartial('parcellaireAffectation/pdfRecap', array('parcellaire' => $this->parcellaire, 'parcellesForRecap' => $this->parcellesForRecap, 'engagement' => !$this->cviFilter, 'lastPage' => false)));
+            $this->printable_document->addPage($this->getPartial('parcellaireAffectation/pdfRecap', array('parcellaire' => $this->parcellaire, 'parcellesForRecap' => $this->parcellesForRecap, 'engagement' => !$this->cviFilter)));
         }
 
-        $this->printable_document->addPage($this->getPartial('parcellaireAffectation/pdf', array(
-            'parcellaire' => $this->parcellaire,
-            'parcellesForDetail' => null,
-            'lastPage' => true
-        )));
     }
 
     protected function getHeaderTitle() {
