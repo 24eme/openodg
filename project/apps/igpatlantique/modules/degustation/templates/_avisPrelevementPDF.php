477fcb4cb2271f1e737d5871f52886c07dff040e
JB LM <lemetayer.jb@gmail.com>
2026-09-08 11:03:48 +0200

    affichage des lots dans les avis de prelevement

--- project/apps/igpatlantique/modules/degustation/templates/_avisPrelevementPDF.php
+++ project/apps/igpatlantique/modules/degustation/templates/_avisPrelevementPDF.php
@@ -57,28 +57,36 @@ Madame, Monsieur,
 Nous avons bien reçu votre déclaration de conditionnement datée du 21/04/2026. Conformément à la nouvelle procédure d’agrément, nous vous informons que nous allons effectuer un prélèvement de votre pour un contrôle produit dans le cadre du contrôle interne :
 <br/>
 <br/>
-<?php foreach ($lots as $lot): ?>
+
 <table class="table-operateur">
     <thead>
         <tr>
-            <th>Fournisseur</th>
-            <th>Désignation</th>
-            <th>Millésime</th>
-            <th>Volume (hl)</th>
-            <th>Dates de mises</th>
+            <th style="width:10%; height:25px;">N° lgmt</th>
+            <th style="width:15%; height:25px;">Cépage</th>
+            <th style="width:15%; height:25px;">Désignation</th>
+            <th style="width:7%; height:25px; text-align: right;">Mill.</th>
+            <th style="width:13%; height:25px; text-align: right;">Volume (hl)</th>
+            <th style="width:25%; height:25px;">Destination</th>
+            <th style="width:15%; height:25px;">Date condi.</th>
         </tr>
     </thead>
     <tbody>
+        <?php foreach ($lots as $operateurlots): ?>
+            <?php foreach ($operateurlots as $lot): ?>
         <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td style="width:10%;"><?php echo $lot->numero_logement_operateur ?></td>
+            <td style="width:15%;"><?php foreach ($lot->cepages as $cepage) {echo $cepage . ' ';} ?></td>
+            <td style="width:15%;"><?php echo $lot->produit_libelle ?></td>
+            <td style="width:7%; text-align: right;"><?php echo $lot->millesime ?></td>
+            <td style="width:13%; text-align: right;"><?php echo $lot->volume ?></td>
+            <td style="width:25%;"><?php echo $lot->destination_type ?></td>
+            <td style="width:15%;"><?php echo format_date($lot->destination_date, "dd/MM/yyyy") ?></td>
         </tr>
+            <?php endforeach; ?>
+        <?php endforeach; ?>
     </tbody>
 </table>
-<?php endforeach; ?>
+
 <br/>
 Nous avons confié la réalisation des prises d’échantillon et la dégustation à <strong>QUALI-BORDEAUX</strong>.
 <br/>
