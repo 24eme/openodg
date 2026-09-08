df41df85edd4aada438ba737a1da7654447bff1f
Tangui Morlier <tangui@tangui.eu.org>
2026-09-08 14:01:12 +0200

    Text des PDF de prélèvement/dégustation (prestaire et opérateur)

--- project/apps/igpatlantique/modules/degustation/templates/_demandePrelevementPDF.php
+++ project/apps/igpatlantique/modules/degustation/templates/_demandePrelevementPDF.php
@@ -31,14 +31,14 @@
 </style>
 <br/><br/>
 <h1 style="text-align: center">NOTIFICATION DE L'ODG IGP ATLANTIQUE</h1>
-<h2 style="text-align: center">Demande de prélèvement et de dégustation de vins IGP Atlantique</h2>
+<h2 style="text-align: center">Demande de prélèvement et de dégustation - <?php echo $degustation->lieu; ?></h2>
 <h2 style="text-align: center">Contrôle interne produit</h2>
 <p>&nbsp;</p>
+<p>Le syndicat des producteurs de vins IGP Atlantique</p>
 <p>&nbsp;</p>
-Madame, Monsieur,
-<br/>
-<br/>
-Voici une demande de prélèvement et de dégustation <?php echo $degustation->lieu ?> pour la campagne <strong><?php echo $degustation->campagne ?></strong>. Les opérateurs sont les suivants :
+<p>Madame, Monsieur,</p>
+<p>&nbsp;</p>
+<p>Voici une demande de prélèvement et de dégustation pour la campagne <strong><?php echo $degustation->campagne ?></strong>. Les opérateurs sont les suivants avec le détail des lots à prélever :</p>
 
 <ul>
 <?php foreach ($lots as $famille => $operateurs): ?>
@@ -51,25 +51,26 @@ Voici une demande de prélèvement et de dégustation <?php echo $degustation->l
                 <table class="table-operateur">
                     <thead>
                         <tr>
-                            <th style="width:10%; height:25px;">N° lgmt</th>
-                            <th style="width:15%; height:25px;">Cépage</th>
-                            <th style="width:15%; height:25px;">Désignation</th>
-                            <th style="width:7%; height:25px; text-align: right;">Mill.</th>
-                            <th style="width:13%; height:25px; text-align: right;">Volume (hl)</th>
-                            <th style="width:25%; height:25px;">Destination</th>
-                            <th style="width:15%; height:25px;">Date condi.</th>
+                            <th style="width:10%;text-align: center;">N° lgmt</th>
+                            <th style="width:40%;text-align: center;">Désignation - Cépage</th>
+                            <th style="width:7%;text-align: right;">Mill.</th>
+                            <th style="width:13%;text-align: right;">Volume (hl)</th>
+                            <th style="width:15%;text-align: center;">Destination</th>
+                            <th style="width:15%;text-align: center;">Date condi.</th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php foreach ($lots as $lot): ?>
                             <tr>
-                                <td style="width:10%;"><?php echo $lot->numero_logement_operateur ?></td>
-                                <td style="width:15%;"><?php foreach ($lot->cepages as $cepage) {echo $cepage . ' ';} ?></td>
-                                <td style="width:15%;"><?php echo $lot->produit_libelle ?></td>
-                                <td style="width:7%; text-align: right;"><?php echo $lot->millesime ?></td>
-                                <td style="width:13%; text-align: right;"><?php echo $lot->volume ?></td>
-                                <td style="width:25%;"><?php echo $lot->destination_type ?></td>
-                                <td style="width:15%;"><?php echo format_date($lot->destination_date, "dd/MM/yyyy") ?></td>
+                                <td style="width:10%;text-align: center;"><?php echo $lot->numero_logement_operateur ?></td>
+                                <td style="width:40%;text-align: center;">
+                                    <?php echo $lot->produit_libelle ?>
+                                    <?php foreach ($lot->cepages as $cepage => $vol) {echo $cepage . ' ';} ?>
+                                </td>
+                                <td style="width:7%;text-align: right;"><?php echo $lot->millesime ?></td>
+                                <td style="width:13%;text-align: right;"><?php echo $lot->volume ?></td>
+                                <td style="width:15%;text-align: center;"><?php echo $lot->destination_type ?></td>
+                                <td style="width:15%;text-align: center;"><?php echo format_date($lot->destination_date, "dd/MM/yyyy") ?></td>
                             </tr>
                         <?php endforeach; ?>
                     </tbody>
@@ -80,20 +81,10 @@ Voici une demande de prélèvement et de dégustation <?php echo $degustation->l
     </li>
 <?php endforeach; ?>
 </ul>
-<?php if ($activite !== 'CONDITIONNEUR'): ?>
+<p>Nous vous remercions de bien vouloir nous tenir informés du résultat de la dégustation.
+<p>Cordialement,</p>
+<p>&nbsp;</p>
+<p></p>
 <br/>
-<br/>
-Conformément au Plan de contrôle de l’ODG IGP ATLANTIQUE, les dégustations devront avoir lieu au plus tard <strong>12 jours ouvrés</strong> après la date d'envoi de l'avis de prélèvement.
-<?php endif; ?>
-<br/>
-<br/>
-Vous trouverez ci-joint, la déclaration de revendication, ainsi que le bulletin d’analyse du lot. Nous vous demandons de bien vouloir nous tenir informés du résultat de la dégustation.
-<br/>
-<br/>
-Meilleures salutations,
-<br/>
-<br/>
-P/o
-<br/>
-<p><?php echo Organisme::getInstance(null, 'degustation')->getResponsable(); ?></p>
+<p>Le syndicat des producteurs de vins IGP Altlantique</p>
 <br/>
