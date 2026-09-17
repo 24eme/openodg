2fd828eecc67d9193b9a23ee91adfcf06c199d3c
Tangui Morlier <tangui@tangui.eu.org>
2026-09-17 16:06:37 +0200

    Permet de renvoyer la DREV à Certipaq

--- project/plugins/acVinDRevPlugin/modules/drev/templates/debugCertipaqSuccess.php
+++ project/plugins/acVinDRevPlugin/modules/drev/templates/debugCertipaqSuccess.php
@@ -1,3 +1,6 @@
 <pre>
     <?php print_r($drevOi->getDebugInfo()->getRawValue()); ?>
 </pre>
+<center>
+    <a href="<?php echo url_for('drev_send_certipaq', $drev); echo ($regionParam)? '?region='.$regionParam : ''; ?>" onclick="return confirm('Êtes vous sûr de vouloir envoyer la DRev à Certipaq ?');"  class="btn btn-warning"> Ré-envoyer à certipaq</a>
+</center>
