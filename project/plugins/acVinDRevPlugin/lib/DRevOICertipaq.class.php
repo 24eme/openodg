79992061c28100ab99b23627c41dac3be5a3dae2
Tangui Morlier <tangui@tangui.eu.org>
2026-09-18 12:21:09 +0200

    Indent

--- project/plugins/acVinDRevPlugin/lib/DRevOICertipaq.class.php
+++ project/plugins/acVinDRevPlugin/lib/DRevOICertipaq.class.php
@@ -1,36 +1,36 @@
 <?php
 class DRevOICertipaq
 {
-	public $drev;
-	public $context;
-	public $regions;
-	private $res;
+    public $drev;
+    public $context;
+    public $regions;
+    private $res;
 
 
-	public function __construct(DRev $drev, $context = null) {
-		if (!$drev->validation_odg) {
-			throw new sfException('DRev non validée');
-		}
-		$this->drev = $drev;
-		$this->context = ($context) ? $context : sfContext::getInstance();
-		$this->regions = sfConfig::get('app_oi_regions');
-	}
+    public function __construct(DRev $drev, $context = null) {
+        if (!$drev->validation_odg) {
+            throw new sfException('DRev non validée');
+        }
+        $this->drev = $drev;
+        $this->context = ($context) ? $context : sfContext::getInstance();
+        $this->regions = sfConfig::get('app_oi_regions');
+    }
 
-	public function send()
-	{
-		$sended = array();
+    public function send()
+    {
+        $sended = array();
         $produits = array();
         $api_res = array();
-		if($this->regions){
-			$regionSended = array();
-			foreach ($this->regions as $region => $regionOpt) {
+        if($this->regions){
+            $regionSended = array();
+            foreach ($this->regions as $region => $regionOpt) {
                 foreach($this->drev->declaration->getProduits($region) as $produit) {
                     $api_res[$produit->getHash()] = $this->sendProduit($produit);
                 }
-				$sended[] = $region;
-			}
-		}else{
-			$sended[] = null;
+                $sended[] = $region;
+            }
+        }else{
+            $sended[] = null;
             foreach($this->drev->declaration->getProduits() as $produit) {
                 $api_res[$produit->getHash()] = $this->sendProduit($produit);
             }
@@ -43,16 +43,16 @@ class DRevOICertipaq
         }
         $this->res = $api_res;
         return $api_res;
-	}
+    }
 
     public function sendProduit($produit){
         try {
-         $res = CertipaqDRev::getInstance()->createDRevLigne($produit);
-         return ['success' => true, 'res' => $res, 'erreor' => null, 'debug' => CertipaqDRev::getInstance()->getLastQuery()];
+            $res = CertipaqDRev::getInstance()->createDRevLigne($produit);
+            return ['success' => true, 'res' => $res, 'erreor' => null, 'debug' => CertipaqDRev::getInstance()->getLastQuery()];
         }catch(sfException $e) {
             return ['success' => false, 'error' => $e->getMessage(), 'res' => null, 'debug' => CertipaqDRev::getInstance()->getLastQuery()];
         }
-	}
+    }
 
     public function storeResultInDrev() {
         $this->drev->add('_attachments');
