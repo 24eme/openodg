98d37bd1796657ec392ae03a41a78d0803da70e3
Tangui Morlier <tangui@tangui.eu.org>
2026-09-22 12:40:21 +0200

    Meilleure identification du site principal

--- project/plugins/acVinHabilitationPlugin/lib/certipaq/CertipaqDRev.class.php
+++ project/plugins/acVinHabilitationPlugin/lib/certipaq/CertipaqDRev.class.php
@@ -57,14 +57,17 @@ class CertipaqDRev extends CertipaqService
         }
 
         $site_id = null;
-        foreach($operateur->sites as $sid => $s) {
-            foreach($s->habilitations as $hid => $a) {
-                if ($a->site_id) {
-                    $site_id = $a->site_id;
-                }
+        $cdcs_nb = 0;
+        foreach($operateur->sites as $id => $s) {
+            if ($s->nom_site == 'Site Principal') {
+                $site_id = $s->id;
+                break;
+            }
+            if ($cdcs_nb < count($s->cdcs)) {
+                $cdcs_nb = count($s->cdcs);
+                $site_id = $s->id;
             }
         }
-
         $params = array();
         $params['operateur_id'] = intval($operateur->id);
         $params['dr_cdc_famille_id'] = $produit->dr_cdc_famille_id;
